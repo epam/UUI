249804f8b2cee208677d75de66dcd6bc8720f61e
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2021-03-23 21:56:24 +0300

    fixed tests, updated snapshots

--- loveship/components/tables/__tests__/DataTableScrollRow.test.tsx
+++ loveship/components/tables/__tests__/DataTableScrollRow.test.tsx
@@ -3,6 +3,14 @@ import renderer from "react-test-renderer";
 import {testData} from "@epam/uui";
 import {DataTableScrollRow} from "../DataTableScrollRow";
 
+class ResizeObserverMock {
+    observe = () => jest.fn();
+    unobserve = () => jest.fn();
+    disconnect = () => jest.fn();
+}
+
+global.ResizeObserver = ResizeObserverMock;
+
 describe("DataTableScrollRow", () => {
     it("should be rendered correctly", () => {
         const tree = renderer
