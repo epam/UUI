0bd9e44cc593f6f5890cd16c280d9e12c1ef974f
aliaksei_manetau <aliaksei_manetau@epam.com>
2021-03-25 17:23:55 +0300

    Remove @types/react & @types/react-dom dependecies. It will used from your project dependencies.

--- uui-build/TSLint/noImportRule.js
+++ uui-build/TSLint/noImportRule.js
@@ -5,7 +5,7 @@ var __extends = (this && this.__extends) || (function () {
             ({ __proto__: [] } instanceof Array && function (d, b) { d.__proto__ = b; }) ||
             function (d, b) { for (var p in b) if (b.hasOwnProperty(p)) d[p] = b[p]; };
         return extendStatics(d, b);
-    }
+    };
     return function (d, b) {
         extendStatics(d, b);
         function __() { this.constructor = d; }
@@ -13,10 +13,11 @@ var __extends = (this && this.__extends) || (function () {
     };
 })();
 exports.__esModule = true;
+exports.Rule = void 0;
 var Lint = require("tslint");
 var regexpNoSumboduleImport = /^@epam\/[\w-]+\//i;
-var regexpAssetsExeption = /^@epam\/assets\/\w+(\/\w+)?(\/\w+)?(\/[\w-]+\.[a-z]+)?$/i;
-var regexpIconsExeption = /^@epam\/(loveship|oswald)\/components\/icons\/[\w-]+\.[a-z]+$/i;
+var regexpAssetsException = /^@epam\/assets\/\w+(\/\w+)?(\/\w+)?(\/[\w-]+\.[a-z]+)?$/i;
+var regexpInternalException = /^@epam\/internal/i;
 var epamModules = ['epam-uui', 'loveship', 'epam-promo', 'uui', 'uui-db', 'uui-components', 'uui-timeline', 'uui-editor', 'draft-rte', 'edu-bo-components', 'extra',
     'epam-assets', 'edu-utils', 'edu-ui-base', 'edu-core-routing', 'edu-core', 'uui-docs', 'grow', 'app'];
 var getNoModuleOutsideRegExp = function (url) {
@@ -56,7 +57,7 @@ var NoImportsWalker = /** @class */ (function (_super) {
     }
     NoImportsWalker.prototype.visitImportDeclaration = function (node) {
         var str = node.moduleSpecifier.getText().replace(/('|\")/g, "");
-        if (regexpNoSumboduleImport.test(str) && !regexpAssetsExeption.test(str) && !regexpIconsExeption.test(str)) {
+        if (regexpNoSumboduleImport.test(str) && !regexpAssetsException.test(str) && !regexpInternalException.test(str)) {
             this.addFailure(this.createFailure(node.getStart(), node.getWidth(), Rule.FAILURE_STRING_SUBMODULE_IMPORT));
         }
         if (getNoModuleOutsideRegExp(node.moduleSpecifier.getSourceFile().fileName).test(str)) {
