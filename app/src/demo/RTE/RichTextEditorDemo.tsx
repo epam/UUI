085d536e94b7becb6c74770caf0fc251eb0cf69e
aliaksei_manetau <aliaksei_manetau@epam.com>
2021-04-07 20:10:49 +0300

    [Site]: add RichTextEditor demo, refactor RTE doc

--- app/src/demo/RTE/RichTextEditorDemo.tsx
+++ app/src/demo/RTE/RichTextEditorDemo.tsx
@@ -1,29 +1,20 @@
 import * as React from 'react';
 import { Value } from 'slate';
-import { Panel, FlexSpacer, FlexRow, Switch, MultiSwitch } from '@epam/promo';
 import { SlateEditor, defaultPlugins, imagePlugin, videoPlugin, attachmentPlugin, toDoListPlugin, baseMarksPlugin,
     linkPlugin, iframePlugin, notePlugin, separatorPlugin, uploadFilePlugin, tablePlugin, quotePlugin, colorPlugin,
     superscriptPlugin, headerPlugin, listPlugin, placeholderPlugin } from '@epam/uui-editor';
-import { svc } from '../../../services';
+import { svc } from '../../services';
 import { initialValue } from './state';
-import * as css from './SlateEditorBasicExample.scss';
+import * as css from './RichTextEditorDemo.scss';
 
 
 interface SlateEditorBasicExampleState {
     value: Value;
-    isReadonly: boolean;
-    mode: 'form' | 'inline';
-    fontSize: '14' | '16';
-    name: string;
 }
 
-export class SlateEditorBasicExample extends React.Component<any, SlateEditorBasicExampleState> {
+export class RichTextEditorDemo extends React.Component<any, SlateEditorBasicExampleState> {
     state: SlateEditorBasicExampleState = {
         value: Value.fromJSON(initialValue),
-        isReadonly: false,
-        mode: 'form',
-        fontSize: '14',
-        name: '',
     };
 
     onChange = (value: Value) => {
@@ -73,39 +64,16 @@ export class SlateEditorBasicExample extends React.Component<any, SlateEditorBas
     render() {
 
         return (
-            <Panel cx={ css.root }>
-                <FlexRow spacing='18' vPadding='12'>
-                    <MultiSwitch
-                        items={ [{ id: '14', caption: '14' }, { id: '16', caption: '16' }] }
-                        value={ this.state.fontSize }
-                        onValueChange={ (val: any) => this.setState({ fontSize: val }) }
-                        color='blue'
-                    />
-                    <FlexSpacer />
-                    <Switch
-                        value={ this.state.mode === 'inline' }
-                        onValueChange={ (val: boolean) => this.setState({ mode: val ? 'inline' : 'form' }) }
-                        label='Inline mode'
-                    />
-                    <Switch
-                        value={ this.state.isReadonly }
-                        onValueChange={ (val: boolean) => this.setState({ isReadonly: val }) }
-                        label='View mode'
-                    />
-                </FlexRow>
-
+            <div className={ css.container }>
                 <SlateEditor
                     value={ this.state.value }
                     onValueChange={ this.onChange }
-                    isReadonly={ this.state.isReadonly }
                     autoFocus={ true }
                     plugins={ this.plugins }
-                    mode={ this.state.mode }
                     placeholder='Add description'
                     minHeight={ 'none' }
-                    fontSize={ this.state.fontSize }
                 />
-            </Panel>
+            </div>
         );
     }
 }
