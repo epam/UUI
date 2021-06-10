adaed5618b648bb1c717ea2b2f40fc55005b2121
Yuliya_Miatlionak <Yuliya_Miatlionak@epam.com>
2021-06-10 10:42:02 +0300

    [useAsyncDataSource]: add dependencies

--- uui/data/processing/hooks/useAsyncDataSource.ts
+++ uui/data/processing/hooks/useAsyncDataSource.ts
@@ -2,10 +2,10 @@ import { useMemo } from 'react';
 import { AsyncDataSource, AsyncDataSourceProps } from '../AsyncDataSource';
 import { useReducer } from 'react';
 
-export function useAsyncDataSource<TItem, TId, TFilter >(params: AsyncDataSourceProps<TItem, TId, TFilter>) {
+export function useAsyncDataSource<TItem, TId, TFilter >(params: AsyncDataSourceProps<TItem, TId, TFilter>,  dependencies?: any[]) {
     const dataSource = useMemo(() => new AsyncDataSource({
         ...params,
-    }), []);
+    }), Object.values(params).concat(dependencies));
 
     return dataSource;
 }
