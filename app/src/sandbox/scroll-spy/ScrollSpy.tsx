1dd5d4fff3278706fcd590bc01841e295ae26efd
rayetzki <lllya.rayetzki@wrkspot.com>
2021-07-23 09:57:53 +0300

    Fix scroll spy on form after first validation

--- app/src/sandbox/scroll-spy/ScrollSpy.tsx
+++ app/src/sandbox/scroll-spy/ScrollSpy.tsx
@@ -4,7 +4,7 @@ export interface ScrollSpyApi {
     scrollToElement: (item?: string) => void;
 }
 interface ScrollSpyProps {
-    items?: string[];
+    items?: Readonly<string[]>;
     children: (api: ScrollSpyApi) => ReactNode;
 }
 
@@ -12,17 +12,18 @@ export const ScrollSpy = ({ children, items }: ScrollSpyProps) => {
     const spyRef = useRef(null);
 
     function scrollToElement(item?: string) {
-        let element: HTMLDivElement;
+        let element;
+
         if (!items || items.length === 0 || !Array.isArray(items) || !item) {
             element = spyRef.current.querySelector('.uui-invalid').closest('.uui-label-top');
-        } else {
+        } else if (item && items.includes(item)) {
             const selectedId = items.find(i => i === item);
             if (selectedId) {
                 element = spyRef.current.querySelector(`#{selectedId}`);
             }
         };
 
-        element.scrollIntoView({ block: 'start', behavior: 'smooth' });
+        element && element.scrollIntoView({ block: 'start', behavior: 'smooth' });
     };
 
     return (
