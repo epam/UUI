2b7fa605e2d1e26b265fe9c5a12a055285ef8119
rayetzki <lllya.rayetzki@wrkspot.com>
2021-07-23 16:29:44 +0300

    [REFACTORING]: Querying separation

--- app/src/sandbox/scroll-spy/ScrollSpy.tsx
+++ app/src/sandbox/scroll-spy/ScrollSpy.tsx
@@ -20,6 +20,10 @@ export function useScrollSpy(
     const [currentActive, setCurrentActive] = useState<string>(Array.isArray(elements) && elements.length > 0 && elements[0]);
     const [observedNodes, setObservedNodes] = useState<HTMLElement[]>([]);
 
+    const getElement = useCallback((root: IScrollSpyProps['root'], id: string): HTMLElement => {
+        return root.querySelector(`[id='${id}'], [data-spy='${id}'], [name='${id}']`)
+    }, []);
+
     const scrollToElement = useCallback(
         (root: IScrollSpyProps['root'], elements?: IScrollSpyProps['elements']): ((item?: string) => void) => {
             return item => {
@@ -29,21 +33,19 @@ export function useScrollSpy(
                     element = root.querySelector('.uui-invalid').closest('.uui-label-top');
                 } else if (item && elements.includes(item)) {
                     const selected = elements.find(i => i === item);
-                    if (selected) {
-                        element = root.querySelector(`[id='${selected}'], [data-spy='${selected}'], [name='${selected}']`);
-                    }
+                    if (selected) element = getElement(root, selected);
                 };
 
-                element && element.scrollIntoView({ block: 'start', behavior: 'smooth' });
+                if (element) {
+                    element.scrollIntoView({ block: 'start', behavior: 'smooth' });
+                } else return;
             };
         }, []
     );
 
     useEffect(() => {
         if (!ref || !elements || !Array.isArray(elements) || elements.length === 0) return;
-        setObservedNodes(elements.map(element => {
-            return ref.querySelector(`[id='${element}'], [data-spy='${element}'], [name='${element}']`)
-        }));
+        setObservedNodes(elements.map(element => getElement(ref, element));
     }, [ref]);
 
     useEffect(() => {
