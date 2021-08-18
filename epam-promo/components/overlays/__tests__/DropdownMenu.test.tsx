6234c7712b8eae37ee8bec1471f8adb24200a005
Olga Studzenichnik <okrypets@gmail.com>
2021-08-18 16:08:31 +0300

    [DropdownMenu]: add tests

---
+++ epam-promo/components/overlays/__tests__/DropdownMenu.test.tsx
@@ -0,0 +1,38 @@
+import React from 'react';
+import { DropdownMenuBody, DropdownMenuButton, DropdownSubMenu, DropdownMenuSplitter, DropdownMenuHeader } from '../';
+import renderer from 'react-test-renderer';
+import { systemIcons } from '../../../icons/icons';
+
+const { accept: icon } = systemIcons["30"];
+
+jest.mock('react-dom', () => ({
+    findDOMNode: jest.fn(),
+}));
+
+describe('DropdownMenu', () => {
+    it('should be rendered DropdownMenuBody correctly', () => {
+        const tree = renderer
+            .create(<DropdownMenuBody style={ { maxWidth: "250px" } }>
+                <DropdownMenuButton
+                    caption="Menu Item in Submenu"
+                />
+                <DropdownMenuSplitter/>
+                <DropdownMenuHeader
+                    caption="An example of DropdownMenuHeader"
+                />
+                <DropdownSubMenu
+                    caption="One More SubMenu"
+                >
+                    <DropdownMenuButton
+                        icon={ icon }
+                        iconPosition="right"
+                        caption="Menu Item with icon in right"
+                    />
+                </DropdownSubMenu>
+            </DropdownMenuBody>)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+});
+
+
