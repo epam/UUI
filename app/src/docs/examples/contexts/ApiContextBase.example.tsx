b4e70f1c55b454a7b9aff13f206069699cc1a4d8
--global <rayetzkiillya@gmail.com>
2021-09-01 13:05:12 +0300

    [FIXUP]: Switch to env file to take current origin for Codesandbox

--- app/src/docs/examples/contexts/ApiContextBase.example.tsx
+++ app/src/docs/examples/contexts/ApiContextBase.example.tsx
@@ -4,7 +4,7 @@ import { useUuiContext } from '@epam/uui';
 
 export default function ApiContextBaseExample() {
     const svc = useUuiContext();
-    const ORIGIN = process.env.NODE_ENV === 'development' ? '' : 'https://uui.epam.com';
+    const ORIGIN = process.env.REACT_APP_PUBLIC_URL || '';
 
     return (
         <div>
