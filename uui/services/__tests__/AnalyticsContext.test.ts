de41de50c68071b996a33353857bc92afda6128e
Anton Andryushin <an.andryushin@gmail.com>
2021-09-17 17:51:35 +0300

    Added more tests

--- uui/services/__tests__/AnalyticsContext.test.ts
+++ uui/services/__tests__/AnalyticsContext.test.ts
@@ -8,7 +8,7 @@ describe("AnalyticsContext", () => {
     afterEach(() => {
         delete (window as any).dataLayer;
     });
-    
+
     it("should call listeners", () => {
         const context = new AnalyticsContext({} as any);
 
@@ -24,9 +24,17 @@ describe("AnalyticsContext", () => {
 
         expect(listener1.sendEvent).toBeCalledTimes(1);
         expect(listener1.sendEvent).toBeCalledWith(testEvent1, {}, "event");
+        
+        const params = {
+            param1: "test param 1",
+            param2: {
+                inner: "inner test param 2",
+            },
+        };
 
         const testEvent2 = {
             name: "test event 2",
+            ...params,
         };
         const listener2 = {
             sendEvent: jest.fn(),
@@ -36,9 +44,9 @@ describe("AnalyticsContext", () => {
         context.sendEvent(testEvent2);
 
         expect(listener1.sendEvent).toBeCalledTimes(2);
-        expect(listener1.sendEvent).toBeCalledWith(testEvent2, {}, "event");
+        expect(listener1.sendEvent).toBeCalledWith(testEvent2, params, "event");
         expect(listener2.sendEvent).toBeCalledTimes(1);
-        expect(listener2.sendEvent).toBeCalledWith(testEvent2, {}, "event");
+        expect(listener2.sendEvent).toBeCalledWith(testEvent2, params, "event");
     });
 
     it("should listen router", () => {
