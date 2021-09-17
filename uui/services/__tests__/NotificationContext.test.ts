5dcd7e504806721ec9f750565b2e0670f333572f
Anton Andryushin <an.andryushin@gmail.com>
2021-09-17 17:51:35 +0300

    Added more tests

--- uui/services/__tests__/NotificationContext.test.ts
+++ uui/services/__tests__/NotificationContext.test.ts
@@ -7,22 +7,34 @@ describe("NotificationContext", () => {
     beforeEach(() => {
         context = new NotificationContext(new LayoutContext());
     });
-    
+
     it("should create operations", () => {
-        context.show(() => "", {});
+        context.show(() => "", { position: "bot-left" });
         expect(context.getNotifications().length).toBe(1);
         
-        context.show(() => "", {});
+        let notification = context.getNotifications()[0];
+        expect(notification.config.position).toBe("bot-left");
+        expect(notification.config.duration).toBe(7);
+        expect(notification.props.id).toBe(0);
+        expect(notification.props.key).toBe("0");
+
+        context.show(() => "", { position: "top-right" });
         expect(context.getNotifications().length).toBe(2);
+
+        notification = context.getNotifications()[1];
+        expect(notification.config.position).toBe("top-right");
+        expect(notification.config.duration).toBe(7);
+        expect(notification.props.id).toBe(1);
+        expect(notification.props.key).toBe("1");
     });
 
     it("should remove operations", () => {
         context.show(() => "", {});
         context.show(() => "", {});
-        
+
         expect(context.getNotifications()[0].props.id).toBe(0);
         expect(context.getNotifications()[1].props.id).toBe(1);
-        
+
         context.remove(0);
         expect(context.getNotifications()[0].props.id).toBe(1);
     });
@@ -34,12 +46,67 @@ describe("NotificationContext", () => {
 
         context.handleRedirect();
         expect(context.getNotifications().length).toBe(0);
-        
+
         context.show(() => "", {});
         context.show(() => "", {});
         expect(context.getNotifications().length).toBe(2);
 
         context.clearAll();
         expect(context.getNotifications().length).toBe(0);
+    });
+
+    it("should be removed after time end", () => {
+        jest.useFakeTimers();
+
+        let removeSpy = jest.spyOn(context, "remove");
+        context.show(() => "", { duration: 1 }).catch(() => {
+        });
+
+        jest.advanceTimersByTime(1000);
+        expect(removeSpy).toBeCalledTimes(1);
+        expect(context.getNotifications().length).toBe(0);
+        removeSpy.mockRestore();
+
+        removeSpy = jest.spyOn(context, "remove");
+        context.show(() => "", { duration: 1 }).catch(() => {
+        });
+        jest.advanceTimersByTime(900);
+        expect(removeSpy).toBeCalledTimes(0);
+        context.getNotifications()[0].props.refreshTimer();
+
+        jest.advanceTimersByTime(900);
+        expect(removeSpy).toBeCalledTimes(0);
+        expect(context.getNotifications().length).toBe(1);
+
+        jest.advanceTimersByTime(100);
+        expect(removeSpy).toBeCalledTimes(1);
+
+        jest.useRealTimers();
+    });
+
+    it("should not be removed", () => {
+        jest.useFakeTimers();
+
+        let removeSpy = jest.spyOn(context, "remove");
+        context.show(() => "", { duration: "forever" }).catch(() => {
+        });
+
+        jest.advanceTimersByTime(15000);
+        expect(removeSpy).not.toBeCalled();
+        
+        context.getNotifications()[0].props.onClose();
+        removeSpy.mockRestore();
+
+        removeSpy = jest.spyOn(context, "remove");
+        context.show(() => "", { duration: 1 }).catch(() => {
+        });
+        jest.advanceTimersByTime(900);
+        
+        context.getNotifications()[0].props.clearTimer();
+        jest.advanceTimersByTime(1000);
+        
+        expect(removeSpy).not.toBeCalled();
+
+        jest.useRealTimers();
     });
 });
