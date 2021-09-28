8bbc26a70b5233d8658a85923bad22141e4aeb96
rayetzki <rayetzkiillya@gmail.com>
2021-09-28 16:12:50 +0300

    [IconButton]: Refactor

---
+++ uui-components/src/buttons/IconButton.tsx
@@ -0,0 +1,24 @@
+import * as React from 'react';
+import { ButtonBaseProps, ButtonBase } from "./ButtonBase";
+import { IconContainer } from '../layout';
+import * as css from './Button.scss';
+
+export interface IconButtonBaseProps extends ButtonBaseProps {}
+
+export class IconButton extends ButtonBase<IconButtonBaseProps> {
+    constructor(props: IconButtonBaseProps) {
+        super(props);
+    }
+
+    getClassName() {
+        return [
+            css.container
+        ];
+    }
+
+    getChildren() {
+        return [
+            <IconContainer key='icon' icon={ this.props.icon } />,
+        ];
+    }
+}
