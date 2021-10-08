b9e7ba6a4036e8be89348c0c6678d5dd169bd60a
rayetzki <rayetzkiillya@gmail.com>
2021-10-08 09:02:08 +0300

    [useForm]: Demo styles

--- app/src/sandbox/use-form/UseFormDemo.tsx
+++ app/src/sandbox/use-form/UseFormDemo.tsx
@@ -1,6 +1,7 @@
-import * as React from 'react';
+import React from 'react';
 import { useForm, useArrayDataSource } from '@epam/uui';
-import { Button, TextInput, PickerInput, LabeledInput } from '@epam/promo';
+import { Button, TextInput, PickerInput, LabeledInput, FlexRow, Panel, FlexCell, FlexSpacer } from '@epam/promo';
+import * as css from './UseFormDemo.module.scss';
 
 enum Styles {
     MODERN = 'Modern',
@@ -11,7 +12,7 @@ enum Styles {
 
 interface Artist {
     name: string;
-    style: Styles | '';
+    style: Styles;
 }
 
 export function UseFormDemo() {
@@ -31,30 +32,39 @@ export function UseFormDemo() {
                 style: { isRequired: true }
             }
         }),
-        value: { name: '', style: '' }
+        value: { name: '', style: Styles.MODERN }
     });
 
     return (
-        <form onSubmit={save}>
-            <LabeledInput { ...lens.prop('name').toProps() } htmlFor="name" label="Artist Name">
-                <TextInput
-                    { ...lens.prop('name').toProps() }
-                    id="name"
-                    placeholder='Artist Name'
-                />
-            </LabeledInput>
-            <LabeledInput { ...lens.prop('style').toProps() } htmlFor="style" label="Painting Style">
-                <PickerInput
-                    { ...lens.prop('style').toProps() }
-                    dataSource={ artistStyleDataSource }
-                    selectionMode='single'
-                    valueType='id'
-                    getName={ ({ style }) => style }
-                    inputId="country"
-                    placeholder='Select Painting Style'
-                />
-            </LabeledInput>
-            <Button rawProps={{ type: 'submit' }} caption='Submit' />
-        </form>
+        <Panel background='white' shadow cx={ css.root }>
+            <FlexCell width='auto'>
+                <FlexRow vPadding='12'>
+                    <LabeledInput { ...lens.prop('name').toProps() } htmlFor="name" label="Artist Name">
+                        <TextInput
+                            { ...lens.prop('name').toProps() }
+                            id="name"
+                            placeholder='Artist Name'
+                        />
+                    </LabeledInput>
+                </FlexRow>
+                <FlexRow vPadding='12'>
+                    <LabeledInput { ...lens.prop('style').toProps() } htmlFor="style" label="Painting Style">
+                        <PickerInput
+                            { ...lens.prop('style').toProps() }
+                            dataSource={ artistStyleDataSource }
+                            selectionMode='single'
+                            valueType='id'
+                            getName={ ({ style }) => style }
+                            inputId="country"
+                            placeholder='Select Painting Style'
+                        />
+                    </LabeledInput>
+                </FlexRow>
+                <FlexRow spacing='12'>
+                    <FlexSpacer />
+                    <Button onClick={ save } rawProps={{ type: 'submit' }} caption='Submit' />
+                </FlexRow>
+            </FlexCell>
+        </Panel>
     )
 }
