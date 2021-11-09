a2c1609d7fa9c8b0cfbe176d4b6e637b8cd08d10
Anton Andryushin <an.andryushin@gmail.com>
2021-11-09 14:58:26 +0300

    Added sliding panel and moved presets api

--- app/src/demo/table/InfoSidebarPanel.tsx
+++ app/src/demo/table/InfoSidebarPanel.tsx
@@ -1,27 +1,17 @@
-import * as React from "react";
+import React from "react";
+import css from "./InfoSidebarPanel.scss";
 import { Badge, EpamAdditionalColor, FlexCell, FlexRow, FlexSpacer, IconButton, Panel, ScrollBars, Text } from "@epam/promo";
 import { Person } from "@epam/uui-docs";
-import * as css from "./InfoSidebarPanel.scss";
-import * as  crossIcon from "@epam/assets/icons/common/navigation-close-24.svg";
-import { useEffect, useState } from "react";
+import crossIcon from "@epam/assets/icons/common/navigation-close-24.svg";
 import { cx } from "@epam/uui";
 
 interface SidebarPanelProps {
-    panelId: number | null;
     data: Person;
-    onClose: () => void;
+    isVisible: boolean;
+    onClose(): void;
 }
 
-export const InfoSidebarPanel: React.FC<SidebarPanelProps> = ({ panelId, data, onClose }) => {
-    const [lastData, setLastData] = useState(data);
-    
-    useEffect(() => {
-        if (panelId !== null) setLastData(data);
-    }, [panelId]);
-    
-    const isPanelOpened = panelId !== null;
-    const resultData = isPanelOpened ? data : lastData;
-
+export const InfoSidebarPanel: React.FC<SidebarPanelProps> = ({ data, isVisible, onClose }) => {
     const renderInfoRow = (title: string, value: any) => {
         return <FlexRow padding="24">
             <FlexCell shrink={ 0 } width={ 162 }>
@@ -34,36 +24,36 @@ export const InfoSidebarPanel: React.FC<SidebarPanelProps> = ({ panelId, data, o
     };
 
     return (
-        <div className={ cx(css.infoSidebarPanelWrapper, isPanelOpened ? "show" : "hide") }>
+        <div className={ cx(css.infoSidebarPanelWrapper, isVisible ? "show" : "hide") }>
             <Panel cx={ css.wrapper } background="white">
                 <FlexRow borderBottom padding="24">
                     <Text size="48" font="sans-semibold">Detailed Information</Text>
                     <FlexSpacer/>
                     <FlexCell shrink={ 0 } width="auto"><IconButton icon={ crossIcon } onClick={ onClose }/></FlexCell>
                 </FlexRow>
-                { resultData &&
-                <ScrollBars>
-                    { renderInfoRow("Name", resultData.name) }
-                    { renderInfoRow("Status",
-                        <Badge cx={ css.status } caption={ resultData.profileStatus } fill="transparent" color={ resultData.profileStatus.toLowerCase() as EpamAdditionalColor }/>) }
-                    { renderInfoRow("Job Title", resultData.jobTitle) }
-                    { renderInfoRow("Title Level", resultData.titleLevel) }
-                    { renderInfoRow("Office", resultData.officeAddress) }
-                    { renderInfoRow("City", resultData.cityName) }
-                    { renderInfoRow("Country", resultData.countryName) }
-                    { renderInfoRow("Manager", resultData.managerName) }
-                    { renderInfoRow("Hire date", new Date(resultData.hireDate).toLocaleDateString()) }
-                    { renderInfoRow("Related NPR", resultData.relatedNPR ? "Completed" : "Uncompleted") }
-                    { renderInfoRow("Department", resultData.departmentName) }
-                    { renderInfoRow("Email", resultData.email) }
-                    { renderInfoRow("Modified", new Date(resultData.modifiedDate).toLocaleDateString()) }
-                    { renderInfoRow("Notes", resultData.notes || "-") }
-                    { renderInfoRow("Primary skill", resultData.primarySkill) }
-                    { renderInfoRow("Production category", resultData.productionCategory) }
-                    { renderInfoRow("UID", resultData.uid) }
-                    { renderInfoRow("Birth date", new Date(resultData.birthDate).toLocaleDateString()) }
-                </ScrollBars>
-                }
+                { data && (
+                    <ScrollBars>
+                        { renderInfoRow("Name", data.name) }
+                        { renderInfoRow("Status",
+                            <Badge cx={ css.status } caption={ data.profileStatus } fill="transparent" color={ data.profileStatus.toLowerCase() as EpamAdditionalColor }/>) }
+                        { renderInfoRow("Job Title", data.jobTitle) }
+                        { renderInfoRow("Title Level", data.titleLevel) }
+                        { renderInfoRow("Office", data.officeAddress) }
+                        { renderInfoRow("City", data.cityName) }
+                        { renderInfoRow("Country", data.countryName) }
+                        { renderInfoRow("Manager", data.managerName) }
+                        { renderInfoRow("Hire date", new Date(data.hireDate).toLocaleDateString()) }
+                        { renderInfoRow("Related NPR", data.relatedNPR ? "Completed" : "Uncompleted") }
+                        { renderInfoRow("Department", data.departmentName) }
+                        { renderInfoRow("Email", data.email) }
+                        { renderInfoRow("Modified", new Date(data.modifiedDate).toLocaleDateString()) }
+                        { renderInfoRow("Notes", data.notes || "-") }
+                        { renderInfoRow("Primary skill", data.primarySkill) }
+                        { renderInfoRow("Production category", data.productionCategory) }
+                        { renderInfoRow("UID", data.uid) }
+                        { renderInfoRow("Birth date", new Date(data.birthDate).toLocaleDateString()) }
+                    </ScrollBars>
+                ) }
             </Panel>
         </div>
     );
