0da833d9b1310fb9859e34786d96847131bfd15f
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2021-11-30 11:34:19 +0300

    update ErrorHandler

--- epam-promo/components/errors/config.ts
+++ epam-promo/components/errors/config.ts
@@ -1,14 +1,14 @@
 import { ApiRecoveryReason } from '@epam/uui';
 import { i18n } from '../../i18n';
 
-export const recoveryMessageConfig: Record<ApiRecoveryReason, { title: string, subtitle: string }> = {
+export const getRecoveryMessageConfig: () => Record<ApiRecoveryReason, { title: string, subtitle: string }> = () => ({
     'auth-lost': i18n.errorHandler.recoveryMessageConfig['auth-lost'],
     'connection-lost': i18n.errorHandler.recoveryMessageConfig['connection-lost'],
     'maintenance': i18n.errorHandler.recoveryMessageConfig.maintenance,
     'server-overload': i18n.errorHandler.recoveryMessageConfig['server-overload'],
-};
+});
 
-export const errorPageConfig = {
+export const getErrorPageConfig = () => ({
     notFound: {
         imageUrl: 'http://static.cdn.epam.com/uploads/690afa39a93c88c4dd13758fe1d869d5/EPM-UUI/error-pages-illustrations/L_Error_404_Monochrome.svg',
         mobileImageUrl: 'http://static.cdn.epam.com/uploads/690afa39a93c88c4dd13758fe1d869d5/EPM-UUI/error-pages-illustrations/L_Error_404_Monochrome.svg',
@@ -39,4 +39,4 @@ export const errorPageConfig = {
         title: i18n.errorHandler.errorPageConfig.default.title,
         subtitle: i18n.errorHandler.errorPageConfig.default.subtitle,
     },
-};
+});
