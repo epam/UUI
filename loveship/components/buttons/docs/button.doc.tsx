9ae16097ce1be8828027c8a3b380aa37ef4e9c3b
rayetzki <rayetzkiillya@gmail.com>
2022-01-05 17:15:32 +0200

    [Loveship][Button]: Fix docs

--- loveship/components/buttons/docs/button.doc.tsx
+++ loveship/components/buttons/docs/button.doc.tsx
@@ -1,13 +1,19 @@
+import * as React from 'react';
 import { allBorderStyles, allFillStyles } from '../../types';
 import { Button, ButtonMods } from '../Button';
-import { DocBuilder } from '@epam/uui-docs';
+import { ColorPicker, DocBuilder } from '@epam/uui-docs';
 import { ButtonProps } from '@epam/uui-components';
-import { onClickDoc, textSettingsDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder, colorDoc, iconWithInfoDoc, iconOptionsDoc } from '../../../docs';
+import { onClickDoc, textSettingsDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder, iconWithInfoDoc, iconOptionsDoc } from '../../../docs';
 import { DefaultContext, ResizableContext, FormContext, GridContext } from '../../../docs';
+import { colors } from '../../../helpers/colorMap';
 
 const ButtonDoc = new DocBuilder<ButtonProps & ButtonMods>({ name: 'Button', component: Button })
     .prop('size', { examples: ['18', "24", "30", "36", "42", "48"] , defaultValue: '36' })
-    .implements([textSettingsDoc, onClickDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder, colorDoc] as any)
+    .implements([textSettingsDoc, onClickDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder] as any)
+    .prop('color', {
+        renderEditor: (editable, examples) => <ColorPicker colors={ examples.map(i => ({ value: i, hex: colors[i] })) } { ...editable } />,
+        examples: ['night600'],
+    })
     .prop('caption', { examples:  [
         { value: 'Click me', isDefault: true },
         { name: 'long text', value: 'kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa' },
