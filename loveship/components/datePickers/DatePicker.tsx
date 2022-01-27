2c33a1ca73a0a9a5b696785648499f4bb4349eb9
Anton Andryushin <an.andryushin@gmail.com>
2022-01-27 14:40:30 +0300

    Added fixed height to <DatePicker/>

--- loveship/components/datePickers/DatePicker.tsx
+++ loveship/components/datePickers/DatePicker.tsx
@@ -59,6 +59,7 @@ export class DatePicker extends BaseDatePicker<DatePickerProps> {
                 renderDay={ this.props.renderDay }
                 isHoliday={ this.props.isHoliday }
                 rawProps={ this.props.rawProps?.body }
+                cx={ css.body }
             />
             { this.props.renderFooter?.() }
         </DropdownContainer>;
