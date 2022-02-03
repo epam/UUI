48cdfb783baa91c86de9308d322307b457bbdf37
rayetzki <rayetzkiillya@gmail.com>
2022-02-03 10:56:26 +0200

    [PopperTargetWrapper]: refactor

--- uui-components/src/overlays/PopperTargetWrapper.tsx
+++ uui-components/src/overlays/PopperTargetWrapper.tsx
@@ -2,18 +2,18 @@ import * as React from "react";
 import { findDOMNode } from "react-dom";
 
 interface TooltipWrapperProps {
-    innerRef: React.Ref<HTMLElement>;
+    innerRef: any;
 }
 
 export class PopperTargetWrapper extends React.Component<TooltipWrapperProps> {
     componentDidMount() {
-        const node = findDOMNode(this) as HTMLElement;
-        (this.props.innerRef as React.RefCallback<HTMLElement>)(node);
+        const node = findDOMNode(this);
+        this.props.innerRef(node);
     }
 
     componentDidUpdate() {
-        const node = findDOMNode(this) as HTMLElement;
-        (this.props.innerRef as React.RefCallback<HTMLElement>)(node);
+        const node = findDOMNode(this);
+        this.props.innerRef(node);
     }
 
     render() {
