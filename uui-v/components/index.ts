b4e69dfe8d8a5962c43c36040c319b78d62cc818
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2022-02-10 11:40:17 +0300

    Added PickerInput, Avatar, Tag, IconContainer, VirtualList to uui-v, fixed icon colors for PickerToggler

--- uui-v/components/index.ts
+++ uui-v/components/index.ts
@@ -2,6 +2,7 @@ export * from './buttons';
 export * from './inputs';
 export * from './layout';
 export * from './overlays';
+export * from './pickers';
 export * from './typography';
 export * from './widgets';
 export * from './types';
