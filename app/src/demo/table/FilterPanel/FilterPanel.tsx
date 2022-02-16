2e35fc870d301d6ad32fa56649a753959219eaef
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2022-02-16 15:06:37 +0300

    added new components - Accordion, TabButton, VerticalTabButton, PickerList, PickerListItem

--- app/src/demo/table/FilterPanel/FilterPanel.tsx
+++ app/src/demo/table/FilterPanel/FilterPanel.tsx
@@ -1,13 +1,11 @@
-import React from "react";
-import { FlexRow, IconButton, ScrollBars, Text } from "@epam/promo";
-import { FlexSpacer } from "@epam/uui-components";
-import { DataColumnProps } from "@epam/uui";
-import { ReactComponent as CloseIcon } from "@epam/assets/icons/common/navigation-close-24.svg";
-
-import { ITableFilter, ITableState } from "../types";
-import { PresetsBlock } from "./PresetsBlock";
-import { FiltersBlock } from "./FiltersBlock";
-import { ColumnsBlock } from "./ColumnsBlock";
+import React from 'react';
+import { DataColumnProps } from '@epam/uui-core';
+import { FlexRow, IconButton, ScrollBars, Text, FlexSpacer } from '@epam/uui';
+import { ITableFilter, ITableState } from '../types';
+import { PresetsBlock } from './PresetsBlock';
+import { FiltersBlock } from './FiltersBlock';
+import { ColumnsBlock } from './ColumnsBlock';
+import { ReactComponent as CloseIcon } from '@epam/assets/icons/common/navigation-close-24.svg';
 
 export interface IFilterPanelProps extends ITableState {
     columns: DataColumnProps<any>[];
@@ -18,8 +16,8 @@ export interface IFilterPanelProps extends ITableState {
 const FilterPanel: React.FC<IFilterPanelProps> = props => {
     return (
         <>
-            <FlexRow background="white" borderBottom size="48" padding="18">
-                <Text fontSize="18" font="sans-semibold">Views</Text>
+            <FlexRow borderBottom size='48' padding='18'>
+                <Text fontSize='18' font='semibold'>Views</Text>
                 <FlexSpacer/>
                 <IconButton icon={ CloseIcon } onClick={ props.closePanel }/>
             </FlexRow>
--- app/src/demo/table/SlidingPanel.tsx
+++ app/src/demo/table/SlidingPanel.tsx
@@ -1,10 +1,11 @@
-import React, { CSSProperties, ReactElement, useEffect, useMemo, useState } from "react";
-import css from "./SlidingPanel.scss";
-import { cx } from "@epam/uui";
+import React, { CSSProperties, ReactElement, useEffect, useMemo, useState } from 'react';
+import cx from 'classnames';
+import { Panel } from '@epam/uui';
+import css from './SlidingPanel.scss';
 
 interface ISlidingPanelProps {
     isVisible: boolean;
-    position: "left" | "right";
+    position: 'left' | 'right';
     width: number;
     children: ReactElement;
 }
@@ -23,22 +24,22 @@ const SlidingPanelImpl: React.FC<ISlidingPanelProps> = ({ isVisible, position, w
     }, [isVisible]);
 
     const style: CustomCssProperties = useMemo(() => ({
-        width: width + "px",
-        "--sliding-panel-margin": -width + "px",
+        width: width + 'px',
+        '--sliding-panel-margin': -width + 'px',
     }), [width, position, isVisible]);
 
     const upperPosition = position[0].toUpperCase() + position.slice(1);
     const className = cx(
         css.container,
-        css["container" + upperPosition],
-        { [css["container" + upperPosition + "Show"]]: isVisible && !isFirstMount },
-        { [css["container" + upperPosition + "Hide"]]: !isVisible && !isFirstMount },
+        css['container' + upperPosition],
+        { [css['container' + upperPosition + 'Show']]: isVisible && !isFirstMount },
+        { [css['container' + upperPosition + 'Hide']]: !isVisible && !isFirstMount },
     );
 
     return (
-        <div className={ className } style={ style }>
+        <Panel background cx={ className } style={ style }>
             { children }
-        </div>
+        </Panel>
     );
 };
 
