01d5cfee2a1377e709e3241b89dcbf44d3543377
rayetzki <rayetzkiillya@gmail.com>
2022-02-22 10:36:51 +0200

    [TextInput]: Added docs for prefix-suffix

--- epam-promo/components/inputs/docs/textInput.doc.ts
+++ epam-promo/components/inputs/docs/textInput.doc.ts
@@ -8,11 +8,13 @@ const TextInputDoc = new DocBuilder<TextInputProps & TextInputMods>({ name: 'Tex
     .prop('size', { examples: ['24', '30', '36', '42', '48'], defaultValue: '36' })
     .implements([onClickDoc, isDisabledDoc, isReadonlyDoc, isInvalidDoc, iconDoc, iconOptionsDoc, iEditable, iHasPlaceholder, dropdownTogglerDoc, IHasEditModeDoc])
     .prop('maxLength', { examples: [10, 20, 30], type: 'number' })
+    .prop('suffix', { examples: [ { value: 'Suffix' } ] })
+    .prop('prefix', { examples: [ { value: 'Prefix: ' } ]})
     .prop('value', { examples: [
-            { value: 'Hello, World!', isDefault: true },
-            { name: 'long text', value: 'kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa' },
-            { name: 'long word', value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
-        ], type: 'string' })
+        { value: 'Hello, World!', isDefault: true },
+        { name: 'long text', value: 'kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa kolbasa' },
+        { name: 'long word', value: 'aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa' },
+    ], type: 'string' })
     .prop('onAccept', { examples: ctx => [ctx.getCallback('onAccept')] })
     .prop('onCancel', { examples: ctx => [ctx.getCallback('onCancel')] })
     .prop('type', { examples: ['text', 'password'], type: 'string', defaultValue: 'text' })
