3cb97395865913632d1a13136030222e238eba0c
Anton Andryushin <an.andryushin@gmail.com>
2022-03-14 17:11:53 +0300

    NumericInput: added table context in docs and some small fixes

--- loveship/components/inputs/docs/numericInput.doc.ts
+++ loveship/components/inputs/docs/numericInput.doc.ts
@@ -2,7 +2,7 @@ import { DocBuilder, isReadonlyDoc } from '@epam/uui-docs';
 import { NumericInputProps } from '@epam/uui-components';
 import { NumericInput, NumericInputMods } from '../NumericInput';
 import { iEditable, sizeDoc, textSettingsDoc, isDisabledDoc, iHasPlaceholder, modeDoc, iFormatter } from '../../../docs';
-import { FormContext, GridContext, ResizableContext, DefaultContext } from '../../../docs';
+import { FormContext, GridContext, ResizableContext, DefaultContext, TableContext } from '../../../docs';
 
 const NumericInputDoc = new DocBuilder<NumericInputProps & NumericInputMods>({ name: 'NumericInput', component: NumericInput })
     .implements([iEditable, iHasPlaceholder, sizeDoc, textSettingsDoc, isDisabledDoc, isReadonlyDoc, modeDoc, iFormatter])
@@ -13,6 +13,6 @@ const NumericInputDoc = new DocBuilder<NumericInputProps & NumericInputMods>({ n
     .prop('mode', { examples: ["form", "cell"] })
     .prop('align', { examples: ["left", "right"] })
     .prop('disableArrows', { examples: [true, false] })
-    .withContexts(DefaultContext, FormContext, ResizableContext, GridContext);
+    .withContexts(DefaultContext, FormContext, TableContext, ResizableContext, GridContext);
 
 export = NumericInputDoc;
