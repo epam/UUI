cc4c25340b109391899779529cf3c73f61a422ba
Sergey Zenin <sergey_zenin@epam.com>
2022-03-31 12:27:34 +0300

    update main menu styles for uui3

--- loveship/components/navigation/MainMenu/MainMenuDropdown.tsx
+++ loveship/components/navigation/MainMenu/MainMenuDropdown.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import cx from 'classnames';
 import { Dropdown, MainMenuDropdownProps } from '@epam/uui-components';
-import { DropdownMenuBody } from '../../overlays';
 import { MainMenuButton } from './MainMenuButton';
 import * as css from './MainMenuDropdown.scss';
 
@@ -13,28 +12,23 @@ export class MainMenuDropdown extends React.Component<MainMenuDropdownProps> {
                     <MainMenuButton
                         { ...props }
                         caption={ this.props.caption }
-                        cx={ cx(props.isOpen && css.open, this.props.cx) }
                         rawProps={ this.props.rawProps }
                         isLinkActive={ this.props.isLinkActive }
+                        isDropdown
                     />
                     ) }
                 renderBody={ (props) => (
-                    <DropdownMenuBody color='night' inMainMenu>
-                        { React.Children.map(this.props.children, (item: any) => {
-                            if (!item) {
-                                return item;
-                            }
-                            const itemProps = {
-                                ...item.props,
-                                onClick: () => {
-                                    item.props.onClick && item.props.onClick();
-                                    props.onClose();
-                                },
-                            };
-
-                            return React.createElement(item.type, itemProps);
-                        }) }
-                    </DropdownMenuBody>
+                    <div className={ cx(css.dropdownBody) } >
+                        { React.Children.map(this.props.children, (item: any) => 
+                             !!item && React.createElement(item.type, {
+                                 ...item.props,
+                                 onClick: () => {
+                                     item.props.onClick?.();
+                                     props.onClose();
+                                 },
+                             }),
+                        ) }
+                    </div>
                 ) }
                 placement='bottom-start'
             />
