cd44c80f9eedb520b941f002f0edb6e12349f441
Sergey Zenin <sergey_zenin@epam.com>
2022-04-04 11:16:41 +0300

    remove useImperativeHandle

--- loveship/components/datePickers/DatePicker.tsx
+++ loveship/components/datePickers/DatePicker.tsx
@@ -21,32 +21,30 @@ export interface DatePickerProps extends BaseDatePickerProps, SizeMod, TextSetti
 }
 
 export class DatePicker extends BaseDatePicker<DatePickerProps> {
-    renderInput = ({ ref, ...props }: IDropdownToggler) => {
+    renderInput = (props: IDropdownToggler) => {
         return (
-            <div ref={ ref }>
-                <TextInput
-                    { ...props }
-                    onClick={ null }
-                    isDropdown={ false }
-                    cx={ cx(this.props.cx, css.dateInput, this.state.isOpen && uuiMod.focus) }
-                    icon={ systemIcons[this.props.size || '36'].calendar }
-                    iconPosition={ this.props.iconPosition || 'left' }
-                    placeholder={ this.props.placeholder ? this.props.placeholder : this.getFormat() }
-                    size={ this.props.size || '36' }
-                    lineHeight={ this.props.lineHeight }
-                    fontSize={ this.props.fontSize }
-                    mode={ this.props.mode || 'form' }
-                    value={ this.state.inputValue }
-                    onValueChange={ this.handleInputChange }
-                    onCancel={ this.props.disableClear ? null : this.state.inputValue && this.handleCancel }
-                    isInvalid={ this.props.isInvalid }
-                    isDisabled={ this.props.isDisabled }
-                    isReadonly={ this.props.isReadonly }
-                    onFocus={ this.handleFocus }
-                    onBlur={ this.handleBlur }
-                    rawProps={ this.props.rawProps?.input }
-                />
-            </div>
+            <TextInput
+                { ...props }
+                onClick={ null }
+                isDropdown={ false }
+                cx={ cx(this.props.cx, css.dateInput, this.state.isOpen && uuiMod.focus) }
+                icon={ systemIcons[this.props.size || '36'].calendar }
+                iconPosition={ this.props.iconPosition || 'left' }
+                placeholder={ this.props.placeholder ? this.props.placeholder : this.getFormat() }
+                size={ this.props.size || '36' }
+                lineHeight={ this.props.lineHeight }
+                fontSize={ this.props.fontSize }
+                mode={ this.props.mode || 'form' }
+                value={ this.state.inputValue }
+                onValueChange={ this.handleInputChange }
+                onCancel={ this.props.disableClear ? null : this.state.inputValue && this.handleCancel }
+                isInvalid={ this.props.isInvalid }
+                isDisabled={ this.props.isDisabled }
+                isReadonly={ this.props.isReadonly }
+                onFocus={ this.handleFocus }
+                onBlur={ this.handleBlur }
+                rawProps={ this.props.rawProps?.input }
+            />
         );
     }
 
