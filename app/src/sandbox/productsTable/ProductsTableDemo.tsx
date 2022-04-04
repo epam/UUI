0bf73061fda7aeac6c87391547e27c71573c3e95
Yakov Zhmourov <yakov_zhmourov@epam.com>
2022-04-05 02:42:59 +0300

    ProductsTableDemo - added save/revert buttons and validation

--- app/src/sandbox/productsTable/ProductsTableDemo.tsx
+++ app/src/sandbox/productsTable/ProductsTableDemo.tsx
@@ -1,24 +1,42 @@
-import { DataTable, useForm, NotificationCard, Text } from 'epam-promo';
+import { DataTable, useForm, NotificationCard, Text, Panel, Button, FlexCell, FlexRow, FlexSpacer  } from 'epam-promo';
 import React from 'react';
-import { DataQueryFilter, useLazyDataSource, useTableState, useUuiContext, UuiContexts } from 'uui-core';
+import { DataQueryFilter, Metadata, useLazyDataSource, useTableState, useUuiContext, UuiContexts } from 'uui-core';
 import { Product } from 'uui-docs';
 import type { TApi } from '../../data';
 import { productColumns } from './columns';
 
-interface EditableState {
+interface FormState {
     items: Record<number, Product>;
+}
+
+const metadata: Metadata<FormState> = {
+    props: {
+        items: {
+            all: {
+                props: {
+                    Name: {
+                        isRequired: true,
+                    },
+                    DaysToManufacture: {
+                        minValue: 1
+                    }
+                }
+            }
+        }
+    }
 }
 
 export const ProductsTableDemo: React.FC = (props) => {
     const svc = useUuiContext<TApi, UuiContexts>();
 
-    const { lens } = useForm<EditableState>({
+    const { lens, save, isChanged, revert, canRevert } = useForm<FormState>({
         value: { items: { 1: { ProductID: 1, Name: "!Changed!" } as any } },
-        onSave: (value) => svc.uuiNotifications.show(() =>
-            <NotificationCard onClose={null} onSuccess={null} color='blue' id={1} key='1'>
+        onSave: (value) => svc.uuiNotifications.show(props =>
+            <NotificationCard { ...props } color='blue' id={1} key='1'>
                 <Text>{ JSON.stringify(value) }</Text>
             </NotificationCard>
-        )
+        ),
+        getMetadata: () => metadata,
     });
 
     const { tableState, setTableState } = useTableState<any>({ columns: productColumns });
@@ -32,15 +50,26 @@ export const ProductsTableDemo: React.FC = (props) => {
         getRowLens: (id) => lens.prop('items').prop(id)
     });
 
-    return <DataTable
-        headerTextCase='upper'
-        getRows={ dataView.getVisibleRows }
-        columns={ productColumns }
-        value={ tableState }
-        onValueChange={ setTableState }
-        showColumnsConfig
-        allowColumnsResizing
-        allowColumnsReordering
-        { ...dataView.getListProps() }
-    />;
+    return <Panel>
+        <FlexRow spacing='12' margin='12'>
+            <FlexSpacer />
+            <FlexCell width='auto'>
+                <Button caption="Save" onClick={ save } isDisabled={ !isChanged } />
+            </FlexCell>
+            <FlexCell width='auto'>
+            <Button caption="Revert" onClick={ revert } isDisabled={ !canRevert } />
+            </FlexCell>
+        </FlexRow>
+        <DataTable
+            headerTextCase='upper'
+            getRows={ dataView.getVisibleRows }
+            columns={ productColumns }
+            value={ tableState }
+            onValueChange={ setTableState }
+            showColumnsConfig
+            allowColumnsResizing
+            allowColumnsReordering
+            { ...dataView.getListProps() }
+        />
+    </Panel>;
 }
