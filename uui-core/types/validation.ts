5442ebb035642cb0385798797d09f5cc76b16db8
Yakov Zhmourov <yakov_zhmourov@epam.com>
2022-04-05 02:41:05 +0300

    Metadata - added typing for 'all' prop

--- uui-core/types/validation.ts
+++ uui-core/types/validation.ts
@@ -1,10 +1,17 @@
 import {ICanBeInvalid, ICanBeReadonly, ICanBeRequired, IDisableable} from "./props";
 
+type ElementType<ArrayOrObject> =
+    ArrayOrObject extends readonly (infer ElementType)[]
+    ? ElementType
+    : ArrayOrObject extends Record<any, (infer ValueType)>
+        ? ValueType
+        : never;
+
 export interface Metadata<T> extends IDisableable, ICanBeReadonly, ICanBeRequired {
     props?: {
         [P in keyof T]?: Metadata<T[P]>
     };
-    all?: Metadata<any>;
+    all?: Metadata<ElementType<T>>;
     minValue?: number;
     maxValue?: number;
     maxLength?: number;
