0e0e1207e5341ddc6782909ac9d95bbccff3fae7
Yakov Zhmourov <yakov_zhmourov@epam.com>
2022-04-14 02:48:32 +0300

    DataTables - better width/grow calculation, z-index tweaks, CSS improvements

--- loveship/components/tables/DataTableCell.tsx
+++ loveship/components/tables/DataTableCell.tsx
@@ -6,7 +6,7 @@ import { DataTableCellMods } from './types';
 import { ReactComponent as FoldingArrow } from '../icons/tree_folding_arrow.svg';
 import * as css from './DataTableCell.scss';
 
-export class DataTableCell<TItem, TId> extends React.Component<DataTableCellProps<TItem, TId> & DataTableCellMods> {
+export class DataTableCell<TItem, TId, TCellValue> extends React.Component<DataTableCellProps<TItem, TId, TCellValue> & DataTableCellMods> {
     hasDepsWidgets = !!(this.props.rowProps?.checkbox?.isVisible || this.props.rowProps?.indent);
 
     isDraggable = () => !!this.props.rowProps?.dnd?.srcData;
@@ -55,7 +55,7 @@ export class DataTableCell<TItem, TId> extends React.Component<DataTableCellProp
         return (
             <FlexCell
                 { ...this.props.column }
-                minWidth={ this.props.column.width || this.props.column.minWidth }
+                minWidth={ this.props.column.width }
                 rawProps={ { role: this.props.role } }
                 cx={ cx(
                     css.cell,
