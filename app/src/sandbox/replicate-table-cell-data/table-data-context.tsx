cf6ac661e03ed7c8f434c08d66903909a2d2e6a7
Sergey Zenin <sergey_zenin@epam.com>
2022-04-13 09:56:32 +0300

    add replication feature
    # Conflicts:
    #	app/src/sandbox/SandboxPage.tsx

---
+++ app/src/sandbox/replicate-table-cell-data/table-data-context.tsx
@@ -0,0 +1,39 @@
+import React, { createContext, Dispatch, FC, SetStateAction, useContext, useMemo, useState } from "react";
+
+export interface DataItemExample {
+    column0: string;
+    column1: {
+        value: string;
+    };
+    column2: string;
+    column3: string;
+    column4: string;
+}
+
+export type TableDataContextState = [
+    value: DataItemExample[],
+    setValue: Dispatch<SetStateAction<DataItemExample[]>>
+];
+
+const initialValue: DataItemExample[] = Array(20)
+    .fill({
+        column0: '1',
+        column1: {
+            value: '2',
+        },
+        column2: '3',
+        column3: '4',
+        column4: '5',
+    });
+
+const TableDataContext = createContext<TableDataContextState>(null);
+
+export const TableDataContextProvider: FC = ({ children }) => {
+    const [value, setValue] = useState<TableDataContextState[0]>(initialValue);
+
+    const state = useMemo<TableDataContextState>(() => ([value, setValue]), [value]);
+
+    return <TableDataContext.Provider value={ state }>{ children }</TableDataContext.Provider>;
+};
+
+export const useTableDataContext = () => useContext(TableDataContext);
