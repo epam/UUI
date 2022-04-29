a90f19c2ce791f1b02e3be25eb8baa8657e390f7
Olga Studzenichnik <okrypets@gmail.com>
2022-04-29 16:19:34 +0300

    fix rewrote imports

--- uui-components/src/pickers/PickerToggler.tsx
+++ uui-components/src/pickers/PickerToggler.tsx
@@ -1,20 +1,5 @@
 import * as React from 'react';
-import {
-    isChildFocusable,
-    IPickerToggler,
-    IHasIcon,
-    IHasCX,
-    ICanBeReadonly,
-    Icon,
-    uuiMod,
-    uuiElement,
-    uuiMarkers,
-    DataRowProps,
-    closest,
-    cx,
-    IHasRawProps,
-    ICanFocus,
-} from "@epam/uui-core";
+import { isChildFocusable, IPickerToggler, IHasIcon, IHasCX, ICanBeReadonly, Icon, uuiMod, uuiElement, uuiMarkers, DataRowProps, closest, cx, IHasRawProps, ICanFocus } from "@epam/uui-core";
 import { IconContainer } from '../layout';
 import * as css from './PickerToggler.scss';
 import { i18n } from "../../i18n";
