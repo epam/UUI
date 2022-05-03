32eedd2361adc742018ade1250b2dde39f290f60
Aliaksei_Manetau <alekseyManetov@gmail.com>
2022-05-03 14:35:41 +0400

    use IHasChildren interface for component children typings. Remove redundant resolutions

--- app/src/common/docs/ContentSection.tsx
+++ app/src/common/docs/ContentSection.tsx
@@ -1,10 +1,9 @@
 import * as React from 'react';
 import * as css from './ContentSection.scss';
 import { ScrollBars } from '@epam/promo';
+import { IHasChildren } from "@epam/uui-core";
 
-interface ContentSectionProps {
-    children: React.ReactNode;
-}
+interface ContentSectionProps extends IHasChildren {}
 
 export class ContentSection extends React.Component<ContentSectionProps> {
     render() {
--- app/src/docs/examples/tables/ColumnsConfig.example.tsx
+++ app/src/docs/examples/tables/ColumnsConfig.example.tsx
@@ -73,7 +73,7 @@ export default function ColumnsConfigurationDataTableExample(props: DataTableMod
         api: svc.api.demo.cities,
     }, []);
 
-    const handleTableStateChange = useCallback((newState: any) => {
+    const handleTableStateChange = useCallback(newState => {
         // Set columns config to localStorage
         svc.uuiUserSettings.set(LOCAL_STORAGE_KEY, newState.columnsConfig || {});
         setTableState(newState);
