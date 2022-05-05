65ffb99240b54d76b846cca86f461390b02bee00
Sergey Zenin <sergey_zenin@epam.com>
2022-05-05 14:02:57 +0300

    refactor

--- app/src/sandbox/replicate-table-cell-data/useReplication.tsx
+++ app/src/sandbox/replicate-table-cell-data/useReplication.tsx
@@ -1,8 +1,8 @@
-import React, { Context, FC, PointerEventHandler, useContext, useEffect, useMemo, useState } from "react";
-import { ReplicationContextState } from "./data-replication";
-import { ReplicationMarker } from "./ReplicationMarker";
+import React, { PointerEventHandler, useContext, useEffect, useMemo, useState } from "react";
+import { DataReplicationContext } from "./data-replication-context";
+import { ReplicationMarkerProps } from "./ReplicationMarker";
 
-interface Params<Value> {
+export interface ReplicationHookParams<Value> {
     columnIndex: number;
     rowIndex: number;
     rowId: string;
@@ -11,20 +11,8 @@ interface Params<Value> {
     dataType?: string;
 }
 
-interface ReplicationContainerHandlers {
-    onPointerEnter: PointerEventHandler;
-    onPointerLeave: PointerEventHandler;
-}
-
-type GetReplicationHook = <Value>(context: Context<ReplicationContextState<Value>>) => (params: Params<Value>) => {
-    replicationContainerEventHandlers: ReplicationContainerHandlers;
-    ReplicationMarker: FC;
-    valueToReplicate: Value;
-    isSelectedForReplication: boolean;
-};
-
-export const getReplicationHook: GetReplicationHook = context => ({ columnIndex, rowIndex, rowId, columnId, value, dataType }) => {
-    const { replicationRange, setReplicationRange, canReplicate } = useContext(context);
+export function useReplication<Value = any>({ columnIndex, rowIndex, rowId, columnId, value, dataType }: ReplicationHookParams<Value>) {
+    const { replicationRange, setReplicationRange, canReplicate } = useContext(DataReplicationContext);
     const [isHovered, setHoverState] = useState(false);
 
     const handlePointerEnter: PointerEventHandler = () => {
@@ -64,21 +52,21 @@ export const getReplicationHook: GetReplicationHook = context => ({ columnIndex,
     }, [isSelectedForReplication]);
 
     const { isLeft, isRight, isTop, isBottom } = useMemo(() => isSelectedForReplication
-        ? ({
-            isLeft: columnIndex === leftColumnIndex || !canReplicate(rowIndex, columnIndex - 1),
-            isRight: columnIndex === rightColumnIndex || !canReplicate(rowIndex, columnIndex + 1),
-            isTop: rowIndex === topRowIndex || !canReplicate(rowIndex - 1, columnIndex),
-            isBottom: rowIndex === bottomRowIndex || !canReplicate(rowIndex + 1, columnIndex),
-        })
-        : {},
+            ? ({
+                isLeft: columnIndex === leftColumnIndex || !canReplicate(rowIndex, columnIndex - 1),
+                isRight: columnIndex === rightColumnIndex || !canReplicate(rowIndex, columnIndex + 1),
+                isTop: rowIndex === topRowIndex || !canReplicate(rowIndex - 1, columnIndex),
+                isBottom: rowIndex === bottomRowIndex || !canReplicate(rowIndex + 1, columnIndex),
+            })
+            : {},
         [replicationRange]);
 
-    const markerParams = { isHovered, handlePointerDown, isSelectedForReplication, isLeft, isRight, isTop, isBottom };
+    const replicationMarkerParams: ReplicationMarkerProps = { isHovered, handlePointerDown, isSelectedForReplication, isLeft, isRight, isTop, isBottom };
 
     return useMemo(() => ({
         replicationContainerEventHandlers: { onPointerEnter: handlePointerEnter, onPointerLeave: handlePointerLeave },
-        ReplicationMarker: () => <ReplicationMarker { ...markerParams } />,
         valueToReplicate: replicationRange?.value,
         isSelectedForReplication,
+        replicationMarkerParams,
     }), [isSelectedForReplication, isLeft, isRight, isTop, isBottom, isHovered, !!replicationRange]);
-};
+}
