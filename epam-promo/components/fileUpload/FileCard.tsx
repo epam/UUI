a5c79e109dcac9e4619bbdb6af9b3bea6989b9e1
IhorKorenets <vik753@gmail.com>
2022-06-18 19:23:51 +0300

    made UploadFile error state and styles

--- epam-promo/components/fileUpload/FileCard.tsx
+++ epam-promo/components/fileUpload/FileCard.tsx
@@ -1,7 +1,7 @@
 import * as React from 'react';
 import * as css from './FileCard.scss';
 import { i18n } from '../..';
-import { cx, FileUploadResponse, formatBytes, IClickable, IHasCX, uuiMod } from '@epam/uui-core';
+import { cx, formatBytes, IClickable, IHasCX, uuiMod } from '@epam/uui-core';
 import { SvgCircleProgress } from './';
 import { FlexCell, FlexRow, IconButton, IconContainer, Text, Tooltip } from '../';
 import { ReactComponent as RemoveIcon } from '@epam/assets/icons/common/navigation-close-18.svg';
@@ -16,23 +16,23 @@ import { ReactComponent as MailIcon } from '../../icons/fileUpload/file-file_eml
 import { ReactComponent as FileIcon } from '../../icons/fileUpload/file-file-24.svg';
 import { ReactComponent as ErrorIcon } from '../../icons/notification-error-fill-10.svg';
 
-type WithError = { uploadError: { isError: boolean, message?: string } };
+interface NewFile extends Partial<File> {
+    id?: number;
+    progress?: number;
+    abortXHR?: () => void;
+    uploadError?: { isError: boolean, message?: string };
+    extension?: string;
+}
 
 export interface FileCardProps extends IClickable, IHasCX {
-    file: { progress?: number } & Partial<FileUploadResponse & WithError>;
+    file: NewFile;
     width?: number;
 }
 
 export const FileCard = React.forwardRef<HTMLDivElement, FileCardProps>((props, ref) => {
-    const [isLoading, setLoading] = React.useState<boolean>(props.file.progress !== undefined);
+    const [isLoadingShow, setIsLoadingShow] = React.useState<boolean>(true);
 
     const { file } = props;
-
-    React.useEffect(() => {
-        if (file?.progress === 100 && isLoading) {
-            setLoading(false);
-        }
-    }, [file, isLoading]);
 
     const getIcon = (extension: string) => {
         switch (extension) {
@@ -65,37 +65,52 @@ export const FileCard = React.forwardRef<HTMLDivElement, FileCardProps>((props, 
     const { cx: componentCx, width, file: { progress, size, name, extension, uploadError }, onClick } = props;
     const fileExtension = extension || name?.split('.').pop();
     const fileName = name?.split('.').slice(0, -1).join('');
+    const isLoading = progress < 100;
+    const mainWrapperStyles = cx(css.fileCardWrapper, isLoading && uuiMod.loading, componentCx, uploadError?.isError && css.errorCardWrapper);
+    const isCrossShow = ((!isLoadingShow && isLoading) || !isLoading) && onClick;
+
+    const mouseLeaveHandler = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setIsLoadingShow(() => true);
+    };
+
+    const mouseEnterHandler = (e: React.MouseEvent) => {
+        e.stopPropagation();
+        setIsLoadingShow(() => false);
+    };
+
+    const renderErrorContent = () => (
+        <Tooltip trigger="hover" content={ file.uploadError.message } placement="bottom-start">
+            <div className={ css.errorBlock }>
+                <ErrorIcon/>
+                { "Upload failed" }
+            </div>
+        </Tooltip>
+    );
+
+    const renderSuccessfulContent = () => (
+        <Text size="18" fontSize="14" lineHeight="18" color="gray60">
+            { fileExtension && `${ fileExtension.toUpperCase() }, ` }
+            { isLoading && formatBytes(size / 100 * progress) + i18n.fileCard.fileSizeProgress }
+            { formatBytes(size) }
+        </Text>
+    );
 
     return (
-        <FlexCell ref={ ref }
-                  cx={ cx(css.fileCardWrapper, isLoading && uuiMod.loading, componentCx, uploadError?.isError && css.errorCardWrapper) }
-                  minWidth={ width }
-                  width={ !width ? '100%' : undefined } >
-            <FlexRow cx={ css.fileCardRow } size='36' alignItems='top' spacing='6'>
+        <FlexCell ref={ ref } cx={ mainWrapperStyles } minWidth={ width } width={ !width ? '100%' : undefined }>
+            <FlexRow cx={ css.fileCardRow } size="36" alignItems="top" spacing="6">
                 { fileExtension && getIcon(fileExtension) }
                 <FlexCell width="100%">
                     <Text size="18" fontSize="14" lineHeight="18" color={ progress < 100 ? 'gray60' : 'gray80' } cx={ css.fileName }>
                         { fileName }
                     </Text>
-                    { file.uploadError?.isError
-                        ? <Tooltip trigger="hover" content={ file.uploadError.message } placement="bottom-start">
-                            <Text cx={ css.errorBlock } fontSize="12" lineHeight="12" color="red-darkest">
-                                <ErrorIcon/>
-                                { "Upload failed" }
-                            </Text>
-                        </Tooltip>
-                        : <Text size="18" fontSize="14" lineHeight="18" color="gray60">
-                            { fileExtension && `${ fileExtension.toUpperCase() }, ` }
-                            { isLoading && formatBytes(size / 100 * progress) + i18n.fileCard.fileSizeProgress }
-                            { formatBytes(size) }
-                        </Text> }
+                    { file.uploadError?.isError ? renderErrorContent() : renderSuccessfulContent() }
                 </FlexCell>
                 <FlexCell minWidth={ 18 }>
-                    { isLoading ? (
-                        <SvgCircleProgress progress={ progress } size={ 18 } />
-                    ) : onClick && (
-                        <IconButton icon={ RemoveIcon } onClick={ onClick } />
-                    ) }
+                    <div onMouseEnter={ mouseEnterHandler } onMouseLeave={ mouseLeaveHandler }>
+                        { (isLoadingShow && isLoading) && <SvgCircleProgress progress={ progress } size={ 18 }/> }
+                        { isCrossShow && <IconButton icon={ RemoveIcon } onClick={ onClick }/> }
+                    </div>
                 </FlexCell>
             </FlexRow>
         </FlexCell>
