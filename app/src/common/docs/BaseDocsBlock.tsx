dbeafb24ded95dfd45d0f2b64e28130bf3652320
Aliaksei_Manetau <alekseyManetov@gmail.com>
2022-06-22 18:45:11 +0200

    [App]: fix crashing when you navigate from component with property explorer to the component which don't have it

--- app/src/common/docs/BaseDocsBlock.tsx
+++ app/src/common/docs/BaseDocsBlock.tsx
@@ -139,7 +139,7 @@ export abstract class BaseDocsBlock extends React.Component<any, BaseDocsBlockSt
 
     renderPropEditor() {
         if (!this.getPropsDocPath()) {
-            svc.uuiRouter.redirect({
+            return svc.uuiRouter.redirect({
                 pathname: '/documents',
                 query: {
                     category: getQuery('category'),
