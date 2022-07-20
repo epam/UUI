d1faf77e68c509624a40da8edc478ef9b9d35d83
Igor Zakharov <igor_zakharov3@epam.com>
2022-07-20 15:45:15 +0600

    [App]: reworked dropdown exmaples

--- app/src/docs/examples/dropdown/CloseFromBody.example.tsx
+++ app/src/docs/examples/dropdown/CloseFromBody.example.tsx
@@ -1,17 +1,25 @@
-import React, { useState } from 'react';
+import React from 'react';
 import { Dropdown, DropdownBodyProps } from '@epam/uui-components';
-import { LinkButton, Button, Panel, Text, FlexRow } from "@epam/promo";
+import { Button, Text, FlexRow, DropdownContainer, FlexCell } from "@epam/promo";
 import { IDropdownToggler } from '@epam/uui';
 
 export default function BasicDropdownExample() {
     const renderDropdownBody = (props: DropdownBodyProps) => {
         return (
-            <Panel background='white' shadow={ true }>
-                <FlexRow padding='12' vPadding='12'>
-                    <Text>Dropdown body content.</Text>
-                    <LinkButton caption='Click to close' onClick={ props.onClose } />
-                </FlexRow>
-            </Panel>
+            <DropdownContainer maxWidth={ 360 } vPadding="24" padding="18" { ...props }>
+                <FlexCell alignSelf="flex-start">
+                    <Text fontSize="18" lineHeight="24" color="gray90" font="museo-slab">Some Title</Text>
+                    <Text fontSize="14">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Autem beatae delectus
+                        distinctio esse explicabo id, optio qui repellat ut veritatis!</Text>
+
+                </FlexCell>
+                <FlexCell alignSelf="flex-start">
+                    <FlexRow spacing="12">
+                        <Button color="green" size="30" caption="Some Action" onClick={ () => null }/>
+                        <Button fill="white" size="30" color="gray50" caption="Cancel" onClick={ () => props.onClose() }/>
+                    </FlexRow>
+                </FlexCell>
+            </DropdownContainer>
         );
     };
 
