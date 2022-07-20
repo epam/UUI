53e3149b64f7b2ffecd729a3d59d05f201906181
Aliaksei_Manetau <alekseyManetov@gmail.com>
2022-07-20 17:57:17 +0200

    change add icon in filter toolbar

--- epam-promo/components/filters/FiltersToolbar.tsx
+++ epam-promo/components/filters/FiltersToolbar.tsx
@@ -4,7 +4,7 @@ import { Button, PickerInput, PickerItem, DataPickerRow } from "../../index";
 import { DataRowOptions, TableFiltersConfig, FiltersConfig, getOrderBetween, DataTableState, useArrayDataSource } from "@epam/uui-core";
 import { PickerTogglerProps, FlexCell } from "@epam/uui-components";
 import { FiltersToolbarItem } from "./FiltersToolbarItem";
-import { ReactComponent as addIcon } from '@epam/assets/icons/common/action-add-18.svg';
+import { ReactComponent as addIcon } from '@epam/assets/icons/common/content-plus_bold-18.svg';
 
 interface FiltersToolbarProps<TFilter> {
     filters: TableFiltersConfig<TFilter>[];
