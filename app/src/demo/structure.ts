9c7aef9148e12f931c8ed0e9bf59160f1a0504c4
IhorKorenets <vik753@gmail.com>
2022-07-21 15:39:45 +0300

    demo Filtered Table v1

--- app/src/demo/structure.ts
+++ app/src/demo/structure.ts
@@ -1,7 +1,8 @@
 import { DemoForm } from './form';
 import { DemoDnd } from './dnd';
 import { TimelineDemo } from './timeline/TimelineDemo';
-import { DemoTable } from "./table";
+import { MasterDetailedTable } from "./tables/masterDetailedTable";
+import { FilteredTable } from "./tables/filteredTable";
 import { RichTextEditorDemo } from "./RTE/RichTextEditorDemo";
 
 export interface DemoItem {
@@ -15,7 +16,8 @@ export interface DemoItem {
 export const demoItems: DemoItem[] = [
     { id: 'dnd', name: 'Drag & Drop', component: DemoDnd, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/dnd', previewImage: '/static/images/DemoDnD.png' },
     { id: 'form', name: 'Form', component: DemoForm, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/form', previewImage: '/static/images/DemoForms.png' },
-    { id: 'table', name: 'Table', component: DemoTable, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/table', previewImage: '/static/images/FilteredTable.png' },
+    { id: 'table', name: 'Table', component: MasterDetailedTable, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/tables/masterDetailedTable', previewImage: '/static/images/DemoTable.png' },
+    { id: 'filteredTable', name: 'Filtered Table', component: FilteredTable, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/tables/FilteredTable', previewImage: '/static/images/DemoTable.png' },
     { id: 'RTE', name: 'Rich Text Editor', component: RichTextEditorDemo, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/RTE', previewImage: '/static/images/DemoRTE.png' },
     { id: 'timeline', name: 'Timeline', component: TimelineDemo, source: 'https://github.com/epam/UUI/tree/main/app/src/demo/timeline', previewImage: '/static/images/DemoTimeline.png' },
 ];
