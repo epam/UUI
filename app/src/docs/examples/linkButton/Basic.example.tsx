b5a055f1dd6f4cdc3a40b70124561606a0300c04
Igor Zakharov <igor_zakharov3@epam.com>
2022-07-26 16:10:38 +0600

    [LinkButton]: updated basic example

--- app/src/docs/examples/linkButton/Basic.example.tsx
+++ app/src/docs/examples/linkButton/Basic.example.tsx
@@ -4,12 +4,12 @@ import { ReactComponent as MyIcon } from '@epam/assets/icons/common/action-eye-1
 
 export default function BasicLinkButtonExample() {
     return (
-      <div>
-          <LinkButton caption='Link text' link={ { pathname: '/' } } size='48'  />
-          <LinkButton caption='Link text' link={ { pathname: '/' } } size='42' icon={ MyIcon } iconPosition='right' />
-          <LinkButton caption='Link text' link={ { pathname: '/' } } size='36' icon={ MyIcon } />
-          <LinkButton caption='Link text' size='30' onClick={ () => {/*redirect implementation*/} } />
-          <LinkButton caption='Link text' size='24' href='https://www.epam.com/' />
-      </div>
+        <div>
+            <LinkButton caption='Link text' link={ { pathname: '/' } } size='48' onClick={ () => null }/>
+            <LinkButton caption='Link text' link={ { pathname: '/' } } size='42' icon={ MyIcon } iconPosition='right' onClick={ () => null }/>
+            <LinkButton caption='Link text' link={ { pathname: '/' } } size='36' icon={ MyIcon } onClick={ () => null }/>
+            <LinkButton caption='Link text' size='30' onClick={ () => null }/>
+            <LinkButton caption='Link text' size='24' href='https://www.epam.com/' onClick={ () => null }/>
+        </div>
     );
 }
