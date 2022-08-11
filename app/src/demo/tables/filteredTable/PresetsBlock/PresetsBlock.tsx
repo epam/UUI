4c54d1326554b5624cc7f26a59ad5311cc4f3d48
IhorKorenets <vik753@gmail.com>
2022-08-11 12:08:57 +0300

    added copyUrlToClipboard method and remade SaveInCurrent method

--- app/src/demo/tables/filteredTable/PresetsBlock/PresetsBlock.tsx
+++ app/src/demo/tables/filteredTable/PresetsBlock/PresetsBlock.tsx
@@ -2,7 +2,7 @@ import React, { useCallback, useState } from "react";
 import cx from "classnames";
 import { ControlGroup, Dropdown, DropdownMenuButton, FlexRow, Panel, Text, Button, TextInput, FlexCell } from "@epam/promo";
 import css from "./PresetsBlock.scss";
-import { IPresetsApi, ITablePreset } from "@epam/uui-core";
+import { DataTableState, IPresetsApi, ITablePreset } from "@epam/uui-core";
 import { TabButton } from "@epam/uui";
 import { ReactComponent as PlusIcon } from "@epam/assets/icons/common/action-add-12.svg";
 import { ReactComponent as menuIcon } from '@epam/assets/icons/common/navigation-more_vert-12.svg';
@@ -25,9 +25,14 @@ interface IPresetsBlockProps {
     duplicatePreset: (preset: ITablePreset) => void;
     deletePreset: (preset: ITablePreset) => void;
     updatePreset: (preset: ITablePreset) => void;
+    tableState: DataTableState;
 }
 
-export const PresetsBlock: React.FC<IPresetsBlockProps> = ({ presets, createNewPreset, isDefaultPresetActive, resetToDefault, activePresetId, hasPresetChanged, choosePreset, duplicatePreset, updatePreset, deletePreset }) => {
+export const PresetsBlock: React.FC<IPresetsBlockProps> = (props) => {
+    const {
+        presets, createNewPreset, isDefaultPresetActive, resetToDefault, activePresetId,
+        hasPresetChanged, choosePreset, duplicatePreset, updatePreset, deletePreset, tableState,
+    } = props;
     const [isAddingPreset, setIsAddingPreset] = useState(false);
     const [renamedPreset, setRenamedPreset] = useState<ITablePreset | null>(null);
     const [renamedPresetCaption, setRenamedPresetCaption] = useState('');
@@ -117,17 +122,25 @@ export const PresetsBlock: React.FC<IPresetsBlockProps> = ({ presets, createNewP
     };
 
     const saveInCurrent = (preset: ITablePreset) => {
-        createNewPreset(preset.name);
-        deletePreset(preset);
+        const newPreset = {
+            ...preset,
+            filter: tableState.filter,
+            columnsConfig: tableState.columnsConfig,
+        };
+        updatePreset(newPreset);
+    };
+
+    const copyUrlToClipboard = async () => {
+        await navigator.clipboard.writeText(location.href);
     };
 
     const renderPresetBody = (preset: ITablePreset) => {
         return (
             <Panel background="white" shadow={ true }>
-                <FlexRow key={ `${ preset.id }-save-in-current` } >
+                <FlexRow key={ `${ preset.id }-save-in-current` }>
                     <DropdownMenuButton isDisabled={ !(isActivePreset?.id === preset.id && hasPresetChanged(preset)) } icon={ SaveInCurrentIcon } caption="Save in current" onClick={ () => saveInCurrent(preset) }/>
                 </FlexRow>
-                <FlexRow key={ `${ preset.id }-save-as-new` } >
+                <FlexRow key={ `${ preset.id }-save-as-new` }>
                     <DropdownMenuButton isDisabled={ !(isActivePreset?.id === preset.id && hasPresetChanged(preset)) } icon={ SaveAsNewIcon } caption="Save as new" onClick={ addPreset }/>
                 </FlexRow>
                 <FlexRow key={ `${ preset.id }-discard` } borderBottom="gray40">
@@ -140,7 +153,7 @@ export const PresetsBlock: React.FC<IPresetsBlockProps> = ({ presets, createNewP
                     <DropdownMenuButton icon={ RenameIcon } caption="Rename" onClick={ () => renamePreset(preset) }/>
                 </FlexRow>
                 <FlexRow borderBottom="gray40" key={ `${ preset.id }-copyLink` }>
-                    <DropdownMenuButton icon={ CopyLinkIcon } caption="Copy Link" onClick={ () => {} }/>
+                    <DropdownMenuButton icon={ CopyLinkIcon } caption="Copy Link" onClick={ copyUrlToClipboard }/>
                 </FlexRow>
                 <FlexRow key={ `${ preset.id }-delete` } cx={ css.deleteRow }>
                     <DropdownMenuButton icon={ DeleteIcon } caption="Delete" cx={ css.deleteButton } onClick={ () => deletePresetHandler(preset) }/>
