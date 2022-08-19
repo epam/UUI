d968779abae3fc4c1c425a0032a34e46fe747d79
IhorKorenets <vik753@gmail.com>
2022-08-19 12:32:31 +0300

    refactoring Presets

--- epam-promo/components/filters/PresetPanel/PresetPanel.tsx
+++ epam-promo/components/filters/PresetPanel/PresetPanel.tsx
@@ -4,7 +4,7 @@ import css from "./PresetPanel.scss";
 import { ControlGroup, FlexRow, Text, TabButton } from "../../index";
 import { DataTableState, IPresetsApi } from "@epam/uui-core";
 import { Preset } from "./presets/Preset";
-import { PresetInput } from "./presets/PresetInput";
+import { InputActionType, PresetInput } from "./presets/PresetInput";
 import { ReactComponent as PlusIcon } from "@epam/assets/icons/common/action-add-12.svg";
 
 export interface IPresetsBlockProps extends IPresetsApi {
@@ -13,47 +13,25 @@ export interface IPresetsBlockProps extends IPresetsApi {
 
 export const PresetPanel: React.FC<IPresetsBlockProps> = (props) => {
     const [isAddingPreset, setIsAddingPreset] = useState(false);
-    const [newPresetCaption, setNewPresetCaption] = useState('');
     const isActivePreset = props.presets.find(p => p.id === props.activePresetId);
-
-    const saveNewPreset = useCallback(() => {
-        if (!newPresetCaption) {
-            setIsAddingPreset(false);
-            return;
-        }
-
-        props.createNewPreset(newPresetCaption);
-        setIsAddingPreset(false);
-        setNewPresetCaption("");
-    }, [newPresetCaption, props.createNewPreset]);
 
     const setDefaultPreset = useCallback(() => {
         if (!props.isDefaultPresetActive) {
             props.resetToDefault();
         }
-    }, [props.isDefaultPresetActive]);
+    }, [props]);
 
-    const addPreset = useCallback(() => {
+    const setAddingPreset = useCallback(() => {
         setIsAddingPreset(true);
     }, [setIsAddingPreset]);
 
-    const cancelNewPreset = useCallback(() => {
-        setNewPresetCaption('');
+    const cancelAddingPreset = useCallback(() => {
         setIsAddingPreset(false);
-    }, [setNewPresetCaption, setIsAddingPreset]);
-
-    const newPresetOnBlurHandler = useCallback(() => {
-        if (newPresetCaption.length) {
-            return;
-        }
-        setIsAddingPreset(false);
-    }, [newPresetCaption.length]);
+    }, [setIsAddingPreset]);
 
     const presetApi = {
-        setRenamedPresetCaption: setNewPresetCaption,
-        renamedPresetCaption: newPresetCaption,
         isActivePreset,
-        addPreset,
+        addPreset: setAddingPreset,
         choosePreset: props.choosePreset,
         hasPresetChanged: props.hasPresetChanged,
         duplicatePreset: props.duplicatePreset,
@@ -61,8 +39,6 @@ export const PresetPanel: React.FC<IPresetsBlockProps> = (props) => {
         tableState: props.tableState,
         resetToDefault: props.resetToDefault,
         deletePreset: props.deletePreset,
-        newPresetCaption,
-        setNewPresetCaption,
     };
 
     return (
@@ -72,7 +48,7 @@ export const PresetPanel: React.FC<IPresetsBlockProps> = (props) => {
                 <ControlGroup
                     key="default-preset"
                     cx={ cx(css.defaultPresetButton, {
-                        [css.presetButtonWrapper]: !isActivePreset?.id,
+                        [css.defaultPresetActive]: !isActivePreset?.id,
                     }) }>
                     <TabButton
                         caption={ 'Default Preset' }
@@ -87,18 +63,16 @@ export const PresetPanel: React.FC<IPresetsBlockProps> = (props) => {
                     ? <ControlGroup key="add-preset">
                         <TabButton
                             caption={ 'Add Preset' }
-                            onClick={ addPreset }
+                            onClick={ setAddingPreset }
                             size="36"
                             icon={ PlusIcon }
                             iconPosition="left"
                         />
                     </ControlGroup>
                     : <PresetInput
-                        value={ newPresetCaption }
-                        onValueChange={ setNewPresetCaption }
-                        onAccept={ saveNewPreset }
-                        onCancel={ cancelNewPreset }
-                        onBlur={ newPresetOnBlurHandler }
+                        actionType={ InputActionType.SAVE_NEW }
+                        onCancel={ cancelAddingPreset }
+                        createNewAction={ props.createNewPreset }
                     /> }
             </FlexRow>
         </>
--- epam-promo/components/filters/PresetPanel/presets/TabButtonDropdown.tsx
+++ epam-promo/components/filters/PresetPanel/presets/TabButtonDropdown.tsx
@@ -1,9 +1,10 @@
 import React, { useCallback } from "react";
 import css from "../PresetPanel.scss";
-import { IDropdownToggler, ITablePreset } from "@epam/uui-core";
-import { Dropdown } from "../../../overlays";
+import { ColumnsConfig, IDropdownToggler, ITablePreset, useUuiContext } from "@epam/uui-core";
+import { Dropdown, SuccessNotification } from "../../../overlays";
 import { PresetDropdownBody } from "./PresetDropdownBody";
 import { Button } from "../../../buttons";
+import { Text } from "../../../typography";
 import { ReactComponent as menuIcon } from "@epam/assets/icons/common/navigation-more_vert-18.svg";
 
 interface ITubButtonDropdownProps {
@@ -13,14 +14,61 @@ interface ITubButtonDropdownProps {
     choosePreset: (preset: ITablePreset) => void;
     duplicatePreset: (preset: ITablePreset) => void;
     renamePreset: (preset: ITablePreset) => void;
-    copyUrlToClipboard: () => void;
     addPreset: () => void;
-    saveInCurrent: (preset: ITablePreset) => void;
     resetToDefault: () => void;
     deletePreset: (preset: ITablePreset) => void;
+    tableStateFilter: any;
+    tableStateColumnConfig: ColumnsConfig;
+    updatePreset: (preset: ITablePreset) => void;
 }
 
 export const TabButtonDropdown = (props: ITubButtonDropdownProps) => {
+    const { uuiNotifications } = useUuiContext();
+
+    const copyUrlToClipboard = async () => {
+        await navigator.clipboard.writeText(location.href);
+        successNotificationHandler('Link copied!');
+    };
+
+    const saveInCurrent = useCallback((preset: ITablePreset) => {
+        const newPreset = {
+            ...preset,
+            filter: props.tableStateFilter,
+            columnsConfig: props.tableStateColumnConfig,
+        };
+        props.updatePreset(newPreset);
+        successNotificationHandler('Changes saved!');
+    }, [props.tableStateFilter, props.tableStateColumnConfig]);
+
+    const deletePresetHandler = useCallback((preset: ITablePreset) => {
+        if (props.isActivePreset && props.isActivePreset.id === preset.id) {
+            props.resetToDefault();
+        }
+        props.deletePreset(preset);
+    }, [props.isActivePreset, props.deletePreset, props.resetToDefault]);
+
+    const successNotificationHandler = useCallback((text: string) => {
+        uuiNotifications.show((props) => (
+            <SuccessNotification { ...props } >
+                <Text size="36" font="sans" fontSize="14">{ text }</Text>
+            </SuccessNotification>
+        ), { position: 'top-right', duration: 3 }).catch(() => null);
+    }, [props]);
+
+    const presetDropdownBodyApi = {
+        preset: props.preset,
+        isActivePreset: props.isActivePreset,
+        hasPresetChanged: props.hasPresetChanged,
+        choosePreset: props.choosePreset,
+        duplicatePreset: props.duplicatePreset,
+        renamePreset: props.renamePreset,
+        copyUrlToClipboard: copyUrlToClipboard,
+        deletePresetHandler: deletePresetHandler,
+        addPreset: props.addPreset,
+        saveInCurrent: saveInCurrent,
+    };
+
+    const renderBody = useCallback(() => <PresetDropdownBody { ...presetDropdownBodyApi } />, [props]);
 
     const renderTarget = useCallback((props: IDropdownToggler) => {
         return (
@@ -33,28 +81,6 @@ export const TabButtonDropdown = (props: ITubButtonDropdownProps) => {
             />
         );
     }, [props]);
-
-    const deletePresetHandler = useCallback((preset: ITablePreset) => {
-        if (props.isActivePreset && props.isActivePreset.id === preset.id) {
-            props.resetToDefault();
-        }
-        props.deletePreset(preset);
-    }, [props.isActivePreset, props.deletePreset, props.resetToDefault]);
-
-    const renderBody = useCallback(() => (
-            <PresetDropdownBody
-                preset={ props.preset }
-                isActivePreset={ props.isActivePreset }
-                hasPresetChanged={ props.hasPresetChanged }
-                choosePreset={ props.choosePreset }
-                duplicatePreset={ props.duplicatePreset }
-                renamePreset={ props.renamePreset }
-                copyUrlToClipboard={ props.copyUrlToClipboard }
-                deletePresetHandler={ deletePresetHandler }
-                addPreset={ props.addPreset }
-                saveInCurrent={ props.saveInCurrent }
-            />
-        ), [props]);
 
     return (
         <>
