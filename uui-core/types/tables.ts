6eda48e32cf33484d3e7df316fe48d4b030c5507
Stanislau Matsiyeuski <Stanislau_Matsiyeuski@epam.com>
2022-09-13 16:26:51 +0300

    [FilterToolbar]: fixe bugs, refactoring, extend filter configuration

--- uui-core/types/tables.ts
+++ uui-core/types/tables.ts
@@ -308,9 +308,9 @@ export type IFilterPredicate = {
 type FilterConfigBase<TFilter> = {
     title: string;
     field: keyof TFilter;
-    columnKey?: string;
+    columnKey: string;
     isAlwaysVisible?: boolean;
-    predicates?: IFilterPredicate[]
+    predicates?: IFilterPredicate[];
 };
 
 type PickerFilterConfig<TFilter> = FilterConfigBase<TFilter> & {
@@ -318,10 +318,12 @@ type PickerFilterConfig<TFilter> = FilterConfigBase<TFilter> & {
     dataSource: IDataSource<any, any, any>;
     getName?: (item: any) => string;
     renderRow?: (props: DataRowProps<any, any>) => ReactNode;
+    valueType?: "id";
 };
 
 type DatePickerFilterConfig<TFilter> = FilterConfigBase<TFilter> & {
     type: "datePicker" | "rangeDatePicker";
+    format?: string;
 };
 
 export type TableFiltersConfig<TFilter> = PickerFilterConfig<TFilter>
