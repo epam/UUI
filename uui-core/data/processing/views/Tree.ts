e57258241e1f18bd279dc9ae50e74784b04d8243
Yakov Zhmurov <zhmurov@ya.ru>
2022-09-16 02:54:05 +0300

    DataSources/Tree - simplified internal data structures and API

--- uui-core/data/processing/views/Tree.ts
+++ uui-core/data/processing/views/Tree.ts
@@ -1,29 +1,28 @@
-export interface TreeNode<TItem, TId> {
-    id: TId;
-    key: string;
-    item: TItem;
-    parentId: TId;
-    index: number;
-}
-
 export interface TreeParams<TItem, TId> {
     getId?(item: TItem): TId;
     getParentId?(item: TItem): TId | undefined;
 }
 
 export class Tree<TItem, TId> {
+    private getId: (item: TItem) => TId;
+    private getParentId: (item: TItem) => TId;
+
     private constructor(
         private params: TreeParams<TItem, TId>,
-        private readonly byId: Map<TId, TreeNode<TItem, TId>>,
-        private readonly byParentId: Map<TId, TreeNode<TItem, TId>[]>,
+        private readonly byId: Map<TId, TItem>,
+        private readonly byParentId: Map<TId, TId[]>,
     ) {
+        this.getId = params.getId;
+        this.getParentId = params.getParentId
+            ? ((item: TItem) => (params.getParentId(item) ?? undefined))
+            : () => undefined;
     }
 
     public static blank<TItem, TId>(params: TreeParams<TItem, TId>) {
         return new Tree<TItem, TId>(
             params,
-            new Map<TId, TreeNode<TItem, TId>>(),
-            new Map<TId, TreeNode<TItem, TId>[]>(),
+            new Map<TId, TItem>(),
+            new Map<TId, TId[]>([[undefined, []]]), // add empty children list for root to avoid corner-cases
         );
     }
 
@@ -39,79 +38,87 @@ export class Tree<TItem, TId> {
         }
     }
 
-    public getRootNodes() {
-        // For historical reasons, both null and undefined can serve as root ID. We use undefined internally.
-        // This opens a way to make null a legal ID, which is usable in certain cases.
-        return this.byParentId.get(undefined) || [];
+    public getRootIds() {
+        return this.byParentId.get(undefined);
+    }
+
+    public getRootItems() {
+        return this.byParentId.get(undefined).map(id => this.byId.get(id)!);
     }
 
     public getById(id: TId) {
-        const node = this.byId.get(id);
-        if (node) {
-            return node.item;
-        } else {
-            return undefined;
-        }
-    }
-
-    public getNodeById(id: TId) {
         return this.byId.get(id);
     }
 
-    public getNodesByParentId(parentId: TId) {
-        return this.byParentId.get(parentId ?? undefined) || [];
+    public getChildren(item: TItem) {
+        const id = this.getId(item);
+        return this.getChildrenByParentId(id);
     }
 
-    public getItemsByParentId(parentId: TId) {
-        return this.getNodesByParentId(parentId).map(i => i.item);
+    public getChildrenByParentId(parentId: TId) {
+        const ids = this.getChildrenIdsByParentId(parentId);
+        const children = ids.map(id => this.byId.get(id));
+        return children;
+    }
+
+    public getChildrenIdsByParentId(parentId: TId) {
+        return this.byParentId.get(parentId) || [];
     }
 
     public getTotalRecursiveCount() {
         return this.byId.size;
     }
 
-    public getParentNodesRecursive(id: TId) {
-        const parents: TreeNode<TItem, TId>[] = [];
-        let node = this.getNodeById(id);
+    public getParentIdsRecursive(id: TId) {
+        const parentIds: TId[] = [];
         while (true) {
-            node = this.getNodeById(node.parentId);
-            if (!node) {
+            let item = this.byId.get(id);
+            if (!item) {
                 break;
             }
-            parents.unshift(node);
+            id = this.getParentId(item)
+            if (!id) {
+                break;
+            }
+            parentIds.unshift(id);
         };
-        return parents;
-    }
-
-    /** Returns all nodes which has children. */
-    public getAllParentNodes(): TreeNode<TItem, TId>[] {
-        const parentIds = Array.from(this.byParentId.keys());
-        const parents = parentIds.map(id => this.byId.get(id)).filter(i => i !== undefined);
-        return parents;
+        return parentIds;
     }
 
     public forEach(
-        action: (node: TreeNode<TItem, TId>) => void,
+        action: (item: TItem, id: TId, parentId: TId) => void,
         options?: {
-            parentId?: TId,
             direction?: 'bottom-up' | 'top-down',
-            includeRoot?: boolean,
+            parentId?: TId,
+            includeParent?: boolean,
         }
     ) {
-        options = { direction: 'top-down', includeRoot: false, ...options };
-        const walkChildrenRec = (node: TreeNode<TItem, TId>) => {
-            if (options.direction === 'top-down' && node) {
-                action(node);
+        options = { direction: 'top-down', includeParent: true, parentId: undefined, ...options };
+
+        const iterateNodes = (ids: TId[]) => {
+            ids.forEach(id => {
+                const item = this.byId.get(id);
+                const parentId = item ? this.getParentId(item) : undefined;
+                walkChildrenRec(item, id, parentId);
+            });
+        }
+
+        const walkChildrenRec = (item: TItem, id: TId, parentId: TId) => {
+            if (options.direction === 'top-down') {
+                action(item, id, parentId);
             }
-            const children = this.getNodesByParentId(node ? node.id : undefined);
-            children && children.forEach(walkChildrenRec);
-            if (options.direction === 'bottom-up' && node) {
-                action(node);
+            const childrenIds = this.byParentId.get(id);
+            childrenIds && iterateNodes(childrenIds);
+            if (options.direction === 'bottom-up') {
+                action(item, id, parentId);
             }
         };
 
-        const node = this.byId.get(options.parentId);
-        walkChildrenRec(node);
+        if (options.parentId != null && options.includeParent) {
+            iterateNodes([options.parentId]);
+        } else {
+            iterateNodes(this.byParentId.get(options.parentId));
+        }
     }
 
     public computeSubtotals<TSubtotals>(
@@ -120,25 +127,25 @@ export class Tree<TItem, TId> {
     ) {
         const subtotalsMap = new Map<TId | undefined, TSubtotals>();
 
-        this.forEach(node => {
-            let itemSubtotals = get(node.item, this.byParentId.has(node.id));
+        this.forEach((item, id, parentId) => {
+            let itemSubtotals = get(item, this.byParentId.has(id));
 
             // add already computed children subtotals
-            if (subtotalsMap.has(node.id)) {
-                itemSubtotals = add(itemSubtotals, subtotalsMap.get(node.id));
+            if (subtotalsMap.has(id)) {
+                itemSubtotals = add(itemSubtotals, subtotalsMap.get(id));
             }
 
             // store
-            subtotalsMap.set(node.id, itemSubtotals);
+            subtotalsMap.set(id, itemSubtotals);
 
             // add value to parent
             let parentSubtotals: TSubtotals;
-            if (!subtotalsMap.has(node.parentId)) {
+            if (!subtotalsMap.has(parentId)) {
                 parentSubtotals = itemSubtotals;
             } else {
-                parentSubtotals = add(itemSubtotals, subtotalsMap.get(node.parentId));
+                parentSubtotals = add(itemSubtotals, subtotalsMap.get(parentId));
             }
-            subtotalsMap.set(node.parentId, parentSubtotals);
+            subtotalsMap.set(parentId, parentSubtotals);
         }, { direction: 'bottom-up' })
         return subtotalsMap;
     }
@@ -156,24 +163,16 @@ export class Tree<TItem, TId> {
         const newByParentId = new Map(this.byParentId); // shallow clone, still need to copy arrays inside!
 
         itemsToAdd.forEach((item) => {
-            const id = this.params.getId(item);
-            const existingNode = this.getNodeById(id);
-            if (!existingNode || existingNode.item !== item) {
-                const id = this.params.getId(item);
-                const node: TreeNode<TItem, TId> = {
-                    id,
-                    key: JSON.stringify(id),
-                    index: 0, // set later, when inserting to the parent
-                    parentId: this.params.getParentId && this.params.getParentId(item),
-                    item: item,
-                }
-                newById.set(id, node);
+            const id = this.getId(item);
+            const existingItem = this.byId.get(id);
+            if (!existingItem || existingItem !== item) {
+                const id = this.getId(item);
+                newById.set(id, item);
 
-                const parentId = this.params.getParentId ? (this.params.getParentId(item) ?? undefined) : undefined;
+                const parentId = this.getParentId(item);
+                const existingItemParentId = existingItem ? this.getParentId(existingItem) : undefined;
 
-                if (!existingNode || existingNode.parentId != node.parentId) {
-                    node.parentId = parentId;
-
+                if (!existingItem || parentId != existingItemParentId) {
                     let list = newByParentId.get(parentId);
                     if (!list) {
                         list = [];
@@ -182,8 +181,7 @@ export class Tree<TItem, TId> {
                         list = [...list];
                         newByParentId.set(parentId, list);
                     }
-                    list.push(node);
-                    node.index = list.length - 1;
+                    list.push(id);
 
                     // TBD: remove item from existing list (if we'll use this method to mutate existing list)
                 }
--- uui-core/data/processing/views/__tests__/Tree.test.ts
+++ uui-core/data/processing/views/__tests__/Tree.test.ts
@@ -28,42 +28,38 @@ const testTree = blankTree.append(testData);
 
 describe('Tree', () => {
     describe('basic queries', () => {
-        it('can get items by id', () => {
+        it('getById', () => {
             expect(testTree.getById(100)).toBe(testData[0]);
         })
 
-        it('can get children by parent Id', () => {
-            const nodes = testTree.getNodesByParentId(100);
-            expect(nodes).toEqual([
-                { id: 110, key: '110', parentId: 100, index: 0, item: testTree.getById(110) },
-                { id: 120, key: '120', parentId: 100, index: 1, item: testTree.getById(120) },
-            ]);
+        it('getChildrenByParentId', () => {
+            const nodes = testTree.getChildrenByParentId(100);
+            expect(nodes).toEqual([testTree.getById(110), testTree.getById(120)]);
         })
 
-        it('can retrieve all parent nodes', () => {
-            const parents = testTree.getAllParentNodes();
-            const parentIds = parents.map(node => node.id);
-            expect(parentIds).toContain(100);
-            expect(parentIds).toContain(300);
-            expect(parentIds).toContain(120);
+        it('getRootItems', () => {
+            const parents = testTree.getRootItems();
+            const parentIds = parents.map(item => item.id);
+            expect(parentIds).toEqual([100, 200, 300]);
+        })
+
+        it('getParentIdsRecursive', () => {
+            const parentIds = testTree.getParentIdsRecursive(122);
+            expect(parentIds).toEqual([100, 120]);
         })
     });
 
     describe('append', () => {
         it('can append new root item by id', () => {
             const newTree = testTree.append([{ id: 400 }]);
-            expect(newTree.getNodeById(400)).toEqual(
-                { id: 400, key: '400', parentId: undefined, index: 3, item: { id: 400 }}
-            );
-            expect(newTree.getRootNodes().map(n => n.id)).toEqual([100, 200, 300, 400]);
+            expect(newTree.getById(400)).toEqual({ id: 400 });
+            expect(newTree.getRootItems().map(n => n.id)).toEqual([100, 200, 300, 400]);
         })
 
         it('can append new child item by id', () => {
             const newTree = testTree.append([{ id: 130, parentId: 100 }]);
-            expect(newTree.getNodeById(130)).toEqual(
-                { id: 130, key: '130', parentId: 100, index: 2, item: { id: 130, parentId: 100 }}
-            );
-            expect(newTree.getNodesByParentId(100).map(n => n.id)).toEqual([110, 120, 130]);
+            expect(newTree.getById(130)).toEqual({ id: 130, parentId: 100 });
+            expect(newTree.getChildrenByParentId(100).map(n => n.id)).toEqual([110, 120, 130]);
         })
 
         it('can move node to a new parent', () => {
@@ -76,35 +72,53 @@ describe('Tree', () => {
         })
     });
 
-    describe('forEachChildrenRecursively', () => {
+    describe('forEach', () => {
+        const testForeach = (tree: (typeof blankTree), options: Parameters<typeof blankTree.forEach>[1], resultIds: number[]) => {
+            const visited: { item: TestItem, id: number, parentId: number }[] = [];
+            tree.forEach((item, id, parentId) => { visited.push({ item, id, parentId }) }, options);
+            const reference = resultIds
+                .map(id => tree.getById(id))
+                .map(item => ({ id: item?.id, item, parentId: item?.parentId}))
+            expect(visited).toEqual(reference);
+        }
+
         it('can iterate empty tree', () => {
-            const visited: number[] = [];
-            blankTree.forEach(node => { visited.push(node.id) });
-            expect(visited).toEqual([]);
+            testForeach(blankTree, {}, []);
         })
 
         it('can iterate top-down a node', () => {
-            const visited: number[] = [];
-            testTree.forEach(node => { visited.push(node.id) }, { parentId: 100 } );
-            expect(visited).toEqual([100, 110, 120, 121, 122]);
+            testForeach(testTree, { parentId: 100 }, [100, 110, 120, 121, 122]);
+        })
+
+        it('can iterate top-down a node (exclude parent)', () => {
+            testForeach(testTree, { parentId: 100, includeParent: false }, [110, 120, 121, 122]);
         })
 
         it('can iterate bottom-up a node', () => {
-            const visited: number[] = [];
-            testTree.forEach(node => { visited.push(node.id) }, { parentId: 100, direction: 'bottom-up' });
-            expect(visited).toEqual([110, 121, 122, 120, 100]);
+            testForeach(testTree, { parentId: 100, direction: 'bottom-up' }, [110, 121, 122, 120, 100]);
+        })
+
+        it('can iterate bottom-up a node (exclude parent)', () => {
+            testForeach(
+                testTree,
+                { parentId: 100, direction: 'bottom-up', includeParent: false },
+                [110, 121, 122, 120]);
         })
 
         it('can iterate top-down whole tree', () => {
-            const visited: number[] = [];
-            testTree.forEach(node => { visited.push(node.id) });
-            expect(visited).toEqual([100, 110, 120, 121, 122, 200, 300, 310, 320, 330]);
+            testForeach(
+                testTree,
+                null,
+                [100, 110, 120, 121, 122, 200, 300, 310, 320, 330],
+            );
         })
 
         it('can iterate bottom-up whole tree', () => {
-            const visited: number[] = [];
-            testTree.forEach(node => { visited.push(node.id) }, { direction: 'bottom-up' });
-            expect(visited).toEqual([110, 121, 122, 120, 100, 200, 310, 320, 330, 300]);
+            testForeach(
+                testTree,
+                { direction: 'bottom-up' },
+                [110, 121, 122, 120, 100, 200, 310, 320, 330, 300],
+            );
         })
     })
 
