accfb8146792d5090dd2dc899bbfe2c6bea6318f
Aleksey Manetov <alekseymanetov@gmail.com>
2022-09-30 12:44:23 +0200

    [Accordion]: Make title prop is not required. Rework doc example

--- app/src/docs/examples/accordion/Custom.example.tsx
+++ app/src/docs/examples/accordion/Custom.example.tsx
@@ -1,27 +1,23 @@
 import React from 'react';
-import { Accordion, FlexCell, FlexRow, Text, Avatar, Badge } from '@epam/promo';
-import css from './CustomExample.scss';
+import { Accordion, FlexCell, FlexRow, Text, Avatar, Badge, FlexSpacer } from '@epam/promo';
 import {demoData} from "@epam/uui-docs";
 
 const renderTitle = () => (
-    <FlexRow spacing='12' padding="6" cx={ css.titleWrap }>
-        <FlexCell grow={ 3 }>
-            <FlexRow>
-                <Avatar alt='avatar' img='https://avatars.dicebear.com/api/human/avatar12.svg?background=%23EBEDF5&radius=50' size='30' cx={ css.userAvatar } />
+        <FlexCell grow={ 1 }>
+            <FlexRow spacing='12' padding="6">
+                <Avatar alt='avatar' img='https://avatars.dicebear.com/api/human/avatar12.svg?background=%23EBEDF5&radius=50' size='30' />
                 <Text fontSize='16' font='sans-semibold'>John Doe</Text>
+                <FlexSpacer />
+                <Badge color='green' fill='transparent' caption='Employee' />
             </FlexRow>
         </FlexCell>
-        <FlexCell width='auto' alignSelf="flex-end">
-            <Badge color='green' fill='transparent' caption='Employee' />
-        </FlexCell>
-    </FlexRow>
 )
 
 export default function BasicAccordionExample() {
     return (
-        <FlexCell width='100%' cx={ css.container }>
-            <Accordion title='Accordion block mode' renderTitle={renderTitle} mode='block'>
-                {demoData.loremIpsum}
+        <FlexCell grow={ 1 }>
+            <Accordion renderTitle={ renderTitle } mode='block'>
+                { demoData.loremIpsum }
             </Accordion>
         </FlexCell>
     );
