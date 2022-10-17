f9ed82c77e409192e97b97176de8f179f8dcc62a
Siarhei Yelin <Siarhei_Yelin@epam.com>
2022-10-17 14:18:29 +0300

    code review

--- app/src/demo/demoItemCard/DemoItemCard.tsx
+++ app/src/demo/demoItemCard/DemoItemCard.tsx
@@ -2,11 +2,6 @@ import { Anchor, Text } from "@epam/promo";
 import * as css from "./DemoItemCard.scss";
 import * as React from "react";
 import { DemoItem } from "../structure";
-import { SlateEditor } from "@epam/uui-editor";
-import { EditableDocContent } from "../../common";
-import { useEffect } from "react";
-import { Blocker } from "@epam/loveship";
-import { loadDocContentByDemoName } from "../../common/appFooterDemo/demoToolbar/useDemoDescriptionEditor";
 
 export interface IDemoItemCard {
     demoItem: DemoItem;
@@ -16,32 +11,12 @@ export function DemoItemCard(props: IDemoItemCard) {
     const {
         onOpenItem,
         demoItem: {
-            previewImage,
-            name,
             id,
+            name,
+            previewImage,
+            shortDescription,
         },
     } = props;
-
-    const [isLoading, setIsLoading] = React.useState(false);
-    const [description, setDescription] = React.useState(null);
-
-    useEffect(() => {
-        let isOutdated = false;
-        setIsLoading(true);
-        loadDocContentByDemoName(name)
-            .then(content => {
-                if (isOutdated) {
-                    return;
-                }
-                setDescription(content);
-            })
-            .finally(() => setIsLoading(false));
-
-        return () => {
-            isOutdated = true;
-            setIsLoading(false);
-        };
-    }, [name]);
 
     return (
         <Anchor cx={ css.container } key={ id } link={ { pathname: '/demo', query: { id } } } onClick={ () => onOpenItem(name) } >
@@ -49,16 +24,7 @@ export function DemoItemCard(props: IDemoItemCard) {
             <div className={ css.navDescription }>
                 <Text cx={ css.title } font='sans-semibold' lineHeight='30' fontSize='24'>{ name }</Text>
                 <div className={ css.description }>
-                    <SlateEditor
-                        plugins={ EditableDocContent.plugins }
-                        mode='inline'
-                        isReadonly={ true }
-                        minHeight={ 10 }
-                        fontSize="14"
-                        value={ description }
-                        onValueChange={ null }
-                    />
-                    <Blocker isEnabled={ isLoading } />
+                    <Text fontSize="12">{ shortDescription }</Text>
                 </div>
             </div>
         </Anchor>
