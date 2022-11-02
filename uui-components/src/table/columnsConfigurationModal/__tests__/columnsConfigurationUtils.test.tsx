837cb5c13bc48ba400c063e15fa0a593e53c43b1
Siarhei Yelin <Siarhei_Yelin@epam.com>
2022-11-02 16:55:48 +0300

    [ColumnsConfigurationModal] test

--- uui-components/src/table/columnsConfigurationModal/__tests__/columnsConfigurationUtils.test.tsx
+++ uui-components/src/table/columnsConfigurationModal/__tests__/columnsConfigurationUtils.test.tsx
@@ -1,3 +1,4 @@
+import React from "react";
 import { GroupedDataColumnProps } from "../types";
 import { ColumnsConfig, DataColumnProps } from "@epam/uui-core";
 import { isColumnFilteredOut, sortColumnsAndAddGroupKey } from "../columnsConfigurationUtils";
@@ -46,6 +47,11 @@ describe('columnsConfigurationUtils', () => {
         });
         it('should not filter out columns which match filter criteria (ignore case)', () => {
             const A: DataColumnProps = { key: '1', fix: 'left', caption: 'name', isAlwaysVisible: false, width: 10 };
+            const result = isColumnFilteredOut(A, 'NAME');
+            expect(result).toBe(false);
+        });
+        it('should not filter out columns with React node as caption', () => {
+            const A: DataColumnProps = { key: '1', fix: 'left', caption: <div />, isAlwaysVisible: false, width: 10 };
             const result = isColumnFilteredOut(A, 'NAME');
             expect(result).toBe(false);
         });
