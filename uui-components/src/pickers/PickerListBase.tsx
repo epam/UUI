ba6589e5c691bc0bf15571a26c93a29f1276b544
Zsofia_Szatmari <zsofia_szatmari@epam.com>
2022-11-16 11:50:57 +0100

    Fix PickerList sort #811

--- uui-components/src/pickers/PickerListBase.tsx
+++ uui-components/src/pickers/PickerListBase.tsx
@@ -167,21 +167,22 @@ export abstract class PickerListBase<TItem, TId, TProps> extends PickerBase<TIte
         }
 
         const dataSourceState = this.getDataSourceState();
-
-        // TBD: What if user doesn't want to sort selection? E.g. he has manually sorted Enum, or already passed ordered ids in defaultIds
         const sorting = dataSourceState.sorting && dataSourceState.sorting[0];
-        const sortBy = this.props.sortBy || ((i: any) => sorting ? i[sorting.field] : this.getName(i));
-        const sign = sorting?.direction == 'asc' ? 1 : -1;
-        const stringComparer = (new Intl.Collator(undefined, {numeric: true, sensitivity: 'base'})).compare;
-        const comparer = (a: DataRowProps<TItem, TId>, b: DataRowProps<TItem, TId>) => {
-            const loadingComparison = (b.isLoading ? 0 : 1) - (a.isLoading ? 0 : 1);
-            if (loadingComparison != 0 || (a.isLoading && b.isLoading)) {
-                return loadingComparison;
-            } else {
-                return sign * stringComparer(sortBy(a.value, sorting), sortBy(b.value, sorting));
-            }
-        };
-        result.sort(comparer);
+
+        if (sorting) {
+            const sortBy = this.props.sortBy || ((i: any) => i[sorting.field]);
+            const sign = sorting.direction == 'asc' ? 1 : -1;
+            const stringComparer = (new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' })).compare;
+            const comparer = (a: DataRowProps<TItem, TId>, b: DataRowProps<TItem, TId>) => {
+                const loadingComparison = (b.isLoading ? 0 : 1) - (a.isLoading ? 0 : 1);
+                if (loadingComparison != 0 || (a.isLoading && b.isLoading)) {
+                    return loadingComparison;
+                } else {
+                    return sign * stringComparer(sortBy(a.value, sorting), sortBy(b.value, sorting));
+                }
+            };
+            result.sort(comparer);
+        }
 
         return result;
     }
