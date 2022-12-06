d1f70c6c4a8a7a409cd549e1df0a6a2916a88610
dzevakov <dmitryzev@gmail.com>
2022-12-06 17:54:22 +0300

    [Docs]: add for tooltip overview types & variants

---
+++ app/src/docs/examples/tooltip/Variants.example.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import { Button, FlexRow, Tooltip } from '@epam/promo';
+
+export default function TypesTooltipExample() {
+    return (
+        <FlexRow spacing='12' >
+            <Tooltip content='Tooltip message' placement='bottom' >
+                <Button caption='Contrast' color='blue' onClick={ () => null } />
+            </Tooltip>
+
+            <Tooltip content='Tooltip message' placement='bottom' color='white'>
+                <Button caption='Default' fill='white' color='gray50' onClick={ () => null } />
+            </Tooltip>
+
+            <Tooltip content='Tooltip message' placement='bottom' color='red'>
+                <Button caption='Critical' fill='white' color='red' onClick={ () => null } />
+            </Tooltip>
+        </FlexRow>
+    );
+}
