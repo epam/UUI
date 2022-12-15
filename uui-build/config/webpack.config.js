1a1f7cf390bdf5e526271a55fac3bb33a75762b5
Siarhei Yelin <Siarhei_Yelin@epam.com>
2022-12-15 12:20:24 +0300

    Fix issue with build module task

--- uui-build/config/webpack.config.js
+++ uui-build/config/webpack.config.js
@@ -429,7 +429,7 @@ function getLocalIdent(
 
                 }),
                 {
-                    loader: 'sass-loader',
+                    loader: require.resolve('sass-loader'),
                 }
             ],
         },
