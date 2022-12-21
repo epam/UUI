c618c44a2d3d277c29d9e0154303e467b7cab11b
dzevakov <dmitryzev@gmail.com>
2022-12-21 12:12:14 +0300

    [Docs]: add tooltip trigger with delay example

--- app/src/docs/Tooltip.doc.tsx
+++ app/src/docs/Tooltip.doc.tsx
@@ -40,6 +40,11 @@ export class TooltipDoc extends BaseDocsBlock {
                     title='Trigger configuration'
                     path='./examples/tooltip/TriggerConfiguration.example.tsx'
                 />
+
+                <DocExample
+                    title='Trigger configuration with delay'
+                    path='./examples/tooltip/TriggerConfigurationWithDelay.example.tsx'
+                />
                 { this.renderSectionTitle('Examples') }
                 <DocExample
                     title='Tooltip with icon'
