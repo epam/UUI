bfca7236804f315c3fd316e5802f3bcd254a177f
Siarhei Yelin <Siarhei_Yelin@epam.com>
2022-12-21 14:16:05 +0300

    Migrate build of modules to Rollup.

--- app/.config/utils/packageUtils.js
+++ app/.config/utils/packageUtils.js
@@ -17,16 +17,51 @@ function readPackages({ uuiRoot }) {
  */
 function getPkgFoldersRegexMatcher({ uuiRoot, folders }) {
     const pkgs = readPackages({ uuiRoot });
-    return pkgs.reduce((acc, p) => {
-        folders.forEach(f => {
-            const e = escapeForRegex(path.join(path.sep, p, f))
-            acc.push(new RegExp(e))
-        })
-        return acc
+    return pkgs.reduce((acc, pkg) => {
+        return acc.concat(getSinglePkgFolderRegexMatcher({ pkg, folders }))
     }, [])
 }
 
+function getSinglePkgFolderRegexMatcher({ pkg, folders }) {
+    const acc = [];
+    folders(pkg).forEach(f => {
+        const e = escapeForRegex(path.join(path.sep, pkg, f))
+        acc.push(new RegExp(e))
+    })
+    return acc;
+}
+function getBabelProcessedFolders({ uuiRoot, isIncluded }) {
+    /**
+     * Folders which babel should include from each module
+     */
+    const PKG_FOLDERS_BABEL_INCLUDED = ['']
+    const PKG_FOLDERS_BABEL_INCLUDED_BUILD = ['build', 'assets']
+    /**
+     * Folders which babel should exclude from each module
+     */
+    const PKG_FOLDERS_BABEL_EXCLUDED = ['build', 'node_modules']
+    const PKG_FOLDERS_BABEL_EXCLUDED_BUILD = ['node_modules']
+
+    const getFoldersFn = isUseBuildFolderOfDeps => pkg => {
+        if (isIncluded) {
+            if (isUseBuildFolderOfDeps) {
+                if (['app', 'epam-assets'].indexOf(pkg) !== -1) {
+                    return PKG_FOLDERS_BABEL_INCLUDED
+                }
+                return PKG_FOLDERS_BABEL_INCLUDED_BUILD
+            }
+            return PKG_FOLDERS_BABEL_INCLUDED;
+        }
+        if (isUseBuildFolderOfDeps) {
+            return PKG_FOLDERS_BABEL_EXCLUDED_BUILD;
+        }
+        return PKG_FOLDERS_BABEL_EXCLUDED;
+    }
+    const DEFAULT = getPkgFoldersRegexMatcher({ uuiRoot,  folders: getFoldersFn(false) });
+    const BUILD_FOLDERS = getPkgFoldersRegexMatcher({ uuiRoot,  folders: getFoldersFn(true) });
+    return { DEFAULT, BUILD_FOLDERS }
+}
 
 module.exports = {
-    getPkgFoldersRegexMatcher,
+    getBabelProcessedFolders,
 }
