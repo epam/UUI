e37ab037817af80979527092160d896c70a4f558
dzevakov <dmitryzev@gmail.com>
2022-12-21 14:38:42 +0300

    [Docs]: change tooltip example styles

--- app/src/docs/examples/tooltip/TriggerConfiguration.example.tsx
+++ app/src/docs/examples/tooltip/TriggerConfiguration.example.tsx
@@ -5,19 +5,19 @@ export default function TriggerConfigurationExample() {
     return (
         <FlexRow spacing='12' >
             <Tooltip content='Instant appearance'>
-                <Button caption='Instant appearance' onClick={ () => null } />
+                <Button fill='white' caption='Instant appearance' onClick={ () => null } />
             </Tooltip>
 
             <Tooltip content='0.5s delay' delay={ 0.5 } >
-                <Button caption='0.5s delay' onClick={ () => null } />
+                <Button fill='white' caption='0.5s delay' onClick={ () => null } />
             </Tooltip>
 
             <Tooltip content='0.1s delay' delay={ 0.1 } >
-                <Button caption='0.1s delay' onClick={ () => null } />
+                <Button fill='white' caption='0.1s delay' onClick={ () => null } />
             </Tooltip>
 
             <Tooltip trigger='click' content='Click' >
-                <Button caption='Click' onClick={ () => null } />
+                <Button fill='white' caption='Click' onClick={ () => null } />
             </Tooltip>
         </FlexRow>
     );
