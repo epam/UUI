1199f341c367caa2349a4198010bda8cc1a6611d
Siarhei Yelin <Siarhei_Yelin@epam.com>
2022-12-26 16:43:45 +0300

    Move doc props from "loveship" to "app"

---
+++ app/src/docProps/loveship/components/datePickers/datePicker.doc.tsx
@@ -0,0 +1,54 @@
+import * as React from 'react';
+import dayjs, { Dayjs } from "dayjs";
+import { DocBuilder } from '@epam/uui-docs';
+import { Day, IconContainer } from '@epam/uui-components';
+import { DatePicker, DatePickerProps } from '@epam/loveship';
+import { LinkButton, FlexRow } from '@epam/loveship';
+import { DefaultContext, FormContext, iEditable, isDisabledDoc, isInvalidDoc,
+    isReadonlyDoc, modeDoc, ResizableContext, sizeDoc, TableContext, textSettingsDoc } from '../../docs';
+import { ReactComponent as Point } from '../../docIcons/radio-point.svg';
+
+const getCustomDay = (day: Dayjs) => {
+    return <>
+        { day.format('D') }
+        <IconContainer style={ { fill: '#fcaa00', height: '4px', width: '4px', position: "absolute", top: '7px', right: '10px' } }  icon={ Point } />
+    </>;
+};
+
+const DatePickerDoc = new DocBuilder<DatePickerProps>({ name: 'DatePicker', component: DatePicker })
+    .implements([iEditable, sizeDoc, textSettingsDoc, modeDoc, isDisabledDoc, isReadonlyDoc, isInvalidDoc])
+    .prop('value', { examples: ['2020-09-03'] })
+    .prop('placeholder', { examples: ['Enter start date'] })
+    .prop('format', { examples: ['MM/DD/YYYY', 'MMM D, YYYY', 'DD.MM.YYYY', 'YYYY-MM-DD'], defaultValue: 'MMM D, YYYY' })
+    .prop('filter', { examples: [
+        {
+            name: 'Filter before current day',
+            value: day => day.valueOf() >= dayjs().subtract(1, 'day').valueOf(),
+        },
+    ] })
+    .prop('renderDay', { examples: ctx => [{
+            name: 'Render custom day',
+            value: (day: Dayjs, onDayClick: (day: Dayjs) => void) => {
+                return <Day renderDayNumber={ getCustomDay } value={ day } onValueChange={ onDayClick } isSelected={ day && day.isSame(ctx.getSelectedProps().value) } filter={ ctx.getSelectedProps().filter } />;
+            },
+        }] })
+    .prop('placement', {
+        examples: [
+            'auto-start', 'auto', 'auto-end', 'top-start',
+            'top', 'top-end', 'right-start', 'right', 'right-end', 'bottom-end',
+            'bottom', {value: 'bottom-start', isDefault: true }, 'left-end', 'left', 'left-start',
+        ],
+    })
+    .prop('disableClear', { examples: [true], defaultValue: false})
+    .prop('isHoliday', { examples: [{ name: 'without Holidays', value: day => false }] })
+    .prop('renderFooter', {
+        examples: ctx => [
+            {
+                name: 'footer',
+                value: () => <FlexRow><LinkButton size='42' caption='TODAY' onClick={ () => ctx.getSelectedProps().onValueChange(dayjs().format('MMM D, YYYY')) } /></FlexRow>,
+            },
+        ],
+    })
+    .withContexts(DefaultContext, FormContext, ResizableContext, TableContext);
+
+export default DatePickerDoc;
