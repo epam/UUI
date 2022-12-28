fa2b3b202287073f1befe0cefbf54f324fe99d80
dzevakov <dmitryzev@gmail.com>
2022-12-28 15:04:50 +0300

    [FlexRow]: updated snapshots with FlexRow

--- uui-components/src/layout/flexItems/FlexRow.tsx
+++ uui-components/src/layout/flexItems/FlexRow.tsx
@@ -2,27 +2,25 @@ import * as React from 'react';
 import css from './FlexRow.scss';
 import { FlexRowProps, uuiMarkers, isClickableChildClicked, cx } from '@epam/uui-core';
 
-export const FlexRow = React.forwardRef<HTMLDivElement, FlexRowProps>((props, ref) => {
-    const style: React.CSSProperties = {...props.rawProps?.style};
-    if (props.columnGap) { style.columnGap = props.columnGap; }
-    if (props.rowGap) { style.rowGap = props.rowGap; }
-
-    return (
-        <div
-            ref={ ref }
-            onClick={ props.onClick ? e => !isClickableChildClicked(e) && props.onClick(e) : undefined }
-            className={ cx(
-                props.cx,
-                css.container,
-                props.onClick && uuiMarkers.clickable,
-                css['align-items-' + (props.alignItems === undefined ? 'center' : props.alignItems)],
-            ) }
-            { ...props.rawProps }
-            style={ style }
-        >
-            { props.children }
-        </div>
-    );
-});
+export const FlexRow = React.forwardRef<HTMLDivElement, FlexRowProps>((props, ref) => (
+    <div
+        ref={ ref }
+        onClick={ props.onClick ? e => !isClickableChildClicked(e) && props.onClick(e) : undefined }
+        className={ cx(
+            props.cx,
+            css.container,
+            props.onClick && uuiMarkers.clickable,
+            css['align-items-' + (props.alignItems === undefined ? 'center' : props.alignItems)],
+        ) }
+        { ...props.rawProps }
+        style={ {
+            ...props.rawProps?.style,
+            columnGap: props.columnGap ? `${props.columnGap}px` : undefined,
+            rowGap: props.rowGap ? `${props.rowGap}px` : undefined,
+        } }
+    >
+        { props.children }
+    </div>
+));
 
 FlexRow.displayName = 'FlexRow';
