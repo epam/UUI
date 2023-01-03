68fb22a09c7b5c6fcf13d1384a01ac7bfa4c62c5
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-01-03 17:51:31 +0300

    1) Fix issue when TS typechecking is missed in watch mode.
    2) Fix imports from 'react-custom-scrollbars-2'
    3) Fix TS compile error in "uui-core/src/types/props.ts"

---
+++ uui-build/utils/cmdUtils.js
@@ -0,0 +1,24 @@
+const child_process = require('child_process')
+
+async function runCliCommand(cwd, command) {
+  await new Promise((resolve, reject) => {
+    child_process.exec(
+      command,
+      {
+        cwd,
+      },
+      (error, stdout, stderr) => {
+        if (error) {
+          console.log(stdout);
+          console.error(stderr);
+          reject(`${command} error: ${error}`);
+        }
+        resolve();
+      }
+    );
+  });
+}
+
+module.exports = {
+  runCliCommand
+}
