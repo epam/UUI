9f97c41f2de83e5b7fef525a5d1b11910390250a
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-01-05 15:12:40 +0300

    remove incorrect PropsDocPath

--- app/src/docs/Anchor.doc.tsx
+++ app/src/docs/Anchor.doc.tsx
@@ -1,12 +1,11 @@
 import * as React from 'react';
-import { EditableDocContent, DocExample, BaseDocsBlock, UUI4, UUI3 } from '../common';
+import { EditableDocContent, DocExample, BaseDocsBlock, UUI4 } from '../common';
 
 export class AnchorDoc extends BaseDocsBlock {
     title = 'Anchor';
 
     getPropsDocPath() {
         return {
-            [UUI3]: './epam-promo/components/navigation/anchor.doc.tsx',
             [UUI4]: './app/src/docProps/epam-promo/components/navigation/anchor.doc.tsx',
         };
     }
