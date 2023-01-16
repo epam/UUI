0d4c8833bd5eabcaddbfbceb50bbf9dd27c1c137
IhorKorenets <vik753@gmail.com>
2023-01-16 17:48:55 +0200

    refactored theme styles in loveship

--- loveship/components/inputs/Checkbox.tsx
+++ loveship/components/inputs/Checkbox.tsx
@@ -1,7 +1,7 @@
 import * as types from '../types';
 import { Checkbox as uuiCheckbox, CheckboxProps, CheckboxMods as UuiCheckboxMods } from '@epam/uui';
 import { withMods } from '@epam/uui-core';
-// import css from './Checkbox.scss'; // keep it for dark / light theme on future
+import css from './Checkbox.scss';
 import { ReactComponent as TickIcon } from '../icons/checkbox-checked.svg';
 import { ReactComponent as IndeterminateIcon } from '../icons/checkbox-partial.svg';
 
@@ -12,7 +12,7 @@ export interface CheckboxMods extends UuiCheckboxMods, types.ColorMod {
 export function applyCheckboxMods(mods: CheckboxMods & CheckboxProps) {
     return [
         'uui-theme-loveship',
-        // css['theme-' + (mods.theme || 'light')],
+        css['theme-' + (mods.theme || 'light')],
     ];
 }
 
