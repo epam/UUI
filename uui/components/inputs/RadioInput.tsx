2852fb864e259f36f59c4d926a52723645f03d67
IhorKorenets <vik753@gmail.com>
2023-01-17 12:38:03 +0200

    made UuiRadioInput with skins

--- uui/components/inputs/RadioInput.tsx
+++ uui/components/inputs/RadioInput.tsx
@@ -1,6 +1,5 @@
 import { withMods } from '@epam/uui-core';
-import { RadioInput as uuiRadioInput, RadioInputProps } from '@epam/uui-components';
-import '../../assets/styles/variables/inputs/radioInput.scss';
+import { RadioInput as uuiRadioInput, RadioInputProps as UuiRadioInputProps } from '@epam/uui-components';
 import css from './RadioInput.scss';
 import { ReactComponent as RadioPoint } from '../../icons/radio-point.svg';
 
@@ -8,12 +7,13 @@ export interface RadioInputMods {
     size?: '12' | '18';
 }
 
-function applyRadioInputMods(mods: RadioInputMods & RadioInputProps) {
+export type RadioInputProps =  RadioInputMods & UuiRadioInputProps;
+
+function applyRadioInputMods(mods: RadioInputProps) {
     return [
-        'radio-input-vars',
         css.root,
         css['size-' + (mods.size || '18')],
     ];
 }
 
-export const RadioInput = withMods<RadioInputProps, RadioInputMods>(uuiRadioInput, applyRadioInputMods, () => ({ icon: RadioPoint }));
+export const RadioInput = withMods<UuiRadioInputProps, RadioInputMods>(uuiRadioInput, applyRadioInputMods, () => ({ icon: RadioPoint }));
