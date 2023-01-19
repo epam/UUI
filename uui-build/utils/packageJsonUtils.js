fe5710afc32a0a23af265d77374609322d2d93c3
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-01-19 12:50:47 +0300

    fix errors

--- uui-build/utils/packageJsonUtils.js
+++ uui-build/utils/packageJsonUtils.js
@@ -1,20 +1,15 @@
 const fs = require("fs");
 const path = require("path");
 
-module.exports = { readPackageJsonContentAsync, readPackageJsonContentSync, copyPackageJsonAsync };
+module.exports = { readPackageJsonContentSync, copyPackageJsonAsync };
 
 function readPackageJsonContentSync(dir) {
     const s = fs.readFileSync(path.resolve(dir, 'package.json')).toString('utf-8');
     return JSON.parse(s)
 }
 
-async function readPackageJsonContentAsync(dir) {
-    const s = await fs.readFile(path.resolve(dir, 'package.json')).toString('utf-8');
-    return JSON.parse(s)
-}
-
 async function copyPackageJsonAsync({ fromDir, toDir, transform }) {
-    const content = await readPackageJsonContentAsync(fromDir);
+    const content = readPackageJsonContentSync(fromDir);
     transform(content);
     const contentStr = JSON.stringify(content, undefined, 2);
     const to = path.resolve(toDir, `./package.json`);
