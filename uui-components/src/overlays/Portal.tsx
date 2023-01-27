9e5f6f6cef7b40f81d6edd2c0b7b032137c1555f
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-01-27 14:40:45 +0300

    code review

--- uui-components/src/overlays/Portal.tsx
+++ uui-components/src/overlays/Portal.tsx
@@ -1,7 +1,6 @@
 import * as React from 'react';
 import ReactDOM from 'react-dom';
-import { IHasChildren } from "@epam/uui-core";
-import { useEffect } from "react";
+import { IHasChildren, useDeferRenderForSsr } from "@epam/uui-core";
 
 export interface PortalProps extends IHasChildren {
     target?: HTMLElement;
@@ -9,10 +8,9 @@ export interface PortalProps extends IHasChildren {
 }
 
 export const Portal: React.FC<PortalProps> = (props) => {
-    const [isMounted, setIsMounted] = React.useState(false);
-    useEffect(() => setIsMounted(true), []);
-    if (!isMounted) return null;
-
+    if (!useDeferRenderForSsr()) {
+        return null;
+    }
     const rootElement = props.target || document.getElementById('main') || document.getElementById('root') || document.body;
     return ReactDOM.createPortal(props.children, rootElement, props.key);
 };
---
+++ uui-core/src/ssrUtils/useDeferRenderForSsr.tsx
@@ -0,0 +1,10 @@
+import { useEffect, useState } from "react";
+
+/**
+ * This hook is useful to delay render of something which could differ between server SSR and the client.
+ */
+export function useDeferRenderForSsr() {
+    const [shouldRender, setShouldRender] = useState(false);
+    useEffect(() => { setShouldRender(true); }, []);
+    return shouldRender;
+}
