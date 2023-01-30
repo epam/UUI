a8eafb8a82f9bcf7ab01788f5d33dc6f70c663b7
IhorKorenets <vik753@gmail.com>
2023-01-30 15:55:47 +0200

    fixed import

--- loveship/components/buttons/LinkButton.tsx
+++ loveship/components/buttons/LinkButton.tsx
@@ -1,7 +1,7 @@
 import * as types from '../types';
 import { withMods } from '@epam/uui-core';
 import { LinkButton as UuiLinkButton, LinkButtonProps } from '@epam/uui';
-import { TextSettings } from "../../build/helpers/textLayout";
+import { TextSettings } from "../../helpers/textLayout";
 
 export interface LinkButtonMods extends TextSettings {
     color?: types.EpamColor;
