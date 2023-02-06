11db2fa1f8f5e507fe9cc69471938b63193e43d8
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-02-06 13:54:50 +0300

    incorporate feedback

--- uui-build/shared/index.js
+++
@@ -1,7 +0,0 @@
-/**
- * This file contains a public api exported from "uui-build".
- */
-const { createRollupConfigForModule } = require('../rollup/rollup.config.js');
-const { buildModuleUsingRollup } = require('../utils/moduleBuildUtils.js');
-
-module.exports = { createRollupConfigForModule, buildModuleUsingRollup }
--- uui-build/utils/loggerUtils.js
+++ uui-build/utils/loggerUtils.js
@@ -2,7 +2,7 @@ const chalk = require('chalk');
 const logger = { info, error, success };
 
 function ModuleBuildProgressLogger({ moduleRootDir, isRollup }) {
-    const notes = isRollup ? '(Building using Rollup)' : '(No index.tsx exists. Will publish package as is, w/o pre-build)';
+    const notes = isRollup ? '(Rollup build)' : '(No index.tsx exists. Will publish package as is, w/o pre-build)';
     this.start = () => logger.success(`Building package: ${moduleRootDir} ${notes}.`);
     this.done = () => logger.success(`Done building package: ${moduleRootDir}`);
     this.error = () => logger.error(`Failed building package: ${moduleRootDir}`);
