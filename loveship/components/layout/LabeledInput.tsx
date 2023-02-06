7228aa6cf415dff730689222936a6547ac36059e
dzevakov <dmitryzev@gmail.com>
2023-02-06 12:43:46 +0300

    [LabeledInput]: uui thematization refactor

--- loveship/components/layout/LabeledInput.tsx
+++ loveship/components/layout/LabeledInput.tsx
@@ -1,15 +1,10 @@
 import { withMods } from '@epam/uui-core';
-import { LabeledInput as uuiLabeledInput, LabeledInputMods as uuiLabeledInputMods } from '@epam/uui';
+import { LabeledInput as uuiLabeledInput, LabeledInputMods } from '@epam/uui';
 import { LabeledInputProps } from '@epam/uui-components';
-import * as types from '../types';
-import styles from '../../assets/styles/scss/loveship-color-vars.scss';
 
-export interface LabeledInputMods extends types.ColorMod, uuiLabeledInputMods {}
-
-function applyLabeledInputMods(mods: LabeledInputMods) {
+function applyLabeledInputMods() {
     return [
         'uui-theme-loveship',
-        styles['color-' + (mods.color || 'night700')],
     ];
 }
 
