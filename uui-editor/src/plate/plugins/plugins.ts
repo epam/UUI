c1a62a96e73b6e4bc88526ad7a6381d2f245292e
haykselina <hayk.simonyan@selina.com>
2023-02-06 20:15:37 +0400

    Remove handleUpload

--- uui-editor/src/plate/plugins/plugins.ts
+++ uui-editor/src/plate/plugins/plugins.ts
@@ -1,18 +1,7 @@
 import {
     createSoftBreakPlugin,
     createExitBreakPlugin,
-    createTablePlugin,
-    ELEMENT_TR, ELEMENT_TD, ELEMENT_TH
 } from '@udecode/plate';
-
-import { svc } from "@epam/app/src/services";
-
-
-const uploadFile = async (file: File, onProgress: (progress: number) => any): Promise<any> => {
-    return svc.uuiApi.uploadFile('/uploadFileMock', file, {
-        onProgress,
-    });
-};
 
 import { baseMarksPlugin } from "./baseMarksPlugin/baseMarksPlugin";
 import { codeBlockPlugin } from "./codeBlockPlugin/codeBlockPlugin";
@@ -30,7 +19,6 @@ import { quotePlugin } from "./quotePlugin/quotePlugin";
 import { separatorPlugin } from "./separatorPlugin/separatorPlugin";
 import { placeholderPlugin } from "./placeholderPlugin/placeholderPlugin";
 import { attachmentPlugin } from "./attachmentPlugin/attachmentPlugin";
-import { uploadFilePlugin } from "./uploadFilePlugin/uploadFilePlugin";
 import { iframePlugin } from "./iframePlugin/iframePlugin";
 import { tablePlugin } from "./tablePlugin/tablePlugin";
 
@@ -65,7 +53,6 @@ export const customPlugins = [
     }),
     attachmentPlugin(),
     iframePlugin(),
-    uploadFilePlugin({ uploadFile }),
     videoPlugin(),
 
     tablePlugin(),
