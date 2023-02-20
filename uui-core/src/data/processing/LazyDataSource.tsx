bcf7919d729b4ec85f390cf7f5361d37a862dfba
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-02-20 12:28:49 +0200

    Fixed types of datasources/listviews.

--- uui-core/src/data/processing/LazyDataSource.tsx
+++ uui-core/src/data/processing/LazyDataSource.tsx
@@ -4,8 +4,7 @@ import { BaseDataSource } from "./BaseDataSource";
 import { useEffect } from "react";
 import { DataSourceState } from '../../types';
 
-export interface LazyDataSourceProps<TItem, TId, TFilter> extends LazyListViewProps<TItem, TId, TFilter> {
-}
+export interface LazyDataSourceProps<TItem, TId, TFilter> extends LazyListViewProps<TItem, TId, TFilter> {}
 
 export class LazyDataSource<TItem = any, TId = any, TFilter = any> extends BaseDataSource<TItem, TId, TFilter> {
     props: LazyDataSourceProps<TItem, TId, TFilter>;
