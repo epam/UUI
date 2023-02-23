ab28e6d7d9aa3407bb3d66e8531570a32d72f317
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-02-23 17:16:33 +0200

    Moved DemoTablePaged to useList.

--- app/src/sandbox/tablePaged/DemoTablePaged.tsx
+++ app/src/sandbox/tablePaged/DemoTablePaged.tsx
@@ -1,7 +1,7 @@
 import React, { useCallback, useEffect, useMemo, useState } from "react";
 import css from "./DemoTablePaged.scss";
-import { DataTable, FlexRow, Paginator,  FlexSpacer, Button } from "@epam/promo";
-import { DataRowOptions, DataTableState, LazyDataSourceApi, useLazyDataSource, useTableState } from "@epam/uui-core";
+import { DataTable, FlexRow, Paginator, FlexSpacer, Button } from "@epam/promo";
+import { DataRowOptions, LazyDataSourceApi, useLazyDataSource, useList, useTableState } from "@epam/uui-core";
 import { Person } from "@epam/uui-docs";
 import { svc } from "../../services";
 import { getFilters } from "./filters";
@@ -11,16 +11,16 @@ import { FlexCell } from "@epam/uui-components";
 export const DemoTablePaged: React.FC = () => {
     const filters = useMemo(getFilters, []);
 
-    const {tableState, setTableState} = useTableState({
+    const { tableState, setTableState } = useTableState<Person>({
         columns: personColumns,
     });
 
     useEffect(() => {
-        setTableState({...tableState, page: 1, pageSize: 100});
+        setTableState({ ...tableState, page: 1, pageSize: 100 });
     }, []);
 
     const [totalCount, setTotalCount] = useState(0);
-    const [appliedFilter, setAppliedFilter] = useState<DataTableState>({});
+    const [appliedFilter, setAppliedFilter] = useState<Person>();
 
     const api: LazyDataSourceApi<Person, number, Person> = useCallback(async request => {
         const result = await svc.api.demo.personsPaged({
@@ -67,32 +67,41 @@ export const DemoTablePaged: React.FC = () => {
         isFoldedByDefault: () => true,
     });
 
+    const { rows, listProps } = useList({
+        type: 'lazy',
+        value: viewTableState,
+        onValueChange: setTableState,
+        api,
+        rowOptions,
+        isFoldedByDefault: () => true,
+    }, [api]);
+
     return (
         <div className={ css.container }>
             <DataTable
                 headerTextCase="upper"
-                getRows={ personsDataView.getVisibleRows }
+                getRows={ () => rows }
                 columns={ personColumns }
                 filters={ filters }
                 showColumnsConfig
                 value={ tableState }
                 onValueChange={ setTableState }
                 allowColumnsResizing
-                { ...personsDataView.getListProps() }
+                { ...listProps }
             />
 
             <FlexRow size="36" padding="12" background="gray5">
                 <FlexCell width='auto'>
                     <Button caption="Apply filter" onClick={ applyFilter } cx={ css.apply } />
                 </FlexCell>
-                <FlexSpacer/>
+                <FlexSpacer />
                 <Paginator
                     value={ tableState.page }
-                    onValueChange={ (page: number) => setTableState({...tableState, page, indexToScroll: 0 }) }
+                    onValueChange={ (page: number) => setTableState({ ...tableState, page, indexToScroll: 0 }) }
                     totalPages={ Math.ceil(totalCount / tableState.pageSize) }
                     size="30"
                 />
-                <FlexSpacer/>
+                <FlexSpacer />
             </FlexRow>
         </div>
     );
