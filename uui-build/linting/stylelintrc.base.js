279268765e8f30c3d4adb774af0af9e2f82a832a
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-03-01 11:45:07 +0300

    adjustments

--- uui-build/linting/stylelintrc.base.js
+++ uui-build/linting/stylelintrc.base.js
@@ -7,22 +7,19 @@ const sassGuidelinesRulesSubsetForLess = Object.keys(sassGuidelinesRules)
     .reduce((acc, k) => { acc[k] = sassGuidelinesRules[k]; return acc; }, {});
 
 const SCSS_AND_LESS_COMMON_RULES = {
-    'declaration-empty-line-before': 'never',
     'order/properties-alphabetical-order': null,
     'max-nesting-depth': null,
     'selector-list-comma-newline-after': null,
-    'no-missing-end-of-source-newline': null,
     'color-named': null,
     'selector-max-compound-selectors': null,
     'shorthand-property-no-redundant-values': null,
-    'color-hex-case': null,
     'color-hex-length': null,
-    'property-no-vendor-prefix': null,
-    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
     'selector-class-pattern': null,
     'selector-no-vendor-prefix': null,
-    'value-no-vendor-prefix': null,
     'selector-no-qualifying-type': null,
+    //
+    'declaration-empty-line-before': 'never',
+    'property-no-unknown': [true, { ignoreProperties: ['composes'] }],
     'declaration-property-value-disallowed-list': [
         {
             border: ['none'],
@@ -31,9 +28,19 @@ const SCSS_AND_LESS_COMMON_RULES = {
             'border-bottom': ['none'],
             'border-left': ['none'],
         },
-        { message: 'E.g.: border: none can be replaced by border: 0 none;' },
+        { message: 'E.g.: border: none can be replaced by "border: 0 none;"' },
     ],
     indentation: 4,
+    'color-hex-case': 'upper',
+    'unit-no-unknown': true,
+    'media-feature-name-no-unknown': true,
+    'color-no-invalid-hex': true,
+    'declaration-block-no-duplicate-properties': [
+        true,
+        {
+            ignore: ['consecutive-duplicates-with-different-values'],
+        },
+    ],
     ...turnOffStylelintRulesToBeFixed(),
 };
 
