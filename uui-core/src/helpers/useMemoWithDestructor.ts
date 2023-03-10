88f6bc523adcd0a9fd11a4810b4edec13a4bed43
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-03-10 15:09:29 +0200

    Destroy actions revert.

--- uui-core/src/helpers/useMemoWithDestructor.ts
+++ uui-core/src/helpers/useMemoWithDestructor.ts
@@ -3,32 +3,35 @@ import { useForceUpdate } from '../hooks';
 
 export function useMemoWithDestructor<T>(create: () => T, update: (instance: T) => void, destroy: (value: T) => any, deps: any[]) {
     const ref = useRef<T>();
-    const preDestroy = useRef<boolean>(true);
     const prevDeps = useRef(deps);
+    const forceUpdate = useForceUpdate();
+    const createView = () => {
+        const isDepsChanged = (prevDeps.current.length != deps.length)
+            || prevDeps.current.some((devVal, index) => devVal != deps[index]);
 
-    const isDepsChanged = (prevDeps.current.length != deps.length)
-        || prevDeps.current.some((devVal, index) => devVal != deps[index]);
+        if (ref.current == null || isDepsChanged) {
+            prevDeps.current = deps;
+            return create();
+        }
+        return ref.current;
+    };
 
-    if (ref.current == null || isDepsChanged) {
-        prevDeps.current = deps;
-        ref.current = create();
-    }
-
+    ref.current = createView();
     update(ref.current);
 
     const current = ref.current;
 
     useEffect(() => {
+        if (ref.current === null) {
+            ref.current = createView();
+            forceUpdate();
+        }
+
         // Value here is memoized in closure at the time of its creation.
         // So we are not destroying the value we just created above.
         return () => {
-            // first destroy is new react's flow destroy.
-            if (preDestroy.current) {
-                preDestroy.current = false;
-            } else {
-                current && destroy(current);
-                ref.current = null;
-            }
+            current && destroy(current);
+            ref.current = null;
         };
     }, deps);
 
