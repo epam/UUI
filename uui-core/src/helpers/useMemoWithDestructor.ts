efa811b3495c09a7954326d5337ca2d43a286115
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-03-10 14:54:04 +0200

    Fixed updates of the Datasource.

--- uui-core/src/helpers/useMemoWithDestructor.ts
+++ uui-core/src/helpers/useMemoWithDestructor.ts
@@ -1,7 +1,9 @@
 import { useEffect, useRef } from 'react';
+import { useForceUpdate } from '../hooks';
 
 export function useMemoWithDestructor<T>(create: () => T, update: (instance: T) => void, destroy: (value: T) => any, deps: any[]) {
     const ref = useRef<T>();
+    const preDestroy = useRef<boolean>(true);
     const prevDeps = useRef(deps);
 
     const isDepsChanged = (prevDeps.current.length != deps.length)
@@ -19,7 +21,15 @@ export function useMemoWithDestructor<T>(create: () => T, update: (instance: T) 
     useEffect(() => {
         // Value here is memoized in closure at the time of its creation.
         // So we are not destroying the value we just created above.
-        return () => current && destroy(current);
+        return () => {
+            // first destroy is new react's flow destroy.
+            if (preDestroy.current) {
+                preDestroy.current = false;
+            } else {
+                current && destroy(current);
+                ref.current = null;
+            }
+        };
     }, deps);
 
     return ref.current;
