2c91669638670f71d4436b579fdfb5974ddd690f
IhorKorenets <vik753@gmail.com>
2023-03-13 18:04:02 +0200

    moved Blocker to UUI

--- uui/components/layout/Blocker.tsx
+++ uui/components/layout/Blocker.tsx
@@ -2,11 +2,9 @@ import React from 'react';
 import css from './Blocker.scss';
 import { withMods } from '@epam/uui-core';
 import { Blocker as uuiBlocker, BlockerProps } from '@epam/uui-components';
-import { Spinner } from '../widgets';
-import * as types from '../types';
+import { Spinner } from '../widgets/Spinner';
 
-export interface BlockerMods extends types.ColorMod { }
 
-export const Blocker = withMods<BlockerProps, BlockerMods>(uuiBlocker, () => [css.root],
+export const Blocker = withMods<BlockerProps>(uuiBlocker, () => [css.root],
     (cmpProps) => ({ renderSpinner: cmpProps.renderSpinner || (() => <Spinner />) }),
 );
