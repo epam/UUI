e049ecb1dfae51d6b7c4c1fd1efa37b7e963a9be
IhorKorenets <vik753@gmail.com>
2023-03-14 16:27:39 +0200

    made UuiRadioGroup

--- epam-promo/components/layout/index.tsx
+++ epam-promo/components/layout/index.tsx
@@ -4,6 +4,5 @@ export * from './ControlGroup';
 export * from './ControlWrapper';
 export * from './FlexItems';
 export * from './LabeledInput';
-export * from './RadioGroup';
 export * from './ScrollBars';
 export * from  './VirtualList';
