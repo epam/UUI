322ffc6a436cc63d2fab7e20e1b4f7ca9ee4d9b3
IhorKorenets <vik753@gmail.com>
2023-03-14 15:12:28 +0200

    made UuiCheckboxGroup

--- loveship/components/layout/index.ts
+++ loveship/components/layout/index.ts
@@ -3,7 +3,6 @@ export * from './LabeledInput';
 export * from './RadioGroup';
 export * from './ControlGroup';
 export * from './ControlWrapper';
-export * from './CheckboxGroup';
 export * from './Blocker';
 export * from './ScrollBars';
 export * from './VirtualList';
