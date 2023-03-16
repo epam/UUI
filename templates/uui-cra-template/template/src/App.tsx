04bb839d122d2be843a5054fd2a7134500ca74c4
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-03-16 16:34:21 +0300

    test

--- templates/uui-cra-template/template/src/App.tsx
+++ templates/uui-cra-template/template/src/App.tsx
@@ -4,6 +4,12 @@ import { Route } from 'react-router-dom';
 import { AppHeader } from './AppHeader';
 import css from './App.module.scss';
 
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
+console.log('Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello-Hello');
 export const App = () => {
     return (
         <div className={ css.app }>
