c29bf5ebc3acb75171e40b6598fe5fff74a72cb6
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-03-17 17:34:52 +0200

    Added Lens for Tree.

--- uui-core/src/data/lenses/types.ts
+++ uui-core/src/data/lenses/types.ts
@@ -1,13 +1,17 @@
 import { IEditable } from '../../types';
+import { ITree } from '../processing';
 
 export type ArrayElement<ArrayType> = ArrayType extends (infer ElementType)[] ? ElementType : never;
+export type TreeElement<TTree> = TTree extends ITree<infer TItem, infer TId> ? TItem : never;
+export type TreeId<TTree> = TTree extends ITree<infer TItem, infer TId> ? TId : never;
 
 export interface ILens<TFocused> {
     get(): TFocused;
     set(value: TFocused): void;
-    update(fn: (current: TFocused) => TFocused): void;   
+    update(fn: (current: TFocused) => TFocused): void;
     prop<K extends keyof TFocused>(name: K): ILens<NonNullable<TFocused[K]>>;
     index(index: number): ILens<ArrayElement<TFocused>>;
+    getById(id: TreeId<TFocused>): ILens<TreeElement<TFocused>>;
     onChange(fn: (oldValue: TFocused, newValue: TFocused) => TFocused): ILens<TFocused>;
     default(value: TFocused): ILens<TFocused>;
     toProps(): IEditable<TFocused>;
