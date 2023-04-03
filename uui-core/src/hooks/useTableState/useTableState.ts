25974b25ceefc9e4797ca77aa02a189a6e8930d4
Aleksey Manetov <alekseymanetov@gmail.com>
2023-04-03 15:55:07 +0200

    [useTableState]: set empty object for columnsConfig if it's absent

--- uui-core/src/hooks/useTableState/useTableState.ts
+++ uui-core/src/hooks/useTableState/useTableState.ts
@@ -22,7 +22,7 @@ export const useTableState = <TFilter = Record<string, any>, TViewState = any>(p
 
         return {
             filter: urlParams.filter,
-            columnsConfig: urlParams.columnsConfig,
+            columnsConfig: urlParams.columnsConfig || {},
             filtersConfig: filtersConfig,
             presetId: urlParams.presetId,
             page: urlParams.page,
