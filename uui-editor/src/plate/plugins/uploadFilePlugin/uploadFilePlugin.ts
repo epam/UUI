6f3f1d78f144ccb7a82146adc829e6d9ba1fd7d1
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-04-05 16:58:14 +0300

    [SlateEditor] add file checks

--- uui-editor/src/plate/plugins/uploadFilePlugin/uploadFilePlugin.ts
+++ uui-editor/src/plate/plugins/uploadFilePlugin/uploadFilePlugin.ts
@@ -1,17 +1,22 @@
-import {
-    createInsertDataPlugin,
-    findEventRange,
-    select,
-} from "@udecode/plate";
+import { createInsertDataPlugin, findEventRange, select } from "@udecode/plate";
+import { isEqual } from 'lodash';
 import { UploadFileOptions, createFileUploader } from "./file_uploader";
+
 
 export const uploadFilePlugin = (uploadOptions?: UploadFileOptions) =>
     createInsertDataPlugin({
         options: { uploadOptions },
         handlers: {
             onDrop: (editor, plugin) => {
-                const uploadFiles = createFileUploader(editor, plugin.options.uploadOptions);
                 return (event) => {
+                    const uploadFiles = createFileUploader(
+                        editor,
+                        plugin.options.uploadOptions
+                    );
+                    if (!isEqual(event.dataTransfer.types, ['Files'])) {
+                        return false;
+                    }
+
                     const { files } = event.dataTransfer;
                     if (files.length === 0) return false;
 
@@ -29,15 +34,26 @@ export const uploadFilePlugin = (uploadOptions?: UploadFileOptions) =>
                 };
             },
             onPaste: (editor, plugin) => {
-                const uploadFiles = createFileUploader(editor, plugin.options.uploadOptions);
                 return (event) => {
+                    const uploadFiles = createFileUploader(
+                        editor,
+                        plugin.options.uploadOptions
+                    );
+
+                    if (!isEqual(event.clipboardData.types, ['Files'])) {
+                        return false;
+                    }
+
                     const { files } = event.clipboardData;
                     if (files.length === 0) return false;
 
                     event.preventDefault();
                     event.stopPropagation();
 
-                    uploadFiles(Array.from(files), plugin.options.uploadOptions);
+                    uploadFiles(
+                        Array.from(files),
+                        plugin.options.uploadOptions
+                    );
 
                     return true;
                 };
