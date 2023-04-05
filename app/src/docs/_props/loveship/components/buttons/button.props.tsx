99677d508a788836019cbf4bc13701fa64fa421b
IhorKorenets <vik753@gmail.com>
2023-04-05 16:18:30 +0300

    removed lineHeight and fontSize, deprecated props from Button property explorer

--- app/src/docs/_props/loveship/components/buttons/button.props.tsx
+++ app/src/docs/_props/loveship/components/buttons/button.props.tsx
@@ -4,12 +4,12 @@ import { Button, ButtonMods } from '@epam/loveship';
 import { ColorPicker, DocBuilder } from '@epam/uui-docs';
 import { ButtonProps } from '@epam/uui-components';
 import { basicPickerTogglerDoc, DefaultContext, dropdownTogglerDoc, FormContext, iCanRedirectDoc,
-    iconOptionsDoc, iconWithInfoDoc, iHasPlaceholder, isDisabledDoc, onClickDoc, ResizableContext, textSettingsDoc } from '../../docs';
+    iconOptionsDoc, iconWithInfoDoc, iHasPlaceholder, isDisabledDoc, onClickDoc, ResizableContext } from '../../docs';
 import { colors } from '../../docs/helpers/colorMap';
 
 const ButtonDoc = new DocBuilder<ButtonProps & ButtonMods>({ name: 'Button', component: Button })
     .prop('size', { examples: ['18', "24", "30", "36", "42", "48"] , defaultValue: '36' })
-    .implements([textSettingsDoc, onClickDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder])
+    .implements([onClickDoc, dropdownTogglerDoc, isDisabledDoc, basicPickerTogglerDoc, iCanRedirectDoc, iHasPlaceholder])
     .prop('color', {
         renderEditor: (editable, examples) => <ColorPicker colors={ examples.map(i => ({ value: i, hex: colors[i] })) } { ...editable } />,
         examples: [...allEpamPrimaryColors.filter(color => color !== 'sun'), 'white', 'night500'],
