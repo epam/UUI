af7817561ae621db56aa326d6c40866bf18a81fc
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-04-05 18:49:19 +0300

    One more fix.

--- uui-core/src/data/processing/views/LazyListView.ts
+++ uui-core/src/data/processing/views/LazyListView.ts
@@ -352,7 +352,7 @@ export class LazyListView<TItem, TId, TFilter = any> extends BaseListView<TItem,
             checkedId,
             isChecked,
             {
-                cascade: isImplicitMode ? isImplicitMode : (isRoot || this.props.cascadeSelection),
+                cascade: isImplicitMode ? this.props.cascadeSelection : (isRoot || this.props.cascadeSelection),
                 isSelectable: (item: TItem) => {
                     const { isCheckable } = this.getRowProps(item, null);
                     return isCheckable;
