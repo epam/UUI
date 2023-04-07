6e7a6b51fa8b6570564675b05a8a240ec82f25f2
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-04-07 18:00:57 +0300

    Fixed ArrayListView selected items missing while searching.

--- uui-core/src/data/processing/views/ArrayListView.ts
+++ uui-core/src/data/processing/views/ArrayListView.ts
@@ -72,7 +72,8 @@ export class ArrayListView<TItem, TId, TFilter = any> extends BaseListView<TItem
     }
 
     public getById = (id: TId, index: number) => {
-        const item = this.tree.getById(id);
+        // if originalTree is not created, but blank tree is defined, get item from it
+        const item = (this.originalTree ?? this.tree).getById(id);
         return this.getRowProps(item, index);
     }
 
