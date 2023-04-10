90ed02bcdd1dd06a643d3b0980d11eb97b5f4cf5
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-04-10 17:35:59 +0300

    [SlateEditor] improve image render

--- uui-editor/src/plate/plugins/imagePlugin/ImageBlock.tsx
+++ uui-editor/src/plate/plugins/imagePlugin/ImageBlock.tsx
@@ -1,18 +1,24 @@
-import React, { useCallback, useRef } from 'react';
+import React, { useCallback, useRef, useState } from 'react';
 import cx from 'classnames';
 
 import { Dropdown } from '@epam/uui-components';
 import { uuiSkin } from "@epam/uui-core";
 
-import { useFocused, useSelected} from 'slate-react';
+import { useFocused, useSelected } from 'slate-react';
 
 import {
-    focusEditor,
     getBlockAbove,
     ImageElement,
     setElements,
     ToolbarButton as PlateToolbarButton,
+    PlatePluginComponent,
+    PlateRenderElementProps,
+    Value,
+    TElement,
+    useResizableStore,
 } from '@udecode/plate';
+
+import { FileUploadResponse } from "@epam/uui-core";
 
 import { ToolbarButton } from '../../../implementation/ToolbarButton';
 
@@ -25,9 +31,41 @@ import css from './ImageBlock.scss';
 
 const { FlexRow, Spinner } = uuiSkin;
 
-export const Image = (props: any) => {
+const IMAGE_STYLES = { paddingTop: 0, paddingBottom: 0 };
+const RESIZABLE_PROPS = { minWidth: 12 };
+
+type ImageSize = { width: number, height: number };
+
+type ImageData = {
+    imageSize?: ImageSize;
+} & (File | FileUploadResponse);
+
+type Align = 'left' | 'center' | 'right';
+
+export interface ImageElement extends TElement {
+    url: string;
+    align: Align;
+    data?: ImageData;
+}
+
+export const Image: PlatePluginComponent<PlateRenderElementProps<Value, ImageElement>> = (props) => {
+    const { editor, element, children } = props;
     const ref = useRef(null);
-    const { editor, element } = props;
+
+    const [align, setAlign] = useState<Align>(element.align || 'left');
+
+    const [imageSize, setImageSize] = useResizableStore().use.width(element.data?.imageSize);
+
+    // postpone image render till size is calculated on load for new image. prevents flickering
+    const style = imageSize ? IMAGE_STYLES : { ...IMAGE_STYLES, display: 'none' };
+
+    const onLoad = (e: any) => {
+        const naturalWidth = e.target.naturalWidth;
+        // set default size only for new image
+        if (!imageSize && !!naturalWidth) {
+            setImageSize({ width: naturalWidth, height: '100%' });
+        }
+    }
 
     const isFocused = useFocused();
     const isSelected = useSelected();
@@ -35,19 +73,13 @@ export const Image = (props: any) => {
     if (element.type === 'loader') {
         return (
             <>
-                <Spinner { ...props } cx={ css.spinner } />
-                { props.children }
+                <Spinner  { ...props } cx={ css.spinner } />
+                { children }
             </>
         );
     }
 
-    const toggleBlockAlignment = (align: string) => {
-        focusEditor(editor);
-        setElements(editor, {
-            ...element,
-            align,
-        });
-    };
+    const toggleBlockAlignment = (align: Align) => setAlign(align);
 
     const setMaxWidth = () => {
         if (ref?.current?.clientWidth) {
@@ -67,7 +99,7 @@ export const Image = (props: any) => {
         return (
             <div className={ cx(css.imageToolbar, 'slate-prevent-blur') }>
                 <PlateToolbarButton
-                    styles={ { root: {width: 'auto', cursor: 'pointer', padding: '0px' }} }
+                    styles={ { root: { width: 'auto', cursor: 'pointer', padding: '0px' } } }
                     active={ true }
                     onMouseDown={
                         editor
@@ -82,7 +114,7 @@ export const Image = (props: any) => {
                 />
 
                 <PlateToolbarButton
-                    styles={ { root: {width: 'auto', cursor: 'pointer', padding: '0px' }} }
+                    styles={ { root: { width: 'auto', cursor: 'pointer', padding: '0px' } } }
                     active={ true }
                     onMouseDown={
                         editor
@@ -97,7 +129,7 @@ export const Image = (props: any) => {
                 />
 
                 <PlateToolbarButton
-                    styles={ { root: {width: 'auto', cursor: 'pointer', padding: '0px' }} }
+                    styles={ { root: { width: 'auto', cursor: 'pointer', padding: '0px' } } }
                     active={ true }
                     onMouseDown={
                         editor
@@ -112,7 +144,7 @@ export const Image = (props: any) => {
                 />
 
                 <PlateToolbarButton
-                    styles={ { root: {width: 'auto', cursor: 'pointer', padding: '0px' }} }
+                    styles={ { root: { width: 'auto', cursor: 'pointer', padding: '0px' } } }
                     active={ true }
                     onMouseDown={
                         editor
@@ -139,8 +171,12 @@ export const Image = (props: any) => {
                         ref={ ref }
                         className={ cx(css.slateImage) }>
                         <ImageElement
-                            align={ element.align }
                             { ...props }
+                            align={ align }
+                            style={ style }
+                            resizableProps={ RESIZABLE_PROPS }
+                            caption={ { disabled: true } }
+                            onLoad={ onLoad }
                         />
                     </div>
                 </div>
