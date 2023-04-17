860c479b221bede9bf04141b0bdba3f9df7fcbb3
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-04-17 18:09:18 +0300

    add more tests for FiltersPanel

--- uui/components/filters/__tests__/filtersPanel.test.tsx
+++ uui/components/filters/__tests__/filtersPanel.test.tsx
@@ -187,15 +187,15 @@ describe('FiltersPanel', () => {
             const statusOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Status' });
             expect(statusOption).toBeInTheDocument();
             fireEvent.click(statusOption);
-            fireEvent.click(withinDialog().queryByRoleAndText({ role: 'option', text: 'Red' }));
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'option', text: 'Red' }));
             notExpectDialog();
             expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { status: 2 } }));
-            fireEvent.click(screen.queryByRoleAndText({ role: 'button', text: 'Status:Red' }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: 'Status:Red' }));
             expectDialog();
-            expect(withinDialog().queryByRoleAndText({ role: 'option', text: 'Red' })).toHaveAttribute('aria-selected', 'true');
-            fireEvent.click(withinDialog().queryAllByRole('button').find((btn) => btn.textContent === 'CLEAR'));
-            expect(withinDialog().queryByRoleAndText({ role: 'option', text: 'Red' })).toHaveAttribute('aria-selected', 'false');
-            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Red' })).toHaveAttribute('aria-selected', 'true');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR' }));
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Red' })).toHaveAttribute('aria-selected', 'false');
+            const removeButton = withinDialog().getByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
             fireEvent.click(removeButton);
             notExpectDialog();
             expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
@@ -212,42 +212,178 @@ describe('FiltersPanel', () => {
             const exitDateOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Exit Date' });
             expect(exitDateOption).toBeInTheDocument();
             fireEvent.click(exitDateOption);
-            fireEvent.click(withinDialog().queryByText(TODAY_DAY_OF_MONTH));
+            fireEvent.click(withinDialog().getByText(TODAY_DAY_OF_MONTH));
             notExpectDialog();
             expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { exitDate: TODAY_DATE_ISO } }));
-            fireEvent.click(screen.queryByRoleAndText({ role: 'button', text: `Exit Date:${TODAY_DATE_FORMATTED}` }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: `Exit Date:${TODAY_DATE_FORMATTED}` }));
             expectDialog();
-            expect(withinDialog().queryByText(TODAY_DAY_OF_MONTH).parentElement).toHaveClass('uui-calendar-selected-day');
-            fireEvent.click(withinDialog().queryAllByRole('button').find((btn) => btn.textContent === 'CLEAR'));
-            expect(withinDialog().queryByText(TODAY_DAY_OF_MONTH).parentElement).not.toHaveClass('uui-calendar-selected-day');
-            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            expect(withinDialog().getByText(TODAY_DAY_OF_MONTH).parentElement).toHaveClass('uui-calendar-selected-day');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR' }));
+            expect(withinDialog().getByText(TODAY_DAY_OF_MONTH).parentElement).not.toHaveClass('uui-calendar-selected-day');
+            const removeButton = withinDialog().getByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
             fireEvent.click(removeButton);
             notExpectDialog();
             expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
     });
     describe('filter type: multiPicker', () => {
-        it('should add / view / clear / remove (without predicate)', () => {
-
+        it('should add / view / clear / remove (without predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithoutPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const positionOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Position' });
+            expect(positionOption).toBeInTheDocument();
+            fireEvent.click(positionOption);
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' }));
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' }));
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { position: [2, 3] } }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: 'Position:QA, Dev' }));
+            expectDialog();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' })).toBeChecked();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' })).toBeChecked();
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR ALL' }));
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' })).not.toBeChecked();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' })).not.toBeChecked();
+            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
-        it('should add / view / clear / remove (with multiPicker predicate)', () => {
-
+        it('should add / view / clear / remove (with multiPicker predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const positionOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Position' });
+            expect(positionOption).toBeInTheDocument();
+            fireEvent.click(positionOption);
+            expect(withinDialog().queryAllByRole('tab').map(t => t.textContent)).toEqual(['is', 'is not']);
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' }));
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' }));
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'tab', text: 'is not' }));
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { position: { nin: [2, 3] } } }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: 'Position is notQA, Dev' }));
+            expectDialog();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' })).toBeChecked();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' })).toBeChecked();
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR ALL' }));
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'QA' })).not.toBeChecked();
+            expect(withinDialog().getByRoleAndText({ role: 'option', text: 'Dev' })).not.toBeChecked();
+            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
     });
     describe('filter type: numeric', () => {
-        it('should add / view / clear / remove (without predicate)', () => {
-
+        it('should add / view / clear / remove (without predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithoutPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const ageOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Age' });
+            expect(ageOption).toBeInTheDocument();
+            fireEvent.click(ageOption);
+            fireEvent.change(withinDialog().getByRole('spinbutton'), { target: { value: 20 }});
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { age: 20 } }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: 'Age:20' }));
+            expectDialog();
+            const input = withinDialog().getByRole('spinbutton') as HTMLInputElement;
+            expect(input.placeholder).toEqual('20');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR' }));
+            expect(input.placeholder).toEqual('Enter a number');
+            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
-        it('should add / view / clear / remove (with numeric predicate)', () => {
-
+        it('should add / view / clear / remove (with numeric predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const ageOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Age' });
+            expect(ageOption).toBeInTheDocument();
+            fireEvent.click(ageOption);
+            expect(withinDialog().queryAllByRole('tab').map(t => t.textContent)).toEqual(['=', '≠', '≤', '≥', 'In Range']);
+            fireEvent.change(withinDialog().getByRole('spinbutton'), { target: { value: 20 }});
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'tab', text: '≠' }));
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { age: { neq: 20 } } }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: 'Age ≠20' }));
+            expectDialog();
+            const input = withinDialog().getByRole('spinbutton') as HTMLInputElement;
+            expect(input.placeholder).toEqual('20');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR' }));
+            expect(input.placeholder).toEqual('Enter a number');
+            const removeButton = withinDialog().queryByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
     });
     describe('filter type: rangeDatePicker', () => {
-        it('should add / view / clear / remove (without predicate)', () => {
-
+        it('should add / view / clear / remove (without predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithoutPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const hireDateOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Hire Date' });
+            expect(hireDateOption).toBeInTheDocument();
+            fireEvent.click(hireDateOption);
+            fireEvent.click(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0]);
+            fireEvent.click(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0]);
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { hireDate: { from: TODAY_DATE_ISO, to: TODAY_DATE_ISO }} }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: `Hire Date:${TODAY_DATE_FORMATTED} - ${TODAY_DATE_FORMATTED}` }));
+            expectDialog();
+            expect(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0].parentElement).toHaveClass('uui-calendar-selected-day');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR ALL' }));
+            expect(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0].parentElement).not.toHaveClass('uui-calendar-selected-day');
+            const removeButton = withinDialog().getByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
-        it('should add / view / clear / remove (with rangeDatePicker predicate)', () => {
-
+        it('should add / view / clear / remove (with rangeDatePicker predicate)', async () => {
+            const { dom, mocks } = await setupFilterPanelComponent({
+                filtersConfig: filtersConfigWithPredicatesAll,
+            });
+            fireEvent.click(dom.add);
+            expectDialog();
+            const hireDateOption = withinDialog().queryByRoleAndText({ role: 'option', text: 'Hire Date' });
+            expect(hireDateOption).toBeInTheDocument();
+            fireEvent.click(hireDateOption);
+            expect(withinDialog().queryAllByRole('tab').map(t => t.textContent)).toEqual(['In Range', 'Not in Range']);
+            fireEvent.click(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0]);
+            fireEvent.click(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0]);
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'tab', text: 'Not in Range' }));
+            fireEvent.click(window.document.body);
+            notExpectDialog();
+            expect(mocks.onSetTableState).lastCalledWith(expect.objectContaining({ filter: { hireDate: { notInRange: { from: TODAY_DATE_ISO, to: TODAY_DATE_ISO }}} }));
+            fireEvent.click(screen.getByRoleAndText({ role: 'button', text: `Hire Date Not in Range${TODAY_DATE_FORMATTED} - ${TODAY_DATE_FORMATTED}` }));
+            expectDialog();
+            expect(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0].parentElement).toHaveClass('uui-calendar-selected-day');
+            fireEvent.click(withinDialog().getByRoleAndText({ role: 'button', text: 'CLEAR ALL' }));
+            expect(withinDialog().getAllByText(TODAY_DAY_OF_MONTH)[0].parentElement).not.toHaveClass('uui-calendar-selected-day');
+            const removeButton = withinDialog().getByRoleAndText({ role: 'button', text: 'REMOVE FILTER' });
+            fireEvent.click(removeButton);
+            notExpectDialog();
+            expect(screen.queryByRoleAndText({ role: 'button', text: 'Add filter' })).toBeInTheDocument();
         });
     });
 });
