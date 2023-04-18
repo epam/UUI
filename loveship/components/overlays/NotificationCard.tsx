3932ed296e297d5f3e2c9f2a1277e3279eb93604
IhorKorenets <vik753@gmail.com>
2023-04-18 16:49:58 +0300

    refactored default color name for NotificationCard

--- loveship/components/overlays/NotificationCard.tsx
+++ loveship/components/overlays/NotificationCard.tsx
@@ -14,6 +14,6 @@ export const NotificationCard = withMods<Omit<uuiNotificationCardProps, 'color'>
     () => [],
     (props) => ({
         ...props,
-        color: props.color ?? null,
+        color: props.color ?? 'default',
     }),
 );
