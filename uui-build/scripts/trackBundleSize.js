4a50c623fc01ffabc79eac2d95a1c64a6eff33b5
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-04-19 12:25:57 +0300

    this is just for testing, don't merge!

--- uui-build/scripts/trackBundleSize.js
+++ uui-build/scripts/trackBundleSize.js
@@ -1,6 +1,13 @@
 const { trackBundleSize } = require('../utils/bundleStatsUtils/trackBundleSize.js');
 const { hasCliArg } = require('../utils/cmdUtils.js');
+const path = require("path");
+const {uuiRoot} = require("../utils/constants");
+const {TRACK_BUNDLE_SIZE_REPORT_MD} = require("../utils/bundleStatsUtils/bundleStatsConstants");
+const {createFileSync} = require("../utils/fileUtils");
 
-const overrideBaseline = hasCliArg('--override-baseline');
+//const overrideBaseline = hasCliArg('--override-baseline');
 
-trackBundleSize({ overrideBaseline });
+//trackBundleSize({ overrideBaseline });
+
+const crPathResolved = path.resolve(uuiRoot, TRACK_BUNDLE_SIZE_REPORT_MD);
+createFileSync(crPathResolved, '# *** test md content! ***');
