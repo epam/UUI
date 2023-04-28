9f4e45d3401c68fd7ee67eae05269d69a4bdb2ce
Aleksey Manetov <alekseymanetov@gmail.com>
2023-04-28 14:12:19 +0200

    change picker modal example

--- app/src/docs/_examples/pickerModal/BasicPickerModal.example.tsx
+++ app/src/docs/_examples/pickerModal/BasicPickerModal.example.tsx
@@ -1,67 +1,40 @@
-import React, { useCallback, useContext, useState } from 'react';
-import { DataPickerRow, PickerItem, PickerModal } from '@epam/uui';
+import React, { useCallback, useContext, useState, useEffect } from 'react';
+import { PickerModal } from '@epam/uui';
 import { FlexRow, FlexCell, Button } from '@epam/promo';
-import { DataRowProps, UuiContext, useArrayDataSource } from '@epam/uui-core';
-
-interface Language {
-    id: number;
-    level: string;
-}
-
-const languageLevels: Language[] = [
-    { id: 2, level: 'A1' },
-    { id: 3, level: 'A1+' },
-    { id: 4, level: 'A2' },
-    { id: 5, level: 'A2+' },
-    { id: 6, level: 'B1' },
-    { id: 7, level: 'B1+' },
-    { id: 8, level: 'B2' },
-    { id: 9, level: 'B2+' },
-    { id: 10, level: 'C1' },
-    { id: 11, level: 'C1+' },
-    { id: 12, level: 'C2' },
-];
+import { UuiContext, useArrayDataSource } from '@epam/uui-core';
+import { svc } from '../../../services';
+import { Location } from '@epam/uui-docs';
 
 export default function LanguagesPickerModal() {
-    const [value, onValueChange] = useState<number>(8);
+    const [value, onValueChange] = useState(['1123004', '1255076', '4094163']);
     const context = useContext(UuiContext);
+    const [items, setItems] = useState<Location[]>([]);
     // Create DataSource outside the Picker, by calling useArrayDataSource hook
-    const dataSource = useArrayDataSource<Language, number, any>(
+
+    useEffect(() => {
+        svc.api.demo.locations({})
+            .then((res) => setItems(res.items));
+    });
+
+    const dataSource = useArrayDataSource<Location, string, unknown>(
         {
-            items: languageLevels,
+            items: items,
         },
         [],
     );
 
-    const renderRow = (rowProps: DataRowProps<Language, number>) => (
-        <DataPickerRow
-            { ...rowProps }
-            key={ rowProps.rowKey }
-            borderBottom="none"
-            size="36"
-            padding="24"
-            renderItem={ (item: Language, pickerItemProps: DataRowProps<Language, number>) => (
-                <PickerItem title={ item.level } size="36" { ...pickerItemProps } />
-            ) }
-        />
-    );
-
     const handleModalOpening = useCallback(() => {
-        context.uuiModals
-            .show<number>(
-            (props) => {
-                return (
-                    <PickerModal
-                        initialValue={ value }
-                        dataSource={ dataSource }
-                        selectionMode="single"
-                        valueType="id"
-                        renderRow={ renderRow }
-                        { ...props }
-                    />
-                );
-            },
-        )
+        context.uuiModals.show<string[]>((props) => {
+            return (
+                <PickerModal
+                    initialValue={ value }
+                    dataSource={ dataSource }
+                    selectionMode="multi"
+                    valueType="id"
+                    { ...props }
+                />
+            );
+        })
             .then((newSelection) => {
                 onValueChange(newSelection);
             })
@@ -71,7 +44,7 @@ export default function LanguagesPickerModal() {
     return (
         <FlexCell width={ 612 }>
             <FlexRow spacing="12">
-                <Button color="blue" caption="Show languages" onClick={ handleModalOpening } />
+                <Button color="blue" caption="Show locations" onClick={ handleModalOpening } />
             </FlexRow>
         </FlexCell>
     );
