f84b169b35b85b630c130931564b572f1fc2232f
IhorKorenets <vik753@gmail.com>
2023-05-02 13:40:14 +0300

    Updated color palete according to the design specs, removed file loveship-color-vars.scss, in Slider and RangeSlider deprecated all color instead defaul = sky.

--- loveship/components/inputs/Slider/RangeSlider.tsx
+++ loveship/components/inputs/Slider/RangeSlider.tsx
@@ -1,13 +1,14 @@
 import { RangeSlider as uuiRangeSlider, SliderBaseProps, RangeSliderValue } from '@epam/uui-components';
 import css from './Slider.scss';
-import styles from '../../../assets/styles/scss/loveship-color-vars.scss';
 import { withMods } from '@epam/uui-core';
 import * as types from '../../types';
 
 export interface RangeSliderMods extends types.ColorMod {}
 
-export function applyRangeSliderMods(mods: RangeSliderMods) {
-    return [css.root, styles['color-' + (mods.color || 'sky')]];
+export function applyRangeSliderMods() {
+    return [
+        css.root,
+    ];
 }
 
 export const RangeSlider = withMods<SliderBaseProps<RangeSliderValue>, RangeSliderMods>(uuiRangeSlider, applyRangeSliderMods);
