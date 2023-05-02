569bc169f18c2a287ae540442c1f7b9073d7cba8
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-05-02 11:20:40 +0300

    add permissions to write comments

--- uui-build/scripts/trackBundleSize.js
+++ uui-build/scripts/trackBundleSize.js
@@ -1,6 +1,7 @@
 const { trackBundleSize } = require('../utils/trackBundleSize/trackBundleSize.js');
 const { hasCliArg } = require('../utils/cmdUtils.js');
+const {saveComparisonResultsMd} = require('../utils/trackBundleSize/trackBundleSizeFileUtils');
 
 const overrideBaseline = hasCliArg('--override-baseline');
 
-trackBundleSize({ overrideBaseline });
+saveComparisonResultsMd('Test!');
