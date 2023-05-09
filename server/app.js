671fe4ffde7a72d1ae1e29cce0d4ae92655e5132
Aleksey Manetov <alekseymanetov@gmail.com>
2023-05-09 17:22:27 +0200

    Added Content-Security-Policy header(moved from develop branch)

--- server/app.js
+++ server/app.js
@@ -25,6 +25,17 @@ app.use((req, res, next) => {
     res.set('Strict-Transport-Security', 'max-age=63072000; includeSubDomains');
     res.set('x-frame-options', 'SAMEORIGIN');
     res.set('X-Content-Type-Options', 'nosniff');
+    res.set(
+        'Content-Security-Policy',
+        "default-src 'self' https://*.epam.com;"
+        + "style-src 'self' 'unsafe-inline' https://*.epam.com https://cdnjs.cloudflare.com/; "
+        + "font-src 'self' https://*.epam.com https://fonts.gstatic.com/; "
+        + "connect-src 'self' https://*.epam.com https://api.amplitude.com/ https://www.google-analytics.com/ wss://menu.epam.com/'; "
+        + 'frame-src *; '
+        + 'img-src * data: ; '
+        + `script-src 'self' ${isDevServer() ? "'unsafe-eval' 'unsafe-inline'" : ''} https://*.epam.com https://www.googletagmanager.com/ https://www.google-analytics.com/;`,
+    );
+
     next();
 });
 
