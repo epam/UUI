3195f785df85e461630e33516929ab316112f080
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-05-09 17:25:01 +0300

    Added datasource state beginning.

--- app/src/docs/advanced/datasources/details/Overview.doc.tsx
+++ app/src/docs/advanced/datasources/details/Overview.doc.tsx
@@ -8,10 +8,16 @@ export class DatasourcesOverviewDoc extends BaseDocsBlock {
         return (
             <>
                 <EditableDocContent fileName="datasources-overview" />
+
                 <DocExample title="BaseDatasourceProps" path="./_examples/datasources/BaseDatasourceProps.code.example.ts" onlyCode={ true } />
                 <EditableDocContent fileName="datasources-base-props-overview" />
+
                 <DocExample title="DataRowOptions" path="./_examples/datasources/DataRowOptions.code.example.ts" onlyCode={ true } />
                 <EditableDocContent fileName="datasources-row-options-overview" />
+
+                <DocExample title="DataSourceState" path="./_examples/datasources/DataSourceState.code.example.ts" onlyCode={ true } />
+                <EditableDocContent fileName="datasources-source-state-overview" />
+
             </>
         );
     }
