9d4de99b9c8300591b81c605b67bf13124840edb
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-05-10 17:58:41 +0300

    Added checkbox property usage example.

--- app/src/docs/advanced/datasources/details/RowOptions.doc.tsx
+++ app/src/docs/advanced/datasources/details/RowOptions.doc.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { BaseDocsBlock, EditableDocContent } from '../../../../common';
+import { BaseDocsBlock, DocExample, EditableDocContent } from '../../../../common';
 
 export class DatasourcesRowOptionsDoc extends BaseDocsBlock {
     title = 'Row Options';
 
     renderContent() {
         return (
-            <EditableDocContent fileName="datasources-row-options" />
+            <>
+                <EditableDocContent fileName="datasources-row-options" />
+                <DocExample title="Checkbox" path="./_examples/datasources/RowOptionsCheckbox.example.tsx" />
+            </>
         );
     }
 }
