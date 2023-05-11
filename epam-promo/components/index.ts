92156d82114a26929bcce052f9bc743919ff74bf
IhorKorenets <vik753@gmail.com>
2023-05-11 14:01:33 +0300

    made reexport for the DataPickerBody, MainMenuLogo, MainMenuCustomElement, PickerModal through the skins.

--- epam-promo/components/index.ts
+++ epam-promo/components/index.ts
@@ -13,5 +13,6 @@ export { FiltersPanel, PresetsPanel, defaultPredicates, TabButton, VerticalTabBu
     IconContainer, Accordion, RichTextView, Form, useForm, PickerInput, PickerItem, DataPickerRow, PickerToggler, PickerList, Blocker, CheckboxGroup, ControlGroup,
     ConfirmationModal, RadioGroup, Anchor, Avatar, AvatarStack, Paginator, DataTable, DataTableCell, DataTableRow, DataTableHeaderRow, ColumnsConfigurationModal,
     WarningNotification, SuccessNotification, HintNotification, ErrorNotification, ClearNotification, DatePicker, RangeDatePicker, Checkbox, FlexSpacer, FlexCell,
-    Spinner, LinkButton,
+    Spinner, LinkButton, DataPickerBody, PickerModal,
 } from '@epam/uui';
+export { MainMenuLogo, MainMenuCustomElement } from '@epam/uui-components';
