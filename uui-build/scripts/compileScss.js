b5893d245d21b6c7b99c92469aada1b27a615064
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-05-12 10:14:34 +0300

    initial script

--- uui-build/scripts/compileScss.js
+++ uui-build/scripts/compileScss.js
@@ -4,6 +4,7 @@ const postcss = require('postcss');
 const scssParser = require('postcss-scss');
 const postcssSass = require('@csstools/postcss-sass');
 const scssModules = require('postcss-modules');
+const { createFileSync, iterateFilesInDirAsync } = require('../utils/fileUtils.js');
 
 function getFilesToCompile() {
     return process.argv.splice(2).filter((pair) => pair.indexOf(':') !== -1).map((pair) => pair.split(':'));
@@ -11,13 +12,14 @@ function getFilesToCompile() {
 
 function assertNoLocalScopeSelectors({ srcPath, targetPath, variablesJson }) {
     if (Object.keys(variablesJson).length > 0) {
-        const err = [
+        /*        const err = [
             'Local scope selectors aren\'t allowed.',
             `srcPath=${srcPath}`,
             `targetPath=${targetPath}`,
             JSON.stringify(variablesJson, undefined, 1),
-        ].join('\n');
-        throw new Error(err);
+        ].join('\n'); */
+        console.error(`Local scope selectors found in result css! ${targetPath} ${JSON.stringify(variablesJson)}`);
+        // throw new Error(err);
     }
 }
 
@@ -35,33 +37,37 @@ function getCompiler() {
 async function compileSingleFile(from, to) {
     const src = await fs.promises.readFile(from, 'utf8');
     const compiler = getCompiler();
-    const result = await compiler.process(src, { map: true, to, from, syntax: scssParser });
-    // check target dir exists
-    const dir = path.dirname(to);
-    if (fs.existsSync(dir)) {
-        fs.existsSync(to) && fs.rmSync(to);
-        fs.existsSync(`${to}.map`) && fs.rmSync(`${to}.map`);
+    const result = await compiler.process(src, { map: { inline: false }, to, from, syntax: scssParser });
+    //
+    const isEmptyCss = result.map._sources.size() === 0;
+    if (isEmptyCss) {
+        console.error(`The result css is empty! ${to}`);
     } else {
-        fs.mkdirSync(dir, { recursive: true });
+        createFileSync(to, result.css);
+        createFileSync(`${to}.map`, result.map.toString());
     }
-    await fs.promises.writeFile(to, result.css, 'utf8');
-    await fs.promises.writeFile(`${to}.map`, result.map.toString(), 'utf8');
 }
 
 async function main() {
     const files = getFilesToCompile();
-    const inProgress = files.reduce((acc, [from, to]) => {
+    const inProgress = [];
+    for (let i = 0; i < files.length; i++) {
+        const [from, to] = files[i];
         if (fs.existsSync(from)) {
             if (fs.lstatSync(from).isFile()) {
                 // file
                 const promise = compileSingleFile(path.resolve(from), path.resolve(to));
-                acc.push(promise);
+                inProgress.push(promise);
             } else {
-                throw new Error(`Unable to find source file ${from}`);
+                await iterateFilesInDirAsync(from, (filePath) => {
+                    const compileToFile = path.resolve(to, path.relative(from, filePath).replace('.scss', '.css'));
+                    const promise = compileSingleFile(path.resolve(filePath), compileToFile);
+                    inProgress.push(promise);
+                }, { recursive: true });
             }
         }
-        return acc;
-    }, []);
+    }
+
     await Promise.all(inProgress);
 }
 
