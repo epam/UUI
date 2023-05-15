50f2a3faac2b3d8a1ea58465aeeb50438483825a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-05-15 18:10:47 +0300

    Added data docs for LazyDatasource.

---
+++ app/src/docs/_examples/datasources/LazyDatasourceData.example.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DataQueryFilter, DataSourceState, useLazyDataSource, useUuiContext } from '@epam/uui-core';
+import { DatasourceViewer } from './DatasourceViewer';
+import { TApi } from '../../../data';
+import { City } from '@epam/uui-docs';
+
+export default function LazyDatasourceDataExample() {
+    const svc = useUuiContext<TApi>();
+
+    const [value, onValueChange] = useState<DataSourceState>({});
+    const datasource = useLazyDataSource<City, string, DataQueryFilter<City>>({
+        api: (req) => svc.api.demo.cities(req),
+    }, []);
+    
+    return (
+        <DatasourceViewer
+            value={ value }
+            onValueChange={ onValueChange }
+            datasource={ datasource }
+        />
+    );
+}
--- app/src/docs/advanced/datasources/details/LazyDatasource.doc.tsx
+++ app/src/docs/advanced/datasources/details/LazyDatasource.doc.tsx
@@ -1,12 +1,15 @@
 import * as React from 'react';
-import { BaseDocsBlock, EditableDocContent } from '../../../../common';
+import { BaseDocsBlock, DocExample, EditableDocContent } from '../../../../common';
 
 export class DatasourcesLazyDatasourceDoc extends BaseDocsBlock {
     title = 'LazyDatasource';
 
     renderContent() {
         return (
-            <EditableDocContent fileName="datasources-lazy-datasource" />
+            <>
+                <EditableDocContent fileName="datasources-lazy-datasource" />
+                <DocExample title="Data" path="./_examples/datasources/LazyDatasourceData.example.tsx" />
+            </>
         );
     }
 }
