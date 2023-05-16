800f0a038abd0a3b6db28de76fb512c3407e4413
IhorKorenets <vik753@gmail.com>
2023-05-16 21:37:51 +0300

    refactored ModalWindow in skins

--- loveship/components/overlays/ModalWindow.tsx
+++ loveship/components/overlays/ModalWindow.tsx
@@ -1,24 +1,15 @@
 import { withMods } from '@epam/uui-core';
 import { ModalWindow as UuiModalWindow, ModalWindowProps as UuiModalWindowProps } from '@epam/uui';
 
-const additionalModalWindowWidth = ['300', '420', '480', '600', '900'] as const;
-const additionalModalWindowHeight = ['300', '700', 'auto'] as const;
-
 export interface ModalWindowMods {
-    width?: typeof additionalModalWindowWidth[number] | number;
-    height?: typeof additionalModalWindowHeight[number] | number;
+    width?: '300' | '420' | '480' | '600' | '900' | number;
+    height?: '300' | '700' | 'auto' | number;
 }
-
-const getWidth = (width: ModalWindowMods['width']): number | undefined => {
-    if (typeof width === 'string') {
-        return additionalModalWindowWidth.includes(width) ? Number(width) : undefined;
-    } else return Number(width);
-};
 
 const getHeight = (height: ModalWindowMods['height']): number | undefined => {
     if (typeof height === 'string') {
-        return additionalModalWindowHeight.includes(height) && height !== 'auto' ? Number(height) : undefined;
-    } else return Number(height);
+        return height === 'auto' ? undefined : Number(height);
+    } else return height;
 };
 
 export const ModalWindow = withMods<Omit<UuiModalWindowProps, 'width' | 'height'>, ModalWindowMods>(
@@ -26,7 +17,7 @@ export const ModalWindow = withMods<Omit<UuiModalWindowProps, 'width' | 'height'
     () => [],
     (props) => ({
         ...props,
-        width: props.width && getWidth(props.width),
+        width: props.width && typeof props.width === 'string' ? Number(props.width) : props.width,
         height: props.height && getHeight(props.height),
     }),
 );
