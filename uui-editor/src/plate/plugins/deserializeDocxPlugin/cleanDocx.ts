e91539361299a3b4c4f9a1cada49017979c2c916
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-05-22 12:16:31 +0300

    [SlateEditor] fix build and empty cells copying from docx

---
+++ uui-editor/src/plate/plugins/deserializeDocxPlugin/cleanDocx.ts
@@ -0,0 +1,48 @@
+import {
+    cleanDocxBrComments,
+    cleanDocxEmptyParagraphs,
+    cleanDocxFootnotes,
+    cleanDocxImageElements,
+    cleanDocxListElements,
+    cleanDocxQuotes,
+    cleanDocxSpans,
+    isDocxContent,
+} from "@udecode/plate";
+import {
+    cleanHtmlBrElements,
+    cleanHtmlFontElements,
+    cleanHtmlLinkElements,
+    cleanHtmlTextNodes,
+    copyBlockMarksToSpanChild,
+    postCleanHtml,
+    preCleanHtml,
+} from "@udecode/plate-common";
+import { cleanHtmlEmptyElements } from "./cleanHtmlEmptyElements";
+
+export const cleanDocx = (html: string, rtf: string): string => {
+    const document = new DOMParser().parseFromString(
+        preCleanHtml(html),
+        "text/html"
+    );
+    const { body } = document;
+
+    if (!rtf && !isDocxContent(body)) {
+        return html;
+    }
+
+    cleanDocxFootnotes(body);
+    cleanDocxImageElements(document, rtf, body);
+    cleanHtmlEmptyElements(body);
+    cleanDocxEmptyParagraphs(body);
+    cleanDocxQuotes(body);
+    cleanDocxSpans(body);
+    cleanHtmlTextNodes(body);
+    cleanDocxBrComments(body);
+    cleanHtmlBrElements(body);
+    cleanHtmlLinkElements(body);
+    cleanHtmlFontElements(body);
+    cleanDocxListElements(body);
+    copyBlockMarksToSpanChild(body);
+
+    return postCleanHtml(body.innerHTML);
+};
