53f2a802f5c9475b5ce8ed92443734351866fb4d
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-05-23 18:38:36 +0300

    fix formatting

--- templates/uui-vite-template/vite.config.js
+++ templates/uui-vite-template/vite.config.js
@@ -1,12 +1,9 @@
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
-import svgr from "vite-plugin-svgr";
+import svgr from 'vite-plugin-svgr';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [
-    svgr(),
-    react()
-  ],
-  optimizeDeps: {}
+    plugins: [svgr(), react()],
+    optimizeDeps: {},
 });
