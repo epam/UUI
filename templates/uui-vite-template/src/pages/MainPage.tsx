e9c36134bc64009720624f719dc0df08a4aa2f2a
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-05-23 17:56:57 +0300

    Create template app for Vite.

---
+++ templates/uui-vite-template/src/pages/MainPage.tsx
@@ -0,0 +1,30 @@
+import { Panel, RichTextView, IconContainer } from '@epam/promo';
+import { ReactComponent as UuiPromoImage } from '../icons/uui-promo-image.svg';
+import css from './MainPage.module.scss';
+
+export const MainPage = () => {
+    return (
+        <main>
+            <div className={css.bgImg}>
+                <div>
+                    <IconContainer icon={UuiPromoImage} />
+                </div>
+            </div>
+            <Panel cx={css.mainPanel} background="white">
+                <RichTextView size="14">
+                    <h3>Welcome to UUI template app</h3>
+                    <p>
+                        UUI docs:{' '}
+                        <a href="https://uui.epam.com/">uui.epam.com</a>
+                    </p>
+                    <p>
+                        Git:{' '}
+                        <a href="https://github.com/epam/uui">
+                            github.com/epam/uui
+                        </a>
+                    </p>
+                </RichTextView>
+            </Panel>
+        </main>
+    );
+};
