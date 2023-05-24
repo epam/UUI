dd8937c309e37cd6138e4169b398ef7523688c25
IhorKorenets <vik753@gmail.com>
2023-05-24 10:51:45 +0300

    Tooltip: fixed ternary expression and set default color, IconContainer: returned property color in both skins and added warning about it's deprecation

--- epam-promo/components/index.ts
+++ epam-promo/components/index.ts
@@ -10,7 +10,7 @@ export * from './fileUpload';
 export * from './errors';
 export { FiltersPanel, PresetsPanel, defaultPredicates, TabButton, VerticalTabButton, Calendar, NumericInput, Switch, TextArea, Dropdown, TextPlaceholder,
     Tag, RadioInput, MainMenu, GlobalMenu, Burger, BurgerButton, BurgerSearch, MainMenuAvatar, MainMenuSearch, MainMenuIcon, MainMenuDropdown, MainMenuButton,
-    IconContainer, Accordion, RichTextView, Form, useForm, PickerInput, PickerItem, DataPickerRow, PickerToggler, PickerList, Blocker, CheckboxGroup, ControlGroup,
+    Accordion, RichTextView, Form, useForm, PickerInput, PickerItem, DataPickerRow, PickerToggler, PickerList, Blocker, CheckboxGroup, ControlGroup,
     ConfirmationModal, RadioGroup, Anchor, Avatar, AvatarStack, Paginator, DataTable, DataTableCell, DataTableRow, DataTableHeaderRow, ColumnsConfigurationModal,
     WarningNotification, SuccessNotification, HintNotification, ErrorNotification, ClearNotification, DatePicker, RangeDatePicker, Checkbox, FlexSpacer, FlexCell,
     Spinner, LinkButton, DataPickerBody, PickerModal, ModalBlocker, ModalHeader, ModalFooter,
