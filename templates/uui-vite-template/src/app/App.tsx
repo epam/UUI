5d842deeb922eea2a5553c6fb29ac9e77270b8f1
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-05-25 10:38:51 +0300

    adjust

--- templates/uui-vite-template/src/app/App.tsx
+++ templates/uui-vite-template/src/app/App.tsx
@@ -2,13 +2,35 @@ import { MainPage } from '../pages/MainPage';
 import { Route } from 'react-router-dom';
 import { AppHeader } from './AppHeader';
 import css from './App.module.scss';
+import { createBrowserHistory } from 'history';
+import { ContextProvider } from '@epam/uui-core';
+import { ErrorHandler } from '@epam/promo';
+import { Modals, Snackbar } from '@epam/uui-components';
+import { Router } from 'react-router-dom';
+import { TAppContext, loadAppContext } from './helpers/appContext';
+import { TApi, apiDefinition } from './helpers/apiDefinition';
 
-export const App = () => {
+const history = createBrowserHistory();
+
+export function App() {
     return (
-        <div className={css.app}>
-            <Route component={AppHeader} />
-            <Route path="/" component={MainPage} />
-            <footer></footer>
-        </div>
+        <Router history={history}>
+            <ContextProvider<TApi, TAppContext>
+                onInitCompleted={() => {}}
+                history={history}
+                apiDefinition={apiDefinition}
+                loadAppContext={loadAppContext}
+            >
+                <ErrorHandler>
+                    <div className={css.app}>
+                        <Route component={AppHeader} />
+                        <Route path="/" component={MainPage} />
+                        <footer></footer>
+                    </div>
+                    <Snackbar />
+                    <Modals />
+                </ErrorHandler>
+            </ContextProvider>
+        </Router>
     );
-};
+}
