33790955ac5b56fe70b5cf2a1f151729fb669b74
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-05-30 13:25:20 +0300

    [SlateEditor] add todo

--- uui-editor/src/plate/plugins/linkPlugin/isUrl.ts
+++ uui-editor/src/plate/plugins/linkPlugin/isUrl.ts
@@ -1,24 +1,16 @@
-/**
- * RegExps.
- * A URL must match #1 and then at least one of #2/#3.
- * Use two levels of REs to avoid REDOS.
- */
-
 const protocolAndDomainRE = /^(?:\w+:)?\/\/(\S+)$/;
 const emailLintRE = /mailto:([^\?]*)/;
 
 const localhostDomainRE = /^localhost[:?\d]*(?:[^:?\d]\S*)?$/;
 const nonLocalhostDomainRE = /^[^\s.]+\.\S{2,}$/;
 
-/**
- * Loosely validate a URL `string`.
- */
 export const isUrl = (string: any) => {
     if (typeof string !== "string") {
         return false;
     }
 
     const generalMatch = string.match(protocolAndDomainRE);
+    // TODO: move to plate
     const matchEmailLink = string.match(emailLintRE);
     const match = generalMatch || matchEmailLink;
     if (!match) {
