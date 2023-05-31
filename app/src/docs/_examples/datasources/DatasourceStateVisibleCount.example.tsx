b8c1af4b031dd37b67dc18e555c4cd08d5a49dc8
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-05-31 13:27:37 +0300

    Added fixes of datasources docs.

--- app/src/docs/_examples/datasources/DatasourceStateVisibleCount.example.tsx
+++ app/src/docs/_examples/datasources/DatasourceStateVisibleCount.example.tsx
@@ -5,17 +5,14 @@ import { DatasourceViewer } from './DatasourceViewer';
 const items = Array(100).fill(0).map((_, index) => ({ id: index, name: `Parent ${index}` }));
 
 export default function DatasourceStateVisibleCountExample() {
-    const [value1, onValueChange1] = useState<DataSourceState>({
-        topIndex: 10,
-        visibleCount: 10,
-    });
+    const [value1, onValueChange1] = useState<DataSourceState>({});
     const datasource1 = useArrayDataSource({
         items,
     }, []);
     
     return (
         <DatasourceViewer
-            exampleTitle="Visible count"
+            exampleTitle={ `visibleCount: ${value1.visibleCount}, topIndex: ${value1.topIndex}` }
             value={ value1 }
             onValueChange={ onValueChange1 }
             datasource={ datasource1 }
