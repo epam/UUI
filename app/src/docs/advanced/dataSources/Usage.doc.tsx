a2305697b8fe3cba8ffffa6048e0ad131198c34c
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-06-01 12:33:01 +0300

    Fixed imgs.

--- app/src/docs/advanced/dataSources/Usage.doc.tsx
+++ app/src/docs/advanced/dataSources/Usage.doc.tsx
@@ -9,7 +9,7 @@ export class DataSourcesUsageDoc extends BaseDocsBlock {
             <>
                 <EditableDocContent fileName="dataSources-usage-in-components" />
 
-                <DocExample title="DataSourceViewer" path="./_examples/dataSources/DataSourceViewer.tsx" onlyCode={ true } />
+                <DocExample title="DataSourceViewer" path="./_examples/dataSources/DataSourceViewer.code.example.tsx" onlyCode={ true } />
             </>
         );
     }
