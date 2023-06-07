ebcfcddd1f117d4c60f5de8dc21b8f197761f839
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-06-07 13:12:30 +0300

    Some refactoring .

--- uui/components/pickers/PickerListItem.tsx
+++ uui/components/pickers/PickerListItem.tsx
@@ -8,39 +8,44 @@ export interface PickerListItemProps<TItem, TId> extends DataRowProps<TItem, TId
     getName(item: TItem): string;
 }
 
-export class PickerListItem<TItem, TId> extends React.Component<PickerListItemProps<TItem, TId>> {
-    render() {
-        let label: any;
+export function PickerListItem<TItem, TId>(props: PickerListItemProps<TItem, TId>) {
+    let label: any;
 
-        if (this.props.isLoading) {
-            label = <TextPlaceholder wordsCount={ 2 } />;
-        } else {
-            label = this.props.getName(this.props.value);
-        }
-
-        let component: any;
-
-        if (this.props.checkbox) {
-            component = (
-                <Checkbox
-                    { ...this.props.checkbox }
-                    isDisabled={ this.props.isLoading || this.props.checkbox.isDisabled || this.props.isDisabled }
-                    label={ label }
-                    value={ this.props.isChecked }
-                    onValueChange={ () => this.props.onCheck(this.props) }
-                />
-            );
-        } else {
-            component = (
-                <RadioInput
-                    label={ label }
-                    value={ this.props.isSelected }
-                    isDisabled={ this.props.isLoading || !this.props.isSelectable || this.props.isDisabled }
-                    onValueChange={ () => this.props.onSelect(this.props) }
-                />
-            );
-        }
-
-        return <div className={ css.row }>{component}</div>;
+    if (props.isLoading) {
+        label = <TextPlaceholder wordsCount={ 2 } />;
+    } else {
+        label = props.getName(props.value);
     }
+
+    let component: any;
+
+    if (props.checkbox) {
+        component = (
+            <Checkbox
+                { ...props.checkbox }
+                isDisabled={ props.isLoading || props.checkbox.isDisabled || props.isDisabled }
+                label={ label }
+                value={ props.isChecked }
+                onValueChange={ () => props.onCheck(props) }
+            />
+        );
+    } else {
+        component = (
+            <RadioInput
+                label={ label }
+                value={ props.isSelected }
+                isDisabled={ props.isLoading || !props.isSelectable || props.isDisabled }
+                onValueChange={ () => props.onSelect(props) }
+            />
+        );
+    }
+
+    return (
+        <div
+            className={ css.row }
+            data-testid={ `uui-PickerListItem-${props.rowKey}` }
+        >
+            {component}
+        </div>
+    );
 }
