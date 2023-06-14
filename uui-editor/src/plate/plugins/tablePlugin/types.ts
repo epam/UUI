e7dba724a9b236af9b4d7b2c7ec23af04f4c2083
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-06-14 13:55:23 +0300

    [SlateEditor] add some utils

--- uui-editor/src/plate/plugins/tablePlugin/types.ts
+++ uui-editor/src/plate/plugins/tablePlugin/types.ts
@@ -3,6 +3,7 @@ import { TTableCellElement } from "@udecode/plate";
 export type ExtendedTTableCellElement = TTableCellElement & {
     rowSpan?: number;
     colIndex?: number;
+    rowIndex?: number;
     data: {
         colSpan?: number;
         rowSpan?: number;
