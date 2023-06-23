5ecebf78a6593facc48be95c10b8db280fede002
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-06-23 16:02:56 +0300

    [SlateEditor] return our behaviour with links

--- uui-editor/src/plate/plugins/linkPlugin/linkPlugin.tsx
+++ uui-editor/src/plate/plugins/linkPlugin/linkPlugin.tsx
@@ -28,10 +28,43 @@ export interface LinkElement extends TElement {
     type: string;
 }
 
+const withOurLink = <
+    V extends Value = Value,
+    E extends PlateEditor<V> = PlateEditor<V>
+>(
+    editor: E,
+    options: WithPlatePlugin<LinkPlugin, V, E>
+) => {
+    const { insertData } = editor;
+
+    editor = withLink(editor, options);
+
+    editor.insertData = (data: DataTransfer) => {
+        const text = data.getData('text/plain');
+        const textHref = options.options.getUrlHref?.(text);
+
+        // validation is important here. if missed, leads to bugs with insertData plugin
+        // TODO: create issue to have it inside plate ui (pasting links within highlighted text should replace text with pasted url)
+        if (text && validateUrl(editor, text)) {
+            const inserted = upsertLink(editor, {
+                text: textHref || text,
+                url: textHref || text,
+                target: '_blank',
+                insertTextInLink: true,
+            });
+            if (inserted) return;
+        }
+
+        insertData(data);
+    }
+    return editor;
+}
+
 const LINK_ELEMENT = 'link';
 
 export const linkPlugin = () => createLinkPlugin({
     type: LINK_ELEMENT,
+    withOverrides: withOurLink,
     then: () => ({ options: { isUrl, } }),
     overrideByKey: {
         [ELEMENT_LINK]: {
