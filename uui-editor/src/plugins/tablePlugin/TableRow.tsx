adb3adb97d37f8383d9bac4face1380664b398c6
Aleksey Manetov <alekseymanetov@gmail.com>
2023-06-29 12:13:33 +0200

    [RTE]: removed files from old version of RTE. Fix imports

--- uui-editor/src/plugins/tablePlugin/TableRow.tsx
+++ uui-editor/src/plugins/tablePlugin/TableRow.tsx
@@ -1,15 +1,33 @@
-export {};
-// import * as React from 'react';
-// import cx from 'classnames';
-// import css from './Table.scss';
-//
-// export class TableRow extends React.Component<any> {
-//
-//     isHeaderRow = () => {
-//         return this.props.node.nodes.toArray()[0].type === 'table_header_cell';
-//     }
-//     render() {
-//         const { attributes, children } = this.props;
-//         return <tr className={ cx(css.row, this.isHeaderRow() && css.headerRow) } { ...attributes }>{ children }</tr>;
-//     }
-// }
+import * as React from 'react';
+import cx from 'classnames';
+
+import { PlateTableRowElement } from '@udecode/plate';
+
+import css from './Table.module.scss';
+
+const DEFAULT_HEADER_ROW_HEIGHT = 46;
+const DEFAULT_HEIGHT = 50;
+
+export function TableRow(props: any) {
+    const { attributes, children, element } = props;
+
+    const isHeaderRow = () => {
+        return element.children[0]?.type === 'table_header_cell';
+    };
+
+    if (!element.size) {
+        if(isHeaderRow()) {
+            element.size = DEFAULT_HEADER_ROW_HEIGHT;
+        } else {
+            element.size = DEFAULT_HEIGHT;
+        }
+    }
+
+    return <PlateTableRowElement
+        { ...attributes }
+        { ...props }
+        className={ cx(css.row, isHeaderRow() && css.headerRow) }
+    >
+        { children }
+    </PlateTableRowElement>;
+}
