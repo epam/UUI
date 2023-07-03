e028e2c2229a25322ae7c10ba46916f340369b27
IhorKorenets <vik753@gmail.com>
2023-07-03 17:44:49 +0300

    added renderConfigColCaption property in DataTable and refactored that code

---
+++ app/src/sandbox/tableColConfigModal/TableColumnConfigModalTest.tsx
@@ -0,0 +1,102 @@
+import React, { useMemo, useState } from 'react';
+import { Badge, DataTable, EpamAdditionalColor, FlexRow, Panel, Text } from '@epam/promo';
+import { DataColumnProps, useLazyDataSource, useUuiContext, UuiContexts } from '@epam/uui-core';
+import { Person } from '@epam/uui-docs';
+import css from './TableColumnConfigModalStyles.module.scss';
+import { TApi } from '../../data/apiDefinition';
+import { ColumnsConfigurationRowProps } from '@epam/uui-components';
+
+export function TableColumnConfigModalTest() {
+    const svc = useUuiContext<TApi, UuiContexts>();
+
+    const [value, onValueChange] = useState({});
+
+    const dataSource = useLazyDataSource<Person, number, Person>(
+        {
+            api: svc.api.demo.persons,
+        },
+        [],
+    );
+
+    const view = dataSource.useView(value, onValueChange, {
+        getRowOptions: () => ({ checkbox: { isVisible: true } }),
+    });
+
+    const productColumns: DataColumnProps<Person>[] = useMemo(
+        () => [
+            {
+                key: 'name',
+                caption: 'Name',
+                render: (p) => <Text>{p.name}</Text>,
+                width: 200,
+                fix: 'left',
+                isSortable: true,
+                info: 'Person full name',
+            }, {
+                key: 'profileStatus',
+                caption: 'Profile Status',
+                render: (p) =>
+                    p.profileStatus && (
+                        <FlexRow>
+                            <Badge fill="transparent" color={ p.profileStatus.toLowerCase() as EpamAdditionalColor } caption={ p.profileStatus } />
+                        </FlexRow>
+                    ),
+                width: 140,
+                minWidth: 80,
+                isSortable: true,
+                alignSelf: 'center',
+                info: 'Person Status according his work profile',
+            }, {
+                key: 'salary',
+                caption: 'Salary',
+                render: (p) => <Text>{p.salary}</Text>,
+                isSortable: true,
+                width: 100,
+                textAlign: 'right',
+                info: 'Salary sum for the last year',
+            }, {
+                key: 'jobTitle',
+                caption: 'Title',
+                render: (r) => <Text>{r.jobTitle}</Text>,
+                width: 200,
+                grow: 1,
+                isSortable: true,
+                info: 'Job full description',
+            }, {
+                key: 'detailed',
+                render: () => {},
+                width: 54,
+                alignSelf: 'center',
+                fix: 'right',
+                info: 'detailed description',
+            },
+        ],
+        [],
+    );
+
+    return (
+        <Panel shadow cx={ css.container }>
+            <DataTable
+                { ...view.getListProps() }
+                getRows={ view.getVisibleRows }
+                value={ value }
+                onValueChange={ onValueChange }
+                columns={ productColumns }
+                showColumnsConfig={ true }
+                allowColumnsResizing={ true }
+                allowColumnsReordering={ true }
+                renderConfigColCaption={ (c: ColumnsConfigurationRowProps) => (
+                    <FlexRow>
+                        <span>{c.caption}</span>
+                        { c.info && (
+                            <span style={ { fontSize: '10px', marginTop: '2px', marginLeft: '3px', color: 'gray' } }>
+                                {' / CustomRender\': '}
+                                { c.info }
+                            </span>
+                        ) }
+                    </FlexRow>
+                ) }
+            />
+        </Panel>
+    );
+}
