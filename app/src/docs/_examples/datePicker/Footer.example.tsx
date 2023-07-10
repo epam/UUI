b1cce3e3c06096fa5a87b75157aa575a99ad66f4
IhorKorenets <vik753@gmail.com>
2023-07-10 17:18:02 +0300

    made DatePicker footer related to new design in examples and PE, and divided Format & Footer example into two examples

---
+++ app/src/docs/_examples/datePicker/Footer.example.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import { DatePicker, FlexRow, LinkButton } from '@epam/promo';
+import css from './FormatDateExample.module.scss';
+import dayjs from 'dayjs';
+
+export default function DatePickerFormatDateExample() {
+    const [value, onValueChange] = useState('');
+
+    return (
+        <FlexRow>
+            <DatePicker
+                value={ value }
+                onValueChange={ onValueChange }
+                renderFooter={ () => (
+                    <FlexRow cx={ css.footer } background="white" size="48">
+                        <LinkButton size="36" caption="Today" onClick={ () => onValueChange(dayjs().format('YYYY-MM-DD')) } />
+                    </FlexRow>
+                ) }
+            />
+        </FlexRow>
+    );
+}
