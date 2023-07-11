7f61b2e80e79d2547ac7d442d62886997901240d
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-07-11 13:50:59 +0300

    Migrate to react-router v.6

--- app/src/index.tsx
+++ app/src/index.tsx
@@ -1,12 +1,17 @@
 import * as React from 'react';
 import { render } from 'react-dom';
-import { Router } from 'react-router';
-import { createBrowserHistory } from 'history';
+import { RouterProvider, UNSAFE_DataRouterContext as DataRouterContext } from 'react-router';
+import { createBrowserRouter } from 'react-router-dom';
 import {
-    ApiCallOptions, ContextProvider, CommonContexts, UuiContexts,
+    ApiCallOptions,
+    UuiContexts,
+    Router6AdaptedRouter,
+    useUuiServices,
+    DragGhost,
+    UuiContext, GAListener, IProcessRequest,
 } from '@epam/uui-core';
 import { Snackbar, Modals } from '@epam/uui-components';
-import { skinContext as promoSkinContext } from '@epam/promo';
+import { skinContext } from '@epam/promo';
 import { AmplitudeListener } from './analyticsEvents';
 import { svc } from './services';
 import App from './App';
@@ -14,45 +19,65 @@ import { getApi, TApi } from './data';
 import '@epam/internal/styles.css';
 import '@epam/assets/theme/theme_vanilla_thunder.scss';
 import './index.module.scss';
+import { useEffect } from 'react';
 
-const history = createBrowserHistory();
-// __COMMIT_HASH__ & __PACKAGE_VERSION__ will be replaced to a real string by Webpack
+// __COMMIT_HASH__ will be replaced to a real string by Webpack
 (window as any).BUILD_INFO = { hash: __COMMIT_HASH__ };
 
-export class UuiEnhancedApp extends React.Component {
-    onInitCompleted = (context: CommonContexts<TApi, UuiContexts>, ampCode: string) => {
-        Object.assign(svc, context);
-        const listener = new AmplitudeListener(ampCode);
-        context.uuiAnalytics.addListener(listener);
-    };
+const GA_CODE = 'UA-132675234-1';
+const isProduction = /uui.epam.com/.test(window.location.hostname);
+const AMP_CODE = isProduction ? '94e0dbdbd106e5b208a33e72b58a1345' : 'b2260a6d42a038e9f9e3863f67042cc1';
 
-    render() {
-        const isProduction = /uui.epam.com/.test(window.location.hostname);
-        const ampCode = isProduction ? '94e0dbdbd106e5b208a33e72b58a1345' : 'b2260a6d42a038e9f9e3863f67042cc1';
+function apiDefinition(processRequest: IProcessRequest) {
+    return getApi(
+        (url: string, method: string, data?: any, options?: ApiCallOptions) => {
+            return processRequest(url, method, data, { fetchOptions: { credentials: undefined }, ...options });
+        },
+    );
+}
 
+function UuiEnhancedApp() {
+    const [isLoaded, setIsLoaded] = React.useState(false);
+    const reactRouter6Context = React.useContext(DataRouterContext);
+    const router = React.useMemo(() => new Router6AdaptedRouter(reactRouter6Context.router), [reactRouter6Context.router]);
+    //
+    const { services } = useUuiServices<TApi, UuiContexts>({
+        apiDefinition,
+        router,
+        skinContext,
+    });
+
+    useEffect(() => {
+        services.uuiAnalytics.addListener(new GAListener(GA_CODE));
+        services.uuiAnalytics.addListener(new AmplitudeListener(AMP_CODE));
+        Object.assign(svc, services);
+        setIsLoaded(true);
+    }, [services]);
+
+    if (isLoaded) {
         return (
-            <ContextProvider<TApi, UuiContexts>
-                apiDefinition={ (processRequest) =>
-                    getApi((url: string, method: string, data?: any, options?: ApiCallOptions) =>
-                        processRequest(url, method, data, { fetchOptions: { credentials: undefined }, ...options })) }
-                onInitCompleted={ (context) => this.onInitCompleted(context, ampCode) }
-                history={ history }
-                gaCode="UA-132675234-1"
-                skinContext={ promoSkinContext }
-            >
+            <UuiContext.Provider value={ services }>
                 <App />
                 <Snackbar />
                 <Modals />
-            </ContextProvider>
+                <DragGhost />
+            </UuiContext.Provider>
         );
     }
+    return null;
 }
 
-render(
-    <React.StrictMode>
-        <Router history={ history }>
-            <UuiEnhancedApp />
-        </Router>
-    </React.StrictMode>,
-    document.getElementById('root'),
-);
+function initApp() {
+    const router = createBrowserRouter([
+        { path: '*', element: <UuiEnhancedApp /> },
+    ]);
+
+    render(
+        <React.StrictMode>
+            <RouterProvider router={ router } />
+        </React.StrictMode>,
+        document.getElementById('root'),
+    );
+}
+
+initApp();
--- uui-core/src/hooks/useUuiServices.ts
+++ uui-core/src/hooks/useUuiServices.ts
@@ -12,7 +12,7 @@ import {
 } from '../services';
 import { ApiCallOptions, CommonContexts, IRouterContext } from '../types';
 import { UserSettingsContext } from '../services/UserSettingsContext';
-import { useMemo } from 'react';
+import { useEffect, useMemo } from 'react';
 
 export interface IUseUuiServicesProps<TApi, TAppContext> extends IUuiServicesProps<TApi> {
     appContext?: TAppContext;
@@ -24,39 +24,44 @@ export const useUuiServices = <TApi, TAppContext>(props: IUseUuiServicesProps<TA
         router, appContext, skinContext, apiDefinition,
     } = props;
 
-    const uuiLayout = new LayoutContext();
-    const uuiModals = new ModalContext(uuiLayout);
-    const uuiNotifications = new NotificationContext(uuiLayout);
-    const uuiAnalytics = new AnalyticsContext({ router });
-    const uuiLocks = new LockContext(router);
-    const uuiErrors = new ErrorContext(uuiAnalytics, uuiModals);
-    const uuiApi = new ApiContext(props, uuiAnalytics);
-
-    const rawApi = apiDefinition ? apiDefinition(uuiApi.processRequest.bind(uuiApi)) : ({} as TApi);
-    const withOptions = (options: ApiCallOptions) => apiDefinition((url, method, data) => uuiApi.processRequest(url, method, data, options));
-    const api = { ...rawApi, withOptions };
-
-    const uuiUserSettings = new UserSettingsContext();
-    const uuiDnD = new DndContext();
-
     uuiSkin.setSkin(skinContext);
 
+    useEffect(() => {
+        (window as any).UUI_VERSION = __PACKAGE_VERSION__; // it replaced with current uui version during build time
+    }, []);
+
     const services = useMemo<CommonContexts<TApi, TAppContext>>(
-        () => ({
-            uuiAnalytics,
-            uuiErrors,
-            uuiApi,
-            api,
-            uuiLayout,
-            uuiNotifications,
-            uuiModals,
-            uuiUserSettings,
-            uuiDnD,
-            uuiRouter: router,
-            uuiLocks,
-            uuiApp: appContext || ({} as TAppContext),
-            uuiSkin: uuiSkin,
-        }),
+        () => {
+            const uuiLayout = new LayoutContext();
+            const uuiModals = new ModalContext(uuiLayout);
+            const uuiNotifications = new NotificationContext(uuiLayout);
+            const uuiAnalytics = new AnalyticsContext({ router });
+            const uuiLocks = new LockContext(router);
+            const uuiErrors = new ErrorContext(uuiAnalytics, uuiModals);
+            const uuiApi = new ApiContext(props, uuiAnalytics);
+
+            const rawApi = apiDefinition ? apiDefinition(uuiApi.processRequest.bind(uuiApi)) : ({} as TApi);
+            const withOptions = (options: ApiCallOptions) => apiDefinition((url, method, data) => uuiApi.processRequest(url, method, data, options));
+            const api = { ...rawApi, withOptions };
+
+            const uuiUserSettings = new UserSettingsContext();
+            const uuiDnD = new DndContext();
+            return {
+                uuiAnalytics,
+                uuiErrors,
+                uuiApi,
+                api,
+                uuiLayout,
+                uuiNotifications,
+                uuiModals,
+                uuiUserSettings,
+                uuiDnD,
+                uuiRouter: router,
+                uuiLocks,
+                uuiApp: appContext || ({} as TAppContext),
+                uuiSkin,
+            };
+        },
         [],
     );
 
