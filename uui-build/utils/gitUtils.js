31eb4db00c0974bce74df3dda48ac5ced51f576c
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-07-18 15:13:53 +0300

    uncomment the git rev-parse tcommand

--- uui-build/utils/gitUtils.js
+++ uui-build/utils/gitUtils.js
@@ -1,20 +1,11 @@
-// const execSync = require('child_process').execSync;
+const execSync = require('child_process').execSync;
 
 module.exports = {
     getHeadCommitHash,
 };
 
 function getHeadCommitHash() {
-    /**
-     * Error: Command failed: git rev-parse --short HEAD
-     * fatal: detected dubious ownership in repository at '/__w/UUI/UUI'
-     * To add an exception for this directory, call:
-     *
-     *    git config --global --add safe.directory /__w/UUI/UUI
-     */
-    return '';
-/*
     const shortHash = 'git rev-parse --short HEAD';
     const hash = execSync(shortHash);
-    return hash.toString().trim(); */
+    return hash.toString().trim();
 }
