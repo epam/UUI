e0c08050859390bbd403d62927a4f779c79f834e
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-07-19 17:44:04 +0300

    Fixed ranking algorithm and tests.

--- uui-core/src/data/querying/__tests__/getSearchFilter.test.ts
+++ uui-core/src/data/querying/__tests__/getSearchFilter.test.ts
@@ -4,12 +4,12 @@ describe('getSearchFilter', () => {
     it('should return matching results', () => {
         const searchFilter = getSearchFilter('John Smith');
 
-        expect(searchFilter(['John', 'Smith'])).toEqual(6);
-        expect(searchFilter(['Johny', 'Smith'])).toEqual(5);
-        expect(searchFilter(['Johny', 'Smitherman'])).toEqual(4);
-        expect(searchFilter(['Rejohn', 'Smitherman'])).toEqual(3);
+        expect(searchFilter(['John', 'Smith'])).toEqual(8);
+        expect(searchFilter(['Johny', 'Smith'])).toEqual(7);
+        expect(searchFilter(['Johny', 'Smitherman'])).toEqual(6);
+        expect(searchFilter(['Rejohn', 'Smitherman'])).toEqual(4);
         expect(searchFilter(['Rejohn', 'Blacksmith'])).toEqual(2);
-        expect(searchFilter(['Smithy', 'Blacksmith John'])).toEqual(3);
+        expect(searchFilter(['Smithy', 'Blacksmith John'])).toEqual(5);
     });
 
     it('should return false if some word was not found', () => {
