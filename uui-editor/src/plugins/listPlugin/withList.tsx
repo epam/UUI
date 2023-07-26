31a85c024f1c3813e173466d617596cf4fd5226c
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-07-26 19:27:23 +0300

    [SlateEditor] update todo & improve isElementEmpty

--- uui-editor/src/plugins/listPlugin/withList.tsx
+++ uui-editor/src/plugins/listPlugin/withList.tsx
@@ -1,6 +1,7 @@
 import { ListPlugin, withList } from '@udecode/plate-list';
 import { PlateEditor, Value, WithPlatePlugin } from '@udecode/plate-common';
 
+// TODO: move to plate
 export const withOurList = <
     V extends Value = Value,
     E extends PlateEditor<V> = PlateEditor<V>
