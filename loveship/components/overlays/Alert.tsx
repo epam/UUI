38c312440b34b2b7fd647b26e63514f5d4d67e49
DIWAKAR <didk9675@gmail.com>
2023-07-27 21:32:18 +0530

    update changelog and regenerate snapshot tests

--- loveship/components/overlays/Alert.tsx
+++ loveship/components/overlays/Alert.tsx
@@ -19,6 +19,7 @@ export const Alert = withMods<Omit<uuiAlertProps, 'color'>, AlertMods>(
     (props) => ({
         ...props,
         color: props.color ?? 'sky',
+        size: props.size ?? '48',
     }),
 );
 
