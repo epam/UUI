ac03b289411394b54e1a9303ee8934c45b207919
maryia-lapata <mary.lopato@gmail.com>
2023-07-27 18:01:30 +0300

    Moving warning to Avatar component

--- uui-components/src/widgets/__tests__/Avatar.test.tsx
+++ uui-components/src/widgets/__tests__/Avatar.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { Avatar } from '../Avatar';
-import { render, screen, fireEvent } from '@epam/uui-test-utils';
+import { render, renderer, screen, fireEvent } from '@epam/uui-test-utils';
 
 describe('Avatar', () => {
     it('should show stub if image is not reachable', () => {
@@ -23,5 +23,17 @@ describe('Avatar', () => {
         fireEvent.error(component);
         expect(component.src).toEqual('https://static.cdn.epam.com/');
         expect(mockOnError).toHaveBeenCalled();
+    });
+
+    it('should be rendered correctly', () => {
+        const tree = renderer.create(<Avatar img="https://avatars.dicebear.com/api/human/avatar12.svg?background=%23EBEDF5&radius=50" size="36" />).toJSON();
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should be rendered correctly with props', () => {
+        const tree = renderer
+            .create(<Avatar img="https://avatars.dicebear.com/api/human/avatar12.svg?background=%23EBEDF5&radius=50" size="36" alt="Test avatar" isLoading={ true } />)
+            .toJSON();
+        expect(tree).toMatchSnapshot();
     });
 });
