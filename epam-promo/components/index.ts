d2f0aece0008dc6f201a23a07c70187f73fff7ed
IhorKorenets <vik753@gmail.com>
2023-08-02 15:07:53 +0300

    [DropMarker]: moved into uui and reexported to skins

--- epam-promo/components/index.ts
+++ epam-promo/components/index.ts
@@ -5,7 +5,6 @@ export * from './inputs';
 export * from './layout';
 export * from './types';
 export * from './typography';
-export * from './dnd';
 export * from './errors';
 export {
     FiltersPanel, PresetsPanel, defaultPredicates, TabButton, VerticalTabButton, Calendar, NumericInput, Switch, TextArea, Dropdown, TextPlaceholder,
@@ -14,6 +13,6 @@ export {
     ConfirmationModal, RadioGroup, Anchor, Avatar, AvatarStack, Paginator, DataTable, DataTableCell, DataTableRow, DataTableHeaderRow, ColumnsConfigurationModal,
     WarningNotification, SuccessNotification, HintNotification, ErrorNotification, ClearNotification, DatePicker, RangeDatePicker, Checkbox, FlexSpacer, FlexCell,
     Spinner, DataPickerBody, PickerModal, ModalBlocker, ModalHeader, ModalFooter, DropdownMenuBody, DropdownMenuButton, DropdownMenuSplitter, DropdownMenuHeader,
-    DropdownSubMenu, DropdownMenuSwitchButton, TimePicker, FileCard, SvgCircleProgress, DropSpot,
+    DropdownSubMenu, DropdownMenuSwitchButton, TimePicker, FileCard, SvgCircleProgress, DropSpot, DropMarker,
 } from '@epam/uui';
 export { MainMenuLogo, MainMenuCustomElement } from '@epam/uui-components';
