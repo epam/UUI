f5bd427009ee13ad067e0e300b2bd7e5e24cdb34
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-08-02 15:47:42 +0300

    Added tests for multi mode and for disallowClickOutside.

--- test-utils/src/testObjects/PickerList.ts
+++ test-utils/src/testObjects/PickerList.ts
@@ -1,5 +1,5 @@
-import { screen, within } from '../extensions/testingLibraryReactExt';
-import { PickerTestObject } from './Picker';
+import { fireEvent, screen, within } from '../extensions/testingLibraryReactExt';
+import { OptionConfig, PickerTestObject } from './Picker';
 
 export class PickerListTestObject extends PickerTestObject {
     static getPickerToggler() {
@@ -15,6 +15,11 @@ export class PickerListTestObject extends PickerTestObject {
         return container.getAllByRole('option', params);
     }
 
+    protected static async findOption(optionText: string, { editMode }: OptionConfig = {}) {
+        const container = editMode ? within(await this.findDialog(editMode)) : screen;
+        return await container.findByRoleAndText({ role: 'option', text: optionText });
+    }
+
     static queryOptions(props: { busy?: boolean, editMode?: string } = {}) {
         const container = props.editMode ? within(this.queryDialog(props.editMode)) : screen;
         const params: any = {};
@@ -28,5 +33,18 @@ export class PickerListTestObject extends PickerTestObject {
         const modal = await this.findDialog('modal');
         const input = await within(modal).findByPlaceholderText('Type text for quick search');
         return input;
+    }
+
+    static async findCheckedOptions(props: { editMode?: string } = {}) {
+        const container = props.editMode ? within(await this.findDialog(props.editMode)) : screen;
+
+        return (await container.findAllByRole('option')).filter((opt) => {
+            return (within(opt).getByRole('checkbox') as HTMLInputElement).checked;
+        }).map((e) => e.textContent?.trim());
+    }
+
+    static clickOnModalBlocker() {
+        const blocker = screen.getByLabelText('Click to close a modal');
+        fireEvent.click(blocker);
     }
 }
