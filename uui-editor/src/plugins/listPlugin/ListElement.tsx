f028953dcbda777252084c2914941a89285b2289
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-08-04 17:40:07 +0300

    [SlateEditor] fix ul margin, fix note block

--- uui-editor/src/plugins/listPlugin/ListElement.tsx
+++ uui-editor/src/plugins/listPlugin/ListElement.tsx
@@ -1,5 +1,8 @@
 import React from 'react';
 import { PlateElement, PlateElementProps } from '@udecode/plate-common';
+import { ELEMENT_OL_CUSTOM } from './constants';
+import cx from 'classnames';
+import css from './ListElement.module.scss';
 
 interface ListElementProps extends PlateElementProps {
     variant: 'ul' | 'ol';
@@ -8,15 +11,14 @@ interface ListElementProps extends PlateElementProps {
 export function ListElement({
     className,
     children,
-    variant = 'ul',
     ...props
 }: ListElementProps) {
-    const Element = variant!;
+    const Element = props.element.type === ELEMENT_OL_CUSTOM ? 'ol' : 'ul';
 
     return (
         <PlateElement
             asChild
-            className={ className }
+            className={ cx(css.list, className) }
             { ...props }
         >
             <Element>{children}</Element>
