1f243b34dc1f6cdba524d237e9bd3541379f2e8b
Aleksey Manetov <alekseymanetov@gmail.com>
2023-08-17 13:09:13 +0200

    [app]: fix clear button in IconPicker

--- app/src/docs/_props/loveship/docs/editors/IconPicker.tsx
+++ app/src/docs/_props/loveship/docs/editors/IconPicker.tsx
@@ -96,6 +96,11 @@ export class IconPicker extends React.Component<IconPickerProps, IconPickerState
                         selectionMode="single"
                         value={ this.state.iconId }
                         onValueChange={ (id: string) => {
+                            if (typeof id === 'undefined') {
+                                this.handleClear();
+                                return;
+                            }
+
                             this.props.onValueChange(icons[id].icon as IHasIcon);
                             this.setState({ iconId: id });
                         } }
