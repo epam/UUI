1691e1df5dc9cf06f4086960590c7778c9f02df8
IhorKorenets <vik753@gmail.com>
2023-08-17 12:06:38 +0300

    [dark theme]: DropdownMenu, Alert

---
+++ app/src/docs/_props/uui/components/overlays/alert.props.tsx
@@ -0,0 +1,66 @@
+import React from 'react';
+import { DocBuilder } from '@epam/uui-docs';
+import { ResizableContext, iconWithInfoDoc, colorDoc } from '../../docs';
+import { Text } from '@epam/promo';
+import { AlertProps, Alert } from '@epam/promo';
+
+const SnackbarCardDoc = new DocBuilder<AlertProps>({ name: 'Alert', component: Alert })
+    .implements([iconWithInfoDoc, colorDoc])
+    .prop('children', {
+        examples: [
+            {
+                value: <Text size="30">Notification Text</Text>,
+                name: 'Short',
+                isDefault: true,
+            }, {
+                value: <Text size="30">Notification with some buttons and long long text</Text>,
+                name: 'Long',
+            },
+        ],
+    })
+    .prop('actions', {
+        examples: [
+            {
+                value: [
+                    {
+                        name: 'ACTION 1',
+                        action: () => {},
+                    },
+                ],
+                name: '1 action',
+            }, {
+                value: [
+                    {
+                        name: 'ACTION 1',
+                        action: () => {},
+                    }, {
+                        name: 'ACTION 2',
+                        action: () => {},
+                    },
+                ],
+                name: '2 actions',
+            },
+        ],
+    })
+    .prop('size', {
+        examples: [
+            {
+                value: '48',
+                name: '48',
+            }, {
+                value: '36',
+                name: '36',
+            },
+        ],
+    })
+    .prop('onClose', {
+        examples: [
+            {
+                value: () => alert('close action'),
+                name: 'OnClose',
+            },
+        ],
+    })
+    .withContexts(ResizableContext);
+
+export default SnackbarCardDoc;
---
+++ app/src/docs/_props/uui/docs/ResizableContext.tsx
@@ -0,0 +1,41 @@
+import * as React from 'react';
+import { DemoComponentProps } from '@epam/uui-docs';
+import { Slider } from '@epam/promo';
+import css from './ResizableContext.module.scss';
+
+interface DemoComponentState {
+    widthPercent: number;
+}
+
+export class ResizableContext extends React.Component<DemoComponentProps, DemoComponentState> {
+    constructor(props: DemoComponentProps) {
+        super(props);
+    }
+
+    state: DemoComponentState = {
+        widthPercent: 100,
+    };
+
+    public static displayName = 'Resizable';
+    render() {
+        const { DemoComponent, props } = this.props;
+
+        return (
+            <div className={ css.panel }>
+                <div className={ css.slider }>
+                    <Slider value={ this.state.widthPercent } onValueChange={ this.resizeHandler } min={ 0 } max={ 100 } step={ 1 } />
+                </div>
+                <div style={ { width: `${this.state.widthPercent}%` } }>
+                    <DemoComponent { ...props } />
+                </div>
+            </div>
+        );
+    }
+
+    private resizeHandler = (value: number) => {
+        this.setState({
+            ...this.state,
+            widthPercent: value,
+        });
+    };
+}
