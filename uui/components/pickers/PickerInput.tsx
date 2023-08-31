dff022e92e6fe32a6fa4a205567abbc2c6562480
Aleksey Manetov <alekseymanetov@gmail.com>
2023-08-31 12:47:56 +0200

    [PickerInput]: fixed return focus on toggler by closing body by esc from footer

--- uui/components/pickers/PickerInput.tsx
+++ uui/components/pickers/PickerInput.tsx
@@ -112,16 +112,11 @@ export class PickerInput<TItem, TId> extends PickerInputBase<TItem, TId, PickerI
         const renderedDataRows = rows.map((row) => this.renderRow(row, this.state.dataSourceState));
         const maxHeight = isMobile() ? document.documentElement.clientHeight : this.props.dropdownHeight || pickerHeight;
         const minBodyWidth = isMobile() ? document.documentElement.clientWidth : this.props.minBodyWidth || pickerWidth;
-        const onKeyDownHandler = (e: React.KeyboardEvent<HTMLElement>) => {
-            if (e.key === 'Escape') {
-                props.onClose?.();
-            }
-        };
 
         return (
             <Panel
                 style={ { width: props.togglerWidth > minBodyWidth ? props.togglerWidth : minBodyWidth } }
-                rawProps={ { tabIndex: -1, onKeyDown: onKeyDownHandler } }
+                rawProps={ { tabIndex: -1, onKeyDown: props.onKeyDown } }
                 cx={ [
                     css.panel, uuiMarkers.lockFocus, this.props.bodyCx,
                 ] }
