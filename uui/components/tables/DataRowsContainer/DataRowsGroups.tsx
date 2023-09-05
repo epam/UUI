3c2443439d4590d75dc7c56504e8370049df4f48
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-09-05 12:53:31 +0300

    Fixed problem with gaps.

--- uui/components/tables/DataRowsContainer/DataRowsGroups.tsx
+++ uui/components/tables/DataRowsContainer/DataRowsGroups.tsx
@@ -23,10 +23,10 @@ function DataRowsGroup<TItem, TId>({
     top = 1,
 }: DataRowsGroupProps<TItem, TId>) {
     const rowRef = useRef<HTMLDivElement>();
-    const childrenPinnedTop = row.isPinned ? (top + (rowRef.current?.clientHeight ?? 0) - 0.5) : top;
+    const childrenPinnedTop = row.isPinned ? (top + (rowRef.current?.clientHeight ?? 0) - 1) : top;
     return ( 
         <div className={ css.group } key={ row.rowKey }>
-            <div className={ row.isPinned ? css.stickyHeader : css.header } style={ { zIndex: row.depth + 10, top: top - 0.5 } } ref={ rowRef }>
+            <div className={ row.isPinned ? css.stickyHeader : css.header } style={ { zIndex: row.depth + 10, top: top - 1 } } ref={ rowRef }>
                 {renderRow(row)}
             </div>
             {childRows.length > 0 && (
