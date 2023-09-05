44bcc589cf58bb367c297be27c3b165191b96c56
Aleksey Manetov <alekseymanetov@gmail.com>
2023-09-05 18:03:24 +0200

    update RTE example doc

--- app/src/docs/RichTextEditor.doc.tsx
+++ app/src/docs/RichTextEditor.doc.tsx
@@ -7,8 +7,8 @@ export class RichTextEditorDoc extends BaseDocsBlock {
         return (
             <>
                 <DocExample path="./_examples/richTextEditor/Basic.example.tsx" />
+                <DocExample title="HTML format" path="./_examples/richTextEditor/Serialization.example.tsx" />
                 <DocExample title="Inner scroll behavior" path="./_examples/richTextEditor/WithInnerScroll.example.tsx" />
-                <DocExample title="Serialization" path="./_examples/richTextEditor/Serialization.example.tsx" />
             </>
         );
     }
