1bf86ecd25730fbd446d410c433db05c2d89da68
IhorKorenets <vik753@gmail.com>
2023-09-06 21:22:14 +0300

    [Informer]: fixed theme classes and PE

--- app/src/docs/_props/epam-promo/components/widgets/informer.props.tsx
+++ app/src/docs/_props/epam-promo/components/widgets/informer.props.tsx
@@ -13,7 +13,10 @@ const colors = {
 };
 
 const informerDoc = new DocBuilder<InformerProps>({ name: 'Informer', component: Informer })
-    .prop('color', { renderEditor: (editable: any, examples) => <ColorPicker colors={ examples.map((i) => ({ value: i, hex: colors[i] })) } { ...editable } />, examples: InformerColors })
+    .prop('color', {
+        renderEditor: (editable: any, examples) => <ColorPicker colors={ examples.map((i) => ({ value: i, hex: colors[i] })) } { ...editable } />,
+        examples: [{ value: 'gray', isDefault: true }, ...InformerColors.slice(1)],
+    })
     .prop('caption', {
         examples: [
             { value: '7', isDefault: true }, { value: '+15' }, { value: '$99.9' },
