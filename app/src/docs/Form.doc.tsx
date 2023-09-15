3d16749e7f3548de02a3f2b11c02e1b4e518d9c4
Aleksey Manetov <alekseymanetov@gmail.com>
2023-09-15 16:20:19 +0200

    [docs]: add form example about beforeLeave behavior

--- app/src/docs/Form.doc.tsx
+++ app/src/docs/Form.doc.tsx
@@ -17,9 +17,11 @@ export class FormDoc extends BaseDocsBlock {
 
                 <DocExample title="Modal with Form" path="./_examples/modals/ModalWithForm.example.tsx" />
 
+                <DocExample title="Leave form with unsaved changes handling" path="./_examples/form/FormLeaveHandling.example.tsx" />
+
                 <DocExample title="Validation on change" path="./_examples/form/FormValidateOnChange.example.tsx" />
 
-                <DocExample title="Undo/redo and storing unsaved changes in localstorage" path="./_examples/form/Advanced.example.tsx" />
+                <DocExample title="Undo/redo and revert form" path="./_examples/form/Advanced.example.tsx" />
 
                 <DocExample title="Server-side validation" path="./_examples/form/ServerValidation.example.tsx" />
 
