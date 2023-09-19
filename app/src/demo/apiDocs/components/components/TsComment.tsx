9c9e36b84d2b08626abc05bed4e9364bdae14538
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-09-19 15:34:25 +0300

    Improvements

--- app/src/demo/apiDocs/components/components/TsComment.tsx
+++ app/src/demo/apiDocs/components/components/TsComment.tsx
@@ -2,30 +2,34 @@ import React, { useMemo } from 'react';
 import { RichTextView } from '@epam/promo';
 import css from './TsComment.module.scss';
 
+function formatComment(commentInput: string) {
+    // Playground to modify and debug https://regex101.com/r/dd4hyi/1
+    const linksRegex = /(?:\[(.*)])?\{\s*@link\s*(https:\/\/\S+?)\s*}/gm;
+    let comment = commentInput;
+    comment = comment.replace(linksRegex, (_, a, b) => `<a href='${b}'>${a ?? b}</a>`);
+    return comment;
+}
+
+function escapeArr(htmlStr: string[]) {
+    return htmlStr.map(escapeLine);
+}
+function escapeLine(htmlStr: string) {
+    return htmlStr.replace(/&/g, '&amp;')
+        .replace(/</g, '&lt;')
+        .replace(/>/g, '&gt;')
+        .replace(/"/g, '&quot;')
+        .replace(/'/g, '&#39;');
+}
+
 export function TsComment(props: { text?: string[], keepBreaks: boolean, isCompact?: boolean }) {
     const { text, keepBreaks, isCompact } = props;
-
-    function formatComment(commentInput: string) {
-        // Playground to modify and debug https://regex101.com/r/dd4hyi/1
-        const linksRegex = /(?:\[(.*)])?\{\s*@link\s*(https:\/\/\S+?)\s*}/gm;
-        let comment = commentInput;
-        comment = comment.replace(linksRegex, (_, a, b) => `<a href='${b}'>${a ?? b}</a>`);
-        return comment;
-    }
-    function escape(htmlStr: string) {
-        return htmlStr.replace(/&/g, '&amp;')
-            .replace(/</g, '&lt;')
-            .replace(/>/g, '&gt;')
-            .replace(/"/g, '&quot;')
-            .replace(/'/g, '&#39;');
-    }
 
     const textStr = useMemo(() => {
         if (text && text.length > 0) {
             if (keepBreaks) {
-                return `<pre>${formatComment(escape(text.join('\n')))}</pre>`;
+                return `<p>${formatComment(escapeArr(text).join('<br />'))}</p>`;
             }
-            return `<p>${formatComment(escape(text.join(' ')))}</p>`;
+            return `<p>${formatComment(escapeLine(text.join(' ')))}</p>`;
         }
     }, [text, keepBreaks]);
 
