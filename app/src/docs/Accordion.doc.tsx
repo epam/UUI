0e5394845500fa9d1fccaac1b295901552c517e5
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-09-27 16:28:38 +0300

    Improvements

--- app/src/docs/Accordion.doc.tsx
+++ app/src/docs/Accordion.doc.tsx
@@ -1,10 +1,13 @@
 import * as React from 'react';
 import {
-    EditableDocContent, DocExample, BaseDocsBlock, UUI4, UUI3,
+    EditableDocContent, DocExample, BaseDocsBlock, UUI4, UUI3, TUuiTsDoc,
 } from '../common';
 
 export class AccordionDoc extends BaseDocsBlock {
     title = 'Accordion';
+
+    override getUuiTsDoc = (): TUuiTsDoc => ('@epam/uui:AccordionProps');
+
     getPropsDocPath() {
         return {
             [UUI3]: './app/src/docs/_props/loveship/components/layout/accordion.props.tsx',
