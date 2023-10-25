a8e8884ddde8902c82a0c0026c5a9e0335393afa
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-10-25 15:18:15 +0300

    add confirmation modal for deleting preset

--- app/src/docs/_examples/tables/PresetsPanelBasic.example.tsx
+++ app/src/docs/_examples/tables/PresetsPanelBasic.example.tsx
@@ -1,9 +1,25 @@
-import React, { useMemo } from 'react';
+import React, { useCallback, useMemo } from 'react';
 import {
-    DataTable, Panel, FlexRow, Text, Badge, EpamAdditionalColor, PresetsPanel,
+    DataTable,
+    Panel,
+    FlexRow,
+    Text,
+    Badge,
+    EpamAdditionalColor,
+    PresetsPanel,
+    ModalBlocker,
+    ModalWindow,
+    ModalHeader,
+    Button,
+    FlexSpacer,
+    ModalFooter,
+    ScrollBars,
+    SuccessNotification,
+    WarningNotification,
+    ErrorNotification,
 } from '@epam/promo';
 import {
-    DataColumnProps, ITablePreset, LazyDataSource, TableFiltersConfig, useLazyDataSource, useTableState, useUuiContext,
+    DataColumnProps, IModal, ITablePreset, LazyDataSource, TableFiltersConfig, useLazyDataSource, useTableState, useUuiContext,
 } from '@epam/uui-core';
 import { Person } from '@epam/uui-docs';
 import dayjs from 'dayjs';
@@ -12,7 +28,7 @@ const personColumns: DataColumnProps<Person, number>[] = [
     {
         key: 'name',
         caption: 'Name',
-        render: (p) => <Text>{p.name}</Text>,
+        render: (p) => <Text>{ p.name }</Text>,
         width: 180,
         isSortable: true,
         isAlwaysVisible: true,
@@ -31,21 +47,21 @@ const personColumns: DataColumnProps<Person, number>[] = [
     }, {
         key: 'departmentName',
         caption: 'Department',
-        render: (p) => <Text>{p.departmentName}</Text>,
+        render: (p) => <Text>{ p.departmentName }</Text>,
         width: 200,
         isSortable: true,
         isFilterActive: (f) => !!f.departmentId,
     }, {
         key: 'jobTitle',
         caption: 'Title',
-        render: (r) => <Text>{r.jobTitle}</Text>,
+        render: (r) => <Text>{ r.jobTitle }</Text>,
         width: 220,
         isSortable: true,
         isFilterActive: (f) => !!f.jobTitleId,
     }, {
         key: 'birthDate',
         caption: 'Birth date',
-        render: (p) => p?.birthDate && <Text>{dayjs(p.birthDate).format('MMM D, YYYY')}</Text>,
+        render: (p) => p?.birthDate && <Text>{ dayjs(p.birthDate).format('MMM D, YYYY') }</Text>,
         width: 140,
         isSortable: true,
     },
@@ -74,8 +90,34 @@ const initialPresets: ITablePreset[] = [
     },
 ];
 
+function BasicModalExample({ presetName, ...modalProps }: IModal<string> & { presetName: string }) {
+    const content = `${presetName} will be deleted and can't be restored.`;
+    return (
+        <ModalBlocker { ...modalProps }>
+            <ModalWindow>
+                <Panel>
+                    <ModalHeader title="Delete preset" onClose={ () => modalProps.abort() } />
+                    <ScrollBars hasTopShadow hasBottomShadow>
+                        <FlexRow padding="24">
+                            <Text size="36">
+                                { content }
+                            </Text>
+                        </FlexRow>
+                    </ScrollBars>
+                    <ModalFooter>
+                        <FlexSpacer />
+                        <Button color="gray" fill="white" caption="Cancel" onClick={ () => modalProps.abort() } />
+                        <Button color="blue" caption="Ok" onClick={ () => modalProps.success('Success action') } />
+                    </ModalFooter>
+                </Panel>
+            </ModalWindow>
+        </ModalBlocker>
+    );
+}
+
 export default function PresetsPanelExample() {
     const svc = useUuiContext();
+    const { uuiModals, uuiNotifications } = useUuiContext();
 
     const filtersConfig: TableFiltersConfig<Person>[] = useMemo(
         () => [
@@ -107,15 +149,6 @@ export default function PresetsPanelExample() {
         [],
     );
 
-    const tableStateApi = useTableState({
-        columns: personColumns,
-        filters: filtersConfig,
-        initialPresets: initialPresets,
-        onPresetCreate: svc.api.presets.createPreset,
-        onPresetUpdate: svc.api.presets.updatePreset,
-        onPresetDelete: svc.api.presets.deletePreset,
-    });
-
     const dataSource = useLazyDataSource<Person, number, Person>(
         {
             api: svc.api.demo.persons,
@@ -123,6 +156,60 @@ export default function PresetsPanelExample() {
         },
         [],
     );
+
+    const handlePresetDelete = useCallback((preset: ITablePreset<any, any>): Promise<void> => {
+        const showSuccessToast = () => uuiNotifications
+            .show((props) => (
+                <SuccessNotification { ...props }>
+                    <FlexRow alignItems="center">
+                        <Text>Preset remove success</Text>
+                    </FlexRow>
+                </SuccessNotification>
+            ))
+            .catch(() => null);
+
+        const showCancelToast = () => uuiNotifications
+            .show((props) => (
+                <WarningNotification { ...props }>
+                    <FlexRow alignItems="center">
+                        <Text>Cancel removal</Text>
+                    </FlexRow>
+                </WarningNotification>
+            ))
+            .catch(() => null);
+
+        const showErrorToast = () => uuiNotifications
+            .show((props) => (
+                <ErrorNotification { ...props }>
+                    <FlexRow alignItems="center">
+                        <Text>Preset remove failure</Text>
+                    </FlexRow>
+                </ErrorNotification>
+            ))
+            .catch(() => null);
+
+        return new Promise(
+            (resolve, reject) => {
+                uuiModals
+                    .show<string>((props) => <BasicModalExample presetName={ preset.name } { ...props } />)
+                    .then(() =>
+                        svc.api.presets.deletePreset(preset)
+                            .then(resolve)
+                            .then(showSuccessToast)
+                            .catch(showErrorToast))
+                    .catch(() => showCancelToast().then(reject));
+            },
+        );
+    }, [svc.api.presets, uuiModals, uuiNotifications]);
+
+    const tableStateApi = useTableState({
+        columns: personColumns,
+        filters: filtersConfig,
+        initialPresets: initialPresets,
+        onPresetCreate: svc.api.presets.createPreset,
+        onPresetUpdate: svc.api.presets.updatePreset,
+        onPresetDelete: handlePresetDelete,
+    });
 
     const view = dataSource.useView(tableStateApi.tableState, tableStateApi.setTableState);
 
