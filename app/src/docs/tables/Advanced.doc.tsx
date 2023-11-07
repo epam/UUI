1f773e6dab30224aed1bc5d3974b5d4afcbb4ebf
IhorKorenets <Ihor_korenets@epam.com>
2023-11-07 16:51:06 +0200

    [DataTable]: fixed example background layer colors, [DatePicker]: fixed selected day color in dark theme

--- app/src/docs/tables/Advanced.doc.tsx
+++ app/src/docs/tables/Advanced.doc.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 import { EditableDocContent, DocExample, BaseDocsBlock } from '../../common';
+import css from '../styles.module.scss';
 
 export class AdvancedTablesDoc extends BaseDocsBlock {
     title = 'Advanced Tables';
@@ -9,15 +10,15 @@ export class AdvancedTablesDoc extends BaseDocsBlock {
                 <EditableDocContent fileName="advanced-tables-descriptions" />
                 {this.renderSectionTitle('Examples')}
 
-                <DocExample title="Columns Configuration" path="./_examples/tables/ColumnsConfig.example.tsx" />
+                <DocExample cx={ css.appBg } title="Columns Configuration" path="./_examples/tables/ColumnsConfig.example.tsx" />
 
-                <DocExample title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" />
 
-                <DocExample title="Table with paging" path="./_examples/tables/PagedTable.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with paging" path="./_examples/tables/PagedTable.example.tsx" />
 
-                <DocExample title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
                 
-                <DocExample title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" />
             </>
         );
     }
