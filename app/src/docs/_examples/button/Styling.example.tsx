89901a497cc43ed5f07bc018414835401fe06bc5
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2023-11-09 14:23:15 +0300

    update secondary colors for "Promo" & "Loveship" skins, update button example

--- app/src/docs/_examples/button/Styling.example.tsx
+++ app/src/docs/_examples/button/Styling.example.tsx
@@ -1,29 +1,35 @@
 import React from 'react';
-import { FlexRow, Button } from '@epam/uui';
+import { FlexRow, Button, Panel } from '@epam/uui';
 
 export default function StyledButtonsExample() {
     return (
         <div>
-            <FlexRow vPadding="12" spacing="12">
-                <Button color="primary" caption="Caption" onClick={ () => null } />
-                <Button color="critical" caption="Caption" onClick={ () => null } />
-                <Button color="accent" caption="Caption" onClick={ () => null } />
-                <Button color="secondary" caption="Caption" onClick={ () => null } />
-            </FlexRow>
-
-            <FlexRow vPadding="12" spacing="12">
-                <Button fill="outline" color="primary" caption="Caption" onClick={ () => null } />
-                <Button fill="outline" color="critical" caption="Caption" onClick={ () => null } />
-                <Button fill="none" color="accent" caption="Caption" onClick={ () => null } />
-                <Button fill="none" color="secondary" caption="Caption" onClick={ () => null } />
-            </FlexRow>
-
-            <FlexRow vPadding="12" spacing="12">
-                <Button fill="ghost" color="primary" caption="Caption" onClick={ () => null } />
-                <Button fill="ghost" color="critical" caption="Caption" onClick={ () => null } />
-                <Button fill="ghost" color="accent" caption="Caption" onClick={ () => null } />
-                <Button fill="ghost" color="secondary" caption="Caption" onClick={ () => null } />
-            </FlexRow>
+            <Panel style={ { background: 'var(--uui-surface-higher)' } }>
+                <FlexRow padding="6" vPadding="12" spacing="12">
+                    <Button color="primary" caption="Caption" onClick={ () => null } />
+                    <Button color="critical" caption="Caption" onClick={ () => null } />
+                    <Button color="accent" caption="Caption" onClick={ () => null } />
+                    <Button color="secondary" caption="Caption" onClick={ () => null } />
+                </FlexRow>
+                <FlexRow padding="6" vPadding="12" spacing="12">
+                    <Button fill="outline" color="primary" caption="Caption" onClick={ () => null } />
+                    <Button fill="outline" color="critical" caption="Caption" onClick={ () => null } />
+                    <Button fill="outline" color="accent" caption="Caption" onClick={ () => null } />
+                    <Button fill="outline" color="secondary" caption="Caption" onClick={ () => null } />
+                </FlexRow>
+                <FlexRow padding="6" vPadding="12" spacing="12">
+                    <Button fill="none" color="primary" caption="Caption" onClick={ () => null } />
+                    <Button fill="none" color="critical" caption="Caption" onClick={ () => null } />
+                    <Button fill="none" color="accent" caption="Caption" onClick={ () => null } />
+                    <Button fill="none" color="secondary" caption="Caption" onClick={ () => null } />
+                </FlexRow>
+                <FlexRow padding="6" vPadding="12" spacing="12">
+                    <Button fill="ghost" color="primary" caption="Caption" onClick={ () => null } />
+                    <Button fill="ghost" color="critical" caption="Caption" onClick={ () => null } />
+                    <Button fill="ghost" color="accent" caption="Caption" onClick={ () => null } />
+                    <Button fill="ghost" color="secondary" caption="Caption" onClick={ () => null } />
+                </FlexRow>
+            </Panel>
         </div>
     );
 }
