80b9deeb753bf7de3a507756430088d108401bef
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2023-11-10 01:52:56 +0300

    update "core" variables

--- uui/components/layout/ScrollBars.tsx
+++ uui/components/layout/ScrollBars.tsx
@@ -1,10 +1,10 @@
 import { ScrollBars as uuiScrollBars, ScrollbarProps } from '@epam/uui-components';
 import { withMods } from '@epam/uui-core';
-import './ScrollBars.module.scss';
+import css from './ScrollBars.module.scss';
 
 function applyScrollBarsMods() {
     return [
-        'uui-scroll-bars', 'uui-shadow-top', 'uui-shadow-bottom',
+        css.root, 'uui-scroll-bars', 'uui-shadow-top', 'uui-shadow-bottom',
     ];
 }
 
