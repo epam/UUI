169048e25758c143643e3a8b7e9d35992f5cb467
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-11-14 12:11:59 +0200

    draft

--- app/src/docs/tables/Advanced.doc.tsx
+++ app/src/docs/tables/Advanced.doc.tsx
@@ -6,18 +6,18 @@ export class AdvancedTablesDoc extends BaseDocsBlock {
     renderContent() {
         return (
             <>
-                <EditableDocContent fileName="advanced-tables-descriptions" />
+                {/* <EditableDocContent fileName="advanced-tables-descriptions" />
                 {this.renderSectionTitle('Examples')}
 
                 <DocExample title="Columns Configuration" path="./_examples/tables/ColumnsConfig.example.tsx" />
 
-                <DocExample title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" />
+                <DocExample title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" /> */}
 
                 <DocExample title="Table with paging" path="./_examples/tables/PagedTable.example.tsx" />
 
-                <DocExample title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
+                {/* <DocExample title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
                 
-                <DocExample title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" />
+                <DocExample title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" /> */}
             </>
         );
     }
