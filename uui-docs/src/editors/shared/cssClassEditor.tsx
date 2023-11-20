4d2f4bc770cadff500bfaad104c11136cf4e63fd
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-11-20 17:19:53 +0300

    Adjustments

--- uui-docs/src/editors/shared/cssClassEditor.tsx
+++ uui-docs/src/editors/shared/cssClassEditor.tsx
@@ -1,28 +1,25 @@
 import * as React from 'react';
 import { TextInput, FlexCell } from '@epam/uui';
 import { CX } from '@epam/uui-core';
-import { useInputValue } from './utils/hooks';
+import { getUseInputValueHook } from './utils/hooks';
 import { IPropDocEditor } from '../../types';
 
+const useInputValue = getUseInputValueHook<CX, string>({
+    inputToValue: (i) => i === '' ? undefined : i,
+    valueToInput: (v) => (v ? String(v).trim() : ''),
+    validateInput: (i) => {
+        return i.split(' ').every((s) => {
+            if (s.trim()) {
+                const matches = s.trim().match(new RegExp(/^-?[_a-zA-Z]+[_a-zA-Z0-9-]*$/g));
+                return matches?.length > 0;
+            }
+            return true;
+        });
+    },
+});
+
 export function CssClassEditor(props: IPropDocEditor<CX>) {
-    const inputProps = useInputValue<CX, string>({
-        clearWhenInvalid: true,
-        value: props.value,
-        onValueChange: (newVal) => {
-            props.onValueChange(newVal === '' ? undefined : newVal);
-        },
-        inputToValue: (i) => i,
-        valueToInput: (v) => (v ? String(v).trim() : ''),
-        validateInput: (i) => {
-            return i.split(' ').every((s) => {
-                if (s.trim()) {
-                    const matches = s.trim().match(new RegExp(/^-?[_a-zA-Z]+[_a-zA-Z0-9-]*$/g));
-                    return matches?.length > 0;
-                }
-                return true;
-            });
-        },
-    });
+    const inputProps = useInputValue({ ...props });
 
     return (
         <FlexCell minWidth={ 150 }>
@@ -31,7 +28,7 @@ export function CssClassEditor(props: IPropDocEditor<CX>) {
                 onValueChange={ inputProps.onInputChange }
                 value={ inputProps.input }
                 size="24"
-                isInvalid={ !inputProps.isValid }
+                isInvalid={ !inputProps.isInputValid }
             />
         </FlexCell>
     );
--- uui-docs/src/editors/shared/jsonEditor.tsx
+++ uui-docs/src/editors/shared/jsonEditor.tsx
@@ -1,34 +1,38 @@
 import React from 'react';
 import { FlexCell, TextArea } from '@epam/uui';
-import { useInputValue } from './utils/hooks';
+import { getUseInputValueHook } from './utils/hooks';
 import { IPropDocEditor } from '../../types';
 import { withMultiUnknownExamplesComponent } from './examples/multiUnknownExamples';
 
 const JSonEditorExamples = withMultiUnknownExamplesComponent({ isValueNodeVisible: false, isVerticalItems: true });
+const valueToInput = (v: object) => (v ? JSON.stringify(v, undefined, 1) : '');
+const inputToValue = (i: string) => jsonParse(i);
+const validateInput = (i: string) => {
+    return i ? !!jsonParse(i) : true;
+};
+function isEqualValue(v1: object, v2: object) {
+    return JSON.stringify(v1) === JSON.stringify(v2);
+}
+function jsonParse(v: string) {
+    try {
+        const res = JSON.parse(v);
+        if (typeof res === 'object') {
+            return res;
+        }
+    } catch (err) {}
+}
 
-export function JsonEditor(props: IPropDocEditor) {
-    const valueToInput = (v: object) => (v ? JSON.stringify(v, undefined, 1) : '');
-    const inputProps = useInputValue<object, string>({
-        clearWhenInvalid: false,
-        value: props.value,
-        onValueChange: props.onValueChange,
-        inputToValue: (i) => i ? JSON.parse(i) : undefined,
-        valueToInput,
-        validateInput: (i) => {
-            let isValid = true;
-            try {
-                if (i) {
-                    const result = JSON.parse(i);
-                    isValid = typeof result === 'object';
-                }
-            } catch {
-                isValid = false;
-            }
-            return isValid;
-        },
-    });
+const useInputValue = getUseInputValueHook<object, string>({
+    valueToInput,
+    inputToValue,
+    validateInput,
+    isEqualValue,
+});
 
-    const size = inputProps.input ? inputProps.input.split('\n').length : 3;
+export function JsonEditor(props: IPropDocEditor<object>) {
+    const inputProps = useInputValue({ ...props });
+
+    const size = Math.max(inputProps.input?.split('\n').length, 5);
     const input = (
         <TextArea
             placeholder="JSON value"
@@ -36,21 +40,16 @@ export function JsonEditor(props: IPropDocEditor) {
             value={ inputProps.input }
             size="24"
             rows={ size }
-            isInvalid={ !inputProps.isValid }
+            isInvalid={ !inputProps.isInputValid }
         />
     );
     if (props.examples.length > 0) {
-        const newOnExampleIdChange = (newExampleId: string) => {
-            const newValue = props.examples.find(({ id }) => newExampleId === id).value;
-            inputProps.onInputChange(valueToInput(newValue));
-            props.onExampleIdChange(newExampleId);
-        };
         return (
             <>
                 <FlexCell minWidth={ 100 }>
                     <JSonEditorExamples
                         { ...props }
-                        onExampleIdChange={ newOnExampleIdChange }
+                        onExampleIdChange={ props.onExampleIdChange }
                     />
                 </FlexCell>
                 <FlexCell minWidth={ 200 }>
