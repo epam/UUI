43d18c7f4a488ffbd55e3ec43e0cd22ff6c0115a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-11-20 17:21:31 +0200

    Added new example for select all rows in paginated table.

--- app/src/docs/tables/Advanced.doc.tsx
+++ app/src/docs/tables/Advanced.doc.tsx
@@ -7,18 +7,19 @@ export class AdvancedTablesDoc extends BaseDocsBlock {
     renderContent() {
         return (
             <>
-                {/* <EditableDocContent fileName="advanced-tables-descriptions" />
+                <EditableDocContent fileName="advanced-tables-descriptions" />
                 {this.renderSectionTitle('Examples')}
 
                 <DocExample cx={ css.appBg } title="Columns Configuration" path="./_examples/tables/ColumnsConfig.example.tsx" />
 
-                <DocExample title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" /> */}
+                <DocExample title="Table with rows Drag&Drop" path="./_examples/tables/TableWithDnD.example.tsx" />
 
-                <DocExample cx={ css.appBg } title="Table with paging" path="./_examples/tables/PagedTable.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with paging and select page" path="./_examples/tables/PagedTable.example.tsx" />
+                <DocExample cx={ css.appBg } title="Table with paging and select all rows" path="./_examples/tables/PagedTableWithSelectAll.example.tsx" />
 
-                {/* <DocExample title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
+                <DocExample title="Table with column filters" path="./_examples/tables/ColumnFiltersTable.example.tsx" />
                 
-                <DocExample title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" /> */}
+                <DocExample title="Table with pinned rows" path="./_examples/tables/TableWithPinnedRows.example.tsx" />
             </>
         );
     }
