f12683ea04c392cefd45115b0f17cc593c1a245c
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-11-23 17:15:11 +0200

    Added focusPrevious.

--- uui-components/src/table/tableCellsFocus/DataTableFocusManager.ts
+++ uui-components/src/table/tableCellsFocus/DataTableFocusManager.ts
@@ -7,6 +7,7 @@ export class DataTableFocusManager<TId> {
     private pendingRowToBeFocused?: TId;
     private focusedRow?: TId;
     private focusedCell?: number;
+    private lastRowIndex?: number;
 
     // eslint-disable-next-line @typescript-eslint/no-unused-vars
     constructor(props: DataTableFocusManagerProps) {
@@ -42,7 +43,7 @@ export class DataTableFocusManager<TId> {
             : undefined;
         const nextRowIndex = currentRowFocusIndex + 1;
 
-        if (!this.focusedRow || currentRowFocusIndex === -1 || !this.hasRowWithIndex(nextRowIndex)) {
+        if (this.focusedRow === undefined || currentRowFocusIndex === -1 || !this.hasRowWithIndex(nextRowIndex)) {
             if (this.hasRowWithIndex(0)) {
                 this.focusRow(this.getRowIdByIndex(0));
             }
@@ -52,7 +53,25 @@ export class DataTableFocusManager<TId> {
         this.focusRow(this.getRowIdByIndex(nextRowIndex));
     }
 
-    public focusPrevRow() {}
+    public focusPrevRow() {
+        const currentRowFocusIndex = this.focusedRow !== undefined
+            ? this.findRowIndexById(this.focusedRow)
+            : undefined;
+        const prevRowIndex = currentRowFocusIndex - 1;
+
+        if (this.focusedRow === undefined || currentRowFocusIndex === -1) {
+            if (this.hasRowWithIndex(0)) {
+                this.focusRow(this.getRowIdByIndex(0));
+            }
+            return;
+        }
+        if (prevRowIndex < 0) {
+            const lastRowId = this.getRowIdByIndex(this.getLastRowIndex());
+            this.focusRow(lastRowId);
+            return;
+        }
+        this.focusRow(this.getRowIdByIndex(prevRowIndex));
+    }
 
     public registerCell(rowInfo: RowInfo<TId>, ref: CellInfo['ref'], cellProps: CellInfo['cellProps']) {
         const rowKey = this.getKeyById(rowInfo.id);
@@ -62,6 +81,9 @@ export class DataTableFocusManager<TId> {
             this.rowsRegistry.set(rowKey, []);
         }
 
+        if (this.lastRowIndex === undefined || this.lastRowIndex < rowInfo.index) {
+            this.lastRowIndex = rowInfo.index;
+        }
         const row = this.rowsRegistry.get(rowKey);
         const cell = { ref, cellProps };
         row[cellProps.index] = cell;
@@ -124,5 +146,9 @@ export class DataTableFocusManager<TId> {
 
     private deleteRowIdByIndex(index: number) {
         return this.rowsIndexToIds.delete(index);
+    }
+
+    private getLastRowIndex() {
+        return this.lastRowIndex;
     }
 }
