7a765203cadddcfcc59232b4f25e748138e86642
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-11-27 12:18:34 +0200

    Extracted DataPickerCell functionality to a separate component from DataTableCell.

--- uui-components/src/pickers/index.ts
+++ uui-components/src/pickers/index.ts
@@ -3,6 +3,7 @@ export * from './PickerBodyBase';
 export * from './KeyboardUtils';
 export * from './PickerToggler';
 export * from './DataPickerRow';
+export * from './DataPickerCell';
 export type {
     PickerInputBaseProps,
     PickerListBaseProps,
---
+++ uui/components/pickers/types.ts
@@ -0,0 +1,7 @@
+import { ControlSize } from '../types';
+
+export interface PickerCellMods {
+    size?: ControlSize | '60';
+    padding?: '0' | '12' | '24';
+    alignActions?: 'top' | 'center';
+}
