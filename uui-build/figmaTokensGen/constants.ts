04d39ecf360e364edb951049014b4964be355091
Siarhei Yelin <Siarhei_Yelin@epam.com>
2023-11-28 17:16:49 +0300

    Generate tokens from Themes.json

---
+++ uui-build/figmaTokensGen/constants.ts
@@ -0,0 +1,12 @@
+// @ts-ignore
+export { logger } from '../utils/loggerUtils';
+// @ts-ignore
+export { uuiRoot } from '../utils/constants';
+
+export const IGNORED_VAR_PLACEHOLDER = '/* var is not supported in UUI */';
+
+export const PATH = {
+    CONFIG: 'uui-build/config/figmaTokensGenConfig.json',
+    FIGMA_VARS_COLLECTION_SRC: 'public/docs/figmaTokensGen/Theme.json',
+    RESULTS_DIR: 'public/docs/figmaTokensGen',
+};
