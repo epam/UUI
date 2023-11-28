04e4aa8af45141064c7153560136c6d1047f32b4
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-11-28 16:14:17 +0300

    update plate version

--- uui-editor/src/plugins/tablePlugin/TableElement.tsx
+++ uui-editor/src/plugins/tablePlugin/TableElement.tsx
@@ -4,7 +4,7 @@ import {
     PlateElementProps,
     getPluginOptions,
 } from '@udecode/plate-common';
-import { ELEMENT_TABLE, TTableElement, TablePlugin, getTableColumnCount, useTableElement, useTableStore } from '@udecode/plate-table';
+import { ELEMENT_TABLE, TTableElement, TablePlugin, getTableColumnCount, getTableOverriddenColSizes, useTableElement, useTableElementState, useTableStore } from '@udecode/plate-table';
 import cx from 'classnames';
 import css from './TableElement.module.scss';
 import { DEFAULT_COL_WIDTH, EMPTY_COL_WIDTH } from './constants';
@@ -22,29 +22,38 @@ const TableElement = React.forwardRef<
 React.ElementRef<typeof PlateElement>,
 PlateElementProps
 >(({ className, children, ...props }, ref) => {
-    const { editor } = props;
-    const element: OldTableElement = props.element;
-    const tableStore = useTableStore().get;
+    const { colSizes, isSelectingCell, minColumnWidth, marginLeft } = useTableElementState();
     const { props: tableProps, colGroupProps } = useTableElement();
 
-    const { minColumnWidth, disableMarginLeft } = getPluginOptions<TablePlugin>(
-        editor,
-        ELEMENT_TABLE,
-    );
-    const marginLeftOverride = useTableStore().get.marginLeftOverride();
-    const marginLeft = disableMarginLeft
-        ? 0
-        : marginLeftOverride ?? element.marginLeft ?? 0;
+    // const { editor } = props;
+    // const element: OldTableElement = props.element;
+    // const tableStore = useTableStore().get;
+    // const { props: tableProps, colGroupProps } = useTableElement();
 
-    if (!element.colSizes) {
-        element.colSizes = (element as OldTableElement).data?.cellSizes || getDefaultColWidths(getTableColumnCount(element));
-    }
+    // const { minColumnWidth, disableMarginLeft } = getPluginOptions<TablePlugin>(
+    //     editor,
+    //     ELEMENT_TABLE,
+    // );
+    // const marginLeftOverride = useTableStore().get.marginLeftOverride();
+    // const marginLeft = disableMarginLeft
+    //     ? 0
+    //     : marginLeftOverride ?? element.marginLeft ?? 0;
 
-    const isCellsSelected = !!tableStore.selectedCells();
-    const colSizeOverrides = tableStore.colSizeOverrides();
-    const currentColSizes = element.colSizes.map((size, index) => colSizeOverrides?.get(index) || size || EMPTY_COL_WIDTH);
+    // if (!element.colSizes) {
+    //     element.colSizes = (element as OldTableElement).data?.cellSizes || getDefaultColWidths(getTableColumnCount(element));
+    // }
 
-    const tableWidth = currentColSizes.reduce((acc, cur) => acc + cur, 0);
+    // const isCellsSelected = !!tableStore.selectedCells();
+    // const colSizeOverrides = tableStore.colSizeOverrides();
+    // // const overriddenColSizes = getTableOverriddenColSizes(
+    // //     element,
+    // //     colSizeOverrides,
+    // // );
+    // const currentColSizes = element.colSizes.map((size, index) => colSizeOverrides?.get(index) || size || EMPTY_COL_WIDTH);
+
+    // const tableWidth = currentColSizes.reduce((acc, cur) => acc + cur, 0);
+
+    // console.log('currentColSizes', currentColSizes);
 
     return (
         <div className={ css.tableWrapper } style={ { paddingLeft: marginLeft } }>
@@ -53,15 +62,17 @@ PlateElementProps
                 ref={ ref }
                 className={ cx(
                     css.table,
-                    isCellsSelected && css.cellsSelectionActive,
+                    isSelectingCell && css.cellsSelectionActive,
                     className,
                 ) }
                 { ...tableProps }
                 { ...props }
             >
-                <table style={ { width: tableWidth } }>
+                <table
+                //style={ { width: tableWidth } }
+                >
                     <colgroup { ...colGroupProps }>
-                        { currentColSizes.map((width, index) => (
+                        { colSizes.map((width, index) => (
                             <col
                                 key={ index }
                                 style={ {
