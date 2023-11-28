d8f44841ec05974fe622e0b5bb4729ba6c6e6cc0
IhorKorenets <Ihor_korenets@epam.com>
2023-11-28 17:41:41 +0200

    [colorDoc]: fixed color type

--- app/src/docs/_props/uui/docs/common.tsx
+++ app/src/docs/_props/uui/docs/common.tsx
@@ -4,12 +4,7 @@ import { ColorPicker, DocBuilder, IPropDocEditor } from '@epam/uui-docs';
 import { getIconList } from '../../../../documents/iconListHelpers';
 import { IconPickerWithInfo } from '../components/iconPicker/IconPicker';
 
-interface ColorMod {
-    /** Component color */
-    color?: 'info' | 'success' | 'warning' | 'error';
-}
-
-export const colorDoc = new DocBuilder<ColorMod>({ name: 'Color' }).prop('color', {
+export const colorDoc = new DocBuilder<{ color: string }>({ name: 'Color' }).prop('color', {
     editorType: function UuiColorPicker(props: IPropDocEditor) {
         const { value, onValueChange } = props;
         const examples = props.examples?.map((ex) => ({ value: ex.value }));
