6d1c706b617bce96202256d397af766e75fe4099
IhorKorenets <Ihor_korenets@epam.com>
2023-12-05 16:52:00 +0200

    [TextInput & Search]: added size 60 in uui, removed copy in skins.

--- app/src/docs/SearchInput.doc.tsx
+++ app/src/docs/SearchInput.doc.tsx
@@ -2,6 +2,7 @@ import * as React from 'react';
 import * as uui from '@epam/uui';
 import * as loveship from '@epam/loveship';
 import * as promo from '@epam/promo';
+import * as electric from '@epam/electric';
 import { DocBuilder, TDocConfig, TDocContext, TSkin } from '@epam/uui-docs';
 import { BaseDocsBlock, DocExample, EditableDocContent } from '../common';
 
@@ -13,10 +14,11 @@ export class SearchInputDoc extends BaseDocsBlock {
         contexts: [TDocContext.Default, TDocContext.Form, TDocContext.Resizable, TDocContext.Table],
         bySkin: {
             [TSkin.UUI]: { type: '@epam/uui:SearchInputProps', component: uui.SearchInput },
-            [TSkin.Loveship]: { type: '@epam/loveship:SearchInputProps', component: loveship.SearchInput },
+            [TSkin.Loveship]: { type: '@epam/uui:SearchInputProps', component: loveship.SearchInput },
             [TSkin.Promo]: { type: '@epam/uui:SearchInputProps', component: promo.SearchInput },
+            [TSkin.Electric]: { type: '@epam/uui:SearchInputProps', component: electric.SearchInput },
         },
-        doc: (doc: DocBuilder<uui.SearchInputProps | loveship.SearchInputProps>) => {
+        doc: (doc: DocBuilder<uui.SearchInputProps>) => {
             doc.merge('iconPosition', { defaultValue: 'left' });
         },
     };
