4203e67a1ea8c28f0a166497b5177208c649bd43
Dzmitry Tamashevich <diaamnj@mail.ru>
2023-12-07 12:20:01 +0300

    add more tests

--- uui-components/src/inputs/__tests__/TextArea.test.tsx
+++ uui-components/src/inputs/__tests__/TextArea.test.tsx
@@ -40,13 +40,78 @@ describe('TextArea', () => {
         expect(defaultProps.onValueChange).toHaveBeenCalledWith('Short text');
     });
 
-    it('updates height on componentDidMount', () => {
-        jest.useFakeTimers();
+    it('calls onBlur when textarea is blurred', () => {
+        const onBlurMock = jest.fn();
+        render(<TextArea { ...defaultProps } onBlur={ onBlurMock } />);
+        const textarea = screen.getByRole('textbox');
+
+        fireEvent.blur(textarea);
+
+        expect(onBlurMock).toHaveBeenCalled();
+    });
+
+    it('updates inFocus state to false when textarea is blurred', () => {
         render(<TextArea { ...defaultProps } />);
+        const textarea = screen.getByRole('textbox');
+
+        fireEvent.blur(textarea);
+
+        expect(textarea).not.toHaveFocus();
+    });
+
+    it('calls onFocus when textarea is focused', () => {
+        const onFocusMock = jest.fn();
+        render(<TextArea { ...defaultProps } onFocus={ onFocusMock } />);
+        const textarea = screen.getByRole('textbox');
+
+        fireEvent.focus(textarea);
+
+        expect(onFocusMock).toHaveBeenCalled();
+    });
+
+    it('updates inFocus state to true when textarea is focused', () => {
+        render(<TextArea { ...defaultProps } />);
+        const textarea = screen.getByRole('textbox');
+
+        fireEvent.focus(textarea);
+
+        expect(textarea).toHaveFocus();
+    });
+
+    it('calls focus on textarea when wrapper is focused', () => {
+        const { container } = render(<TextArea { ...defaultProps } />);
+        const wrapper = container.firstChild;
+        const textarea = screen.getByRole('textbox');
+
+        fireEvent.focus(wrapper);
+
+        expect(textarea).toHaveFocus();
+    });
+
+    it('updates height when value is updated', () => {
+        const { rerender } = render(<TextArea { ...defaultProps } autoSize={ true } />);
+        const textarea = screen.getByRole('textbox');
+
+        jest.spyOn(textarea, 'offsetHeight', 'get').mockReturnValue(200);
+        jest.spyOn(textarea, 'clientHeight', 'get').mockReturnValue(50);
+        jest.spyOn(textarea, 'scrollHeight', 'get').mockReturnValue(25);
+
+        rerender(<TextArea { ...defaultProps } value="Updated Value" autoSize={ true } />);
+
+        expect(textarea.style.height).toBe('175px');
+    });
+
+    it('updates height when component is mounted', () => {
+        jest.useFakeTimers();
+        render(<TextArea { ...defaultProps } autoSize={ true } />);
+        const textarea = screen.getByRole('textbox');
+
+        jest.spyOn(textarea, 'offsetHeight', 'get').mockReturnValue(200);
+        jest.spyOn(textarea, 'clientHeight', 'get').mockReturnValue(50);
+        jest.spyOn(textarea, 'scrollHeight', 'get').mockReturnValue(25);
+
         jest.runAllTimers();
 
-        // expect(jest.setTimeout).toHaveBeenCalledTimes(1);
-        // expect(jest.setTimeout).toHaveBeenLastCalledWith(expect.any(Function), 0);
-        expect(defaultProps.onValueChange).toHaveBeenCalled();
+        expect(textarea.style.height).toBe('175px');
     });
 });
