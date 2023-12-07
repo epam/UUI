55bf8d70749d524bca16ea17d0073e005f7d1a6c
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-12-07 12:58:06 +0200

    [LazyListView]: fixed background reloading blocker.

--- uui-core/src/data/processing/views/LazyListView.ts
+++ uui-core/src/data/processing/views/LazyListView.ts
@@ -455,9 +455,11 @@ export class LazyListView<TItem, TId, TFilter = any> extends BaseListView<TItem,
         if (this.props.legacyLoadDataBehavior) {
             this.loadData();
         }
+
         // if data is reloading, to prevent twitching the UI (of pagination, for example)
         // it is required to return previous listProps.
         if (this.isReloading && this.listProps) {
+            this.listProps = { ...this.listProps, isReloading: this.isReloading };
             return this.listProps;
         }
 
