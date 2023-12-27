a350f4e4c4f792e1d068cdab716714a4e1d9d4e2
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-12-27 17:59:08 +0200

    [useTree]: useAsyncTree force reload.

--- uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/useClientAsyncTree.ts
+++ uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/useClientAsyncTree.ts
@@ -1,33 +1,51 @@
-import { useMemo } from 'react';
+import { useCallback, useEffect, useMemo, useState } from 'react';
 import { ClientAsyncTreeProps } from './types';
 import { Tree } from '../../../Tree';
 import { usePlainTreeStrategy } from '../plainTree';
 import { useLoadData } from './useLoadData';
+import { useSimplePrevious } from '../../../../../../../hooks';
 
 export function useClientAsyncTree<TItem, TId, TFilter = any>(
     { mode, ...props }: ClientAsyncTreeProps<TItem, TId, TFilter>,
     deps: any[],
 ) {
     const fullTree = useMemo(() => Tree.blank(props), deps);
+    const [isForceReload, setIsForceReload] = useState(false);
+    const prevIsForceReload = useSimplePrevious(isForceReload);
 
-    const { tree: treeWithData, isLoading } = useLoadData({
+    const { tree: treeWithData, isLoading, isFetching } = useLoadData({
         api: () => props.api().then((items) => ({ items })),
         tree: fullTree,
         dataSourceState: {
             visibleCount: props.dataSourceState.visibleCount,
             topIndex: props.dataSourceState.topIndex,
         },
+        forceReload: isForceReload,
     }, deps);
+
+    const prevIsFetching = useSimplePrevious(isFetching);
 
     const { tree, ...restProps } = usePlainTreeStrategy(
         { ...props, items: treeWithData, type: 'plain' },
         [...deps, treeWithData],
     );
 
+    useEffect(() => {
+        if (prevIsForceReload !== isForceReload && isForceReload
+                && prevIsFetching !== isFetching && !isFetching) {
+            setIsForceReload(false);
+        }
+    }, [treeWithData]);
+
+    const reload = useCallback(() => {
+        setIsForceReload(true);
+    }, [setIsForceReload]);
+
     return {
         tree,
+        reload,
         isLoading,
-        isFetching: isLoading,
+        isFetching,
         ...restProps,
     };
 }
