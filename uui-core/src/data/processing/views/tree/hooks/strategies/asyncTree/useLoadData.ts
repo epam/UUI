ff494ff189abec9b9d7bd4449c389558ae84085a
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2023-12-27 11:42:18 +0200

    [useTree]: refactored useLoadData.

--- uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/useLoadData.ts
+++ uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/useLoadData.ts
@@ -1,4 +1,4 @@
-import { useCallback, useEffect } from 'react';
+import { useCallback, useEffect, useState } from 'react';
 import { DataSourceState, LazyDataSourceApi } from '../../../../../../../types';
 import { ITree } from '../../..';
 
@@ -6,56 +6,64 @@ export interface LoadResult<TItem, TId> {
     isUpdated: boolean;
     isOutdated: boolean;
     tree: ITree<TItem, TId>;
+    error?: Error;
 }
 
 export interface UseLoadDataProps<TItem, TId, TFilter = any> {
     tree: ITree<TItem, TId>;
     api: LazyDataSourceApi<TItem, TId, TFilter>;
-    dataSourceState: DataSourceState<TFilter, TId>;
-    onStart?: () => void,
-    onComplete: (result: LoadResult<TItem, TId>) => void;
-    onError?: (e: Error) => void;
+    dataSourceState?: DataSourceState<TFilter, TId>;
 }
 
 export function useLoadData<TItem, TId, TFilter = any>(
-    { tree, api, dataSourceState, onStart, onError, onComplete }: UseLoadDataProps<TItem, TId, TFilter>,
+    { tree, api, dataSourceState }: UseLoadDataProps<TItem, TId, TFilter>,
     deps: any[],
 ) {
+    const [loadedTree, setLoadedTree] = useState(tree);
+    const [isLoading, setIsLoading] = useState(false);
+
     const loadData = useCallback(async (
         sourceTree: ITree<TItem, TId>,
-        dsState?: DataSourceState<TFilter, TId>,
+        dsState: DataSourceState<TFilter, TId> = {},
     ): Promise<LoadResult<TItem, TId>> => {
         const loadingTree = sourceTree;
-        const newTreePromise = sourceTree.loadAll(
-            {
-                api,
-                filter: {
-                    ...dsState?.filter,
+
+        try {
+            const newTreePromise = sourceTree.loadAll(
+                {
+                    api,
+                    filter: {
+                        ...dsState?.filter,
+                    },
                 },
-            },
-            dsState,
-        );
+                dsState,
+            );
 
-        const newTree = await newTreePromise;
-        const linkToTree = sourceTree;
+            const newTree = await newTreePromise;
+            const linkToTree = sourceTree;
 
-        // If tree is changed during this load, than there was reset occurred (new value arrived)
-        // We need to tell caller to reject this result
-        const isOutdated = linkToTree !== loadingTree;
-        const isUpdated = linkToTree !== newTree;
-        return { isUpdated, isOutdated, tree: newTree };
+            // If tree is changed during this load, than there was reset occurred (new value arrived)
+            // We need to tell caller to reject this result
+            const isOutdated = linkToTree !== loadingTree;
+            const isUpdated = linkToTree !== newTree;
+            return { isUpdated, isOutdated, tree: newTree };
+        } catch (e) {
+            // TBD - correct error handling
+            console.error('useLoadData: Error while loading items.', e);
+            return { isUpdated: false, isOutdated: false, tree: loadingTree, error: e };
+        }
     }, [api]);
 
     useEffect(() => {
-        onStart?.();
+        setIsLoading(true);
         loadData(tree, dataSourceState)
-            .then((result) => {
-                onComplete(result);
+            .then(({ isOutdated, isUpdated, tree: newTree }) => {
+                if (isUpdated && !isOutdated) {
+                    setLoadedTree(newTree);
+                }
             })
-            .catch((e) => {
-                onComplete({ isUpdated: false, isOutdated: false, tree });
-                console.error('useLoadData: Error while loading items.', e);
-                onError?.(e);
-            });
+            .finally(() => setIsLoading(false));
     }, deps);
+
+    return { tree: loadedTree, isLoading };
 }
