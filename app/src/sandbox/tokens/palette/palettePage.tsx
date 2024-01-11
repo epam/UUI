66ce625ebde1be2d1d26e08e0c62e4793e9eddc5
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-01-11 12:06:51 +0300

    Update Theme.json

--- app/src/sandbox/tokens/palette/palettePage.tsx
+++ app/src/sandbox/tokens/palette/palettePage.tsx
@@ -1,23 +1,19 @@
 import React, { useState } from 'react';
 import { Layout } from './components/layout/layout';
 import { PaletteTable } from './components/paletteTable/paletteTable';
-import { useCurrentTheme } from './hooks/useCurrentTheme';
-import { TExpectedValueType } from './types/types';
-import { useThemeTokens } from './hooks/useThemeTokens';
+import { TLoadThemeTokensParams } from './types/types';
+import { useThemeTokens } from './hooks/useThemeTokens/useThemeTokens';
+import { DEFAULT_LOAD_PARAMS } from './constants';
 
 export function PalettePage() {
-    const uuiTheme = useCurrentTheme();
-    const [filter, setFilter] = useState<{ path: string }>({ path: 'core' });
-    const [expectedValueType, setExpectedValueType] = useState<TExpectedValueType>(TExpectedValueType.chain);
-    const result = useThemeTokens({ uuiTheme, expectedValueType, filter });
+    const [params, setParams] = useState<TLoadThemeTokensParams>(DEFAULT_LOAD_PARAMS);
+    const result = useThemeTokens(params);
+
     const settings = {
         grouped: true,
+        params,
+        onChangeParams: setParams,
         result,
-        uuiTheme,
-        expectedValueType,
-        onChangeExpectedValueType: setExpectedValueType,
-        filter,
-        onChangeFilter: setFilter,
     };
     return (
         <Layout>
