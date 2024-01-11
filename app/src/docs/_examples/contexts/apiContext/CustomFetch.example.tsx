1ee6f8d3e654f7526ae241fa02775a5b1b97300b
AlekseyManetov <aliaksei_manetau@epam.com>
2024-01-11 18:58:44 +0100

    [ApiContext]: added possibility to provide your own fetcher for requests

---
+++ app/src/docs/_examples/contexts/apiContext/CustomFetch.example.tsx
@@ -0,0 +1,21 @@
+import React from 'react-dom';
+import { UuiContext, HistoryAdaptedRouter, useUuiServices } from '@epam/uui-core';
+import { createBrowserHistory } from 'history';
+
+const history = createBrowserHistory();
+const router = new HistoryAdaptedRouter(history);
+
+const myFetch: typeof fetch = (input, init) => {
+    const headers = new Headers(init.headers);
+    headers.set('my-header', 'header-value');
+    return fetch(input, { ...init, headers });
+};
+export function App() {
+    const { services } = useUuiServices({ router, fetch: myFetch });
+
+    return (
+        <UuiContext.Provider value={ services }>
+            App
+        </UuiContext.Provider>
+    );
+}
