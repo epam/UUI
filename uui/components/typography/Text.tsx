2e6d7cd21c74b61cc377703d9ef9131c0e2680a9
IhorKorenets <Ihor_korenets@epam.com>
2024-01-15 18:49:12 +0200

    returned 'none' in Text size type

--- uui/components/typography/Text.tsx
+++ uui/components/typography/Text.tsx
@@ -4,7 +4,7 @@ import * as uuiComponents from '@epam/uui-components';
 import css from './Text.module.scss';
 
 type TextColor = 'info' | 'warning' | 'critical' | 'success' | 'brand' | 'primary' | 'secondary' | 'disabled' | 'white';
-type TextSize = '18' | '24' | '30' | '36' | '42' | '48';
+type TextSize = 'none' | '18' | '24' | '30' | '36' | '42' | '48';
 type TextFontStyle = 'normal' | 'italic';
 type TextFontWeight = '200' | '300' | '400' | '600' | '700' | '900';
 
