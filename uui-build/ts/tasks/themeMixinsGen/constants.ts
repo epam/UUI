297b1c0a038efccf412d9734b7bca824665cd6b4
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-01-24 13:09:49 +0300

    Adjust mixins generator + improve stylelint rule for tokens

--- uui-build/ts/tasks/themeMixinsGen/constants.ts
+++ uui-build/ts/tasks/themeMixinsGen/constants.ts
@@ -1,25 +1,28 @@
 import { TFigmaThemeName } from '../themeTokensGen/types/sharedTypes';
 import { PATH } from '../themeTokensGen/constants';
 
+export const TOKENS_MIXIN_NAME = 'theme-tokens';
+
 const THEME_DIR = './epam-assets/theme';
+const THEME_TOKENS_DIR = './epam-assets/theme/tokens';
 
 export const tokensFile = PATH.FIGMA_VARS_COLLECTION_OUT_TOKENS;
 export const coreThemeMixinsConfig: Record<TFigmaThemeName, { themeFile: string, mixinsFile: string }> = {
     [TFigmaThemeName.EPAM]: {
         themeFile: `${THEME_DIR}/theme_electric.scss`,
-        mixinsFile: `${THEME_DIR}/theme_electric_mixins.scss`,
+        mixinsFile: `${THEME_TOKENS_DIR}/_theme_electric.scss`,
     },
     [TFigmaThemeName.PROMO]: {
         themeFile: `${THEME_DIR}/theme_promo.scss`,
-        mixinsFile: `${THEME_DIR}/theme_promo_mixins.scss`,
+        mixinsFile: `${THEME_TOKENS_DIR}/_theme_promo.scss`,
     },
     [TFigmaThemeName.LOVESHIP_LIGHT]: {
         themeFile: `${THEME_DIR}/theme_loveship.scss`,
-        mixinsFile: `${THEME_DIR}/theme_loveship_mixins.scss`,
+        mixinsFile: `${THEME_TOKENS_DIR}/_theme_loveship.scss`,
     },
     [TFigmaThemeName.LOVESHIP_DARK]: {
         themeFile: `${THEME_DIR}/theme_loveship_dark.scss`,
-        mixinsFile: `${THEME_DIR}/theme_loveship_dark_mixins.scss`,
+        mixinsFile: `${THEME_TOKENS_DIR}/_theme_loveship_dark.scss`,
     },
 };
 
@@ -37,4 +40,3 @@ export const GROUPS_CONFIG: Record<string, { title: string, condition: string[] 
         condition: undefined, // fallback
     },
 };
-export const MIXIN_NAME = 'theme_core-mixin';
