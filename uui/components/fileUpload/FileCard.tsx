315c15db7cf1bbb1cca39e52921a6c192f0e899f
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2024-01-25 17:35:25 +0300

    update docs

--- uui/components/fileUpload/FileCard.tsx
+++ uui/components/fileUpload/FileCard.tsx
@@ -4,10 +4,10 @@ import { i18n } from '../../i18n';
 import { cx, FileUploadResponse, formatBytes, IClickable, IHasCX, uuiMod } from '@epam/uui-core';
 import { SvgCircleProgress } from './SvgCircleProgress';
 import { IconContainer } from '@epam/uui-components';
-import { FlexCell, FlexRow } from '../../components/layout';
-import { IconButton } from '../../components/buttons';
-import { Text } from '../../components/typography';
-import { Tooltip } from '../../components/overlays';
+import { FlexCell, FlexRow } from '../layout';
+import { IconButton } from '../buttons';
+import { Text } from '../typography';
+import { Tooltip } from '../overlays';
 import { fileIcons } from '../../icons/icons';
 import { ReactComponent as RemoveIcon } from '@epam/assets/icons/common/navigation-close-18.svg';
 import { ReactComponent as ErrorIcon } from '../../icons/notification-error-fill-10.svg';
@@ -27,8 +27,9 @@ export interface FileCardItem extends Partial<File & FileUploadResponse> {
 export interface FileCardProps extends IClickable, IHasCX {
     /** Defines file card item */
     file: FileCardItem;
-    /** Defines card width
-     * @default 100%
+    /**
+     * Defines card width
+     * @default '100%'
      */
     width?: number;
 }
