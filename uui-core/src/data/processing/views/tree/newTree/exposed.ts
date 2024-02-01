36439cb91d4562d13a38d42d02bbb2481bdae6b8
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-02-01 10:51:38 +0200

    [useTree]: TreeNodeStatus: Symbol -> string literal.

--- uui-core/src/data/processing/views/tree/newTree/exposed.ts
+++ uui-core/src/data/processing/views/tree/newTree/exposed.ts
@@ -3,4 +3,4 @@ export * from './treeStructure/exposed';
 export type { ITree, ItemsInfo, TreeNodeStatus } from './ITree';
 export { Tree } from './Tree';
 export type { TreeLoadResult } from './Tree';
-export { NOT_FOUND_RECORD, PARTIALLY_LOADED, FULLY_LOADED, EMPTY } from './constants';
+export { NOT_FOUND_RECORD } from './constants';
