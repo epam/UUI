8ec2e92057b756d3870fc744a98b53abf665effc
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-02-14 19:13:05 +0200

    [PickerInput]: reverted example.

--- app/src/docs/_examples/pickerInput/LazyTreeInput.example.tsx
+++ app/src/docs/_examples/pickerInput/LazyTreeInput.example.tsx
@@ -1,27 +1,18 @@
-import React, { useCallback, useMemo, useState } from 'react';
+import React, { useState } from 'react';
 import { FlexCell, PickerInput } from '@epam/uui';
-import { DataQueryFilter, UuiContexts, useLazyDataSource, useUuiContext } from '@epam/uui-core';
+import { DataQueryFilter, useLazyDataSource, useUuiContext } from '@epam/uui-core';
 import { Location } from '@epam/uui-docs';
-import { TApi } from '../../../data';
 
 export default function LazyTreePicker() {
-    const svc = useUuiContext<TApi, UuiContexts>();
-    const [selectedId, onValueChange] = useState<string>('2509031');
-
-    const itemsMap = useMemo(() => new Map(), []);
-
-    const isFoldedByDefault = useCallback((item: Location) => {
-        return !['c-AF', 'DZ'].includes(item.id);
-    }, []);
+    const svc = useUuiContext();
+    const [value, onValueChange] = useState<string[]>([]);
 
     const dataSource = useLazyDataSource<Location, string, DataQueryFilter<Location>>(
         {
-            api: async (request, ctx) => {
+            api: (request, ctx) => {
                 const { search } = request;
                 const filter = search ? {} : { parentId: ctx?.parentId };
-                const result = await svc.api.demo.locations({ ...request, search, filter });
-                result.items.forEach((item) => itemsMap.set(item.id, item));
-                return result;
+                return svc.api.demo.locations({ ...request, search, filter });
             },
             cascadeSelection: true,
             getId: (i) => i.id,
@@ -35,13 +26,12 @@ export default function LazyTreePicker() {
         <FlexCell width={ 300 }>
             <PickerInput
                 dataSource={ dataSource }
-                value={ selectedId }
+                value={ value }
                 onValueChange={ onValueChange }
                 entityName="location"
-                selectionMode="single"
+                selectionMode="multi"
                 valueType="id"
                 cascadeSelection="explicit"
-                isFoldedByDefault={ isFoldedByDefault }
             />
         </FlexCell>
     );
