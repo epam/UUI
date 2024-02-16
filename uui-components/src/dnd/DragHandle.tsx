366a5879dc327096cf28451f06bdf938e6529c56
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-02-16 09:42:53 +0300

    Redesign columns configuration dialog.

--- uui-components/src/dnd/DragHandle.tsx
+++ uui-components/src/dnd/DragHandle.tsx
@@ -1,14 +1,30 @@
 import * as React from 'react';
 import {
+    Icon,
     IDisableable, IHasCX, IHasRawProps, uuiMod,
 } from '@epam/uui-core';
 import cx from 'classnames';
 import css from './DragHandle.module.scss';
+import { IconContainer } from '../layout';
 
-export interface DragHandleProps extends IHasCX, IHasRawProps<React.HTMLAttributes<HTMLDivElement>>, IDisableable {}
+export interface DragHandleProps extends IHasCX, IHasRawProps<React.HTMLAttributes<HTMLDivElement>>, IDisableable {
+    dragHandleIcon?: Icon;
+}
 
 export class DragHandle extends React.Component<DragHandleProps> {
     render() {
-        return <div className={ cx(this.props.cx, css.container, 'uui-drag-handle', this.props.isDisabled && uuiMod.disabled) } { ...this.props.rawProps } />;
+        const { rawProps, dragHandleIcon } = this.props;
+        const classes = cx(
+            this.props.cx,
+            css.container,
+            'uui-drag-handle',
+            !dragHandleIcon && css.noIcon,
+            this.props.isDisabled && uuiMod.disabled,
+        );
+        return (
+            <div className={ classes } { ...rawProps }>
+                { dragHandleIcon && <IconContainer icon={ dragHandleIcon } />}
+            </div>
+        );
     }
 }
