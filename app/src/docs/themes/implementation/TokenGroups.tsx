a32adc540dd9ea8e55645e912de47cfa1e31c337
IhorKorenets <Ihor_korenets@epam.com>
2024-02-26 17:24:15 +0200

    [TokensPage]: corrected Info and Critical descriptions. Corrected semantic tooltip.

--- app/src/docs/themes/implementation/TokenGroups.tsx
+++ app/src/docs/themes/implementation/TokenGroups.tsx
@@ -86,16 +86,9 @@ function SemanticBlocks(props: SemanticBlocksProps) {
                             <Text fontSize="12" color="tertiary">CSS variable</Text>
                             <Text fontSize="12">{ item.cssVar }</Text>
                         </div>
-                        <div className={ css.semanticTooltipBlockMiddle }>
+                        <div className={ cx(css.semanticTooltipBlockMiddle, css.withBorder) }>
                             <Text fontSize="12" color="tertiary">Figma variable</Text>
                             <Text fontSize="12">{ item.cssVar.replace(/^--uui-/, '') }</Text>
-                        </div>
-                        <div className={ cx(css.semanticTooltipBlockLast, css.withBorder) }>
-                            <Text fontSize="12" color="tertiary">Reference token</Text>
-                            <Text fontSize="12">{ item.baseToken }</Text>
-                            <FlexSpacer />
-                            <FlexSpacer />
-                            {/* <LinkButton size="24" caption="Learn more" link={ { pathname: '' } } /> */}
                         </div>
                     </FlexCell>
                     <FlexCell grow={ 1 } textAlign="center" cx={ css.semanticTooltipTitle }>
