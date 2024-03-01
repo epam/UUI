6abf2d82da1398e9a0b579c03a9da24dc4819022
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-03-01 14:31:36 +0200

    [useTree]: async fold-all test.

--- test-utils/src/mocks/dataSources/asyncDataSourceMock.ts
+++ test-utils/src/mocks/dataSources/asyncDataSourceMock.ts
@@ -1,12 +1,9 @@
 import { AsyncDataSource, AsyncDataSourceProps } from '@epam/uui-core';
 
-type Props<TItem, TId, TFilter> = Exclude<AsyncDataSourceProps<TItem, TId, TFilter>, 'api'>;
+type Props<TItem, TId, TFilter> = Omit<AsyncDataSourceProps<TItem, TId, TFilter>, 'api'>;
 
 export function getAsyncDataSourceMock<TItem extends { id: string }, TId, TFilter>(data: TItem[], props: Props<TItem, TId, TFilter>) {
-    const api = props.api === undefined
-        ? () => Promise.resolve(({ items: data }))
-        : props.api;
-
+    const api = () => Promise.resolve(data);
     const apiMock = jest.fn().mockImplementation(api);
     return { dataSource: new AsyncDataSource({ ...props, api: apiMock }), apiMock };
 }
