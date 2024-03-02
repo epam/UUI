bab25c9af4ccb0f870b6d132872160a36560ad5f
IhorKorenets <vik753@gmail.com>
2024-03-02 13:08:50 +0200

    added keys when items are mapping

--- app/src/sandbox/adaptivePanel/AdaptivePanelDemo.tsx
+++ app/src/sandbox/adaptivePanel/AdaptivePanelDemo.tsx
@@ -10,7 +10,7 @@ const items: AdaptiveItemProps<{ data?: { name: string } }>[] = [
     //         renderBody={ () => <DropdownContainer><Text>{ hiddenItems.map(i => i.render()) }</Text></DropdownContainer> }
     //     />,
     //     collapsedContainer: true, priority: 99 },
-    { id: '2', render: () => <Button caption="Administrators-4" />, priority: 4 }, { id: '3', render: () => <Button caption="Only age more thanfgdgdfgdg 40-1" />, priority: 1 }, { id: '4', render: () => <Button caption="Managers-2" />, priority: 2 }, { id: '6', render: () => <Button caption="Senior Admins-11" />, priority: 11 }, { id: '7', render: () => <Button caption="Senior Admins-12" />, priority: 12 }, { id: '8', render: () => <Button caption="Senior Admins-13" />, priority: 13 }, {
+    { id: '2', render: () => <Button key="Administrators-4" caption="Administrators-4" />, priority: 4 }, { id: '3', render: () => <Button key="Only age more thanfgdgdfgdg 40-1" caption="Only age more thanfgdgdfgdg 40-1" />, priority: 1 }, { id: '4', render: () => <Button key="Managers-2" caption="Managers-2" />, priority: 2 }, { id: '6', render: () => <Button key="Senior Admins-11" caption="Senior Admins-11" />, priority: 11 }, { id: '7', render: () => <Button key="Senior Admins-12" caption="Senior Admins-12" />, priority: 12 }, { id: '8', render: () => <Button key="Senior Admins-13" caption="Senior Admins-13" />, priority: 13 }, {
         id: '5',
         render: (item, hiddenItems) => (
             <Dropdown
