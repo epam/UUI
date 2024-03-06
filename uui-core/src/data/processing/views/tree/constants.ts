50270dd1d92c17a53e5f528d8b0e519e07b8a17e
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-03-06 12:40:38 +0200

    [useTree]: added comments.

--- uui-core/src/data/processing/views/tree/constants.ts
+++ uui-core/src/data/processing/views/tree/constants.ts
@@ -1,9 +1,14 @@
 export const ROOT_ID: undefined = undefined;
+
+// TreeNodeStatus
 export const PARTIALLY_LOADED = 'PARTIALLY_LOADED';
 export const FULLY_LOADED = 'FULLY_LOADED';
 export const EMPTY = 'EMPTY';
 
+// RecordStatus && not found item in tree
 export const NOT_FOUND_RECORD = Symbol('NOT_FOUND_RECORD');
+
+// RecordStatus
 export const PENDING_RECORD = Symbol('PENDING_RECORD');
 export const LOADING_RECORD = Symbol('LOADING_RECORD');
 export const LOADED_RECORD = Symbol('LOADED_RECORD');
