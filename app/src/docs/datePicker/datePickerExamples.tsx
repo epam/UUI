84c807f6a2a57330e8794e270a1eb16ad53d8dd0
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-03-06 15:34:07 +0300

    fix examples

--- app/src/docs/datePicker/datePickerExamples.tsx
+++ app/src/docs/datePicker/datePickerExamples.tsx
@@ -1,18 +1,24 @@
 import dayjs, { Dayjs } from 'dayjs';
-import { Day, IconContainer } from '@epam/uui-components';
+import {
+    Day, DayProps, IconContainer,
+} from '@epam/uui-components';
 import * as React from 'react';
 import { ReactComponent as Point } from '@epam/assets/icons/common/radio-point-10.svg';
 import { IPropSamplesCreationContext } from '@epam/uui-docs';
-import { DatePickerProps, FlexRow, LinkButton } from '@epam/uui';
+import {
+    DatePickerProps, FlexRow, LinkButton,
+} from '@epam/uui';
 import css from './datePickerExamples.module.scss';
-import { DayProps } from '@epam/uui-core';
 
 export const renderFooter = (ctx: IPropSamplesCreationContext<DatePickerProps>) => [
     {
         name: 'footer',
         value: () => (
             <FlexRow cx={ css.footer } size="48">
-                <LinkButton size="36" caption="Today" onClick={ () => ctx.getSelectedProps().onValueChange(dayjs().format('YYYY-MM-DD')) } />
+                <LinkButton
+                    size="36" caption="Today"
+                    onClick={ () => ctx.getSelectedProps().onValueChange(dayjs().format('YYYY-MM-DD')) }
+                />
             </FlexRow>
         ),
     },
@@ -29,7 +35,12 @@ export const renderCustomDayExample = (ctx: IPropSamplesCreationContext<DatePick
                             {dayInner.format('D')}
                             <IconContainer
                                 style={ {
-                                    fill: '#fcaa00', height: '4px', width: '4px', position: 'absolute', top: '7px', right: '10px',
+                                    fill: '#fcaa00',
+                                    height: '4px',
+                                    width: '4px',
+                                    position: 'absolute',
+                                    top: '7px',
+                                    right: '10px',
                                 } }
                                 icon={ Point }
                             />
