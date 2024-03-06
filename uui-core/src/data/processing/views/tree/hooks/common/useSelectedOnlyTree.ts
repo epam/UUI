216241a9bd198fc9d753e3f3751e41c277aa6b49
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-03-06 11:21:43 +0200

    [useTree]: added refactoring of useMemo.

--- uui-core/src/data/processing/views/tree/hooks/common/useSelectedOnlyTree.ts
+++ uui-core/src/data/processing/views/tree/hooks/common/useSelectedOnlyTree.ts
@@ -1,8 +1,8 @@
-import { useMemo, useRef } from 'react';
 import { useSimplePrevious } from '../../../../../../hooks';
 import { DataSourceState } from '../../../../../../types';
 import { TreeState } from '../../newTree';
 import { getChecked, isSelectedOrCheckedChanged } from '../strategies/checked';
+import { useUpdateTree } from './useUpdateTree';
 
 export type UseSelectedOnlyTreeProps<TItem, TId, TFilter = any> = {
     tree: TreeState<TItem, TId>;
@@ -14,23 +14,13 @@ export function useSelectedOnlyTree<TItem, TId, TFilter = any>(
     { tree, dataSourceState, isLoading }: UseSelectedOnlyTreeProps<TItem, TId, TFilter>,
     deps: any[],
 ) {
-    const prevTree = useSimplePrevious(tree);
     const prevDataSourceState = useSimplePrevious(dataSourceState);
-    const prevDeps = useSimplePrevious(deps);
 
-    const selectedOnlyTreeRef = useRef<TreeState<TItem, TId>>(null);
-
-    const selectedOnlyTree = useMemo(() => {
-        const isDepsChanged = prevDeps?.length !== deps.length || (prevDeps ?? []).some((devVal, index) => devVal !== deps[index]);
-        if (selectedOnlyTreeRef.current === null
-            || prevTree !== tree
-            || isSelectedOrCheckedChanged(dataSourceState, prevDataSourceState)
-            || isDepsChanged
-        ) {
-            selectedOnlyTreeRef.current = tree.buildSelectedOnly(getChecked(dataSourceState));
-        }
-        return selectedOnlyTreeRef.current;
-    }, [tree, dataSourceState, prevDataSourceState, ...deps]);
+    const selectedOnlyTree = useUpdateTree({
+        tree,
+        shouldUpdate: () => isSelectedOrCheckedChanged(dataSourceState, prevDataSourceState),
+        update: (currentTree) => currentTree.buildSelectedOnly(getChecked(dataSourceState)),
+    }, [dataSourceState, ...deps]);
 
     if (isLoading) {
         return tree;
--- uui-core/src/data/processing/views/tree/hooks/common/useSortTree.ts
+++ uui-core/src/data/processing/views/tree/hooks/common/useSortTree.ts
@@ -1,7 +1,7 @@
-import { useMemo, useRef } from 'react';
 import { useSimplePrevious } from '../../../../../../hooks';
 import { DataSourceState, SortingOption } from '../../../../../../types';
 import { TreeState } from '../../newTree';
+import { useUpdateTree } from './useUpdateTree';
 
 export type UseSortTreeProps<TItem, TId, TFilter = any> = {
     sortBy?(item: TItem, sorting: SortingOption): any;
@@ -19,18 +19,13 @@ export function useSortTree<TItem, TId, TFilter = any>(
     }: UseSortTreeProps<TItem, TId, TFilter>,
     deps: any[],
 ): TreeState<TItem, TId> {
-    const prevTree = useSimplePrevious(tree);
     const prevSorting = useSimplePrevious(sorting);
-    const prevDeps = useSimplePrevious(deps);
-    const sortedTreeRef = useRef<TreeState<TItem, TId>>(null);
 
-    const sortTree = useMemo(() => {
-        const isDepsChanged = prevDeps?.length !== deps.length || (prevDeps ?? []).some((devVal, index) => devVal !== deps[index]);
-        if (sortedTreeRef.current === null || prevTree !== tree || sorting !== prevSorting || isDepsChanged) {
-            sortedTreeRef.current = tree.sort({ sorting, sortBy });
-        }
-        return sortedTreeRef.current;
-    }, [tree, sorting, ...deps]);
+    const sortTree = useUpdateTree({
+        tree,
+        shouldUpdate: () => sorting !== prevSorting,
+        update: (currentTree) => currentTree.sort({ sorting, sortBy }),
+    }, [sorting, ...deps]);
 
     if (isLoading) {
         return tree;
