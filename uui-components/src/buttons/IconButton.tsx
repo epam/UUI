536a645f9dc5f2f1e54c07bcc978e770e8a4fb63
Ihor Korenets <Ihor_korenets@epam.com>
2024-03-06 14:17:57 +0200

    [IconButton]: fixed isDropdown prop to show icon.

--- uui-components/src/buttons/IconButton.tsx
+++ uui-components/src/buttons/IconButton.tsx
@@ -3,6 +3,7 @@ import { IDropdownToggler, Icon } from '@epam/uui-core';
 import { Clickable, ClickableComponentProps } from '../widgets';
 import { IconContainer } from '../layout';
 import css from './IconButton.module.scss';
+import { ReactComponent as foldingArrow } from '@epam/assets/icons/navigation-chevron_down-outline.svg';
 
 export type IconButtonProps = ClickableComponentProps & IDropdownToggler & {
     /** Icon can be a React element (usually an SVG element) */
@@ -12,6 +13,8 @@ export type IconButtonProps = ClickableComponentProps & IDropdownToggler & {
 };
 
 export const IconButton = React.forwardRef<HTMLButtonElement | HTMLAnchorElement, IconButtonProps>((props, ref) => {
+    const DropdownIcon = props.dropdownIcon ? props.dropdownIcon : foldingArrow;
+
     return (
         <Clickable
             { ...props }
@@ -20,6 +23,9 @@ export const IconButton = React.forwardRef<HTMLButtonElement | HTMLAnchorElement
             ref={ ref }
         >
             <IconContainer icon={ props.icon } />
+            { props.isDropdown && (
+                <IconContainer icon={ DropdownIcon } flipY={ props.isOpen } />
+            ) }
         </Clickable>
     );
 });
