50804cd274b8db560285c90b8349fb73c8ce2cb2
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-03-13 15:41:47 +0200

    [useTree]: some refactoring.

--- uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/types.ts
+++ uui-core/src/data/processing/views/tree/hooks/strategies/asyncTree/types.ts
@@ -1,7 +1,6 @@
-import { ItemsMap, ItemsStorage } from '../../../../../../processing';
-import { IMap, PatchItemsOptions, SortingOption } from '../../../../../../../types';
+import { IMap, PatchItemsOptions } from '../../../../../../../types';
 import { STRATEGIES } from '../constants';
-import { CommonDataSourceConfig } from '../types/common';
+import { CommonDataSourceConfig, FilterConfig, SearchConfig, SharedItemsState, SortConfig } from '../types/common';
 import { RecordStatus } from '../../../types';
 
 /**
@@ -9,7 +8,11 @@ import { RecordStatus } from '../../../types';
  */
 export interface AsyncTreeProps<TItem, TId, TFilter> extends
     CommonDataSourceConfig<TItem, TId, TFilter>,
-    PatchItemsOptions<TItem, TId> {
+    PatchItemsOptions<TItem, TId>,
+    SharedItemsState<TItem, TId>,
+    SearchConfig<TItem>,
+    SortConfig<TItem>,
+    FilterConfig<TItem, TFilter> {
 
     /**
      * Type of the tree to be supported.
@@ -27,54 +30,9 @@ export interface AsyncTreeProps<TItem, TId, TFilter> extends
     items?: TItem[];
 
     /**
-     * Map of loaded/shared items.
-     */
-    itemsMap?: ItemsMap<TId, TItem>;
-
-    /**
-     * Items updating listener, which fires on items loading/reloading/reset.
-     */
-    setItems?: ItemsStorage<TItem, TId>['setItems'];
-
-    /**
      * Map of items statuses.
      */
     itemsStatusMap?: IMap<TId, RecordStatus>;
-
-    /**
-     * A pure function that gets search value for each item.
-     * @default (item) => item.name.
-     */
-    getSearchFields?(item: TItem): string[];
-
-    /**
-     * A pure function that gets sorting value for current sorting value
-     */
-    sortBy?(item: TItem, sorting: SortingOption): any;
-
-    /**
-     * A pure function that returns filter callback to be applied for each item.
-     * The callback should return true, if item passed the filter.
-     */
-    getFilter?(filter: TFilter): (item: TItem) => boolean;
-
-    /**
-     * Enables sorting of search results by relevance.
-     * - The highest priority has records, which have a full match with a search keyword.
-     * - The lower one has records, which have a search keyword at the 0 position, but not the full match.
-     * - Then, records, which contain a search keyword as a separate word, but not at the beginning.
-     * - And the lowest one - any other match of the search keyword.
-     *
-     * Example:
-     * - `search`: 'some'
-     * - `record string`: 'some word', `rank` = 4
-     * - `record string`: 'someone', `rank` = 3
-     * - `record string`: 'I know some guy', `rank` = 2
-     * - `record string`: 'awesome', `rank` = 1
-     *
-     * @default true
-     */
-    sortSearchByRelevance?: boolean;
 
     /**
      * Disables loading of records, if isLoaded = true.
