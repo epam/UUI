5b3cf0eea71f22bccd68c111f6b21596d3e77878
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-03-20 19:07:42 +0300

    update md example content

--- uui-docs/src/demoData/slateMdSerializationInitialData.ts
+++ uui-docs/src/demoData/slateMdSerializationInitialData.ts
@@ -1,12 +1,13 @@
-export const slateMdSerializationInitialData = `### Bullet lists:
+export const slateMdSerializationInitialData = `### Basic layout
+We support inline text styles: **bold**, _italic_, underlined, several UUI-friendly text colors: red, yellow, and green.
+Numbered lists:
+
+1. In edit mode, we detect  '1. ' and start list automatically
+1. You can use 'tab' / 'shift/tab' to indent the list
+Bullet lists:
 
 - Type '- ' to start the list
 - You can create multi-level lists with 'tab' / 'shift+tab'. Example:
   - Level 2
     - Level 3
-
-
-### Ordered lists:
-
-1. There's also support 3 levels of headers, [hyperlinks](hyperlinks), **bold and** _emphasis_.
 `;
