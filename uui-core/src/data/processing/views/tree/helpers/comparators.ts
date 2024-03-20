653bccfd05369bf1868024340b7aac4e25015472
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-03-20 17:19:59 +0200

    [useTree]: ordering of patch before zip (only patch sorting part).

---
+++ uui-core/src/data/processing/views/tree/helpers/comparators.ts
@@ -0,0 +1,55 @@
+import { ApplySortOptions } from '../newTree/treeState/types';
+
+export const buildComparators = <TItem, TId, TFilter>(options: ApplySortOptions<TItem, TId, TFilter>) => {
+    const compareScalars = new Intl.Collator(undefined, { numeric: true, sensitivity: 'base' }).compare;
+    const comparators: ((a: TItem, b: TItem) => number)[] = [];
+
+    if (options.sorting) {
+        options.sorting.forEach((sortingOption) => {
+            const sortByFn = options.sortBy || ((i: TItem) => i[sortingOption.field as keyof TItem] || '');
+            const sign = sortingOption.direction === 'desc' ? -1 : 1;
+            comparators.push((a, b) => sign * compareScalars(sortByFn(a, sortingOption) + '', sortByFn(b, sortingOption) + ''));
+        });
+    }
+
+    return comparators;
+};
+
+export const composeComparetors = <TItem>(comparators: ((a: TItem, b: TItem) => number)[]) => {
+    return (a: TItem, b: TItem) => {
+        for (let n = 0; n < comparators.length; n++) {
+            const compare = comparators[n];
+            const result = compare(a, b);
+            if (result !== 0) {
+                return result;
+            }
+        }
+        return 0;
+    };
+};
+
+export const buildSorter = <TItem, TId, TFilter>(options: ApplySortOptions<TItem, TId, TFilter>) => {
+    const comparators = buildComparators(options);
+    const composedComparator = composeComparetors(comparators);
+    return (items: TItem[]) => {
+        if (comparators.length === 0) {
+            return items;
+        }
+
+        const indexes = new Map<TItem, number>();
+        items.forEach((item, index) => indexes.set(item, index));
+
+        const comparer = (a: TItem, b: TItem) => {
+            const result = composedComparator(a, b);
+            if (result !== 0) {
+                return result;
+            }
+            // to make sort stable, compare items indices if other comparers return 0 (equal)
+            return indexes.get(a) - indexes.get(b);
+        };
+
+        items = [...items];
+        items.sort(comparer);
+        return items;
+    };
+};
--- uui-core/src/data/processing/views/tree/hooks/common/usePatchTree.ts
+++ uui-core/src/data/processing/views/tree/hooks/common/usePatchTree.ts
@@ -1,14 +1,116 @@
 import { useMemo } from 'react';
-import { TreeState } from '../../newTree';
-import { PatchItemsOptions } from '../../../../../../types';
+import { TreeState, cloneMap, newMap } from '../../newTree';
+import { DataSourceState, IImmutableMap, IMap, PatchItemsOptions } from '../../../../../../types';
+import { SortConfig } from '../strategies/types';
+import { buildComparators, buildSorter, composeComparetors } from '../../helpers';
+import { PatchOrderingTypes } from '../../PatchOrderingMap';
 
-export interface UsePatchTreeProps<TItem, TId> extends PatchItemsOptions<TItem, TId> {
+export interface UsePatchTreeProps<TItem, TId, TFilter = any> extends PatchItemsOptions<TItem, TId>, SortConfig<TItem> {
     tree: TreeState<TItem, TId>;
+    sorting: DataSourceState<TFilter, TId>['sorting'];
+    complexIds?: boolean;
+    getParentId?: (item: TItem) => TId;
+    getId: (item: TItem) => TId;
 }
 
-export function usePatchTree<TItem, TId>(
-    { tree, patchItems, isDeleted, getPosition }: UsePatchTreeProps<TItem, TId>,
+const groupByParentId = <TItem, TId>(
+    patchItems: IMap<TId, TItem> | IImmutableMap<TId, TItem>,
+    getParentId?: (item: TItem) => TId,
+    complexIds?: boolean,
+): IMap<TId, TItem[]> => {
+    const patchByParentId = newMap<TId, TItem[]>({ getParentId, complexIds });
+    for (const [, item] of patchItems) {
+        const parentId = getParentId?.(item);
+        if (!patchByParentId.has(parentId)) {
+            patchByParentId.set(parentId, []);
+        }
+        const prevItems = patchByParentId.get(parentId);
+        patchByParentId.set(parentId, [...prevItems, item]);
+    }
+    return patchByParentId;
+};
+
+export const sortPatchByParentId = <TItem, TId, TFilter>(
+    groupedByParentId: IMap<TId, TItem[]>,
+    getNewItemPosition: PatchItemsOptions<TItem, TId>['getNewItemPosition'],
+    patchItemsAtLastSort: IMap<TId, TItem> | IImmutableMap<TId, TItem>,
+    sortBy: SortConfig<TItem>['sortBy'],
+    sorting: DataSourceState<TFilter, TId>['sorting'],
+    getId: (item: TItem) => TId,
+    getParentId: (item: TItem) => TId,
+) => {
+    const comparators = buildComparators({ sorting, sortBy });
+    const composedComparator = composeComparetors(comparators);
+
+    const sorted = cloneMap(groupedByParentId);
+    for (const [parentId, items] of sorted) {
+        const indexes = new Map<TItem, number>();
+        items.forEach((item, index) => indexes.set(item, index));
+
+        const sortedItems = [...items].sort((a, b) => {
+            const aId = getId(a);
+            const bId = getId(b);
+
+            if (!patchItemsAtLastSort.has(bId) && !patchItemsAtLastSort.has(aId)) {
+                const aPosition = getNewItemPosition(b) === PatchOrderingTypes.BOTTOM ? -1 : 1;
+                const bPosition = getNewItemPosition(b) === PatchOrderingTypes.BOTTOM ? -1 : 1;
+                return (aPosition === bPosition) ? -aPosition : aPosition - bPosition;
+            }
+
+            if (!patchItemsAtLastSort.has(bId)) {
+                return getNewItemPosition(b) === PatchOrderingTypes.BOTTOM ? -1 : 1;
+            }
+
+            if (!patchItemsAtLastSort.has(aId)) {
+                return getNewItemPosition(a) === PatchOrderingTypes.BOTTOM ? 1 : -1;
+            }
+            const prevBParentId = getParentId(patchItemsAtLastSort.get(bId));
+            const bParentId = getParentId(b);
+            if (prevBParentId !== bParentId) {
+                return -1;
+            }
+            const prevAParentId = getParentId(patchItemsAtLastSort.get(aId));
+            const aParentId = getParentId(a);
+            if (prevAParentId !== aParentId) {
+                return -1;
+            }
+
+            const result = composedComparator(patchItemsAtLastSort.get(aId), patchItemsAtLastSort.get(bId));
+            if (result === 0) {
+                return indexes.get(a) - indexes.get(b);
+            }
+            return result;
+        });
+
+        sorted.set(parentId, sortedItems);
+    }
+    return sorted;
+};
+
+const getSortedPatchByParentId = <TItem, TId, TFilter>(
+    patchItems: IMap<TId, TItem> | IImmutableMap<TId, TItem>,
+    patchItemsAtLastSort: IMap<TId, TItem> | IImmutableMap<TId, TItem>,
+    getNewItemPosition: PatchItemsOptions<TItem, TId>['getNewItemPosition'],
+    sortBy: SortConfig<TItem>['sortBy'],
+    sorting: DataSourceState<TFilter, TId>['sorting'],
+    getId?: (item: TItem) => TId,
+    getParentId?: (item: TItem) => TId,
+    complexIds?: boolean,
+) => {
+    const grouped = groupByParentId(patchItems, getParentId, complexIds);
+    return sortPatchByParentId(grouped, getNewItemPosition, patchItemsAtLastSort, sortBy, sorting, getId, getParentId);
+};
+
+export function usePatchTree<TItem, TId, TFilter = any>(
+    { tree, patchItems, getNewItemPosition, isDeleted, getPosition, sorting, sortBy, getId, getParentId, complexIds }: UsePatchTreeProps<TItem, TId, TFilter>,
 ) {
+    const patchItemsAtLastSort = useMemo(() => patchItems, [sorting]);
+
+    const sortedPatch = useMemo(
+        () => getSortedPatchByParentId(patchItems, patchItemsAtLastSort, getNewItemPosition, sortBy, sorting, getId, getParentId, complexIds),
+        [patchItems, sorting],
+    );
+
     return useMemo(() => {
         return tree.patchItems({ patchItems, isDeleted, getPosition });
     }, [tree, patchItems]);
