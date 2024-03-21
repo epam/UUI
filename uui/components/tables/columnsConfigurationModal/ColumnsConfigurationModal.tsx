825b6c012f952a80462b98996566eedbcbfc39a9
Ihor Korenets <Ihor_korenets@epam.com>
2024-03-21 17:38:56 +0200

    [Tokens page]: fixed copied token,[ ColumnsConfigurationModal]: fixed counter.

--- uui/components/tables/columnsConfigurationModal/ColumnsConfigurationModal.tsx
+++ uui/components/tables/columnsConfigurationModal/ColumnsConfigurationModal.tsx
@@ -13,7 +13,7 @@ import { FlexRow, FlexSpacer, Panel, ScrollBars } from '../../layout';
 import { Button, LinkButton } from '../../buttons';
 import { Dropdown, DropdownMenuButton, ModalBlocker, ModalFooter, ModalHeader, ModalWindow, Tooltip } from '../../overlays';
 import { Text } from '../../typography';
-import { Badge } from '../../widgets';
+import { CountIndicator } from '../../widgets';
 import { SearchInput } from '../../inputs';
 
 import { i18n as uuiI18n } from '../../../i18n';
@@ -32,7 +32,8 @@ const renderGroupTitle = (title: string, amount: number) => (
         <Text cx={ styles.groupTitleText } fontWeight="600" lineHeight="24" fontSize="14">
             {title}
         </Text>
-        <Badge cx={ styles.groupTitleBadge } caption={ amount } color="neutral" size="18" />
+        <CountIndicator caption={ amount } color="neutral" size="18" />
+        <FlexSpacer />
     </FlexRow>
 );
 
