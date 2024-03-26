ab89517eda0e9b3b0de5bc801646af83536fa042
Ihor Korenets <Ihor_korenets@epam.com>
2024-03-26 15:11:38 +0200

    [PickerTogglerTag]: fixed TS error

--- uui/components/pickers/PickerTogglerTag.tsx
+++ uui/components/pickers/PickerTogglerTag.tsx
@@ -3,14 +3,14 @@ import * as types from '../types';
 import { Tag, TagProps } from '../widgets';
 import { DataRowProps } from '@epam/uui-core';
 
-export type PickerTogglerTagProps<TItem, TId> = TagProps & {
+export interface PickerTogglerTagProps<TItem, TId> extends TagProps {
     /** Defines component size */
     size?: types.ControlSize;
     /** If this is true, then the PickerTogglerTag will be an additional tag with the number of collapsed elements in the caption. */
     isCollapsed?: boolean;
     /** Defines row props (see more: uui-components/src/pickers/PickerToggler.tsx PickerTogglerProps<TItem = any, TId = any>) */
     rowProps?: DataRowProps<TItem, TId>;
-};
+}
 
 const getPickerTogglerButtonSize = (propSize?: types.ControlSize):TagProps['size'] => {
     switch (propSize) {
@@ -29,7 +29,7 @@ const getPickerTogglerButtonSize = (propSize?: types.ControlSize):TagProps['size
     }
 };
 
-export const PickerTogglerTag = React.forwardRef((props: PickerTogglerTagProps<any, any>, ref: React.Ref<HTMLSpanElement>): JSX.Element => {
+export const PickerTogglerTag = React.forwardRef((props: PickerTogglerTagProps<any, any>, ref: React.Ref<HTMLElement>) => {
     const tagProps = {
         ...props,
         tabIndex: -1,
