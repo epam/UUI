032a37fa119c0f88536ba5cf72d8c6d92e7afcc8
Ihor Korenets <Ihor_korenets@epam.com>
2024-04-08 12:23:20 +0300

    [RadioInput]: refactored tests

--- uui/components/inputs/__tests__/RadioInput.test.tsx
+++ uui/components/inputs/__tests__/RadioInput.test.tsx
@@ -3,13 +3,24 @@ import { RadioInput } from '../RadioInput';
 import { renderSnapshotWithContextAsync } from '@epam/uui-test-utils';
 
 describe('RadioInput', () => {
-    it('should be rendered correctly', async () => {
+    it('should be rendered correctly with minimum props', async () => {
         const tree = await renderSnapshotWithContextAsync(<RadioInput value={ null } onValueChange={ jest.fn } />);
         expect(tree).toMatchSnapshot();
     });
 
-    it('should be rendered correctly', async () => {
-        const tree = await renderSnapshotWithContextAsync(<RadioInput value={ null } onValueChange={ jest.fn } size="18" label="Open" />);
+    it('should be rendered correctly with maximum', async () => {
+        const tree = await renderSnapshotWithContextAsync(
+            <RadioInput
+                value={ null }
+                onValueChange={ jest.fn }
+                size="18"
+                label="Open"
+                isDisabled={ true }
+                isInvalid={ true }
+                isReadonly={ true }
+                isRequired={ true }
+            />,
+        );
         expect(tree).toMatchSnapshot();
     });
 });
