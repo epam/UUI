7df3e7c82efd0596b60657cd5a96fdc1a52af463
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-04-09 11:53:57 +0300

    [UUICore]: added tests for range.

--- uui-core/src/helpers/range.ts
+++ uui-core/src/helpers/range.ts
@@ -1,13 +1,25 @@
 function rangeFromTo(start: number, end: number, step?: number) {
     const defaultStepForRange = (end >= start) ? 1 : -1;
-    const stepForRangeValues = step ?? defaultStepForRange;
 
     return Array.from({ length: (end - start) / (step || defaultStepForRange) }, (_, index) => {
-        return start + (index * stepForRangeValues);
+        if (step === 0) {
+            return 1;
+        }
+
+        return start + (index * (step ?? defaultStepForRange));
     });
 }
 
-export function range(start: number, end: number, step?: number) {
+/**
+ * Factory of numeric sequences with the exact step.
+ * @param start - a start number of the sequence inclusively.
+ * @param end - an end number of the sequence exclusively.
+ * @param step - step of the progression. Defaults:
+ * - if start <= end, step = 1
+ * - if start > end, step = -1.
+ * @returns a sequence of numbers in progression, described by bounds with a specific step.
+ */
+export function range(start: number, end?: number, step?: number) {
     if (end === undefined) {
         return rangeFromTo(0, start, step);
     }
