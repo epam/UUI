7990b5ed7a558b58c1ea961990252de8c1ab0a54
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-04-10 11:13:00 +0300

    remove comments

--- next-app/app/AppView.tsx
+++ next-app/app/AppView.tsx
@@ -62,16 +62,3 @@ export function AppView({ children }: PropsWithChildren) {
         </UuiContext.Provider>
     );
 }
-
-// import 'normalize.css';
-// import '../styles/globals.scss';
-// import '@epam/uui-components/styles.css';
-// import '@epam/promo/styles.css';
-// import '@epam/uui/styles.css';
-// import '@epam/uui-editor/styles.css';
-// import { Blocker, ErrorHandler } from "@epam/promo";
-// import { AppHeader } from "../components/AppHeader";
-// import { SideBar } from "../components/SideBar";
-// import { Modals, Snackbar } from "@epam/uui-components";
-// import { DragGhost } from "@epam/uui-core";
-// import type { ComponentType } from "react";
