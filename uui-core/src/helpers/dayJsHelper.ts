985d494489edaf976fdab9268e556703f43efb25
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-04-11 11:06:15 +0300

    Fix dayjs helpers

--- uui-core/src/helpers/dayJsHelper.ts
+++ uui-core/src/helpers/dayJsHelper.ts
@@ -3,13 +3,13 @@ import isSameOrBefore from 'dayjs/plugin/isSameOrBefore';
 import isSameOrAfter from 'dayjs/plugin/isSameOrAfter';
 
 let _extended = false;
-const dayjs = ((...args: any[]) => {
+const dayjs = /* @__PURE__ */Object.assign((...args: any[]) => {
     if (!_extended) {
         dayjsOrig.extend(isSameOrBefore);
         dayjsOrig.extend(isSameOrAfter);
         _extended = true;
     }
     return dayjsOrig(...args);
-}) as typeof dayjsOrig;
+}, dayjsOrig) as typeof dayjsOrig;
 
 export { dayjs, type Dayjs };
