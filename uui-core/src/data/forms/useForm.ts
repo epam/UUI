f14d98f3a2c683e8575f34bfa22f0d4896e69a8e
Ihor Korenets <Ihor_korenets@epam.com>
2024-04-16 17:15:48 +0300

    [Form]: added nextLocation and currentLocation data into handleLeave method. Added example into sandbox ProjectTasksDemo

--- uui-core/src/data/forms/useForm.ts
+++ uui-core/src/data/forms/useForm.ts
@@ -5,7 +5,7 @@ import {
     validateServerErrorState,
 } from '../../data/validation';
 import { useForceUpdate } from '../../hooks';
-import { UuiContexts } from '../../types/contexts';
+import { UuiContexts, Link } from '../../types';
 import { ValidationState } from '../lenses';
 import { useUuiContext } from '../../services';
 import { LensBuilder } from '../lenses/LensBuilder';
@@ -88,12 +88,11 @@ export function useForm<T>(props: UseFormProps<T>): IFormApi<T> {
         context.uuiUserSettings.set(props.settingsKey, null);
     }, [context.uuiUserSettings, props.settingsKey]);
 
-    const handleLeave = useCallback(() => {
+    const handleLeave = useCallback(async (nextLocation?: Link, currentLocation?: Link) => {
         if (props.beforeLeave) {
-            return props.beforeLeave().then((res) => {
-                if (res) return handleSave(true);
-                removeUnsavedChanges();
-            });
+            const res = await props.beforeLeave(nextLocation, currentLocation);
+            if (res) return handleSave(true);
+            removeUnsavedChanges();
         }
         return Promise.resolve();
     }, [
