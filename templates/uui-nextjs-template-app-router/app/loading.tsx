ada07d01e95d0de5a4f51d608f7f65c29e7911fb
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-04-16 12:35:47 +0300

    update examples

--- templates/uui-nextjs-template-app-router/app/loading.tsx
+++
@@ -1,7 +0,0 @@
-"use client";
-
-import { Blocker } from "@epam/uui-components";
-
-export default function Loading() {
-    return <Blocker isEnabled />;
-}
--- templates/uui-nextjs-template-app-router/components/AppContext.tsx
+++
@@ -1,8 +0,0 @@
-import { PropsWithChildren } from "react";
-import { getAppContext } from "../helpers/appContext";
-import { AppView } from "./AppView";
-
-export default async function AppContext({ children }: PropsWithChildren) {
-    const appContext = await getAppContext();
-    return <AppView appContext={appContext}>{children}</AppView>;
-}
