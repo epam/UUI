b0294d75154a6256e377a807fb4e419db5bfcca7
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-04-16 15:55:38 +0300

    Create Rollup plugin to mark some statements as pure. Migrate to newer Rollup version.

---
+++ uui-build/rollup/plugins/annotatePureFunctionCallsPlugin.js
@@ -0,0 +1,46 @@
+const MagicString = require('magic-string');
+
+const PURE_ANNOTATION = '/* @__PURE__ */';
+const PLUGIN_NAME = 'annotate-pure-function-calls-plugin';
+
+module.exports = annotatePureFunctionCallsPlugin;
+
+/**
+ * THis plugin only changes the resulting chunks only.
+ * It adds @__PURE__ annotation before every function call listed in the pureFunctions parameter.
+ *
+ * @param pluginOpts
+ * @param pluginOpts.pureFunctions
+ */
+function annotatePureFunctionCallsPlugin(pluginOpts = { pureFunctions: [] }) {
+    const { pureFunctions = [] } = pluginOpts;
+    if (!pureFunctions?.length) {
+        throw new Error(`[${PLUGIN_NAME}] 'pureFunctions' parameter is mandatory`);
+    }
+
+    const replaceWhat = new RegExp(
+        `(=|export default)(\\s)+(?<!${escapeForRegex(PURE_ANNOTATION)})(${pureFunctions.map(escapeForRegex).join('|')})(<.+>)?(\\()`,
+        'g',
+    );
+    const replaceTo = `$1$2${PURE_ANNOTATION}$3$4$5`;
+
+    return {
+        name: PLUGIN_NAME,
+        renderChunk(code, chunk, options) {
+            const codeAfterReplace = code.replace(replaceWhat, replaceTo);
+            if (codeAfterReplace !== code) {
+                const magicString = new MagicString(codeAfterReplace);
+                const result = { code: magicString.toString() };
+                if (options.sourcemap) {
+                    result.map = magicString.generateMap({ hires: true });
+                }
+                return result;
+            }
+            return null;
+        },
+    };
+}
+
+function escapeForRegex(str) {
+    return str.replace(/[-[\]/{}()*+?.\\^$|]/g, '\\$&');
+}
