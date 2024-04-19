1d8d96d5519437dd0b47b9dc3ae0ef22b67172e3
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-04-19 17:03:59 +0300

    [useTree]: Patch with fixItemBetweenSortings moving between parents fixed.

--- app/src/demo/tables/editableTable/helpers.ts
+++ app/src/demo/tables/editableTable/helpers.ts
@@ -1,43 +1,5 @@
-import { IImmutableMap, ITree, getOrderBetween, maxOrderStr, minOrderStr } from '@epam/uui-core';
+import { IImmutableMap, ITree } from '@epam/uui-core';
 import { Task } from './types';
-
-/**
- * Finds a string, which is placed alphabetically in desired position of the list.
- * If relativeTo is not specified, it is assumed that you need 'before' the first or 'after' the last item
- * If relativeTo is specified, location is before/after the relativeTo string.
- *  @see getOrderString function for details about order strings.
- */
-export function getInsertionOrder(existingOrders: string[], position: 'before' | 'after', relativeTo?: string) {
-    if (!relativeTo) {
-        if (position === 'before') {
-            // inserting at the top of the list, is the same as inserting after the minOrder
-            relativeTo = minOrderStr;
-            position = 'after';
-        } else if (position === 'after') {
-            // inserting at the bottom of the list, is the same as inserting before the maxOrder
-            relativeTo = maxOrderStr;
-            position = 'before';
-        }
-    }
-
-    if (position === 'before') {
-        let maxOrder = minOrderStr;
-        existingOrders.forEach((order) => {
-            if (order < relativeTo && order > maxOrder) {
-                maxOrder = order;
-            }
-        });
-        return getOrderBetween(maxOrder, relativeTo);
-    } else {
-        let minOrder = maxOrderStr;
-        existingOrders.forEach((order) => {
-            if (order > relativeTo && order < minOrder) {
-                minOrder = order;
-            }
-        });
-        return getOrderBetween(relativeTo, minOrder);
-    }
-}
 
 const findAllChildren = (tree: ITree<Task, number>, parentTaskId: number) => {
     const { ids: children } = tree.getItems(parentTaskId);
@@ -65,7 +27,7 @@ export const deleteTaskWithChildren = (taskToDelete: Task | null, tasks: IImmuta
     let currentTasks = tasks;
     const childrenIds = findAllChildren(tree, taskToBeDeleted.id);
     [taskToBeDeleted.id, ...childrenIds].forEach((id) => {
-        currentTasks = currentTasks.set(id, { ...currentTasks.get(id), isDeleted: true });
+        currentTasks = currentTasks.set(id, { ...tree.getById(id) as Task, isDeleted: true });
     });
 
     return currentTasks;
