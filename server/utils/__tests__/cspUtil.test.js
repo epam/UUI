416094080452a42bc5000f419168f7802034d108
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-04-24 15:13:23 +0300

    Temporarily enable Cloudflare domain for custom theme testing

--- server/utils/__tests__/cspUtil.test.js
+++ server/utils/__tests__/cspUtil.test.js
@@ -4,7 +4,7 @@ describe('cspUtil', () => {
     describe('getCspHeaderValue', () => {
         it('should create CSP for PROD', () => {
             const headerValue = getCspHeaderValue(false);
-            expect(headerValue).toEqual("default-src 'self' https://*.epam.com; style-src 'self' 'unsafe-inline' https://*.epam.com https://cdnjs.cloudflare.com/ https://fonts.googleapis.com/; font-src 'self' https://*.epam.com https://fonts.gstatic.com/; connect-src 'self' https://*.epam.com https://api.amplitude.com/ wss://menu.epam.com/* https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com; frame-src *; img-src * data: ; script-src 'self' https://*.epam.com https://www.googletagmanager.com/ https://www.google-analytics.com/;");
+            expect(headerValue).toEqual("default-src 'self' https://*.epam.com; style-src 'self' 'unsafe-inline' https://*.pages.dev https://*.epam.com https://cdnjs.cloudflare.com/ https://fonts.googleapis.com/; font-src 'self' https://*.pages.dev https://*.epam.com https://fonts.gstatic.com/; connect-src 'self' https://*.pages.dev https://*.epam.com https://api.amplitude.com/ wss://menu.epam.com/* https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com; frame-src *; img-src * data: ; script-src 'self' https://*.epam.com https://www.googletagmanager.com/ https://www.google-analytics.com/;");
         });
         it('should create CSP for DEV', () => {
             const headerValue = getCspHeaderValue(true);
