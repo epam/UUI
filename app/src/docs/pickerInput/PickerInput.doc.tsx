c6d858f0efd3c88aeda70cd911116e7194779f06
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-04-24 17:48:18 +0300

    [Pickers]: some debugging.

--- app/src/docs/pickerInput/PickerInput.doc.tsx
+++ app/src/docs/pickerInput/PickerInput.doc.tsx
@@ -63,7 +63,7 @@ export class PickerInputDoc extends BaseDocsBlock {
             <>
                 <EditableDocContent fileName="pickerInput-descriptions" />
                 { this.renderSectionTitle('Examples') }
-                <DocExample title="Basic" path="./_examples/pickerInput/ArrayPickerInput.example.tsx" />
+                {/* <DocExample title="Basic" path="./_examples/pickerInput/ArrayPickerInput.example.tsx" />
                 <DocExample title="Lazy list" path="./_examples/pickerInput/LazyPickerInput.example.tsx" />
                 <DocExample title="Lazy tree" path="./_examples/pickerInput/LazyTreeInput.example.tsx" />
                 <DocExample title="Async list" path="./_examples/pickerInput/AsyncPickerInput.example.tsx" />
@@ -73,14 +73,14 @@ export class PickerInputDoc extends BaseDocsBlock {
                 <DocExample title="Picker toggler configuration and options" path="./_examples/pickerInput/TogglerConfiguration.example.tsx" />
                 <DocExample title="Setting row options" path="./_examples/pickerInput/GetRowOptions.example.tsx" />
                 <DocExample title="Getting selected entity" path="./_examples/pickerInput/ValueType.example.tsx" />
-                <DocExample title="Search positions" path="./_examples/pickerInput/SearchPositions.example.tsx" />
+                <DocExample title="Search positions" path="./_examples/pickerInput/SearchPositions.example.tsx" /> */}
                 <DocExample title="Flatten search results mode" path="./_examples/pickerInput/LazyTreeSearch.example.tsx" />
-                <DocExample title="Turn off 'Select All' button in footer" path="./_examples/pickerInput/PickerInputTurnOffSelectAll.example.tsx" />
+                {/* <DocExample title="Turn off 'Select All' button in footer" path="./_examples/pickerInput/PickerInputTurnOffSelectAll.example.tsx" />
                 <DocExample title="Open picker in modal" path="./_examples/pickerInput/EditMode.example.tsx" />
                 <DocExample title="Picker with changed array of items" path="./_examples/pickerInput/PickerWithChangingItemsArray.example.tsx" />
                 <DocExample title="Linked pickers" path="./_examples/pickerInput/LinkedPickers.example.tsx" />
                 <DocExample title="Change portal target and dropdown placement" path="./_examples/pickerInput/ConfigurePortalTargetAndPlacement.example.tsx" />
-                <DocExample title="Custom toggler tag render" path="./_examples/pickerInput/PickerTogglerTagDemo.example.tsx" />
+                <DocExample title="Custom toggler tag render" path="./_examples/pickerInput/PickerTogglerTagDemo.example.tsx" /> */}
             </>
         );
     }
