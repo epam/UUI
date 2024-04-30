6b543db1cbbbf64f1e3b5d8bca04e9ac7146545a
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-04-30 12:37:26 +0300

    Generate screenshots for newly added tests (Accordion & Alert)

--- app/src/docs/Alert.doc.tsx
+++ app/src/docs/Alert.doc.tsx
@@ -42,7 +42,7 @@ export class AlertDoc extends BaseDocsBlock {
         preview: (docPreview: DocPreviewBuilder<uui.AlertProps | loveship.AlertProps | promo.AlertProps>) => {
             const TEST_DATA = {
                 icon: 'action-account-fill.svg',
-                actions1: '1 actions',
+                actions1: '1 action',
                 actions2: '2 actions',
                 children1line: <uui.Text size="30">Test Test</uui.Text>,
                 // eslint-disable-next-line
@@ -64,11 +64,11 @@ export class AlertDoc extends BaseDocsBlock {
                 id: TAlertPreview['Layout with icon'],
                 matrix: {
                     children: { values: [TEST_DATA.children1line, TEST_DATA.children2lines] },
+                    actions: { examples: [undefined, TEST_DATA.actions1, TEST_DATA.actions2] },
                     size: { examples: '*' },
                     color: { examples: ['info'] },
                     icon: { examples: [TEST_DATA.icon] },
                     onClose: { examples: [undefined, 'callback'] },
-                    actions: { examples: [undefined, TEST_DATA.actions1, TEST_DATA.actions2] },
                 },
                 cellSize: '260-120',
             });
@@ -76,11 +76,11 @@ export class AlertDoc extends BaseDocsBlock {
                 id: TAlertPreview['Layout without icon'],
                 matrix: {
                     children: { values: [TEST_DATA.children1line, TEST_DATA.children2lines] },
+                    actions: { examples: [undefined, TEST_DATA.actions1, TEST_DATA.actions2] },
                     size: { examples: '*' },
                     color: { examples: ['info'] },
                     icon: { examples: [undefined] },
                     onClose: { examples: [undefined, 'callback'] },
-                    actions: { examples: [undefined, TEST_DATA.actions1, TEST_DATA.actions2] },
                 },
                 cellSize: '260-120',
             });
