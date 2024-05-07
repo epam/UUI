664c885ceec1e4c5df4aefa637109a0dda5a5922
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-05-07 17:15:50 +0300

    Improve e2e API. Improve performance of e2e tests on CI. Fix some tests.

--- uui-e2e-tests/playwright.config.ts
+++ uui-e2e-tests/playwright.config.ts
@@ -10,7 +10,10 @@ const { UUI_APP_BASE_URL, UUI_APP_BASE_URL_CI } = readEnvFile();
 const timeout = isCi ? 20000 : 50000;
 const maxFailures = isCi ? 10 : undefined;
 const retries = isCi ? 1 : 0;
-const workers = isCi ? 1 : undefined;
+/**
+ * The fastest option (for both CI and Local) is to use default (undefined) amount of workers (which is 50% of CPU cores).
+ */
+const workers = isCi ? undefined : undefined;
 const forbidOnly = isCi;
 const trace = (isCi ? 'retry-with-trace' : 'retain-on-failure') as TraceMode;
 const server = {
@@ -42,6 +45,7 @@ export default defineConfig({
         ['json', { outputFile: outputJsonFile }],
     ],
     use: {
+        bypassCSP: true,
         baseURL: server.baseUrl,
         trace,
         ...SHARED_DEVICE_CFG.DEFAULT,
