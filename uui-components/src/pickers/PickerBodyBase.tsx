160a717ec0d320bf088de72b459282f4c447b510
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-05-09 12:22:41 +0300

    [FiltersPanel]: Fixed focus on search after body opening and focus on PickerToggler after closing in PickerInput.

--- uui-components/src/pickers/PickerBodyBase.tsx
+++ uui-components/src/pickers/PickerBodyBase.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import isEqual from 'react-fast-compare';
 
 import {
-    DataSourceListProps, DataSourceState, IEditable, IHasRawProps,
+    DataSourceListProps, DataSourceState, IEditable, IHasRawProps, isMobile,
 } from '@epam/uui-core';
 
 export interface PickerBodyBaseProps extends DataSourceListProps, IEditable<DataSourceState>, IHasRawProps<React.HTMLAttributes<HTMLDivElement>> {
@@ -19,7 +19,15 @@ export interface PickerBodyBaseProps extends DataSourceListProps, IEditable<Data
 export abstract class PickerBodyBase<TProps extends PickerBodyBaseProps> extends React.Component<TProps> {
     needFocusSearch = this.showSearch();
     searchRef = React.createRef<HTMLInputElement>();
+
     componentDidUpdate(prevProps: PickerBodyBaseProps) {
+        // Focusing of searchInput is done via ref.focus(), but not via autoFocus on SearchInput,
+        // because otherwise, after body close, focus on PickerToggler is lost and on  press Tab, it is moved to document.body.
+        if (this.needFocusSearch && !isMobile()) {
+            this.searchRef.current?.focus({ preventScroll: true });
+            this.needFocusSearch = false;
+        }
+
         if (prevProps.rows.length !== this.props.rows.length || (!isEqual(prevProps.value.checked, this.props.value.checked) && !this.props.fixedBodyPosition)) {
             this.props.scheduleUpdate?.();
         }
