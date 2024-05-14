7982d39dc81f98b4aa6cf7e80fb688cfc4800d11
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-05-14 13:18:16 +0300

    [VirtualList]: fixed scrolling on focus to the partially visible row.

--- uui-core/src/hooks/useVirtualList/useVirtualList.ts
+++ uui-core/src/hooks/useVirtualList/useVirtualList.ts
@@ -67,7 +67,7 @@ export function useVirtualList<List extends HTMLElement = any, ScrollContainer e
         const focusedIndexOffset = rowOffsets.current[value.focusedIndex] || 0;
         const focusedIndexHeight = rowHeights.current[value.focusedIndex] || 0;
         const scrollBottom = scrollTop + clientHeight - listOffset;
-        if (focusedIndexOffset < scrollTop - focusedIndexHeight || scrollBottom < focusedIndexOffset) {
+        if (focusedIndexOffset < (scrollTop - focusedIndexHeight / 2) || scrollBottom < (focusedIndexOffset + focusedIndexHeight)) {
             const middleOffset = focusedIndexOffset - clientHeight / 2 + focusedIndexHeight / 2;
             const indexToScroll = rowOffsets.current.findIndex((rowOffset) => middleOffset <= rowOffset);
             scrollToIndex({ index: indexToScroll, behavior: 'smooth' });
