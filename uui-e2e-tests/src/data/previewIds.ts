bdb2b78ad5d7e0becedfd9b912ddf9a47d7cbae1
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-05-16 18:37:51 +0300

    Add E2E tests for Text component

--- uui-e2e-tests/src/data/previewIds.ts
+++ uui-e2e-tests/src/data/previewIds.ts
@@ -75,35 +75,32 @@ export enum TVerticalTabButtonPreview {
 }
 
 export enum TPickerInputPreview {
-    /* FORM */
+    // FORM
     'Form SingleSelect'= 'Form SingleSelect',
     'Form SingleSelect States'= 'Form SingleSelect States',
     'Form MultiSelect'= 'Form MultiSelect',
     'Form MultiSelect States'= 'Form MultiSelect States',
     'Form MultiSelect Multiline'= 'Form MultiSelect Multiline',
     'Form MultiSelect Multiline States'= 'Form MultiSelect Multiline States',
-    /* FORM OPENED */
+    // FORM OPENED
     'Form Opened SingleSelect' = 'Form Opened SingleSelect',
     'Form Opened MultiSelect' = 'Form Opened MultiSelect',
     'Form Opened SingleSelect Tree' = 'Form Opened SingleSelect Tree',
     'Form Opened MultiSelect Tree' = 'Form Opened MultiSelect Tree',
-
-    /* INLINE */
+    // INLINE
     'Inline SingleSelect'= 'Inline SingleSelect',
     'Inline SingleSelect States'= 'Inline SingleSelect States',
     'Inline MultiSelect'= 'Inline MultiSelect',
     'Inline MultiSelect States'= 'Inline MultiSelect States',
     'Inline MultiSelect Multiline'= 'Inline MultiSelect Multiline',
     'Inline MultiSelect Multiline States'= 'Inline MultiSelect Multiline States',
-
-    /* CELL */
+    // CELL
     'Cell SingleSelect'= 'Cell SingleSelect',
     'Cell SingleSelect States'= 'Cell SingleSelect States',
     'Cell MultiSelect'= 'Cell MultiSelect',
     'Cell MultiSelect States'= 'Cell MultiSelect States',
     'Cell MultiSelect Multiline'= 'Cell MultiSelect Multiline',
     'Cell MultiSelect Multiline States'= 'Cell MultiSelect Multiline States'
-
 }
 
 export enum TDatePickerPreview {
@@ -126,4 +123,11 @@ export enum TRangeDatePickerPreview {
 export enum TTextAreaPreview {
     Sizes = 'Sizes',
     States = 'States'
+}
+
+export enum TTextPreview {
+    'Sizes'= 'Sizes',
+    'Font'= 'Font',
+    'Line Height' = 'Line Height',
+    Colors = 'Colors'
 }
