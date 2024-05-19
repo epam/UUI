320a02e2aeb4babd11c61078e5b621d91046d461
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-05-19 10:16:11 +0300

    add normalizeNode table todo

--- uui-editor/src/plugins/tablePlugin/TableElement.tsx
+++ uui-editor/src/plugins/tablePlugin/TableElement.tsx
@@ -33,7 +33,7 @@ const TableElement = withHOC(TableProvider, withRef<typeof PlateElement>(({ clas
     const colSizeOverrides = tableStore.colSizeOverrides();
     const currentColSizes = (
         !element.colSizes
-            ? getDefalutColSizes()
+            ? getDefalutColSizes() // TODO: move that to mormalizeNode
             : [...element.colSizes]
     ).map(
         (size, index) => colSizeOverrides.get(index) || size || EMPTY_COL_WIDTH,
