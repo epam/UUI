71e1e410e933c0ea2806878b5f3ba35890287339
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-05-19 11:04:42 +0300

    fix eslint

--- uui-editor/src/plugins/autoformatPlugin/formatUtils.ts
+++ uui-editor/src/plugins/autoformatPlugin/formatUtils.ts
@@ -1,7 +1,6 @@
 import { AutoformatBlockRule } from '@udecode/plate-autoformat';
-import { isType, PlateEditor } from '@udecode/plate-common';
+import { getParentNode, isElement, isType, PlateEditor } from '@udecode/plate-common';
 import { toggleList, unwrapList } from '@udecode/plate-list';
-import { getParentNode, isElement } from '@udecode/slate';
 import { INLINE_CODE_KEY } from '../inlineCodePlugin/constants';
 
 export const preFormat: AutoformatBlockRule['preFormat'] = (editor) =>
