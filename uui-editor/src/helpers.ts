dc2b1bc8dfb01d90e838fd66cc2895d7a3193c15
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-05-20 14:23:18 +0300

    add getMigratedPlateValue helper

--- uui-editor/src/helpers.ts
+++ uui-editor/src/helpers.ts
@@ -124,3 +124,12 @@ export const isSlateSchema = (value: EditorValue): value is SlateSchema => {
 export const isPlateValue = (value: EditorValue): value is Value => {
     return Array.isArray(value);
 };
+
+/** migrate deprecated slate format if needed */
+export const getMigratedPlateValue = (value: EditorValue): Value | undefined => {
+    if (!value) return undefined; // get rid of nulls
+    if (isSlateSchema(value)) {
+        return migrateSlateSchema(value);
+    }
+    return value;
+};
