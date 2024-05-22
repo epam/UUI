6346a36edee8443f5a730599494a179a12908573
Ihor Korenets <Ihor_korenets@epam.com>
2024-05-22 17:50:19 +0300

    [Modals]: made first test

---
+++ uui-components/src/overlays/__tests__/modals.test.tsx
@@ -0,0 +1,42 @@
+import React from 'react';
+import { fireEvent, renderWithContextAsync, screen } from '@epam/uui-test-utils';
+import { useUuiContext } from '@epam/uui-core';
+import { Button } from '../../buttons';
+import { Modals } from '../Modals';
+
+function TestContent() {
+    return (<div>Test content</div>);
+}
+
+function Test() {
+    const { uuiModals } = useUuiContext();
+    return (
+        <div>
+            <Button
+                caption="Show modal"
+                onClick={
+                    async () => {
+                        try {
+                            return await uuiModals
+                                .show((props) => <TestContent { ...props } />);
+                        } catch {
+                        }
+                    }
+                }
+            />
+            <Modals />
+        </div>
+    );
+}
+
+describe('Modals', () => {
+    it('should render children component', async () => {
+        const tree = await renderWithContextAsync(<Test />);
+        const modalElement = await screen.findByRole('button');
+
+        fireEvent.click(modalElement);
+        const testElement = await screen.findByText('Test content');
+        expect(testElement).toBeInTheDocument();
+        expect(tree).toMatchSnapshot();
+    });
+});
