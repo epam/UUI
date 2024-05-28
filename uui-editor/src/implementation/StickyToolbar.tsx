f5f8cbc84cb13b83b0f0852ac40492f4af6f6bc3
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-05-28 12:53:18 +0300

    fix toolbar focus

--- uui-editor/src/implementation/StickyToolbar.tsx
+++ uui-editor/src/implementation/StickyToolbar.tsx
@@ -1,39 +1,21 @@
-import { isBlock, useEditorState } from '@udecode/plate-common';
-
+import React, { MouseEventHandler } from 'react';
 import cx from 'classnames';
-import React, {
-    MouseEventHandler, useEffect, useRef, useState,
-} from 'react';
 
 import css from './StickyToolbar.module.scss';
 import { useLayer } from '@epam/uui-core';
+import { useEditorRef, useEventEditorSelectors } from '@udecode/plate-common';
 
 interface SidebarProps {
-    isReadonly: boolean;
     children: JSX.Element[];
 }
 
 // eslint-disable-next-line react/function-component-definition
-export const StickyToolbar: React.FC<SidebarProps> = ({ isReadonly, children }) => {
-    const editor = useEditorState(); // TODO: use useEditorRef
-    const isBlockSelected = isBlock(editor, editor.value);
-    const [isVisible, setIsVisible] = useState(false);
-    const sidebarRef = useRef<HTMLDivElement>(null);
-    const timeoutIdRef = useRef<ReturnType<typeof setTimeout>>(null);
+export const StickyToolbar: React.FC<SidebarProps> = ({ children }) => {
+    const editorRef = useEditorRef();
+    const focusedEditorId = useEventEditorSelectors.focus();
+    const isFocused = editorRef.id === focusedEditorId;
+
     const zIndex = useLayer()?.zIndex;
-
-    useEffect(() => {
-        const isSidebarVisible = true;
-
-        if (isSidebarVisible !== isVisible) {
-            // delay is used to make mouse click work on elements outside editor before they moved because of sidebar disappearing
-            timeoutIdRef.current = setTimeout(() => {
-                setIsVisible(isSidebarVisible);
-            }, 50);
-        }
-
-        return () => clearTimeout(timeoutIdRef.current);
-    }, [isBlockSelected, editor?.readOnly]);
 
     /**
      * Prevents unwanted event propagation of focus change (cursor hide)
@@ -44,7 +26,9 @@ export const StickyToolbar: React.FC<SidebarProps> = ({ isReadonly, children }) 
         event.stopPropagation();
     };
 
-    if (isReadonly || !isVisible) return null;
+    if (!isFocused) {
+        return null;
+    }
 
     return (
         <div style={ {
@@ -58,7 +42,6 @@ export const StickyToolbar: React.FC<SidebarProps> = ({ isReadonly, children }) 
             <div
                 onMouseDown={ onMouseDown }
                 className={ cx('slate-prevent-blur', css.sidebar) }
-                ref={ sidebarRef }
             >
                 { children }
             </div>
