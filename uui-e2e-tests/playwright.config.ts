357964c7fe95ef749eb0731c87743e60cd2cb8be
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-06-07 13:06:47 +0300

    Increase timeout for e2e in dev mode

--- uui-e2e-tests/playwright.config.ts
+++ uui-e2e-tests/playwright.config.ts
@@ -9,8 +9,8 @@ const { UUI_APP_BASE_URL, UUI_APP_BASE_URL_CI } = readEnvFile();
 
 const testTimeout = isCi ? 10000 : 30000;
 export const timeoutForFixture = isCi ? 20000 : 50000;
-// The "expect" timeout for slow tests. It should be less than "testTimeout".
-export const slowTestExpectTimeout = isCi ? 7000 : 15000;
+// The "expect" timeout for slow tests. It should not exceed "testTimeout".
+export const slowTestExpectTimeout = Math.min(20000, testTimeout);
 const maxFailures = isCi ? 10 : undefined;
 const retries = isCi ? 1 : 0;
 /**
