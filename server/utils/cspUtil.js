657bde8d3d0ee4205e9a631b23e9c1418e29a042
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-06-14 17:24:47 +0300

    Improve naming of some constants. Add comments.

--- server/utils/cspUtil.js
+++ server/utils/cspUtil.js
@@ -1,11 +1,12 @@
 module.exports = { getCspHeaderValue };
 
-const EXTRA_ASSETS_FOR_DEV = [
-    'http://localhost:*',
-].join(' ');
-
-// TODO: probably we should limit to specific pages deployed to CF, not entire CF.
-const CLOUDFLARE_PAGES = 'https://*.pages.dev';
+// Origins where custom themes are hosted
+const CUSTOM_THEME_ASSETS = {
+    // On localhost
+    LOCAL: 'http://localhost:*',
+    // On external server. TODO: probably we should limit to specific pages deployed to CF, not entire CF
+    CLOUDFLARE_PAGES: 'https://*.pages.dev',
+};
 
 /**
  * @param isDevServer {boolean}
@@ -16,20 +17,20 @@ function getCspHeaderValue(isDevServer) {
         "default-src 'self' https://*.epam.com; ",
         dir(
             "style-src 'self' 'unsafe-inline'",
-            isDevServer && EXTRA_ASSETS_FOR_DEV,
-            CLOUDFLARE_PAGES,
+            isDevServer && CUSTOM_THEME_ASSETS.LOCAL,
+            CUSTOM_THEME_ASSETS.CLOUDFLARE_PAGES,
             'https://*.epam.com https://cdnjs.cloudflare.com/ https://fonts.googleapis.com/',
         ),
         dir(
             "font-src 'self'",
-            isDevServer && EXTRA_ASSETS_FOR_DEV,
-            CLOUDFLARE_PAGES,
+            isDevServer && CUSTOM_THEME_ASSETS.LOCAL,
+            CUSTOM_THEME_ASSETS.CLOUDFLARE_PAGES,
             'https://*.epam.com https://fonts.gstatic.com/',
         ),
         dir(
             "connect-src 'self'",
-            isDevServer && EXTRA_ASSETS_FOR_DEV,
-            CLOUDFLARE_PAGES,
+            isDevServer && CUSTOM_THEME_ASSETS.LOCAL,
+            CUSTOM_THEME_ASSETS.CLOUDFLARE_PAGES,
             'https://*.epam.com https://api.amplitude.com/ wss://menu.epam.com/ https://*.google-analytics.com https://*.analytics.google.com https://*.googletagmanager.com',
         ),
         'frame-src *; ',
