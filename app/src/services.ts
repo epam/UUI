8613fd0865d185dbf0e79cd2bfa9ec4994292bcb
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-06-14 15:01:11 +0300

    Remove unused interface

--- app/src/services.ts
+++ app/src/services.ts
@@ -1,8 +1,4 @@
 import { CommonContexts } from '@epam/uui-core';
 import type { AppContext, TApi } from './data';
 
-export interface AppSettings {
-
-}
-
 export const svc: Partial<CommonContexts<TApi, AppContext>> = {};
