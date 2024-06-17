77bfb4c2f83770c96f9bb0aba534df25fa840f7c
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-06-17 11:39:40 +0300

    add type comments

--- uui-editor/src/plugins/notePlugin/types.ts
+++ uui-editor/src/plugins/notePlugin/types.ts
@@ -1,14 +1,20 @@
 import { WithToolbarButton } from '../../implementation/Toolbars';
 
+/** note entry config */
 export interface NoteEntryConfig extends NoteNodeProps {
+    /** type of note */
     type: `note-${string}`;
 }
 
 export interface NoteNodeProps {
+    /** border color */
     borderColor?: string;
+    /** background color */
     backgroundColor?: string;
 }
 
+/** note plugin options */
 export type NotePluginOptions = WithToolbarButton & {
+    /** notes list */
     notes?: NoteEntryConfig[];
 };
