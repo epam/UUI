0e9cf5be4b46ce7be4dcb00637a89120391b04c5
Dzmitry Tamashevich <diaamnj@mail.ru>
2024-06-20 11:37:21 +0300

    generate default notes

--- uui-editor/src/plugins/notePlugin/notePlugin.tsx
+++ uui-editor/src/plugins/notePlugin/notePlugin.tsx
@@ -3,42 +3,11 @@ import {
     createPluginFactory, PlatePlugin,
 } from '@udecode/plate-common';
 import { NotePluginBlock } from './NotePluginBlock';
-import { defaultNotesConfig, NODE_PLUGIN_KEY, NOTE_ERROR_PLUGIN_KEY, NOTE_ERROR_TYPE, NOTE_LINK_PLUGIN_KEY, NOTE_LINK_TYPE, NOTE_QUOTE_PLUGIN_KEY, NOTE_QUOTE_TYPE, NOTE_WARN_PLUGIN_KEY, NOTE_WARN_TYPE } from './constants';
+import { defaultNotesConfig, NODE_PLUGIN_KEY } from './constants';
 import { NoteEntryConfig, NoteNodeProps, NotePluginOptions } from './types';
 import { NoteButton } from './NoteBar';
 
-const defaultNodes = [
-    {
-        key: NOTE_ERROR_PLUGIN_KEY,
-        type: NOTE_ERROR_TYPE,
-        isElement: true,
-        isVoid: false,
-        component: NotePluginBlock,
-    },
-    {
-        key: NOTE_WARN_PLUGIN_KEY,
-        type: NOTE_WARN_TYPE,
-        isElement: true,
-        isVoid: false,
-        component: NotePluginBlock,
-    },
-    {
-        key: NOTE_LINK_PLUGIN_KEY,
-        type: NOTE_LINK_TYPE,
-        isElement: true,
-        isVoid: false,
-        component: NotePluginBlock,
-    },
-    {
-        key: NOTE_QUOTE_PLUGIN_KEY,
-        type: NOTE_QUOTE_TYPE,
-        isElement: true,
-        isVoid: false,
-        component: NotePluginBlock,
-    },
-];
-
-const createNote = (config: NoteEntryConfig): PlatePlugin => {
+const createPlugin = (config: NoteEntryConfig): PlatePlugin => {
     return {
         key: config.type,
         type: config.type,
@@ -55,16 +24,17 @@ const createNote = (config: NoteEntryConfig): PlatePlugin => {
     };
 };
 
-export const notePlugin = (...notes: NoteEntryConfig[]): PlatePlugin => {
+export const notePlugin = (...userNotes: NoteEntryConfig[]): PlatePlugin => {
+    const notes = !!userNotes.length ? userNotes : defaultNotesConfig;
     const createNotePlugin = createPluginFactory<AnyObject>({
         key: NODE_PLUGIN_KEY,
         isElement: true,
         isVoid: false,
         component: NotePluginBlock,
-        plugins: !!notes.length ? notes.map((config) => createNote(config)) : defaultNodes,
+        plugins: notes.map((config) => createPlugin(config)),
         options: {
             bottomBarButton: NoteButton,
-            notes: !!notes.length ? notes : defaultNotesConfig,
+            notes,
         } as NotePluginOptions,
     });
 
