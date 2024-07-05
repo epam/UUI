0bc4359f54034b9de89ab862cf0c15595f11d0b2
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-07-05 12:50:00 +0300

    Remove "only"

--- uui-e2e-tests/tests/screenshot.e2e.ts
+++ uui-e2e-tests/tests/screenshot.e2e.ts
@@ -19,7 +19,7 @@ const {
 const builder = new TestBuilder();
 
 builder
-    .only(badge, [
+    .add(badge, [
         { previewId: [TBadgePreview['Color Variants']], skins: SKINS.promo_loveship_electric },
         { previewId: [TBadgePreview['Size Variants']] },
         {
