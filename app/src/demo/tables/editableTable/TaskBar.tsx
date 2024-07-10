5e37d2b3f9bcfa7622c418c28f6221a2805cab1c
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-07-10 13:24:42 +0300

    [Timeline]: added deadlines and refactored.

--- app/src/demo/tables/editableTable/TaskBar.tsx
+++ app/src/demo/tables/editableTable/TaskBar.tsx
@@ -6,39 +6,28 @@ import { resources, statuses } from './demoData';
 import { uuiDayjs } from '../../../helpers';
 
 import css from './TaskBar.module.scss';
+import classNames from 'classnames';
+import { formatDatePickerDate, getDueDateFromTask, getEstimatedTo, getTaskBarWidth, getTaskColor, getTo, getWidth } from './helpers';
 
-const getTaskColor = (status: string) => statuses.find((s) => s.id === status)?.color ?? '#e1e3eb';
-const formatDate = (date: string) => {
-    if (!date?.length) {
-        return '';
-    }
-    return uuiDayjs.dayjs(date, 'YYYY-MM-DD').format('DD.MM.YYYY');
-};
-
-const getDueDate = (task: Task) => {
-    if (task.type === 'story') {
-        return uuiDayjs.dayjs(task.dueDate, 'YYYY-MM-DD').toDate();
-    }
-
-    const startDate = uuiDayjs.dayjs(task.exactStartDate, 'YYYY-MM-DD');
-
-    if (task.exactStartDate && task.estimate !== undefined) {
-        return startDate.add(task.estimate, 'day').toDate();
-    }
-    
-    return null;
-};
+interface Coordinates {
+    width?: number;
+    taskBarWidth?: number;
+    deadlineBarWidth?: number;
+    left?: number;
+}
 
 export function TaskBar({ task, timelineController }: { task: Task, timelineController: TimelineController }) {
-    const [coords, setCoords] = useState<{ width?: number, left?: number }>({});
+    const [coords, setCoords] = useState<Coordinates | null>(null);
     const startDate = task.type === 'story' ? task.startDate : task.exactStartDate;
 
-    const to = getDueDate(task);
-    
+    const deadline = getDueDateFromTask(task);
+
     const item = {
         id: task.id,
         from: startDate ? uuiDayjs.dayjs(startDate, 'YYYY-MM-DD').toDate() : null,
-        to,
+        to: getTo(task),
+        deadline,
+        estimatedTo: getEstimatedTo(task),
         color: getTaskColor(task.status),
         minPixPerDay: 0.1,
         fillType: 'solid',
@@ -48,15 +37,23 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
 
     const updateCoords = useCallback((t: TimelineTransform) => {
         const segment = { ...t.transformSegment(item.from, item.to) };
-        const left = t.getX(item.from);
-        const width = t.getX(item.to) - t.getX(item.from);
         const includes = segment !== undefined && item.from !== null && item.to !== null && segment.isVisible && item.opacity > 0.01;
         if (!includes) {
             setCoords(null);
-        } else if (width !== coords?.width || left !== coords?.left) {
-            setCoords({ left, width });
+        } else {
+            const realWidth = getWidth(item.from, item.to, t);
+            const taskBarWidth = getTaskBarWidth(item.from, item.deadline, item.estimatedTo, t);
+            const originalLeft = t.getX(item.from);
+            const left = Math.max(originalLeft, 0);
+
+            if (realWidth !== coords?.width
+                || left !== coords?.left
+                || taskBarWidth !== coords.taskBarWidth
+            ) {
+                setCoords({ left, width: realWidth, taskBarWidth });
+            }
         }
-    }, [coords?.left, coords?.width, item.from, item.opacity, item.to]);
+    }, [coords?.left, coords?.taskBarWidth, coords?.width, item.deadline, item.estimatedTo, item.from, item.opacity, item.to]);
 
     useLayoutEffect(() => {
         updateCoords(timelineController.getTransform());
@@ -75,6 +72,7 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
 
     const status = statuses.find((s) => s.id === task.status);
     const renderTaskStatus = () => {
+        const outOfDeadline = item.deadline && item.deadline.getTime() < item.to.getTime(); 
         return (
             <div>
                 <Text cx={ css.header } fontSize="14" lineHeight="18" fontWeight="600">
@@ -119,7 +117,7 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
                     </FlexCell>
                     <FlexCell width="auto">
                         <Text cx={ css.content } fontSize="12" lineHeight="18">
-                            { formatDate(task.startDate) }
+                            { formatDatePickerDate(task.startDate) }
                         </Text>
                     </FlexCell>
                 </FlexRow>
@@ -133,7 +131,7 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
                         </FlexCell>
                         <FlexCell width="auto">
                             <Text cx={ css.content } fontSize="12" lineHeight="18">
-                                { formatDate(task.exactStartDate) }
+                                { formatDatePickerDate(task.exactStartDate) }
                             </Text>
                         </FlexCell>
                     </FlexRow>
@@ -142,13 +140,13 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
                 && (
                     <FlexRow columnGap="12" size="24" alignItems="center" justifyContent="start">
                         <FlexCell width="auto" minWidth={ 100 }>
-                            <Text cx={ css.content } fontSize="12" lineHeight="18" fontWeight="600">
+                            <Text cx={ css.content } fontSize="12" lineHeight="18" fontWeight="600" color={ outOfDeadline ? 'critical' : 'primary' }>
                                 Due date:
                             </Text>
                         </FlexCell>
                         <FlexCell width="auto">
-                            <Text cx={ css.content } fontSize="12" lineHeight="18">
-                                { formatDate(task.dueDate) }
+                            <Text cx={ css.content } fontSize="12" lineHeight="18" color={ outOfDeadline ? 'critical' : 'primary' }>
+                                { formatDatePickerDate(task.dueDate) }
                             </Text>
                         </FlexCell>
                     </FlexRow>
@@ -161,7 +159,7 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
                     </FlexCell>
                     <FlexCell width="auto">
                         <Text cx={ css.content } fontSize="12" lineHeight="18">
-                            { task.type === 'story' ? formatDate(task.dueDate) : (to && uuiDayjs.dayjs(to).format('DD.MM.YYYY')) }
+                            { formatDatePickerDate(item.estimatedTo) }
                         </Text>
                     </FlexCell>
                 </FlexRow>
@@ -173,16 +171,27 @@ export function TaskBar({ task, timelineController }: { task: Task, timelineCont
         <Tooltip renderContent={ renderTaskStatus } openDelay={ 500 } cx={ css.container } color="neutral">
             <div
                 key={ item.id }
+                className={ css.taskBarWrapper }
                 style={ {
-                    position: 'absolute' as any,
                     height: item.height ?? 18,
-                    background: item.color,
                     width: `${coords.width}px`,
-                    left: 0,
                     transform: `translateX(${coords.left}px)`,
                 } }
             >
-                { coords.width > 50 && <Text color="white" cx={ css.assingeeText }>{ task.name }</Text> }
+                <div
+                    style={ {
+                        background: item.color,
+                        width: `${coords.taskBarWidth}px`,
+                    } }
+                    className={ css.taskBar }
+                >
+                    { coords.taskBarWidth > 50 && <Text color="white" cx={ css.assingeeText }>{ task.name }</Text> }
+                </div>
+                <div
+                    className={ classNames(css.taskBar, css.taskBarDeadline) }
+                    style={ { width: `${coords.width - coords.taskBarWidth}px` } }
+                >
+                </div>
             </div>
         </Tooltip>
     );
