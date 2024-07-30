f5e382c8df4dec2c5f6ba18cd19b9e3cc6779796
Siarhei Yelin <Siarhei_Yelin@epam.com>
2024-07-30 12:33:58 +0300

    Code refactoring

--- uui-e2e-tests/tests/docExampleTests/pickerInput/pickerInput.e2e.ts
+++ uui-e2e-tests/tests/docExampleTests/pickerInput/pickerInput.e2e.ts
@@ -1,49 +1,39 @@
 import { test } from '../../../framework/fixtures/docExamplePage/fixture';
 import { PickerInputObject } from '../../../framework/pageObjects/pickerInputObject';
-import { type TestInfo } from '@playwright/test';
-import { type DocExamplePage } from '../../../framework/fixtures/docExamplePage/docExamplePage';
-import { DocExamplePath } from '../constants';
-
-async function setupTest(
-    params: { testInfo: TestInfo, examplePath: DocExamplePath, pageWrapper: DocExamplePage },
-) {
-    const { pageWrapper, testInfo, examplePath } = params;
-    await pageWrapper.clientRedirect({ examplePath });
-    const comp = new PickerInputObject(pageWrapper.page);
-    const expectScreenshot = async (stepName: string) => {
-        const screenshotName = `${testInfo.title}_${stepName}.png`;
-        await pageWrapper.expectScreenshot(screenshotName);
-    };
-    return { comp, expectScreenshot };
-}
+import { DocExamplePath, setupDocExampleTest } from '../testUtils';
 
 test(DocExamplePath['pickerInput/LazyTreeInput'], async ({ pageWrapper }, testInfo) => {
-    const { comp, expectScreenshot } = await setupTest({ testInfo, examplePath: DocExamplePath['pickerInput/LazyTreeInput'], pageWrapper });
-
-    await comp.focusInput();
+    const { pageObject, expectScreenshot } = await setupDocExampleTest({
+        testInfo,
+        examplePath: DocExamplePath['pickerInput/LazyTreeInput'],
+        pageWrapper,
+        PageObjectConstructor: PickerInputObject,
+    });
+    // 1
+    await pageObject.focusInput();
     await expectScreenshot('step1-focus-input');
-
-    await comp.keyboardPress('Enter');
-    await comp.waitDropdownLoaderAppearsAndDisappears();
+    // 2
+    await pageObject.keyboardPress('Enter');
+    await pageObject.waitDropdownLoaderAppearsAndDisappears();
     await expectScreenshot('step2-focus-search');
-
-    await comp.keyboardType('franc');
-    await comp.waitDropdownLoaderAppearsAndDisappears();
+    // 3
+    await pageObject.keyboardType('franc');
+    await pageObject.waitDropdownLoaderAppearsAndDisappears();
     await expectScreenshot('step3-search-results');
-
-    await comp.keyboardPress('ArrowDown', 8);
-    await comp.waitDropdownOptionFocused('FranceEurope');
+    // 4
+    await pageObject.keyboardPress('ArrowDown', 8);
+    await pageObject.waitDropdownOptionFocused('FranceEurope');
     await expectScreenshot('step4-option-france-focused');
-
-    await comp.keyboardPress('Enter');
-    await comp.waitDropdownOptionChecked('FranceEurope');
+    // 5
+    await pageObject.keyboardPress('Enter');
+    await pageObject.waitDropdownOptionChecked('FranceEurope');
     await expectScreenshot('step5-option-france-checked');
-
-    await comp.keyboardPress('Backspace', 5);
-    await comp.waitDropdownOptionCheckedMixed('Europe');
+    // 6
+    await pageObject.keyboardPress('Backspace', 5);
+    await pageObject.waitDropdownOptionCheckedMixed('Europe');
     await expectScreenshot('step6-option-europe-checked-mixed');
-
-    await comp.keyboardPress('Escape');
-    await comp.waitDropdownDisappears();
+    // 7
+    await pageObject.keyboardPress('Escape');
+    await pageObject.waitDropdownDisappears();
     await expectScreenshot('step7-france-selected');
 });
