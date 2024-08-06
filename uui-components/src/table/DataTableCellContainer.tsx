d93b68c5d6b15990d6b99c0e7d6f67897be1bd0b
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-08-06 22:16:36 +0300

    [DataTable]: Added and reused DataTableCellContainer.

--- uui-components/src/table/DataTableCellContainer.tsx
+++ uui-components/src/table/DataTableCellContainer.tsx
@@ -1,14 +1,21 @@
 import * as React from 'react';
-import { cx, DataColumnProps, IHasCX, IHasRawProps, uuiDataTableHeaderCell } from '@epam/uui-core';
+import { cx, DataColumnProps, IClickable, IHasCX, IHasRawProps } from '@epam/uui-core';
 import { FlexCell } from '../layout';
 import css from './DataTableCellContainer.module.scss';
  
 /**
  * Header cell props of DataTable with Timeline.
  */
-export interface DataTableCellContainerProps extends IHasCX, React.PropsWithChildren, IHasRawProps<React.HTMLAttributes<HTMLDivElement>> {
-    /** DataColumnProps object for the column the cell is at */
+export interface DataTableCellContainerProps extends
+    IHasCX,
+    IClickable,
+    React.PropsWithChildren,
+    IHasRawProps<React.HTMLAttributes<HTMLDivElement>> {
     column: DataColumnProps;
+    textAlign?: 'left' | 'center' | 'right';
+    alignSelf?: string;
+    style?: React.CSSProperties;
+    ref?: React.RefObject<HTMLDivElement> | React.RefCallback<HTMLDivElement>;
 }
  
 export function DataTableCellContainer(props: DataTableCellContainerProps) {
@@ -16,19 +23,20 @@ export function DataTableCellContainer(props: DataTableCellContainerProps) {
         <FlexCell
             { ...props.column }
             minWidth={ props.column.width }
-            rawProps={ {
-                role: 'columnheader',
-                ...props.rawProps,
-            } }
+            rawProps={ props.rawProps }
             cx={
                 cx(
                     'uui-dt-vars',
-                    uuiDataTableHeaderCell.uuiTableHeaderCell,
                     css.cell,
                     props.column.cx,
                     props.cx,
                 ) 
             }
+            onClick={ props.onClick }
+            textAlign={ props.textAlign }
+            alignSelf={ props.alignSelf }
+            ref={ props.ref }
+            style={ props.style }
         >
             { props.children }
         </FlexCell>
