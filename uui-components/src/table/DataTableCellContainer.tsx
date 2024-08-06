16fa5005d9dd97f168422336c2caf8d418996417
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-08-06 22:59:05 +0300

    [DataTable]: added docs.

--- uui-components/src/table/DataTableCellContainer.tsx
+++ uui-components/src/table/DataTableCellContainer.tsx
@@ -11,10 +11,25 @@ export interface DataTableCellContainerProps extends
     IClickable,
     React.PropsWithChildren,
     IHasRawProps<React.HTMLAttributes<HTMLDivElement>> {
+    /**
+     * DataTable column configuration.
+     */
     column: DataColumnProps;
+    /**
+     * CSS text-align property.
+     */
     textAlign?: 'left' | 'center' | 'right';
+    /**
+     * Flexbox align-self property. Aligns items vertically for horizontal flexbox. [Flexbox Guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/#aa-align-self)
+     */
     alignSelf?: string;
+    /**
+     * Flexbox shrink property [Flexbox Guide](https://css-tricks.com/snippets/css/a-guide-to-flexbox/#aa-flex-shrink).
+     */
     shrink?: number;
+    /**
+     * Standard style attribute. Styles are added to element style, overriding supplied flex styles.
+     */
     style?: React.CSSProperties;
 }
  
