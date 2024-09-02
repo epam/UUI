08b8a7a9d1f5e3d3dd800c0227cd5346b9f53889
Yaroslav Kuznietsov <kuznetsov.yaroslav.yk@gmail.com>
2024-09-02 15:35:12 +0300

    [useLazyTree]: fixed api calls while very fast scrolling.

--- uui-core/src/data/processing/views/tree/hooks/strategies/lazyTree/useLazyTree.ts
+++ uui-core/src/data/processing/views/tree/hooks/strategies/lazyTree/useLazyTree.ts
@@ -161,6 +161,9 @@ export function useLazyTree<TItem, TId, TFilter = any>(
         shouldFetch,
         shouldLoad,
         shouldRefetch,
+        dataSourceState.topIndex,
+        dataSourceState.visibleCount,
+        filter,
     ]);
 
     const treeWithSelectedOnly = useSelectedOnlyTree({
