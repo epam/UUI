36fd9c87c787eff196dacadc33c304967c857e56
AlekseyManetov <aliaksei_manetau@epam.com>
2024-10-16 12:21:25 +0200

    remove redundant css class from doc examples

--- app/src/docs/RichTextEditor.doc.tsx
+++ app/src/docs/RichTextEditor.doc.tsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
-import cx from 'classnames';
 import { BaseDocsBlock, DocExample } from '../common';
-import css from './styles.module.scss';
 
 export class RichTextEditorDoc extends BaseDocsBlock {
     title = 'Rich Text Editor';
     renderContent() {
         return (
-            <span className={ cx(css.wrapper) }>
+            <>
                 <DocExample path="./_examples/richTextEditor/Basic.example.tsx" />
                 <DocExample title="Inner scroll behavior" path="./_examples/richTextEditor/WithInnerScroll.example.tsx" />
-            </span>
+            </>
         );
     }
 }
