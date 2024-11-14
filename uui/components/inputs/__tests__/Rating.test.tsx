8d9d8cbebaf07865590298d53299e1c52a303698
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2024-11-14 18:16:09 +0300

    [Rating]: added example with other icon & color [LinkButton]: fixed layout of basic example

--- uui/components/inputs/__tests__/Rating.test.tsx
+++ uui/components/inputs/__tests__/Rating.test.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Rating } from '../Rating';
 import { renderSnapshotWithContextAsync } from '@epam/uui-test-utils';
+import { ReactComponent as FavoriteIcon } from '@epam/assets/icons/communication-favorite-fill.svg';
 
 describe('Rating', () => {
     it('should be rendered correctly', async () => {
@@ -11,6 +12,12 @@ describe('Rating', () => {
 
     it('should be rendered correctly with more props', async () => {
         const tree = await renderSnapshotWithContextAsync(<Rating value={ null } onValueChange={ jest.fn } size={ 24 } step={ 1 } />);
+
+        expect(tree).toMatchSnapshot();
+    });
+
+    it('should be rendered with other icon', async () => {
+        const tree = await renderSnapshotWithContextAsync(<Rating icon={ FavoriteIcon } value={ null } onValueChange={ jest.fn } size={ 24 } step={ 1 } />);
 
         expect(tree).toMatchSnapshot();
     });
