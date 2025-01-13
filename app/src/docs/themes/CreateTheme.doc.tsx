c7070c03e0615b883555d170ed8247a1f5505f3f
AlekseyManetov <aliaksei_manetau@epam.com>
2025-01-13 18:18:27 +0100

    update create theme doc

--- app/src/docs/themes/CreateTheme.doc.tsx
+++ app/src/docs/themes/CreateTheme.doc.tsx
@@ -2,7 +2,7 @@ import * as React from 'react';
 import { EditableDocContent, BaseDocsBlock } from '../../common';
 
 export class CreateThemeDoc extends BaseDocsBlock {
-    title = 'Create your own theme';
+    title = 'Create custom theme';
 
     renderContent() {
         return (
