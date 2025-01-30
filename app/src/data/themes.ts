2026c63fa8f630b3afc1a731a4be263540a54f5f
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2025-01-30 16:57:16 +0300

    [Assests]: 'Loveship 4px' theme changed to 'Nature 4px' with colors update

--- app/src/data/themes.ts
+++ app/src/data/themes.ts
@@ -5,10 +5,9 @@ import propsOverride_4px from './propsOverride_4px';
 export enum BuiltInTheme {
     electric = 'electric',
     loveship = 'loveship',
-    loveship_4px = 'loveship_4px',
     loveship_dark = 'loveship_dark',
-    loveship_dark_4px = 'loveship_dark_4px',
     promo = 'promo',
+    nature_4px = 'nature_4px',
     vanilla_thunder = 'vanilla_thunder',
     eduverse_light = 'eduverse_light',
     eduverse_dark = 'eduverse_dark'
@@ -27,11 +26,10 @@ export interface ThemeBaseParams extends Partial<CustomThemeManifest> {
 export const builtInThemes: ThemeBaseParams[] = [
     { id: BuiltInTheme.electric, name: 'Electric' },
     { id: BuiltInTheme.loveship, name: 'Loveship Light' },
-    { id: BuiltInTheme.loveship_4px, name: 'Loveship Light 4px', settings: settings_4px, propsOverride: propsOverride_4px, devOnly: true, css: null },
     { id: BuiltInTheme.loveship_dark, name: 'Loveship Dark' },
-    { id: BuiltInTheme.loveship_dark_4px, name: 'Loveship Dark 4px', settings: settings_4px, propsOverride: propsOverride_4px, devOnly: true, css: null },
-    { id: BuiltInTheme.vanilla_thunder, name: 'Vanilla Thunder' },
+    { id: BuiltInTheme.nature_4px, name: 'Nature Light 4px', settings: settings_4px, propsOverride: propsOverride_4px, devOnly: true, css: null },
     { id: BuiltInTheme.promo, name: 'Promo' },
+    { id: BuiltInTheme.vanilla_thunder, name: 'Vanilla Thunder' },
     { id: BuiltInTheme.eduverse_light, name: 'Eduverse Light' },
     { id: BuiltInTheme.eduverse_dark, name: 'Eduverse Dark' },
 ];
