619461c546fc01d764e72b03e0cfaef24e13add5
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2025-01-30 18:55:19 +0300

    [Dropdown]: updated e2e tests

--- uui-e2e-tests/tests/Integration/dropdown/dropdown.e2e.ts
+++ uui-e2e-tests/tests/Integration/dropdown/dropdown.e2e.ts
@@ -4,7 +4,7 @@ import { DropdownObject } from '../../../framework/pageObjects/dropdownObject';
 import { DocExamplePath, setupDocExampleTest } from '../testUtils';
 
 test('Dropdown / Boundary mode', async ({ pageWrapper }, testInfo) => {
-    const { pageObject, expectScreenshot } = await setupDocExampleTest({
+    const { pageObject } = await setupDocExampleTest({
         testInfo,
         pageWrapper,
         PageObjectConstructor: DropdownObject,
@@ -16,8 +16,6 @@ test('Dropdown / Boundary mode', async ({ pageWrapper }, testInfo) => {
     await test.step('Body appears when the target element is hovered over', async () => {
         await pageObject.hoverTarget();
         await pageObject.waitDropdownBodyVisible();
-        await pageObject.page.waitForTimeout(100); // wait for API call
-        await expectScreenshot(1, 'body-opened');
     });
 
     await test.step('Body closure with delay when mouse in boundary area', async () => {
@@ -26,7 +24,6 @@ test('Dropdown / Boundary mode', async ({ pageWrapper }, testInfo) => {
         expect(await pageObject.locators.dropdown.body.isVisible()).toBe(true);
         await pageObject.page.waitForTimeout(1000);
         expect(await pageObject.locators.dropdown.body.isVisible()).toBe(false);
-        await expectScreenshot(2, 'body-closed');
     });
 
     await test.step('Body closes immediately when mouse is out of boundary area', async () => {
@@ -34,12 +31,11 @@ test('Dropdown / Boundary mode', async ({ pageWrapper }, testInfo) => {
         await pageObject.waitDropdownBodyVisible();
         await pageObject.mouseMoveFromLocatorToBottom({ locator: pageObject.locators.dropdown.body, directionY: 40 });
         expect(await pageObject.locators.dropdown.body.isVisible()).toBe(false);
-        await expectScreenshot(3, 'body-closed');
     });
 });
 
 test('Dropdown / Scrolling behavior', async ({ pageWrapper }, testInfo) => {
-    const { pageObject, expectScreenshot } = await setupDocExampleTest({
+    const { pageObject } = await setupDocExampleTest({
         testInfo,
         pageWrapper,
         PageObjectConstructor: DropdownObject,
@@ -54,14 +50,14 @@ test('Dropdown / Scrolling behavior', async ({ pageWrapper }, testInfo) => {
         await pageObject.waitDropdownBodyVisible();
         await expect(pageObject.locators.dropdown.body).toHaveAttribute('data-placement', 'top-start');
         await pageObject.mouseWheel(250);
+        expect(await pageObject.locators.dropdown.body.isVisible()).toBe(true);
         await expect(pageObject.locators.dropdown.body).toHaveAttribute('data-placement', 'bottom-start');
-        await expectScreenshot(1, 'body-position-top');
     });
 
     await test.step('Body disappears when the target element is scrolled out of the viewport', async () => {
+        expect(await pageObject.locators.dropdown.body.isVisible()).toBe(true);
         await pageObject.mouseWheel(800);
         await pageObject.waitDropdownBodyHidden();
-        await expectScreenshot(2, 'body-closed');
         await pageObject.removeStylesFromContentWrapper();
     });
 });
