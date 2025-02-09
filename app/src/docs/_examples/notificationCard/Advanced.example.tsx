a06b7f66bba4291b3ad3691bf3629217586baf23
bdvx <bdv2507@gmail.com>
2025-02-09 20:55:53 +0400

    2638: add new examples with titles

--- app/src/docs/_examples/notificationCard/Advanced.example.tsx
+++ app/src/docs/_examples/notificationCard/Advanced.example.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import { FlexCell, NotificationCard, Text } from '@epam/uui';
+import { FlexCell, NotificationCard, Text, RichTextView, Anchor } from '@epam/uui';
 import css from './BasicExample.module.scss';
 import { ReactComponent as AccountIcon24 } from '@epam/assets/icons/common/action-account-24.svg';
 
@@ -8,8 +8,23 @@ export default function AdvancedExample() {
 
     return (
         <FlexCell width={ 350 } cx={ css.container }>
-            <NotificationCard id={ 1 } key="propsKey" actions={ actions } color="warning" icon={ AccountIcon24 } onClose={ () => alert('close action') } onSuccess={ () => null }>
+            <NotificationCard id={ 1 } key="propsKey1" actions={ actions } color="warning" icon={ AccountIcon24 } onClose={ () => alert('close action') } onSuccess={ () => null }>
                 <Text>Warning notification with some buttons</Text>
+            </NotificationCard>
+            <NotificationCard id={ 2 } key="propsKey2" actions={ actions } color="success" icon={ AccountIcon24 } onClose={ () => alert('close action') } onSuccess={ () => null }>
+                <Text>Notification text message when it has a lot of text message</Text>
+            </NotificationCard>
+            <NotificationCard id={ 3 } key="propsKey3" actions={ actions } color="error" icon={ AccountIcon24 } onClose={ () => alert('close action') } onSuccess={ () => null }>
+                <Text fontWeight="600">Notification title, some long title can be here, even 2nd row appears</Text>
+                <Text>Notification text message when it has a lot of text message</Text>
+            </NotificationCard>
+            <NotificationCard id={ 4 } key="propsKey4" actions={ actions } color="info" icon={ AccountIcon24 } onClose={ () => alert('close action') } onSuccess={ () => null }>
+                <RichTextView>
+                    Any your text with
+                    {' '}
+                    <Anchor href="https://uui.epam.com/">link</Anchor>
+                    .
+                </RichTextView>
             </NotificationCard>
         </FlexCell>
     );
