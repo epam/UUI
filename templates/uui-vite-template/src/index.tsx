60afb20f43921da141017fd53ed3b92dfe3e3c74
AlekseyManetov <aliaksei_manetau@epam.com>
2025-03-17 17:12:36 +0100

    [template]: fixed MainMenu rerendering on router change

--- templates/uui-vite-template/src/index.tsx
+++ templates/uui-vite-template/src/index.tsx
@@ -2,7 +2,6 @@ import '@epam/uui-components/styles.css';
 import '@epam/uui/styles.css';
 import '@epam/assets/theme/theme_loveship.scss';
 import './index.module.scss';
-import logo from './icons/logo.svg';
 //
 import { StrictMode } from 'react';
 import { createRoot } from 'react-dom/client';
@@ -14,7 +13,7 @@ import {
     useUuiServices,
     UuiContext,
 } from '@epam/uui-core';
-import { MainMenu, MainMenuButton } from '@epam/uui';
+import { Menu } from './common/Menu';
 import { ErrorHandler } from '@epam/uui';
 import { Modals, Snackbar } from '@epam/uui-components';
 import { svc } from './services';
@@ -31,16 +30,7 @@ function UuiEnhancedApp() {
         <UuiContext.Provider value={services}>
             <ErrorHandler>
                 <Router history={history}>
-                    <Route>
-                        <MainMenu appLogoUrl={logo}>
-                            <MainMenuButton
-                                caption="Home"
-                                link={{ pathname: '/' }}
-                                priority={1}
-                                estimatedWidth={72}
-                            />
-                        </MainMenu>
-                    </Route>
+                    <Route> <Menu /> </Route>
                     <Route path="/" component={MainPage} />
                 </Router>
                 <Snackbar />
