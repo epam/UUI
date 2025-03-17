a2e539b475344e53397325c033db04a91b8fe134
AlekseyManetov <aliaksei_manetau@epam.com>
2025-03-17 17:27:22 +0100

    [template]: fixed MainMenu rerendering on router change

--- templates/uui-cra-template/template/src/index.tsx
+++ templates/uui-cra-template/template/src/index.tsx
@@ -3,14 +3,14 @@ import '@epam/uui/styles.css';
 import '@epam/assets/theme/theme_loveship.scss';
 import './index.module.scss';
 
-import logo from "./icons/logo.svg";
 //
 import { StrictMode } from "react";
 import { createRoot } from 'react-dom/client';
 import { createBrowserHistory } from "history";
 import { Route, Router } from "react-router-dom";
 import { DragGhost, HistoryAdaptedRouter, useUuiServices, UuiContext } from "@epam/uui-core";
-import { MainMenu, MainMenuButton, ErrorHandler } from "@epam/uui";
+import { ErrorHandler } from "@epam/uui";
+import { Menu } from "./common/Menu";
 import { Modals, Snackbar } from "@epam/uui-components";
 import { svc } from "./services";
 
@@ -26,16 +26,7 @@ function UuiEnhancedApp() {
         <UuiContext.Provider value={ services }>
             <ErrorHandler>
                 <Router history={history}>
-                    <Route>
-                        <MainMenu appLogoUrl={logo}>
-                            <MainMenuButton
-                                caption="Home"
-                                link={{ pathname: '/' }}
-                                priority={1}
-                                estimatedWidth={72}
-                            />
-                        </MainMenu>
-                    </Route>
+                    <Route component={Menu} />
                     <Route path="/" component={MainPage} />
                 </Router>
                 <Snackbar />
