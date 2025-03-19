c52b66335eba184044db31728d59f59b1816a18f
AlekseyManetov <aliaksei_manetau@epam.com>
2025-03-19 17:36:33 +0100

    [docs]: change context provider with router6 doc

--- app/src/docs/_examples/contexts/UseUuiServicesRR6.example.tsx
+++ app/src/docs/_examples/contexts/UseUuiServicesRR6.example.tsx
@@ -1,15 +1,28 @@
 // Note: please remove @ts-nocheck comment in real app, it's here only because it's our local code example.
 // @ts-nocheck
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { render } from 'react-dom';
 import { UuiContext, useUuiServices, DragGhost, Router6AdaptedRouter } from '@epam/uui-core';
 import { Modals, Snackbar } from '@epam/uui-components';
 import { ErrorHandler } from '@epam/promo';
 import { svc } from '../../../services';
 import { createBrowserRouter } from 'react-router-dom';
-import { RouterProvider } from 'react-router';
+import { Route, RouterProvider } from 'react-router';
+import React from 'react';
+
+const MyApp = () => {
+    return (
+        <ErrorHandler>
+            <Route path="/" Component={ MyPage } />
+            <Snackbar />
+            <Modals />
+            <DragGhost />
+        </ErrorHandler>
+    );
+};
 
 const router6 = createBrowserRouter([
-    { path: '*', element: '<YourAppComponent />' },
+    { path: '*', element: '<MyApp />' },
 ]);
 const router = new Router6AdaptedRouter(router6);
 
@@ -19,12 +32,7 @@ function UuiEnhancedApp() {
 
     return (
         <UuiContext.Provider value={ services }>
-            <ErrorHandler>
-                <RouterProvider router={ router6 } />
-            </ErrorHandler>
-            <Snackbar />
-            <Modals />
-            <DragGhost />
+            <RouterProvider router={ router6 } />
         </UuiContext.Provider>
     );
 }
