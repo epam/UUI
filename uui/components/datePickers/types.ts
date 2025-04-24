46be5852022815bfe058402daecd401709d3db48
AlekseyManetov <aliaksei_manetau@epam.com>
2025-04-24 16:38:00 +0200

    fix build

--- uui/components/datePickers/types.ts
+++ uui/components/datePickers/types.ts
@@ -1,6 +1,8 @@
 import type { IDisableable, IHasCX, IHasRawProps, RangeDatePickerPresets, DayProps } from '@epam/uui-core';
 import type { Dayjs } from '../../helpers/dayJsHelper';
 
+export type ViewType = 'DAY_SELECTION' | 'MONTH_SELECTION' | 'YEAR_SELECTION';
+
 export interface CommonDatePickerBodyProps extends IHasCX, IDisableable, IHasRawProps<React.HTMLAttributes<HTMLDivElement>> {
     filter?(day: Dayjs): boolean;
     presets?: RangeDatePickerPresets;
