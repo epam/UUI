362690ce63a28c93ddee9f27311cc3f6a95de6bb
Ihor_Korenets <ihor_korenets@epam.com>
2025-05-24 20:07:36 +0300

    [Table, filters]: refactored, created FilterPredicatePanel

--- uui/components/filters/FilterColumnBody.tsx
+++ uui/components/filters/FilterColumnBody.tsx
@@ -1,25 +1,13 @@
 import { FilterPredicateName, IFilterItemBodyProps } from '@epam/uui-core';
 import React, { useEffect, useState } from 'react';
-import { DropdownContainer, FilterItemBody, MultiSwitch } from '../index';
-import css from './FiltersPanelItem.module.scss';
-import { UUI_FILTERS_PANEL_ITEM_BODY } from './constants';
-import { settings } from '../../settings';
-import cx from 'classnames';
-
-const isRangeTypeValue = (val: string) => val === 'inRange' || val === 'notInRange';
+import { getDefaultPredicate } from '../index';
+import { FilterPredicatePanel } from './FilterPredicatePanel';
+import { FilterPredicateBody } from './FilterPredicateBody';
 
 export function FilterColumnBody(props: IFilterItemBodyProps<any>) {
     const isPickersType = props?.type === 'multiPicker' || props?.type === 'singlePicker';
 
-    const [predicate, setPredicate] = useState(null);
-
-    useEffect(() => {
-        const newPredicate = Array.isArray(props.predicates)
-            ? props.predicates.find((p) => p?.isDefault)?.predicate ?? props.predicates?.[0]?.predicate ?? null
-            : null;
-
-        setPredicate(newPredicate as FilterPredicateName);
-    }, [props.predicates]);
+    const [predicate, setPredicate] = useState<FilterPredicateName>(getDefaultPredicate(props.predicates, props.value));
 
     useEffect(() => {
         // This effect needs when the filter dropdown was closed and opened again
@@ -31,58 +19,28 @@ export function FilterColumnBody(props: IFilterItemBodyProps<any>) {
         }
     }, [props.value]);
 
-    const getValue = () => {
-        return predicate ? props.value?.[predicate] : props.value;
-    };
-
-    const onValueChange = (value: any) => {
-        if (props.predicates) {
-            let isNullishOrUndefined = false;
-
-            if (typeof value === 'object' && value !== null) {
-                if (Object.values(value).every((v) => v === null || v === undefined)) {
-                    isNullishOrUndefined = true;
-                }
-            }
-
-            props.onValueChange(isNullishOrUndefined ? undefined : { [predicate]: value });
-        } else {
-            props.onValueChange(value);
-        }
-    };
-
-    const changePredicate = (val: FilterPredicateName) => {
-        const arePredicatesInSameType = props?.value && isRangeTypeValue(predicate) === isRangeTypeValue(val);
-
-        if (arePredicatesInSameType) {
-            props.onValueChange({ [val]: props?.value?.[predicate] });
-        } else {
-            props.onValueChange(undefined);
-        }
-
-        setPredicate(val);
-    };
-
     const renderHeader = () => (
-        <div className={ cx(css.header, isPickersType && (props.showSearch ?? css.withSearch)) }>
-            <MultiSwitch
-                items={ props.predicates.map((i) => ({ id: i.predicate, caption: i.name })) }
-                value={ predicate }
-                onValueChange={ changePredicate }
-                size={ settings.filtersPanel.sizes.pickerBodyMultiSwitch }
-            />
-        </div>
+        <FilterPredicatePanel
+            filterType="column"
+            predicates={ props.predicates }
+            predicate={ predicate }
+            isPickersType={ isPickersType }
+            type={ props.type }
+            onValueChange={ props.onValueChange }
+            value={ props.value }
+            setPredicate={ setPredicate }
+        />
     );
 
     return (
-        <DropdownContainer cx={ [css.body, UUI_FILTERS_PANEL_ITEM_BODY] } { ...props }>
-            { props.predicates && renderHeader() }
-            <FilterItemBody
-                { ...props }
-                selectedPredicate={ predicate }
-                value={ getValue() }
-                onValueChange={ onValueChange }
-            />
-        </DropdownContainer>
+        <FilterPredicateBody
+            { ...props }
+            filterType="column"
+            predicate={ predicate }
+            renderHeader={ renderHeader }
+            isPickersType={ isPickersType }
+            value={ props.value }
+            onValueChange={ props.onValueChange }
+        />
     );
 }
---
+++ uui/components/filters/FilterPredicatePanel.tsx
@@ -0,0 +1,62 @@
+import React from 'react';
+import cx from 'classnames';
+import { FilterPredicate, FilterPredicateName, FilterType, IFilterPredicate } from '@epam/uui-core';
+import { settings } from '../../settings';
+import { MultiSwitch } from '../inputs';
+import { getValue } from './helpers/predicateHelpers';
+import css from './FiltersPanelItem.module.scss';
+
+interface IPredicatesHeaderProps {
+    filterType: FilterType;
+    predicates: IFilterPredicate[];
+    predicate: keyof FilterPredicate<any>;
+    isPickersType: boolean;
+    type: 'singlePicker' | 'multiPicker' | 'datePicker' | 'numeric' | 'rangeDatePicker' | 'custom';
+    onValueChange: (newValue: any) => void;
+    value: unknown;
+    setPredicate: React.Dispatch<React.SetStateAction<keyof FilterPredicate<any>>>;
+}
+
+export function FilterPredicatePanel(props: IPredicatesHeaderProps) {
+    if (!props.predicates) return null;
+
+    const changePredicate = (val: FilterPredicateName) => {
+        const isInRange = (p: FilterPredicateName) => p === 'inRange' || p === 'notInRange';
+        if (props.type === 'numeric') {
+            let predicateValue = { [val]: getValue(props.predicate, props.value) };
+
+            if (isInRange(val) && !isInRange(props.predicate as FilterPredicateName)) {
+                // from simple predicate -> to Range
+                predicateValue = { [val]: { from: null, to: null } };
+            } else if (!isInRange(val) && isInRange(props.predicate as FilterPredicateName)) {
+                // from Range -> to simple predicate
+                predicateValue = { [val]: null };
+            }
+            props.onValueChange(predicateValue);
+        } else {
+            props.onValueChange({ [val]: getValue(props.predicate, props.value) });
+        }
+        props.setPredicate(val);
+    };
+
+    const getWrapper = (children: React.ReactNode) => {
+        switch (props.filterType) {
+            case 'column':
+                return (
+                    <div className={ cx(css.header, props.isPickersType) }>
+                        { children }
+                    </div>
+                );
+            default: return children;
+        }
+    };
+
+    return getWrapper(
+        <MultiSwitch
+            items={ props.predicates?.map((i) => ({ id: i.predicate, caption: i.name })) }
+            value={ props.predicate }
+            onValueChange={ changePredicate }
+            size={ settings.filtersPanel.sizes.pickerBodyMultiSwitch }
+        />,
+    );
+}
