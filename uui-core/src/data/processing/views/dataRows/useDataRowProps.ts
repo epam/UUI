814c7394c291b1abe26937cb9cda3027620b5c3c
Siarhei_Dzeraviannik <siarhei_dzeraviannik@epam.com>
2025-07-01 19:47:42 +0300

    [DataSources]: added tests to check tree selection

--- uui-core/src/data/processing/views/dataRows/useDataRowProps.ts
+++ uui-core/src/data/processing/views/dataRows/useDataRowProps.ts
@@ -71,7 +71,7 @@ export function useDataRowProps<TItem, TId, TFilter = any>(
         row.onFold = row.isFoldable ? handleOnFold : undefined;
         row.isFolded = row.isFoldable && isFolded(row.value);
 
-        row.isChildrenChecked = row.isChildrenChecked || isRowChildrenChecked(row);
+        row.isChildrenChecked = isRowChildrenChecked(row);
         row.isChildrenSelected = isRowChildSelected(row);
 
         return row;
