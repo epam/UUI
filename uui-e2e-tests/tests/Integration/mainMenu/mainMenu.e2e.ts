735c4f6927eefe39dc07d6d86de7fdfca4e1fcff
AlekseyManetov <aliaksei_manetau@epam.com>
2025-07-03 15:04:39 +0200

    remove only from mainmenu e2e tests

--- uui-e2e-tests/tests/Integration/mainMenu/mainMenu.e2e.ts
+++ uui-e2e-tests/tests/Integration/mainMenu/mainMenu.e2e.ts
@@ -8,7 +8,7 @@ const testPageUrl = {
     serverBadgePreview: '/preview?theme=loveship&isSkin=true&componentId=mainMenu&previewId=All+Variants',
 };
 
-test.only('mainMenu/Responsive scenario', async ({ pageWrapper }, testInfo) => {
+test('mainMenu/Responsive scenario', async ({ pageWrapper }, testInfo) => {
     const { pageObject, expectScreenshot } = await setupDocExampleTest({
         testInfo,
         pageWrapper,
