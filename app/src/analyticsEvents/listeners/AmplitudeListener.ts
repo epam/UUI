b7f8d7103562930b1c15e31d312b3b6bafad20ab
AlekseyManetov <aliaksei_manetau@epam.com>
2025-07-07 14:00:47 +0200

    Update amplitude to the new api and add cookieOptions: { domain: 'uui.epam.com' } to set amp cookie directly to the uui.epam.com domain instead of epam.com

--- app/src/analyticsEvents/listeners/AmplitudeListener.ts
+++ app/src/analyticsEvents/listeners/AmplitudeListener.ts
@@ -1,22 +1,30 @@
 import { IAnalyticsListener, AnalyticsEvent } from '@epam/uui-core';
-import amplitude from 'amplitude-js';
+import * as amplitude from '@amplitude/analytics-browser';
+import { BrowserClient } from '@amplitude/analytics-core';
 
 export class AmplitudeListener implements IAnalyticsListener {
     public ampCode: string;
-    public client: amplitude.AmplitudeClient;
+    public client: BrowserClient;
     constructor(ampCode: string) {
         this.ampCode = ampCode;
         this.client = this.getAmplitudeClient();
     }
 
-    private getAmplitudeClient(): amplitude.AmplitudeClient {
-        const ampclient = amplitude.getInstance();
-        ampclient.init(this.ampCode, undefined, { includeReferrer: true, includeUtm: true, saveParamsReferrerOncePerSession: false });
-        return ampclient;
+    private getAmplitudeClient(): BrowserClient {
+        const ampClient = amplitude.createInstance();
+
+        ampClient.init(this.ampCode, undefined, {
+            // includeReferrer: true,
+            // includeUtm: true,
+            // saveParamsReferrerOncePerSession: false,
+            cookieOptions: { domain: 'uui.epam.com' },
+        });
+
+        return ampClient;
     }
 
     public sendEvent(event: AnalyticsEvent, parameters: Omit<AnalyticsEvent, 'name'>, eventType: string) {
         if (eventType !== 'event') return;
-        this.client.logEvent(event.name, parameters);
+        this.client.track(event.name, parameters);
     }
 }
