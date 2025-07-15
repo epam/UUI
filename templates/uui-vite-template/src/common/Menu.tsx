d3caeee12fa9b14f0cc294f17ab8392d38f449b8
AlekseyManetov <aliaksei_manetau@epam.com>
2025-07-15 17:55:30 +0200

    update vite template

--- templates/uui-vite-template/src/common/Menu.tsx
+++ templates/uui-vite-template/src/common/Menu.tsx
@@ -1,18 +1,12 @@
-import { useLocation } from "react-router-dom";
-import { MainMenu, MainMenuButton } from "@epam/uui";
+import { useLocation } from 'react-router-dom';
+import { MainMenu, MainMenuButton } from '@epam/uui';
 import logo from '../icons/logo.svg';
-
 
 export const Menu = () => {
     useLocation();
     return (
         <MainMenu appLogoUrl={logo}>
-            <MainMenuButton
-                caption="Home"
-                link={{ pathname: '/' }}
-                priority={1}
-                estimatedWidth={72}
-            />
+            <MainMenuButton caption="Home" link={{ pathname: '/' }} priority={1} estimatedWidth={72} />
         </MainMenu>
-    )
-}
+    );
+};
