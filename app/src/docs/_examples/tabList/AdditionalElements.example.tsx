9ec9be57b2cf52290c7b6027a3a10d9834348c78
Vasilii Kovalev <Vasilii_Kovalev@epam.com>
2025-07-28 17:57:35 +0500

    Remove `tabIndex` from `TabPanel` in examples

--- app/src/docs/_examples/tabList/AdditionalElements.example.tsx
+++ app/src/docs/_examples/tabList/AdditionalElements.example.tsx
@@ -21,12 +21,10 @@ const getTabpanelId = (tabId: string): string => {
 
 interface TabPanelProps {
     tabId: string;
-    tabIndex?: number;
 }
 
 function TabPanel({
     tabId,
-    tabIndex,
     children,
 }: PropsWithChildren<TabPanelProps>): ReactNode {
     return (
@@ -35,7 +33,6 @@ function TabPanel({
             id={ getTabpanelId(tabId) }
             role="tabpanel"
             aria-labelledby={ tabId }
-            tabIndex={ tabIndex }
         >
             {children}
         </div>
@@ -51,7 +48,6 @@ export default function AdditionalElementsTabListExample(): ReactNode {
             return (
                 <TabPanel
                     tabId={ tabId }
-                    tabIndex={ 0 }
                 >
                     <Text>
                         Tab 1 content
@@ -64,7 +60,6 @@ export default function AdditionalElementsTabListExample(): ReactNode {
             return (
                 <TabPanel
                     tabId={ tabId }
-                    tabIndex={ 0 }
                 >
                     <Text>
                         Tab 2 content
