3dabe81c9c1bdbed32cbd04626fb7d3c6fffb8b1
Vasilii Kovalev <Vasilii_Kovalev@epam.com>
2025-07-31 10:03:10 +0500

    Use `TabList` in `TabsNav` component

--- app/src/common/docs/docsBlock/components/tabsNav.tsx
+++ app/src/common/docs/docsBlock/components/tabsNav.tsx
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react';
-import { Button, FlexRow, FlexSpacer, TabButton } from '@epam/uui';
+import { Button, FlexRow, FlexSpacer, TabList, type TabListItemProps } from '@epam/uui';
 import { TMode } from '../../docsConstants';
 import { ReactComponent as NavigationShowOutlineIcon } from '@epam/assets/icons/navigation-show-outline.svg';
 import css from './tabsNav.module.scss';
@@ -49,9 +49,7 @@ export function TabsNav(props: TTabsNavProps) {
     }
 
     return (
-        // Can't use `TabList` because of the `Button` as a child.
         <FlexRow
-            rawProps={ { role: 'tablist' } }
             padding="12"
             cx={ [css.secondaryNavigation] }
             borderBottom
@@ -66,24 +64,29 @@ export function TabsNav(props: TTabsNavProps) {
                 >
                 </Button>
             ) }
-            {
-                Object.keys(allTabs).reduce<React.ReactNode[]>((acc, tm) => {
-                    if (supportedModes.includes(tm as TMode) || (tm as TMode) === mode) {
-                        const data = allTabs[tm as TMode];
-                        acc.push((
-                            <TabButton
-                                key={ tm }
-                                size="60"
-                                caption={ data.caption }
-                                isLinkActive={ mode === tm }
-                                rawProps={ { title: data.tooltip } }
-                                onClick={ () => onChangeMode(tm as TMode) }
-                            />
-                        ));
-                    }
-                    return acc;
-                }, [])
-            }
+
+            <TabList
+                value={ mode }
+                onValueChange={ (modeNext: TMode) => {
+                    onChangeMode(modeNext);
+                } }
+                items={
+                    Object.keys(allTabs).reduce<TabListItemProps[]>((acc, tm) => {
+                        if (supportedModes.includes(tm as TMode) || (tm as TMode) === mode) {
+                            const data = allTabs[tm as TMode];
+
+                            acc.push({
+                                id: tm,
+                                size: '60',
+                                caption: data.caption,
+                                rawProps: { title: data.tooltip },
+                            });
+                        }
+                        return acc;
+                    }, [])
+                }
+                borderBottom={ false }
+            />
             <FlexSpacer />
             { renderSkinSwitcher() }
         </FlexRow>
