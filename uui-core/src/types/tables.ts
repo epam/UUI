91522f0c437272b9b5fdd0d44f1649de82dccede
AlekseyManetov <aliaksei_manetau@epam.com>
2025-08-11 19:24:38 +0200

    [FiltersPanel]: added 'disableClear' prop for Picker filters

--- uui-core/src/types/tables.ts
+++ uui-core/src/types/tables.ts
@@ -429,7 +429,7 @@ type FilterConfigBase<TFilter> = {
     togglerWidth?: number;
 };
 
-export type PickerFilterConfig<TFilter> = FilterConfigBase<TFilter> & Pick<PickerInputBaseProps<any, any>, 'dataSource' | 'getName' | 'renderRow' | 'renderFooter' | 'minCharsToSearch'> & {
+export type PickerFilterConfig<TFilter> = FilterConfigBase<TFilter> & Pick<PickerInputBaseProps<any, any>, 'dataSource' | 'getName' | 'renderRow' | 'renderFooter' | 'minCharsToSearch' | 'disableClear'> & {
     /** Type of the filter */
     type: 'singlePicker' | 'multiPicker';
     /**
