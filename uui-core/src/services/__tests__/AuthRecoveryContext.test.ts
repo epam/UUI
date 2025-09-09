bfaddccb771552bbd74ebc5c52c8ae145368a4f4
Siarhei Yelin <siarhei_yelin@epam.com>
2025-09-09 12:25:42 +0300

    Remove copyright message from source code

--- uui-core/src/services/__tests__/AuthRecoveryContext.test.ts
+++ uui-core/src/services/__tests__/AuthRecoveryContext.test.ts
@@ -1,10 +1,3 @@
-/*
- * Copyright © 2025 EPAM Systems, Inc. All Rights Reserved. All information contained herein is, and remains the
- * property of EPAM Systems, Inc. and/or its suppliers and is protected by international intellectual
- * property law. Dissemination of this information or reproduction of this material is strictly forbidden,
- * unless prior written permission is obtained from EPAM Systems, Inc
- */
-
 import { AuthRecoveryContext } from './../AuthRecoveryContext';
 
 const apiReloginPath = '/test';
