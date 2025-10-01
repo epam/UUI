306241111ba59f3dc8e6b7459662ca86e26040ce
Siarhei Dzeraviannik <siarhei_dzeraviannik@epam.com>
2025-10-01 14:22:45 +0300

    fix: update layout in BasicTabButtonExample for right UI

--- app/src/docs/_examples/tabButton/Basic.example.tsx
+++ app/src/docs/_examples/tabButton/Basic.example.tsx
@@ -1,25 +1,27 @@
 import React from 'react';
-import { TabButton, FlexCell } from '@epam/uui';
+import { TabButton, FlexCell, FlexRow } from '@epam/uui';
 import { ReactComponent as ContentFlagFillIcon } from '@epam/assets/icons/content-flag-fill.svg';
 
 export default function BasicTabButtonExample() {
     return (
-        <FlexCell width={ 50 }>
-            <TabButton
-                caption="Tab"
-            />
+        <FlexCell width="auto">
+            <FlexRow borderBottom>
+                <TabButton
+                    caption="Tab"
+                />
 
-            <TabButton
-                caption="Active Tab"
-                isActive={ true }
-            />
+                <TabButton
+                    caption="Active Tab"
+                    isActive={ true }
+                />
 
-            <TabButton
-                caption="Feature-rich Tab"
-                count={ 18 }
-                withNotify={ true }
-                icon={ ContentFlagFillIcon }
-            />
+                <TabButton
+                    caption="Feature-rich Tab"
+                    count={ 18 }
+                    withNotify={ true }
+                    icon={ ContentFlagFillIcon }
+                />
+            </FlexRow>
         </FlexCell>
     );
 }
