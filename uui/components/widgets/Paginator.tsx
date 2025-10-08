a927c7d3018e7926a2841829e8d78a227912bebb
Vasilii Kovalev <Vasilii_Kovalev@epam.com>
2025-10-08 15:21:30 +0500

    Wrap the buttons in a list and adjust styles

--- uui/components/widgets/Paginator.tsx
+++ uui/components/widgets/Paginator.tsx
@@ -21,53 +21,67 @@ export interface PaginatorProps extends UuiPaginatorProps, Overwrite<PaginatorMo
 
 export function Paginator(props: PaginatorProps) {
     const renderPaginator = (params: PaginatorRenderParams) => (
-        <nav role="navigation" className={ cx(css.root, 'uui-paginator', `uui-size-${props.size || settings.paginator.sizes.default}`) } { ...params.rawProps }>
-            <Button
-                size={ props.size || settings.paginator.sizes.default }
-                icon={ settings.paginator.icons.leftArrowIcon }
-                onClick={ params.goToPrev }
-                isDisabled={ params.isFirst || props.isDisabled }
-                fill="outline"
-                color="secondary"
-            />
-            {params.pages.map((page, index) => {
-                if (page.type === 'spacer') {
-                    return (
-                        <Button
-                            cx={ cx(css.spacer, css.page) }
-                            size={ props.size || settings.paginator.sizes.default }
-                            key={ `${index}_spacer` }
-                            caption="..."
-                            fill="ghost"
-                            color="secondary"
-                            tabIndex={ -1 }
-                            isDisabled={ props.isDisabled }
-                        />
-                    );
-                } else {
-                    return (
-                        <Button
-                            cx={ cx(css[`mode-${!page.isActive && 'ghost'}`], css.page) }
-                            size={ props.size || settings.paginator.sizes.default }
-                            key={ page.pageNumber }
-                            caption={ page.pageNumber }
-                            onClick={ () => page.onClick?.() }
-                            rawProps={ { 'aria-current': page.isActive } }
-                            fill={ (page.isActive && 'outline') || 'ghost' }
-                            color="primary"
-                            isDisabled={ props.isDisabled }
-                        />
-                    );
-                }
-            })}
-            <Button
-                size={ props.size || settings.paginator.sizes.default }
-                icon={ settings.paginator.icons.rightArrowIcon }
-                onClick={ params.goToNext }
-                isDisabled={ params.isLast || props.isDisabled }
-                fill="outline"
-                color="secondary"
-            />
+        <nav role="navigation" className={ cx('uui-paginator', `uui-size-${props.size || settings.paginator.sizes.default}`) } { ...params.rawProps }>
+            <ul
+                className={ css.root }
+            >
+                <li>
+                    <Button
+                        size={ props.size || settings.paginator.sizes.default }
+                        icon={ settings.paginator.icons.leftArrowIcon }
+                        onClick={ params.goToPrev }
+                        isDisabled={ params.isFirst || props.isDisabled }
+                        fill="outline"
+                        color="secondary"
+                    />
+                </li>
+                {params.pages.map((page, index) => {
+                    if (page.type === 'spacer') {
+                        return (
+                            <li
+                                key={ `${index}_spacer` }
+                            >
+                                <Button
+                                    cx={ cx(css.spacer, css.page) }
+                                    size={ props.size || settings.paginator.sizes.default }
+                                    caption="..."
+                                    fill="ghost"
+                                    color="secondary"
+                                    tabIndex={ -1 }
+                                    isDisabled={ props.isDisabled }
+                                />
+                            </li>
+                        );
+                    } else {
+                        return (
+                            <li
+                                key={ page.pageNumber }
+                            >
+                                <Button
+                                    cx={ cx(css[`mode-${!page.isActive && 'ghost'}`], css.page) }
+                                    size={ props.size || settings.paginator.sizes.default }
+                                    caption={ page.pageNumber }
+                                    onClick={ () => page.onClick?.() }
+                                    rawProps={ { 'aria-current': page.isActive } }
+                                    fill={ (page.isActive && 'outline') || 'ghost' }
+                                    color="primary"
+                                    isDisabled={ props.isDisabled }
+                                />
+                            </li>
+                        );
+                    }
+                })}
+                <li>
+                    <Button
+                        size={ props.size || settings.paginator.sizes.default }
+                        icon={ settings.paginator.icons.rightArrowIcon }
+                        onClick={ params.goToNext }
+                        isDisabled={ params.isLast || props.isDisabled }
+                        fill="outline"
+                        color="secondary"
+                    />
+                </li>
+            </ul>
         </nav>
     );
 
