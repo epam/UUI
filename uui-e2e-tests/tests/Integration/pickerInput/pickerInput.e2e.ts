faafb4b552bf8fca23952f22262c369c012c5ba0
AlekseyManetov <aliaksei_manetau@epam.com>
2025-11-03 12:33:01 +0100

    remove 'only' from e2e test

--- uui-e2e-tests/tests/Integration/pickerInput/pickerInput.e2e.ts
+++ uui-e2e-tests/tests/Integration/pickerInput/pickerInput.e2e.ts
@@ -7,7 +7,7 @@ const OPTION_TEXT = {
     FRANCE_GARGES: 'Garges-lès-GonesseEurope / France',
 };
 
-test.only('pickerInput/LazyTreeInput', async ({ pageWrapper }, testInfo) => {
+test('pickerInput/LazyTreeInput', async ({ pageWrapper }, testInfo) => {
     const { pageObject, expectScreenshot } = await setupDocExampleTest({
         testInfo,
         pageWrapper,
