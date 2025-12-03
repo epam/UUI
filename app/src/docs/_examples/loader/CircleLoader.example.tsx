1b0568cdad2edbf6ce9894451f326dc4bbf46069
MarharytaMalets <rita.maletz@gmail.com>
2025-12-03 22:49:11 +0300

    [Loader] Add loader description

--- app/src/docs/_examples/loader/CircleLoader.example.tsx
+++ app/src/docs/_examples/loader/CircleLoader.example.tsx
@@ -1,10 +1,10 @@
 import React from 'react';
 import { IconContainer } from '@epam/uui';
 import { ReactComponent as CircleLoaderIcon } from '@epam/assets/icons/loaders/circle-loader.svg';
-import css from '../../pages/assets/IconsPage.module.scss';
+import css from './styles.module.scss';
 
 export default function CircleLoaderExample() {
     return (
-        <IconContainer size="60" icon={ CircleLoaderIcon } cx={ css.previewIcon } />
+        <IconContainer size="60" icon={ CircleLoaderIcon } cx={ css.fillIcon } />
     );
 }
