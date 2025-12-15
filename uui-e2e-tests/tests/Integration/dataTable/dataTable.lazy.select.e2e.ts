4b6a50827cc475ae38b6a1fe59dd45f19baa6aae
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-15 17:04:37 +0200

    Added tests for filters.

--- uui-e2e-tests/tests/Integration/dataTable/dataTable.lazy.select.e2e.ts
+++ uui-e2e-tests/tests/Integration/dataTable/dataTable.lazy.select.e2e.ts
@@ -1,52 +1,54 @@
-import { test } from '../../../framework/fixtures/integrationTestPage/fixture';
+import { test } from '../../../framework/fixtures';
 import { setupDocExampleTest } from '../testUtils';
 import { DataTableObject } from '../../../framework/pageObjects/dataTableObject';
 
-test('DataTable Lazy: Select All/Unselect All.', async ({ pageWrapper }, testInfo) => {
-    const { pageObject, expectScreenshot } = await setupDocExampleTest({
-        testInfo,
-        pageWrapper,
-        PageObjectConstructor: DataTableObject,
-        testUrl: '/docExample?theme=loveship&examplePath=tables%2FColumnFiltersTable',
+test.describe('DataTable Lazy: Select All', () => {
+    test('Select All/Unselect All.', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: DataTableObject,
+            testUrl: '/docExample?theme=loveship&examplePath=tables%2FColumnFiltersTable',
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Select all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeChecked();
+            await expectScreenshot(1, 'select-all-items');
+        });
+
+        await test.step('Unselect all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeNotChecked();
+            await expectScreenshot(2, 'unselect-all-items');
+        });
     });
 
-    await pageObject.waitForTableRendered();
-    await test.step('Select all items', async () => {
-        await pageObject.clickSelectAllCheckbox();
-        await pageObject.waitSelectAllCheckboxToBeChecked();
-        await expectScreenshot(1, 'select-all-items');
-    });
+    test('Select All/Unselect All. [Using keyboard]', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: DataTableObject,
+            testUrl: '/docExample?theme=loveship&examplePath=tables%2FColumnFiltersTable',
+        });
 
-    await test.step('Unselect all items', async () => {
-        await pageObject.clickSelectAllCheckbox();
-        await pageObject.waitSelectAllCheckboxToBeNotChecked();
-        await expectScreenshot(2, 'unselect-all-items');
-    });
-});
+        await pageObject.waitForTableRendered();
+        await test.step('Put focus on selectAll checkbox', async () => {
+            await pageObject.focusFirstElement();
+            await expectScreenshot(1, 'first-element-focus');
+        });
 
-test('DataTable Lazy: Select All/Unselect All. [Using keyboard]', async ({ pageWrapper }, testInfo) => {
-    const { pageObject, expectScreenshot } = await setupDocExampleTest({
-        testInfo,
-        pageWrapper,
-        PageObjectConstructor: DataTableObject,
-        testUrl: '/docExample?theme=loveship&examplePath=tables%2FColumnFiltersTable',
-    });
+        await test.step('Select all items', async () => {
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsChecked();
+            await expectScreenshot(2, 'select-all-items');
+        });
 
-    await pageObject.waitForTableRendered();
-    await test.step('Put focus on selectAll checkbox', async () => {
-        await pageObject.focusFirstElement();
-        await expectScreenshot(1, 'first-element-focus');
-    });
-
-    await test.step('Select all items', async () => {
-        await pageObject.page.keyboard.press('Space');
-        await pageObject.waitFocusedCheckboxIsChecked();
-        await expectScreenshot(2, 'select-all-items');
-    });
-
-    await test.step('Unselect all items', async () => {
-        await pageObject.page.keyboard.press('Space');
-        await pageObject.waitFocusedCheckboxIsNotChecked();
-        await expectScreenshot(3, 'unselect-all-items');
+        await test.step('Unselect all items', async () => {
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsNotChecked();
+            await expectScreenshot(3, 'unselect-all-items');
+        });
     });
 });
