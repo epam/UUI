79d65786a0e5b9013c7cdf3c07639b8c733fbd6c
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-16 13:05:22 +0200

    Added tests for filters in lazy data table.

--- uui-e2e-tests/tests/Integration/dataTable/dataTable.lazy.filter.e2e.ts
+++ uui-e2e-tests/tests/Integration/dataTable/dataTable.lazy.filter.e2e.ts
@@ -17,6 +17,8 @@ test.describe('DataTable Lazy: Filters', () => {
 
             await pageObject.expectMultiPickerFilterModalToBeOpened();
             await pageObject.checkFilterOptionsInMultiPickerFilterModal(['Accountant', 'Accounts Manager']);
+            await expectScreenshot(1, 'job-title-filters-filled');
+
             await pageObject.closeFilterModal();
 
             await pageObject.expectRowNameInViewport('Aaron Vogt');
@@ -24,16 +26,43 @@ test.describe('DataTable Lazy: Filters', () => {
             await expectScreenshot(1, 'job-title-filters-applied');
         });
 
-        // await test.step('Filter by Job Title [exclude]', async () => {
-        //     await pageObject.clickSelectAllCheckbox();
-        //     await pageObject.waitSelectAllCheckboxToBeNotChecked();
-        //     await expectScreenshot(2, 'unselect-all-items');
-        // });
+        await test.step('Filter by Job Title [exclude]', async () => {
+            await pageObject.openFilterModal('Title');
+            await pageObject.expectMultiPickerFilterModalToBeOpened();
+            await pageObject.applyIsNotFilter();
+            await expectScreenshot(1, 'job-title-filters-exclude-filled');
 
-        // await test.step('Filter by Job Title using search', async () => {
-        //     await pageObject.clickSelectAllCheckbox();
-        //     await pageObject.waitSelectAllCheckboxToBeNotChecked();
-        //     await expectScreenshot(2, 'unselect-all-items');
-        // });
+            await pageObject.closeFilterModal();
+
+            await pageObject.expectRowNameInViewport('Aaron Benoît');
+
+            await expectScreenshot(2, 'job-title-filters-exclude-applied');
+        });
+
+        await test.step('Filter by Job Title using search', async () => {
+            await pageObject.openFilterModal('Title');
+            await pageObject.expectMultiPickerFilterModalToBeOpened();
+
+            await pageObject.searchInFilterModal('Production Technician - WC30');
+            await pageObject.checkFilterOptionsInMultiPickerFilterModal(['Production Technician - WC30']);
+            await expectScreenshot(1, 'job-title-filters-search-in-filter-filled');
+
+            await pageObject.closeFilterModal();
+
+            await pageObject.expectRowNameInViewport('Aaron Bravo');
+            await expectScreenshot(3, 'job-title-filters-search-in-filter-applied');
+        });
+
+        await test.step('Filter by Salary', async () => {
+            await pageObject.openFilterModal('Salary');
+            await expectScreenshot(4, 'numberic-range-picker-filter-modal-opened');
+
+            await pageObject.clickOnGteInFilterModal();
+            await pageObject.fillNumericFilterInput('1500');
+            await pageObject.closeFilterModal();
+
+            await pageObject.expectRowNameInViewport('Anthony Gautier');
+            await expectScreenshot(5, 'salary-filter-applied');
+        });
     });
 });
