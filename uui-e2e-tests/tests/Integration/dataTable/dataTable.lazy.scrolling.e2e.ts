254511463ec90682bf57ba484ba2add76f88b8fc
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-16 21:30:07 +0200

    Added scrolling and filters with keyboard navigation tests.

---
+++ uui-e2e-tests/tests/Integration/dataTable/dataTable.lazy.scrolling.e2e.ts
@@ -0,0 +1,22 @@
+import { DataTableObject } from '../../../framework/pageObjects';
+import { test } from '../../../framework/fixtures';
+import { setupDocExampleTest } from '../testUtils';
+
+test.describe('DataTable Lazy: Scrolling', () => {
+    test('Load extra data with scroll', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: DataTableObject,
+            testUrl: '/docExample?theme=loveship&examplePath=tables%2FColumnFiltersTable',
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Scroll', async () => {
+            await pageObject.scrollScreen(5);
+            await pageObject.expectRowNameInViewport('Adele Herrero', false);
+
+            await expectScreenshot(1, 'loaded data');
+        });
+    });
+});
