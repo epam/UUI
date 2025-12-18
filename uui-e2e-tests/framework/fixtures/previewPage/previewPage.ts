c04e0a01c9786fdbd8bd223629149e3b63df2cea
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-18 17:54:01 +0200

    Fixed tests.

--- uui-e2e-tests/framework/fixtures/previewPage/previewPage.ts
+++ uui-e2e-tests/framework/fixtures/previewPage/previewPage.ts
@@ -2,6 +2,8 @@ import { expect, type Locator } from '@playwright/test';
 import { type PreviewPageParams } from '../../types';
 import { AbsPage, type IPageParams } from '../shared/absPage';
 import type { TComponentPreview } from '@epam/uui-docs';
+import { Link } from '@epam/uui-core';
+import { PlayWrightInterfaceName } from '../../constants';
 
 const INLINE_PREVIEW_PREFIX = 'json:';
 
@@ -30,7 +32,7 @@ export class PreviewPage extends AbsPage {
     }
 
     async clientRedirect(params: PreviewPageParams) {
-        await super._clientRedirect({
+        await this._clientRedirect({
             pathname: '/preview',
             query: {
                 theme: params.theme,
@@ -51,4 +53,40 @@ export class PreviewPage extends AbsPage {
         });
         await expect(this.page).toHaveScreenshot(params.screenshotName, screenshotOptions);
     }
+
+    protected async _clientRedirect(link: Link) {
+        await this.page.mouse.move(0, 0);
+        await this.page.evaluate(async (_params: string) => {
+            const [p, i] = _params.split('[||||]');
+            // @ts-ignore Reason: this specific code will be run in context of web page
+            const globalObj = window as any;
+            const waitForInterface = () => {
+                return new Promise<any>((resolve, reject) => {
+                    const get = () => globalObj[i];
+                    if (get()) {
+                        resolve(get());
+                    } else {
+                        const MAX_ATTEMPTS = 5;
+                        let _attempts = 0;
+                        const _intervalId = globalObj.setInterval(() => {
+                            _attempts++;
+                            if (get()) {
+                                globalObj.clearInterval(_intervalId);
+                                resolve(get());
+                            } else if (_attempts === MAX_ATTEMPTS) {
+                                globalObj.clearInterval(_intervalId);
+                                reject(new Error(`Unable to find window.${i} global variable after ${_attempts} attempts.`));
+                            }
+                        }, 500);
+                    }
+                });
+            };
+            const playWrightInterface = await waitForInterface();
+            playWrightInterface.clientRedirect(p);
+        }, [jsonStringify(link), PlayWrightInterfaceName].join('[||||]'));
+    }
+}
+
+function jsonStringify(json: object) {
+    return JSON.stringify(json, undefined, 1);
 }
