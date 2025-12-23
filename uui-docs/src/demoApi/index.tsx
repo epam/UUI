3c58c2a52d379428c176b7f1f12a5eda04e68b6c
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-23 14:33:58 +0200

    Made context optional.

--- uui-docs/src/demoApi/index.tsx
+++ uui-docs/src/demoApi/index.tsx
@@ -10,29 +10,29 @@ export function getDemoApi(processRequest: IProcessRequest, origin: string = '')
     function lazyApi<TEntity, TId>(name: string) {
         return (
             rq: LazyDataSourceApiRequest<TEntity, TId, DataQueryFilter<TEntity>>,
-            context: LazyDataSourceApiRequestContext<TEntity, TId>,
+            context?: LazyDataSourceApiRequestContext<TEntity, TId>,
         ) => {
-            return processRequest<LazyDataSourceApiResponse<TEntity>>(origin.concat('/api/').concat(name), 'POST', rq, { fetchOptions: { signal: context.signal } });
+            return processRequest<LazyDataSourceApiResponse<TEntity>>(origin.concat('/api/').concat(name), 'POST', rq, { fetchOptions: { signal: context?.signal } });
         };
     }
 
     function personGroups(
         request: LazyDataSourceApiRequest<models.PersonEmploymentGroup, number, DataQueryFilter<models.PersonEmploymentGroup>>,
-        ctx: LazyDataSourceApiRequestContext<models.PersonEmploymentGroup, number>,
+        ctx?: LazyDataSourceApiRequestContext<models.PersonEmploymentGroup, number>,
     ): Promise<LazyDataSourceApiResponse<models.PersonEmploymentGroup>>;
     function personGroups(
         request: LazyDataSourceApiRequest<models.PersonLocationGroup, string, DataQueryFilter<models.PersonLocationGroup>>,
-        ctx: LazyDataSourceApiRequestContext<models.PersonLocationGroup, string>,
+        ctx?: LazyDataSourceApiRequestContext<models.PersonLocationGroup, string>,
     ): Promise<LazyDataSourceApiResponse<models.PersonLocationGroup>>;
     function personGroups(
         request: unknown,
-        context: LazyDataSourceApiRequestContext<unknown, unknown>,
+        ctx?: LazyDataSourceApiRequestContext<unknown, unknown>,
     ) {
         return processRequest(
             origin.concat('/api/personGroups'),
             'POST',
             request,
-            { fetchOptions: { signal: context.signal } },
+            { fetchOptions: { signal: ctx?.signal } },
         );
     }
 
@@ -50,9 +50,9 @@ export function getDemoApi(processRequest: IProcessRequest, origin: string = '')
         persons: lazyApi<models.Person, number>('persons'),
         personsPaged: (
             rq: LazyDataSourceApiRequest<models.Person, number, DataQueryFilter<models.Person>>,
-            ctx: LazyDataSourceApiRequestContext<models.Person, number>,
+            ctx?: LazyDataSourceApiRequestContext<models.Person, number>,
         ) =>
-            processRequest<LazyDataSourceApiResponse<models.Person>>(origin.concat('/api/persons-paged'), 'POST', rq, { fetchOptions: { signal: ctx.signal } }),
+            processRequest<LazyDataSourceApiResponse<models.Person>>(origin.concat('/api/persons-paged'), 'POST', rq, { fetchOptions: { signal: ctx?.signal } }),
         personGroups,
         departments: lazyApi<models.Department, number>('departments'),
         jobTitles: lazyApi<models.JobTitle, number>('jobTitles'),
