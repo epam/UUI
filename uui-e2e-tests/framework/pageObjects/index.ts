0e40952b4b1fc02a002f1b9400352263a23f9c8a
Yaroslav Kuznietsov <kuznietsov.yaroslav.yk@gmail.com>
2025-12-29 15:22:07 +0200

    Added tests for selectAll functionality.

--- uui-e2e-tests/framework/pageObjects/index.ts
+++ uui-e2e-tests/framework/pageObjects/index.ts
@@ -3,3 +3,4 @@ export * from './dropdownObject';
 export * from './mainMenuObject';
 export * from './pickerInputObject';
 export * from './pinnedRowsDataTableObject';
+export * from './locationsDataTableObject';
---
+++ uui-e2e-tests/tests/Integration/dataTable/dataTable.select.e2e.ts
@@ -0,0 +1,122 @@
+import { expect } from '@playwright/test';
+import { test } from '../../../framework/fixtures';
+import { setupDocExampleTest } from '../testUtils';
+import { ColumnFiltersDataTableObject, LocationsDataTableObject } from '../../../framework/pageObjects';
+
+test.describe('DataTable: Select', () => {
+    test('Select All/Unselect All.', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: ColumnFiltersDataTableObject,
+            testUrl: ColumnFiltersDataTableObject.testUrl,
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Select all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeChecked();
+            await expectScreenshot(1, 'select-all-items');
+        });
+
+        await test.step('Unselect all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeNotChecked();
+            await expectScreenshot(2, 'unselect-all-items');
+        });
+    });
+
+    test('Select All/Unselect All. [Using keyboard]', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: ColumnFiltersDataTableObject,
+            testUrl: ColumnFiltersDataTableObject.testUrl,
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Put focus on selectAll checkbox', async () => {
+            await pageObject.focusFirstElement();
+            await expectScreenshot(1, 'first-element-focus');
+        });
+
+        await test.step('Select all items', async () => {
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsChecked();
+            await expectScreenshot(2, 'select-all-items');
+        });
+
+        await test.step('Unselect all items', async () => {
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsNotChecked();
+            await expectScreenshot(3, 'unselect-all-items');
+        });
+    });
+
+    test('Tree: Select All/Unselect All', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: LocationsDataTableObject,
+            testUrl: LocationsDataTableObject.testUrl,
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Select all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeChecked();
+            await expectScreenshot(1, 'tree-select-all-items');
+        });
+
+        await test.step('Unfold first row', async () => {
+            await pageObject.unfold('Africa');
+
+            await expectScreenshot(1, 'tree-unfolded-select-all-items');
+        });
+
+        await test.step('Unselect all items', async () => {
+            await pageObject.clickSelectAllCheckbox();
+            await pageObject.waitSelectAllCheckboxToBeNotChecked();
+            await expectScreenshot(2, 'tree-unselect-all-items');
+        });
+    });
+
+    test('Tree: Select All/Unselect All. [Using keyboard]', async ({ pageWrapper }, testInfo) => {
+        const { pageObject, expectScreenshot } = await setupDocExampleTest({
+            testInfo,
+            pageWrapper,
+            PageObjectConstructor: LocationsDataTableObject,
+            testUrl: LocationsDataTableObject.testUrl,
+        });
+
+        await pageObject.waitForTableRendered();
+        await test.step('Put focus on selectAll checkbox', async () => {
+            await pageObject.focusFirstElement();
+            await expectScreenshot(1, 'tree-keyboard-first-element-focus');
+        });
+
+        await test.step('Select all items', async () => {
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsChecked();
+            await expectScreenshot(2, 'tree-keyboard-select-all-items');
+            await pageObject.moveFocusForward(7);
+            await pageObject.page.keyboard.press('Space');
+
+            const row = pageObject.getTableRows().nth(1);
+            await expect(row).toContainText('Algeria');
+        });
+
+        await test.step('Unselect all items', async () => {
+            await pageObject.moveFocusBackward();
+            await pageObject.moveFocusBackward();
+            await pageObject.moveFocusBackward();
+            await pageObject.moveFocusBackward();
+            await pageObject.moveFocusBackward();
+            await pageObject.moveFocusBackward();
+
+            await pageObject.page.keyboard.press('Space');
+            await pageObject.waitFocusedCheckboxIsNotChecked();
+            await expectScreenshot(3, 'tree-keyboard-unselect-all-items');
+        });
+    });
+});
