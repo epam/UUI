d5625320620d29f6154c5a92dacf93fe54a62e8b
Aleh Makaranka <github.goy98@passmail.net>
2026-04-06 18:09:43 +0400

    [RTE] Fix floating toolbar position in shadow DOM (#3074)
    * Removed `SelectionUtils` from `helpers` (shadow selection helpers no longer used for toolbar placement)
    * Updated `PositionedToolbar`: positioned the floating toolbar using Plate `toDOMRange`; removed the obsolete props & funcs; added a note about Safari in Shadow DOM and `useEventEditorSelectors`
    * Updated `Toolbars` and `tablePlugin`: stopped passing `editor` into `FloatingToolbar`

--- uui-editor/src/helpers.ts
+++ uui-editor/src/helpers.ts
@@ -63,31 +63,6 @@ export const isEditorValueEmpty = (value: Value) => {
     return false;
 };
 
-export class SelectionUtils {
-    static getSelection(params: { shadowRoot: ShadowRoot | undefined }) {
-        const { shadowRoot } = params;
-        if (shadowRoot) {
-            if ((shadowRoot as any).getSelection) {
-                // Chrome/Edge. See details here: https://caniuse.com/mdn-api_shadowroot_getselection
-                return (shadowRoot as any).getSelection();
-            }
-        }
-        // Works fine in other cases
-        return window.getSelection();
-    }
-
-    static getSelectionRange0(params: { selection: Selection; shadowRoot: ShadowRoot | undefined }) {
-        const { selection, shadowRoot } = params;
-        if (shadowRoot) {
-            if ((selection as any).getComposedRanges) {
-                // Webkit. https://w3c.github.io/selection-api/#dom-selection-getcomposedranges
-                return (selection as any).getComposedRanges(shadowRoot)[0];
-            }
-        }
-        return selection.getRangeAt(0);
-    }
-}
-
 export const createTempEditor = (plugins: PlatePlugin[]): PlateEditor => {
     return createPlateEditor({
         plugins: createPlugins((plugins).flat(), {
