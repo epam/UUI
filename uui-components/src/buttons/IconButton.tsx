403fa0017b6ae60fdd70132cbc7790eaa86101a6
Ivan Runets <ivan_runets@epam.com>
2026-04-06 15:15:50 +0300

    fix(PresetsPanel): avoid nested buttons in preset tab
    Preset actions dropdown used IconButton (button) inside the tab button. Render the toggler with IconButton type=span and rely on Clickable span support so the DOM stays valid and the nesting warning goes away.

--- uui-components/src/buttons/IconButton.tsx
+++ uui-components/src/buttons/IconButton.tsx
@@ -1,10 +1,10 @@
 import * as React from 'react';
 import { IDropdownToggler, Icon } from '@epam/uui-core';
-import { Clickable, ClickableComponentProps } from '../widgets';
+import { Clickable, ClickableComponentProps, ClickableType } from '../widgets';
 import { IconContainer } from '../layout';
 import css from './IconButton.module.scss';
 
-export type IconButtonProps = ClickableComponentProps & Omit<IDropdownToggler, 'isDropdown'> & {
+export type IconButtonProps = ClickableComponentProps & ClickableType & Omit<IDropdownToggler, 'isDropdown'> & {
     /** Icon can be a React element (usually an SVG element) */
     icon?: Icon;
     /** Icon for drop-down toggler */
@@ -32,7 +32,7 @@ export const IconButton = React.forwardRef<HTMLButtonElement | HTMLAnchorElement
     return (
         <Clickable
             { ...props }
-            type="button"
+            type={ props.type || 'button' }
             cx={ [css.container, props.cx] }
             ref={ ref }
         >
