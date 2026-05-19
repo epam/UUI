7db8c90669072ab9570adda31b726bf1c70332d3
Ivan Runets <35107045+i-runets@users.noreply.github.com>
2026-05-19 13:27:59 +0300

    feat(RangeDatePicker): create mobile view for RangeDatePicker (#3094)
    * feat(RangeDatePicker): create mobile view for RangeDatePicker
    
    * fix e2e tests
    
    * fix selected days indicator
    
    * fixes according to code review
    
    * add branch to qa list
    
    * open modal on input click
    
    * fix initial focus input
    
    ---------
    
    Co-authored-by: Ivan Runets <ivan_runets@epam.com>

--- uui/components/datePickers/Calendar.tsx
+++ uui/components/datePickers/Calendar.tsx
@@ -1,12 +1,27 @@
 import { CalendarProps, Calendar as uuiCalendar } from '@epam/uui-components';
+import type { Overwrite } from '@epam/uui-core';
 import { withMods } from '@epam/uui-core';
 import { Dayjs } from '../../helpers/dayJsHelper';
 import { settings } from '../../settings';
 
 import css from './Calendar.module.scss';
 
-export function applyDateSelectionMods() {
-    return [css.root, `uui-size-${settings.datePicker.sizes.body}`];
+type CalendarMods = {
+    /**
+     * Defines calendar size.
+     * @default '36'
+     */
+    size?: '24' | '30' | '36' | '42' | '48';
+};
+
+export interface CalendarModsOverride {}
+
+export type CalendarCoreProps = CalendarProps<Dayjs>;
+
+export interface CalendarPropsExtended extends CalendarCoreProps, Overwrite<CalendarMods, CalendarModsOverride> {}
+
+export function applyDateSelectionMods(mods: CalendarPropsExtended) {
+    return [css.root, `uui-size-${mods.size ?? settings.datePicker.sizes.body}`];
 }
 
-export const Calendar = withMods<CalendarProps<Dayjs>, CalendarProps<Dayjs>>(uuiCalendar, applyDateSelectionMods);
+export const Calendar = withMods<CalendarProps<Dayjs>, CalendarPropsExtended>(uuiCalendar, applyDateSelectionMods);
